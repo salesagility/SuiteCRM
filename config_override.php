591570a96f7098bab706d937522b3d378ce4fddd
Will <will.rennie@salesagility.com>
2014-01-09 10:31:19 +0000

    Fixed: footer language issues

--- config_override.php
+++ config_override.php
@@ -1,3 +1,4 @@
 <?php
 /***CONFIGURATOR***/
+$sugar_config['disabled_themes'] = '';
 /***CONFIGURATOR***/
---
+++ custom/Extension/modules/Users/Ext/Language/en_us.suitecrm.php
@@ -0,0 +1,11 @@
+<?php
+
+$mod_strings = array_merge($mod_strings,
+    array(
+        'LBL_SUITE_SUPERCHARGED' => "Supercharged by SuiteCRM",
+        'LBL_SUITE_POWERED_BY' => "Powered By SugarCRM",
+        'LBL_SUITE_TOP' => "Back to top",
+        'LBL_SUITE_PRINT' => "Print this page",
+    )
+);
+?>
