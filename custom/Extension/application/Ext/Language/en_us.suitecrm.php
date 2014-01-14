67a5b7cc130b05908957f2f0df6409373c5c2563
William Rennie <will.rennie@salesagility.com>
2014-01-14 20:47:38 +0000

    Added missing language file for globalLinks.tpl

---
+++ custom/Extension/application/Ext/Language/en_us.suitecrm.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package SuiteCRM
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author Salesagility Ltd <support@salesagility.com>
+ */
+
+$app_strings['LBL_QUICK_ACCOUNT'] = 'Create Account';
+$app_strings['LBL_QUICK_CONTACT'] = 'Create Contact';
+$app_strings['LBL_QUICK_OPPORTUNITY'] = 'Create Opportunity';
+$app_strings['LBL_QUICK_LEAD'] = 'Create Lead';
+$app_strings['LBL_QUICK_DOCUMENT'] = 'Create Document';
+$app_strings['LBL_QUICK_CALL'] = 'Log Call';
+$app_strings['LBL_QUICK_TASK'] = 'Create Task';
