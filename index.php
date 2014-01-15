a1b8ec41c2bbcd8c7fdd74316ab7ec0cb36c99d5
Matthew <matthew.lorimer@salesagility.com>
2014-01-15 11:21:37 +0000

    removed display errors from index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-ini_set('display_errors',1);
 if(!defined('sugarEntry'))define('sugarEntry', true);
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
