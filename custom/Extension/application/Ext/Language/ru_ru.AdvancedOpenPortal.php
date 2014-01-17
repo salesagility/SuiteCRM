d483936769912556f129f762e6c874cf7756e9b0
likhobory <grego@list.ru>
2014-01-17 15:12:30 +0400

    RUSSIAN RAPIRA language files is updated

---
+++ custom/Extension/application/Ext/Language/ru_ru.AdvancedOpenPortal.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ *
+ * @package Advanced OpenPortal
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author Salesagility Ltd <support@salesagility.com>
+ */
+
+
+$app_list_strings['moduleList']['AOP_AOP_Case_Events'] = 'События обращения';
+$app_list_strings['moduleList']['AOP_AOP_Case_Updates'] = 'Обновления обращения';
+$app_list_strings['moduleList']['AOP_Case_Events'] = 'События обращения';
+$app_list_strings['moduleList']['AOP_Case_Updates'] = 'Обновления обращения';
+$app_strings['LBL_AOP_EMAIL_REPLY_DELIMITER'] = '========== Please reply above this line ==========';
+
+$app_list_strings['case_state_dom'] =
+  array (
+      'Open' => 'Открыто',
+      'Closed' => 'Закрыто',
+  );
+$app_list_strings['case_state_default_key'] = 'Open';
+$app_list_strings['case_status_default_key'] = 'Open_New';
+$app_list_strings['case_status_dom'] =
+  array (
+      'Open_New' => 'Новое',
+      'Open_Assigned' => 'Назначенное',
+      'Closed_Closed' => 'Закрытое',
+      'Open_Pending Input' => 'Ожидание решения',
+      'Closed_Rejected' => 'Отклонённое',
+      'Closed_Duplicate' => 'Продублированное',
+  );
+  
+  $app_list_strings['aop_distribution_method'] =
+    array (
+        'singleUser' => 'Указанный пользователь',
+        'roundRobin' => 'В цикле',
+        'leastBusy' => 'Наименее занятой',
+    ); 
--- custom/modules/Home/language/ru_ru.lang.php
+++ custom/modules/Home/language/ru_ru.lang.php
@@ -11,9 +11,14 @@ $mod_strings['LBL_CONTRIBUTOR_SECURITY_SUITE'] = 'SecuritySuite by Jason Eggers'
 $mod_strings['LBL_CONTRIBUTOR_JJW_GMAPS'] = 'JJWDesign Google Maps by Jeffrey J. Walters';
 $mod_strings['LBL_CONTRIBUTOR_QUICKCRM'] = 'QuickCRM Mobile by Benoit Luquet';
 $mod_strings['LBL_CONTRIBUTOR_CONSCIOUS'] = 'SuiteCRM LOGO Provided by Conscious Solutions';
+$mod_strings['LBL_LANGUAGE_SPANISH'] = 'Spanish Translation Provided by Disytel openConsulting';
+$mod_strings['LBL_LANGUAGE_RUSSIAN'] = 'Russian Translation Provided by likhobory';
 
 $mod_strings['LBL_ABOUT_SUITE_1'] = 'SuiteCRM  - бесплатный форк SugarCRM. Вы можете найти множество материалов в интернете, поясняющих необходимость появления данного проекта';
 $mod_strings['LBL_ABOUT_SUITE_2'] = 'SuiteCRM распространяется под лицензией GPL3';
 $mod_strings['LBL_ABOUT_SUITE_3'] = 'SuiteCRM полностью совместим с SugarCRM 6.5.x';
 $mod_strings['LBL_ABOUT_SUITE_4'] = 'Весь исходный код проекта SuiteCRM является открытым и публикуется под лицензией GPL3';
 $mod_strings['LBL_ABOUT_SUITE_5'] = 'Поддержка SuiteCRM осуществляется как на бесплатной, так и на платной основе';
+
+
+$mod_strings['LBL_SUITE_PARTNERS'] = 'С полным списком наших партнёров, активно поддерживающих проекты с открытым исходным кодом, вы можете ознакомиться на нашем сайте';
