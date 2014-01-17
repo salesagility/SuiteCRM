edc11e46d3529c6f89e97dafe5504fe98878f821
Matthew <matthew.lorimer@salesagility.com>
2014-01-17 19:59:36 +0000

    fix for config

--- config_override.php
+++ config_override.php
@@ -1,10 +1,15 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['colourselector']['menu'] = '#f102023';
+$sugar_config['colourselector']['menu'] = '#f10202';
 $sugar_config['colourselector']['menufont'] = '#f102023';
 $sugar_config['colourselector']['menubrd'] = '#f10202';
 $sugar_config['colourselector']['pageheader'] = '#f10202';
 $sugar_config['colourselector']['pagelink'] = '#f10202';
 $sugar_config['colourselector']['dashlet'] = '#f10202';
 $sugar_config['colourselector']['button'] = '#f102023';
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['dashlet_auto_refresh_min'] = '30';
+$sugar_config['enable_action_menu'] = true;
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['developerMode'] = true;
 /***CONFIGURATOR***/
