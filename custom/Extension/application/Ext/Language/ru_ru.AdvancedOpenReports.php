7b5a2fb4a6ddbea2ff03c85861e3a65e42e7f942
likhobory <grego@list.ru>
2014-01-17 15:12:30 +0400

    RUSSIAN RAPIRA language files is updated

---
+++ custom/Extension/application/Ext/Language/ru_ru.AdvancedOpenReports.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Advanced OpenReports, SugarCRM Reporting.
+ * @package Advanced OpenReports for SugarCRM
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author SalesAgility <info@salesagility.com>
+ */
+
+
+$app_list_strings['moduleList']['AOR_Reports'] = 'Отчёты';
+$app_list_strings['moduleList']['AOR_Conditions'] = 'Условия отчёта';
+$app_list_strings['moduleList']['AOR_Charts'] = 'Диаграммы отчета';
+$app_list_strings['moduleList']['AOR_Fields'] = 'Поля отчета';
+
+$app_list_strings['aor_operator_list']['Equal_To'] = '=';
+$app_list_strings['aor_operator_list']['Not_Equal_To'] = '!=';
+$app_list_strings['aor_operator_list']['Greater_Than'] = '>';
+$app_list_strings['aor_operator_list']['Less_Than'] = '<';
+$app_list_strings['aor_operator_list']['Greater_Than_or_Equal_To'] = '>=';
+$app_list_strings['aor_operator_list']['Less_Than_or_Equal_To'] = '<=';
+
+$app_list_strings['aor_sql_operator_list']['Equal_To'] = '=';
+$app_list_strings['aor_sql_operator_list']['Not_Equal_To'] = '!=';
+$app_list_strings['aor_sql_operator_list']['Greater_Than'] = '>';
+$app_list_strings['aor_sql_operator_list']['Less_Than'] = '<';
+$app_list_strings['aor_sql_operator_list']['Greater_Than_or_Equal_To'] = '>=';
+$app_list_strings['aor_sql_operator_list']['Less_Than_or_Equal_To'] = '<=';
+
+$app_list_strings['aor_condition_operator_list']['And'] = 'И';
+$app_list_strings['aor_condition_operator_list']['OR'] = 'ИЛИ';
+
+$app_list_strings['aor_condition_type_list']['Value'] = 'Значение';
+$app_list_strings['aor_condition_type_list']['Field'] = 'Поле';
+$app_list_strings['aor_condition_type_list']['Date'] = 'Дата';
+$app_list_strings['aor_condition_type_list']['Multi'] = 'Мультивыбор';
+
+$app_list_strings['aor_date_type_list'][''] = '';
+$app_list_strings['aor_date_type_list']['minute'] = 'мин.';
+$app_list_strings['aor_date_type_list']['hour'] = 'час.';
+$app_list_strings['aor_date_type_list']['day'] = 'дн.';
+$app_list_strings['aor_date_type_list']['week'] = 'нед.';
+$app_list_strings['aor_date_type_list']['month'] = 'мес.';
+$app_list_strings['aor_date_type_list']['business_hours'] = 'рабочих часов';
+
+$app_list_strings['aor_date_options']['now'] = 'Сейчас';
+$app_list_strings['aor_date_options']['field'] = 'Это поле';
+
+$app_list_strings['aor_date_operator']['now'] = '';
+$app_list_strings['aor_date_operator']['plus'] = '+';
+$app_list_strings['aor_date_operator']['minus'] = '-';
+$app_list_strings['aor_sort_operator'][''] = '';
+$app_list_strings['aor_sort_operator']['ASC'] = 'Восходящая';
+$app_list_strings['aor_sort_operator']['DESC'] = 'Нисходящая';
+
+$app_list_strings['aor_function_list'][''] = '';
+$app_list_strings['aor_function_list']['COUNT'] = 'КОЛИЧЕСТВО';
+$app_list_strings['aor_function_list']['MIN'] = 'МИНИМУМ';
+$app_list_strings['aor_function_list']['MAX'] = 'МАКСИМУМ';
+$app_list_strings['aor_function_list']['SUM'] = 'СУММА';
+$app_list_strings['aor_function_list']['AVG'] = 'СРЕДНЕЕ';
--- modules/Home/language/ru_ru.lang.php
+++ modules/Home/language/ru_ru.lang.php
@@ -301,6 +301,8 @@ $mod_strings['LBL_CONTRIBUTOR_SECURITY_SUITE'] = 'SecuritySuite by Jason Eggers'
 $mod_strings['LBL_CONTRIBUTOR_JJW_GMAPS'] = 'JJWDesign Google Maps by Jeffrey J. Walters';
 $mod_strings['LBL_CONTRIBUTOR_QUICKCRM'] = 'QuickCRM Mobile by Benoit Luquet';
 $mod_strings['LBL_CONTRIBUTOR_CONSCIOUS'] = 'SuiteCRM LOGO Provided by Conscious Solutions';
+$mod_strings['LBL_LANGUAGE_SPANISH'] = 'Spanish Translation Provided by Disytel openConsulting';
+$mod_strings['LBL_LANGUAGE_RUSSIAN'] = 'Russian Translation Provided by likhobory';
 
 $mod_strings['LBL_ABOUT_SUITE_1'] = 'SuiteCRM  - бесплатный форк SugarCRM. Вы можете найти множество материалов в интернете, поясняющих необходимость появления данного проекта';
 $mod_strings['LBL_ABOUT_SUITE_2'] = 'SuiteCRM распространяется под лицензией GPL3';
