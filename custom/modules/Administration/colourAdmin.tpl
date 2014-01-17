4a5bc4e74b6f2d2fe98cabe8cd5db8b90a7f40e3
William Rennie <will.rennie@salesagility.com>
2014-01-17 22:03:30 +0000

    Added jscolour library to enable users to easily select colours using chart, or enter hex code manually.

--- custom/modules/Administration/colourAdmin.tpl
+++ custom/modules/Administration/colourAdmin.tpl
@@ -15,34 +15,34 @@
     <table width="100%" border="0" cellspacing="1" cellpadding="0" class="edit view">
         <tr><th align="left" scope="row" colspan="4"><h4><strong>{$MOD.LBL_COLOUR_ADMIN_MENU}</strong></h4></th>
         <tr>
-            <td>Base menu colour: </td><td><input type="text" id="colourselector_menu" name="colourselector_menu" class="colour" value="{$config.colourselector.menu}" /></td>
-            <td>Top gradient colour: </td><td><input type="text" id="colourselector_menuto" name="colourselector_menuto" class="colour" value="{$config.colourselector.menuto}" /></td>
-            <td>Bottom gradient colour: </td><td><input type="text" id="colourselector_menufrom" name="colourselector_menufrom" class="colour" value="{$config.colourselector.menufrom}" /></td>
+            <td>Base menu colour: </td><td><input type="text" id="colourselector_menu" name="colourselector_menu" class="color" value="{$config.colourselector.menu}" /></td>
+            <td>Top gradient colour: </td><td><input type="text" id="colourselector_menuto" name="colourselector_menuto" class="color" value="{$config.colourselector.menuto}" /></td>
+            <td>Bottom gradient colour: </td><td><input type="text" id="colourselector_menufrom" name="colourselector_menufrom" class="color" value="{$config.colourselector.menufrom}" /></td>
         </tr>
         <tr>
-            <td>{$MOD.LBL_COLOUR_ADMIN_MENUFONT}: </td><td><input type="text" id="colourselector_menufont" name="colourselector_menufont" class="colour" value="{$config.colourselector.menufont}" /></td>
-            <td>{$MOD.LBL_COLOUR_ADMIN_MENUBRD}: </td><td><input type="text" id="colourselector_menubrd" name="colourselector_menubrd" class="colour" value="{$config.colourselector.menubrd}" /></td>
+            <td>{$MOD.LBL_COLOUR_ADMIN_MENUFONT}: </td><td><input type="text" id="colourselector_menufont" name="colourselector_menufont" class="color" value="{$config.colourselector.menufont}" /></td>
+            <td>{$MOD.LBL_COLOUR_ADMIN_MENUBRD}: </td><td><input type="text" id="colourselector_menubrd" name="colourselector_menubrd" class="color" value="{$config.colourselector.menubrd}" /></td>
         </tr>
     </table>
 
     <table width="100%" border="0" cellspacing="1" cellpadding="0" class="edit view">
         <tr><th align="left" scope="row" colspan="4"><h4><strong>{$MOD.LBL_COLOUR_ADMIN_BUTTON}</strong></h4></th>
         <tr>
-            <td>Button colour top: </td><td><input type="text" id="colourselector_button" name="colourselector_button1" class="colour" value="{$config.colourselector.button1}" /></td>
-            <td>Button colour mid-top: </td><td><input type="text" id="colourselector_button" name="colourselector_button2" class="colour" value="{$config.colourselector.button2}" /></td>
-            <td>Button colour mid-bottom: </td><td><input type="text" id="colourselector_button" name="colourselector_button3" class="colour" value="{$config.colourselector.button3}" /></td>
+            <td>Button colour top: </td><td><input type="text" id="colourselector_button" name="colourselector_button1" class="color" value="{$config.colourselector.button1}" /></td>
+            <td>Button colour mid-top: </td><td><input type="text" id="colourselector_button" name="colourselector_button2" class="color" value="{$config.colourselector.button2}" /></td>
+            <td>Button colour mid-bottom: </td><td><input type="text" id="colourselector_button" name="colourselector_button3" class="color" value="{$config.colourselector.button3}" /></td>
         <tr>
 
-            <td>Button colour bottom: </td><td><input type="text" id="colourselector_button" name="colourselector_button4" class="colour" value="{$config.colourselector.button4}" /></td>
-            <td>Button hover colour: </td><td><input type="text" id="colourselector_buttonhover" name="colourselector_buttonhover" class="colour" value="{$config.colourselector.buttonhover}" /></td>
+            <td>Button colour bottom: </td><td><input type="text" id="colourselector_button" name="colourselector_button4" class="color" value="{$config.colourselector.button4}" /></td>
+            <td>Button hover colour: </td><td><input type="text" id="colourselector_buttonhover" name="colourselector_buttonhover" class="color" value="{$config.colourselector.buttonhover}" /></td>
         </tr>
     </table>
 
     <table width="100%" border="0" cellspacing="1" cellpadding="0" class="edit view">
         <tr><th align="left" scope="row" colspan="4"><h4><strong>{$MOD.LBL_COLOUR_ADMIN_PAGE}</strong></h4></th>
         <tr>
-            <td>{$MOD.LBL_COLOUR_ADMIN_PAGEHEADER}: </td><td><input type="text" id="colourselector_pageheader" name="colourselector_pageheader" class="colour" value="{$config.colourselector.pageheader}" /></td>
-            <td>{$MOD.LBL_COLOUR_ADMIN_PAGELINK}: </td><td><input type="text" id="colourselector_pagelink" name="colourselector_pagelink" class="colour" value="{$config.colourselector.pagelink}" /></td>
+            <td>{$MOD.LBL_COLOUR_ADMIN_PAGEHEADER}: </td><td><input type="text" id="colourselector_pageheader" name="colourselector_pageheader" class="color" value="{$config.colourselector.pageheader}" /></td>
+            <td>{$MOD.LBL_COLOUR_ADMIN_PAGELINK}: </td><td><input type="text" id="colourselector_pagelink" name="colourselector_pagelink" class="color" value="{$config.colourselector.pagelink}" /></td>
         </tr>
     </table>
 
@@ -50,7 +50,7 @@
         <tr><th align="left" scope="row" colspan="4"><h4><strong>{$MOD.LBL_COLOUR_ADMIN_DASHLET}</strong></h4></th>
         <tr>
             <td>
-                <input type="text" id="colourselector_dashlet" name="colourselector_dashlet" class="colour" value="{$config.colourselector.dashlet}" />
+                <input type="text" id="colourselector_dashlet" name="colourselector_dashlet" class="color" value="{$config.colourselector.dashlet}" />
             </td>
         </tr>
     </table>
--- themes/Suite7/css/colourSelector.php
+++ themes/Suite7/css/colourSelector.php
@@ -19,19 +19,19 @@ header("Content-type: text/css; charset: UTF-8");
 
 #header {
 
-    background:<?php echo $sugar_config['colourselector']['menu']; ?>;
+    background: #<?php echo $sugar_config['colourselector']['menu']; ?>;
     background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzMzMzMzMyIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMxMjEyMTIiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
-    background: -moz-linear-gradient(top,  <?php echo $sugar_config['colourselector']['menuto']; ?> 0%, <?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* FF3.6+ */
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,<?php echo $sugar_config['colourselector']['menuto']; ?>), color-stop(100%,<?php echo $sugar_config['colourselector']['menufrom']; ?>)); /* Chrome,Safari4+ */
-    background: -webkit-linear-gradient(top,  <?php echo $sugar_config['colourselector']['menuto']; ?> 0%,<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* Chrome10+,Safari5.1+ */
-    background: -o-linear-gradient(top,  <?php echo $sugar_config['colourselector']['menuto']; ?> 0%,<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* Opera 11.10+ */
-    background: -ms-linear-gradient(top,  <?php echo $sugar_config['colourselector']['menuto']; ?> 0%,<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* IE10+ */
-    background: linear-gradient(to bottom,  <?php echo $sugar_config['colourselector']['menuto']; ?> 0%,<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* W3C */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='<?php echo $sugar_config['colourselector']['menuto']; ?>', endColorstr='<?php echo $sugar_config['colourselector']['menufrom']; ?>',GradientType=0 ); /* IE6-8 */
-    border-bottom:2px solid <?php echo $sugar_config['colourselector']['menubrd']; ?>;
+    background: -moz-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%, #<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* FF3.6+ */
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#<?php echo $sugar_config['colourselector']['menuto']; ?>), color-stop(100%,#<?php echo $sugar_config['colourselector']['menufrom']; ?>)); /* Chrome,Safari4+ */
+    background: -webkit-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%,#<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* Chrome10+,Safari5.1+ */
+    background: -o-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%,#<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* Opera 11.10+ */
+    background: -ms-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%,#<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* IE10+ */
+    background: linear-gradient(to bottom,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%,#<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* W3C */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#<?php echo $sugar_config['colourselector']['menuto']; ?>', endColorstr='#<?php echo $sugar_config['colourselector']['menufrom']; ?>',GradientType=0 ); /* IE6-8 */
+    border-bottom:2px solid #<?php echo $sugar_config['colourselector']['menubrd']; ?>;
 }
 
-h1, h2, h3, h4 {color:<?php echo $sugar_config['colourselector']['pageheader']; ?>;}
+h1, h2, h3, h4 {color: #<?php echo $sugar_config['colourselector']['pageheader']; ?>;}
 
 input[type=button],
 input[type=submit],
@@ -39,16 +39,16 @@ input[type=reset],
 .button {
 padding: 3px 8px 3px 8px;
 margin: 0 3px 0 3px;
-background: <?php echo $sugar_config['colourselector']['button1']; ?>; /* Old browsers */
+background: #<?php echo $sugar_config['colourselector']['button1']; ?>; /* Old browsers */
 /* IE9 SVG, needs conditional override of 'filter' to 'none' */
 background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ZmZmZmZiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjUwJSIgc3RvcC1jb2xvcj0iI2YzZjNmMyIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjUxJSIgc3RvcC1jb2xvcj0iI2VkZWRlZCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNmZmZmZmYiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
-background: -moz-linear-gradient(top,  <?php echo $sugar_config['colourselector']['button1']; ?> 0%, <?php echo $sugar_config['colourselector']['button2']; ?> 50%, <?php echo $sugar_config['colourselector']['button3']; ?> 51%, <?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* FF3.6+ */
-background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,<?php echo $sugar_config['colourselector']['button1']; ?>), color-stop(50%,<?php echo $sugar_config['colourselector']['button2']; ?>), color-stop(51%,<?php echo $sugar_config['colourselector']['button3']; ?>), color-stop(100%,<?php echo $sugar_config['colourselector']['button4']; ?>)); /* Chrome,Safari4+ */
-background: -webkit-linear-gradient(top,  <?php echo $sugar_config['colourselector']['button1']; ?> 0%,<?php echo $sugar_config['colourselector']['button2']; ?> 50%,<?php echo $sugar_config['colourselector']['button3']; ?> 51%,<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* Chrome10+,Safari5.1+ */
-background: -o-linear-gradient(top,  <?php echo $sugar_config['colourselector']['button1']; ?> 0%,<?php echo $sugar_config['colourselector']['button2']; ?> 50%,<?php echo $sugar_config['colourselector']['button3']; ?> 51%,<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* Opera 11.10+ */
-background: -ms-linear-gradient(top,  <?php echo $sugar_config['colourselector']['button1']; ?> 0%,<?php echo $sugar_config['colourselector']['button2']; ?> 50%,<?php echo $sugar_config['colourselector']['button3']; ?> 51%,<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* IE10+ */
-background: linear-gradient(to bottom,  <?php echo $sugar_config['colourselector']['button1']; ?> 0%,<?php echo $sugar_config['colourselector']['button2']; ?> 50%,<?php echo $sugar_config['colourselector']['button3']; ?> 51%,<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* W3C */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='<?php echo $sugar_config['colourselector']['button1']; ?>', endColorstr='<?php echo $sugar_config['colourselector']['button3']; ?>',GradientType=0 ); /* IE6-8 */
+background: -moz-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%, #<?php echo $sugar_config['colourselector']['button2']; ?> 50%, #<?php echo $sugar_config['colourselector']['button3']; ?> 51%, #<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,<?php echo $sugar_config['colourselector']['button1']; ?>), color-stop(50%,#<?php echo $sugar_config['colourselector']['button2']; ?>), color-stop(51%,#<?php echo $sugar_config['colourselector']['button3']; ?>), color-stop(100%,#<?php echo $sugar_config['colourselector']['button4']; ?>)); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%,#<?php echo $sugar_config['colourselector']['button2']; ?> 50%,#<?php echo $sugar_config['colourselector']['button3']; ?> 51%,#<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%,#<?php echo $sugar_config['colourselector']['button2']; ?> 50%,#<?php echo $sugar_config['colourselector']['button3']; ?> 51%,#<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%,#<?php echo $sugar_config['colourselector']['button2']; ?> 50%,#<?php echo $sugar_config['colourselector']['button3']; ?> 51%,#<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* IE10+ */
+background: linear-gradient(to bottom,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%,#<?php echo $sugar_config['colourselector']['button2']; ?> 50%,#<?php echo $sugar_config['colourselector']['button3']; ?> 51%,#<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#<?php echo $sugar_config['colourselector']['button1']; ?>', endColorstr='#<?php echo $sugar_config['colourselector']['button3']; ?>',GradientType=0 ); /* IE6-8 */
 filter:none !important;
 border: solid 1px #999999;
 color: #121212;
@@ -61,12 +61,12 @@ input[type=button]:hover,
 input[type=submit]:hover,
 input[type=reset]:hover {
 filter:none !important;
-background:<?php echo $sugar_config['colourselector']['buttonhover']; ?>;
+background: #<?php echo $sugar_config['colourselector']['buttonhover']; ?>;
 border:1px solid #999;
 }
 
 .dashletPanel .h3Row {
 
-background: <?php echo $sugar_config['colourselector']['dashlet']; ?>;
+background: #<?php echo $sugar_config['colourselector']['dashlet']; ?>;
 
 }
