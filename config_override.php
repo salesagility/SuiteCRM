a16818ac6a52bdadef9258f2b854da81d6e9d805
William Rennie <will.rennie@salesagility.com>
2014-01-18 19:19:11 +0000

    Removed conflicting css in colourSelector.php preventing css being changed.

--- config_override.php
+++ config_override.php
@@ -1,14 +1,18 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['colourselector']['menu'] = '#f10202';
-$sugar_config['colourselector']['menufont'] = '#f102023';
-$sugar_config['colourselector']['menubrd'] = '#121212';
-$sugar_config['colourselector']['pageheader'] = '#f10202';
-$sugar_config['colourselector']['pagelink'] = '#f10202';
-$sugar_config['colourselector']['dashlet'] = '#f10202';
+$sugar_config['colourselector']['menu'] = 'F10202';
+$sugar_config['colourselector']['menufont'] = 'FFFFFF';
+$sugar_config['colourselector']['menubrd'] = '121212';
+$sugar_config['colourselector']['pageheader'] = 'F10202';
+$sugar_config['colourselector']['pagelink'] = 'F10202';
+$sugar_config['colourselector']['dashlet'] = 'F10202';
 $sugar_config['colourselector']['button'] = '#f102023';
-$sugar_config['colourselector']['menufrom'] = '#121212';
-$sugar_config['colourselector']['menuto'] = '#333333';
+$sugar_config['colourselector']['menufrom'] = '121212';
+$sugar_config['colourselector']['menuto'] = '333333';
+$sugar_config['colourselector']['button1'] = 'FFFFFF';
+$sugar_config['colourselector']['button2'] = 'FEFEFE';
+$sugar_config['colourselector']['button3'] = 'EDEDED';
+$sugar_config['colourselector']['button4'] = 'FFFFFF';
 $sugar_config['default_module_favicon'] = false;
 $sugar_config['dashlet_auto_refresh_min'] = '30';
 $sugar_config['enable_action_menu'] = true;
--- themes/Suite7/css/colourSelector.php
+++ themes/Suite7/css/colourSelector.php
@@ -18,7 +18,6 @@ header("Content-type: text/css; charset: UTF-8");
 /*custom colour css*/
 
 #header {
-
     background: #<?php echo $sugar_config['colourselector']['menu']; ?>;
     background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzMzMzMzMyIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMxMjEyMTIiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
     background: -moz-linear-gradient(top,  #<?php echo $sugar_config['colourselector']['menuto']; ?> 0%, #<?php echo $sugar_config['colourselector']['menufrom']; ?> 100%); /* FF3.6+ */
@@ -37,8 +36,6 @@ input[type=button],
 input[type=submit],
 input[type=reset],
 .button {
-padding: 3px 8px 3px 8px;
-margin: 0 3px 0 3px;
 background: #<?php echo $sugar_config['colourselector']['button1']; ?>; /* Old browsers */
 /* IE9 SVG, needs conditional override of 'filter' to 'none' */
 background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ZmZmZmZiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjUwJSIgc3RvcC1jb2xvcj0iI2YzZjNmMyIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjUxJSIgc3RvcC1jb2xvcj0iI2VkZWRlZCIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNmZmZmZmYiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
@@ -50,11 +47,6 @@ background: -ms-linear-gradient(top,  #<?php echo $sugar_config['colourselector'
 background: linear-gradient(to bottom,  #<?php echo $sugar_config['colourselector']['button1']; ?> 0%,#<?php echo $sugar_config['colourselector']['button2']; ?> 50%,#<?php echo $sugar_config['colourselector']['button3']; ?> 51%,#<?php echo $sugar_config['colourselector']['button4']; ?> 100%); /* W3C */
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#<?php echo $sugar_config['colourselector']['button1']; ?>', endColorstr='#<?php echo $sugar_config['colourselector']['button3']; ?>',GradientType=0 ); /* IE6-8 */
 filter:none !important;
-border: solid 1px #999999;
-color: #121212;
-border-radius: 2px;
-cursor: pointer;
-font-size:12px;
 }
 
 input[type=button]:hover,
