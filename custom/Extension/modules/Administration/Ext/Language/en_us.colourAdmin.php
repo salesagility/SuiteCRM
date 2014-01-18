2db3013d68b603cf71c693e39620b7ce23366b89
William Rennie <will.rennie@salesagility.com>
2014-01-18 00:38:18 +0000

    Added more colour options to configuration. Added labels with related language mod_strings added to admin language file.

--- custom/Extension/modules/Administration/Ext/Language/en_us.colourAdmin.php
+++ custom/Extension/modules/Administration/Ext/Language/en_us.colourAdmin.php
@@ -10,10 +10,26 @@ $mod_strings['LBL_COLOUR_SETTINGS'] = 'Colour Settings';
 $mod_strings['LBL_COLOUR_DESC'] = 'Customise your SuiteCRM theme';
 
 $mod_strings['LBL_COLOUR_ADMIN_MENU'] = 'Menu colour config';
-$mod_strings['LBL_COLOUR_ADMIN_MENUFONT'] = 'Menu font colour';
-$mod_strings['LBL_COLOUR_ADMIN_MENUBRD'] = 'Menu border colour';
 $mod_strings['LBL_COLOUR_ADMIN_PAGE'] = 'Page colour config';
 $mod_strings['LBL_COLOUR_ADMIN_PAGEHEADER'] = 'Page header colour';
 $mod_strings['LBL_COLOUR_ADMIN_PAGELINK'] = 'Page link colour';
 $mod_strings['LBL_COLOUR_ADMIN_DASHLET'] = 'Dashlet colour config';
 $mod_strings['LBL_COLOUR_ADMIN_BUTTON'] = 'Button colour config';
+
+$mod_strings['LBL_COLOUR_ADMIN_BASE'] = 'Base menu colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_TOPGR'] = 'Top gradient colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTMGR'] = 'Bottom gradient colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_MENUBRD'] = 'Menu border colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_VISITED'] = 'Visited link colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_MENUHOVER'] = 'Menu hover colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_DDLINK'] = 'Drop down link colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_DDMENU'] = 'Drop down menu colour: ';
+
+$mod_strings['LBL_COLOUR_ADMIN_MENUFONT'] = 'Menu link colour';
+$mod_strings['LBL_COLOUR_ADMIN_MENULNKHVR'] = 'Menu link hover colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTNTOP'] = 'Button colour top: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTNMID1'] = 'Button colour mid-top: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTNMID2'] = 'Button colour mid-bottom: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTNBTM'] = 'Button colour bottom: ';
+$mod_strings['LBL_COLOUR_ADMIN_BTNHOVER'] = 'Button hover colour: ';
+$mod_strings['LBL_COLOUR_ADMIN_DASHHEAD'] = 'Dashlet header colour: ';
--- install/suite_install/colourSelector.php
+++ install/suite_install/colourSelector.php
@@ -6,19 +6,24 @@ function install_colourSelector() {
     global $sugar_config;
 
     $sugar_config['colourselector']['version'] = '1.0';
-    if(!isset($sugar_config['colourselector']['menu'])) $sugar_config['colourselector']['menu'] = '#f10202';
-    if(!isset($sugar_config['colourselector']['menufrom'])) $sugar_config['colourselector']['menufrom'] = '#f10202';
-    if(!isset($sugar_config['colourselector']['menuto'])) $sugar_config['colourselector']['menuto'] = '#f10202';
-    if(!isset($sugar_config['colourselector']['menufont'])) $sugar_config['colourselector']['menufont'] = '#f10202';
+    if(!isset($sugar_config['colourselector']['menu'])) $sugar_config['colourselector']['menu'] = '#121212';
+    if(!isset($sugar_config['colourselector']['menufrom'])) $sugar_config['colourselector']['menufrom'] = '#121212';
+    if(!isset($sugar_config['colourselector']['menuto'])) $sugar_config['colourselector']['menuto'] = '#333333';
+    if(!isset($sugar_config['colourselector']['menufont'])) $sugar_config['colourselector']['menufont'] = '#cccccc';
     if(!isset($sugar_config['colourselector']['menubrd'])) $sugar_config['colourselector']['menubrd'] = '#f10202';
     if(!isset($sugar_config['colourselector']['pageheader'])) $sugar_config['colourselector']['pageheader'] = '#f10202';
     if(!isset($sugar_config['colourselector']['pagelink'])) $sugar_config['colourselector']['pagelink'] = '#f10202';
-    if(!isset($sugar_config['colourselector']['dashlet'])) $sugar_config['colourselector']['dashlet'] = '#f10202';
+    if(!isset($sugar_config['colourselector']['dashlet'])) $sugar_config['colourselector']['dashlet'] = '#777777';
     if(!isset($sugar_config['colourselector']['button1'])) $sugar_config['colourselector']['button1'] = '#ffffff';
     if(!isset($sugar_config['colourselector']['button2'])) $sugar_config['colourselector']['button2'] = '#f3f3f3';
     if(!isset($sugar_config['colourselector']['button3'])) $sugar_config['colourselector']['button3'] = '#ededed';
     if(!isset($sugar_config['colourselector']['button4'])) $sugar_config['colourselector']['button4'] = '#ffffff';
     if(!isset($sugar_config['colourselector']['buttonhover'])) $sugar_config['colourselector']['buttonhover'] = '#f10202';
+    if(!isset($sugar_config['colourselector']['modlinkvisited'])) $sugar_config['colourselector']['modlinkvisited'] = '#cccccc';
+    if(!isset($sugar_config['colourselector']['modlisthover'])) $sugar_config['colourselector']['modlisthover'] = '#565656';
+    if(!isset($sugar_config['colourselector']['modlinkhover'])) $sugar_config['colourselector']['modlinkhover'] = '#ffffff';
+    if(!isset($sugar_config['colourselector']['cssmenu'])) $sugar_config['colourselector']['cssmenu'] = '#333333';
+    if(!isset($sugar_config['colourselector']['cssmenulink'])) $sugar_config['colourselector']['cssmenulink'] = '#cccccc';
 
     ksort($sugar_config);
     write_array_to_file('sugar_config', $sugar_config, 'config.php');
