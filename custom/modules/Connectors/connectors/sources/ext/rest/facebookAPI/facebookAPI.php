aef31abc4b68e65713decbd1f66640e8a90fc5ef
Ian Davie <ian.davie@salesagility.com>
2014-01-20 12:31:04 +0000

    connectors for facebook API.

--- custom/modules/Connectors/connectors/sources/ext/rest/facebookAPI/facebookAPI.php
+++ custom/modules/Connectors/connectors/sources/ext/rest/facebookAPI/facebookAPI.php
@@ -11,7 +11,7 @@
             parent::__construct();
 
             //Used for testing
-            $this->_has_testing_enabled = true;
+            $this->_has_testing_enabled = false;
 
             //Used to enable hover for the formatter
             $this->_enable_in_hover = true;
@@ -29,27 +29,8 @@
          */
         public function getItem($args=array(), $module=null)
         {
-            $result = null;
-            if ($args['id'] == 1)
-            {
-                $result = array();
-                $result['id'] = '1'; //Unique record identifier
-                $result['firstname'] = 'John';
-                $result['lastname'] = 'Doe';
-                $result['email'] = 'john.doe@sugar.crm';
-                $result['state'] = 'CA';
-            }
-            else if ($args['id'] == 2)
-            {
-                $result = array();
-                $result['id'] = '2'; //Unique record identifier
-                $result['firstname'] = 'Jane';
-                $result['lastname'] = 'Doe';
-                $result['email'] = 'jane.doe@sugar.crm';
-                $result['state'] = 'HI';
-            }
 
-            return $result;
+            return null;
         }
 
         /**
@@ -62,27 +43,8 @@
          */
         public function getList($args=array(), $module=null)
         {
-            $results = array();
-            if(!empty($args['name']['last']) && strtolower($args['name']['last']) == 'doe')
-            {
-                $results[1] = array('
-                    id' => 1,
-                    'firstname' => 'John',
-                    'lastname' => 'Doe',
-                    'email' => 'john.doe@sugar.crm',
-                    'state' => 'CA'
-                );
 
-                $results[2] = array(
-                    'id' => 2,
-                    'firstname' => 'Jane',
-                    'lastname' => 'Doe',
-                    'email' => 'john.doe@sugar.crm',
-                    'state' => 'HI'
-                );
-            }
-
-            return $results;
+            return null;
         }
     }
 
