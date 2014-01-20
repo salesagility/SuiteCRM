3090204af65d4e6ecd0e8826c9efefddf9c84d61
Lewis Jessett <lewis.jessett@salesagility.com>
2014-01-20 12:27:42 +0000

    Fixed

--- custom/modules/Connectors/connectors/sources/ext/rest/facebook/facebook_sdk/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$base = realpath(dirname(__FILE__) . '/..');
-require "$base/src/base_facebook.php";
-require "$base/src/facebook.php";
---
+++ custom/modules/Connectors/connectors/sources/ext/rest/twitter/mapping.php
@@ -0,0 +1,13 @@
+<?php
+// created: 2014-01-20 10:34:22
+$mapping = array (
+  'beans' => 
+  array (
+    'Accounts' => 
+    array (
+    ),
+    'Contacts' => 
+    array (
+    ),
+  ),
+);
