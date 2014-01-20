e797fe4bdaafe0a7af70e731aa9bdd7aab6e8313
Will <will.rennie@salesagility.com>
2014-01-20 11:50:20 +0000

    Reverted colours to original SuiteCRM as they should be.

--- config_override.php
+++ config_override.php
@@ -1,11 +1,11 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['colourselector']['menu'] = 'F10202';
+$sugar_config['colourselector']['menu'] = '121212';
 $sugar_config['colourselector']['menufont'] = 'FFFFFF';
-$sugar_config['colourselector']['menubrd'] = '121212';
+$sugar_config['colourselector']['menubrd'] = 'F10202';
 $sugar_config['colourselector']['pageheader'] = 'F10202';
 $sugar_config['colourselector']['pagelink'] = 'F10202';
-$sugar_config['colourselector']['dashlet'] = 'F10202';
+$sugar_config['colourselector']['dashlet'] = '777777';
 $sugar_config['colourselector']['button'] = '#f102023';
 $sugar_config['colourselector']['menufrom'] = '121212';
 $sugar_config['colourselector']['menuto'] = '333333';
@@ -19,7 +19,7 @@ $sugar_config['colourselector']['modlisthover'] = '565656';
 $sugar_config['colourselector']['modlinkhover'] = 'FFFFFF';
 $sugar_config['colourselector']['cssmenu'] = '333333';
 $sugar_config['colourselector']['cssmenulink'] = 'CCCCCC';
-$sugar_config['colourselector']['custom'] = 'test';
+$sugar_config['colourselector']['custom'] = '';
 $sugar_config['default_module_favicon'] = false;
 $sugar_config['dashlet_auto_refresh_min'] = '30';
 $sugar_config['enable_action_menu'] = true;
