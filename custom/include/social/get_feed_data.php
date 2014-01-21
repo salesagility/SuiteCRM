ab41420f14971cdf7d02266ae35b3159f40bc63b
Lewis Jessett <lewis.jessett@salesagility.com>
2014-01-21 09:22:49 +0000

    Fixed Twitter Issues

--- custom/include/social/get_feed_data.php
+++ custom/include/social/get_feed_data.php
@@ -5,7 +5,6 @@
  * Date: 15/01/14
  * Time: 09:14
  */
-$var=5;
 require_once('custom/include/social/twitter/twitter_auth/twitteroauth/twitteroauth.php');
 require('custom/modules/Connectors/connectors/sources/ext/rest/twitter/config.php');
 require('custom/include/social/twitter/twitter_helper.php');
@@ -15,8 +14,37 @@ global $current_user;
 
 session_start();
 
-//Authenicate User and authenicate them if they are not already authenticated
-$html = auth_twitter();
+$settings = array(
+    'oauth_access_token' => $config['properties']['oauth_access_token'],
+    'oauth_access_token_secret' => $config['properties']['oauth_access_token_secret'],
+    'consumer_key' => $config['properties']['consumer_key'],
+    'consumer_secret' => $config['properties']['consumer_secret'],
+    'call_back_url' => $config['properties']['OAUTH_CALLBACK'],
+);
+
+if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
+    if ($settings['consumer_key'] === '' || $settings['consumer_secret'] === '') {
+        echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://dev.twitter.com/apps">dev.twitter.com/apps</a>';
+        exit;
+    }
+
+    /* Build TwitterOAuth object with client credentials. */
+    $connection = new TwitterOAuth($settings['consumer_key'], $settings['consumer_secret']);
+
+    /* Get temporary credentials. */
+    $request_token = $connection->getRequestToken($settings['call_back_url']);
+
+    /* Save temporary credentials to session. */
+    $_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
+    $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+
+    $html ='';
+
+    /* Build authorize URL and redirect user to Twitter. */
+    $url = $connection->getAuthorizeURL($token);
+    $html = "<a href='". $url ."'>Log into Twitter</a>";
+
+}
 
 /* Get user access tokens out of the session. */
 $access_token = $_SESSION['access_token'];
