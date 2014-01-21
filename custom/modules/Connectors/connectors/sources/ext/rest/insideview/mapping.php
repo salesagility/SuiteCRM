12ff58be609c9ec56632e1a2f1a46c401b4f27bc
Ian Davie <ian.davie@salesagility.com>
2014-01-21 10:58:33 +0000

    changes to the activity stream to allow facebook profile pictures

---
+++ custom/modules/Connectors/connectors/sources/ext/rest/insideview/mapping.php
@@ -0,0 +1,19 @@
+<?php
+// created: 2014-01-20 14:18:18
+$mapping = array (
+  'beans' => 
+  array (
+    'Accounts' => 
+    array (
+    ),
+    'Contacts' => 
+    array (
+    ),
+    'Leads' => 
+    array (
+    ),
+    'Opportunities' => 
+    array (
+    ),
+  ),
+);
