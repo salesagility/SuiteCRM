9e6c217254385985794561b3999d5848a8d5d2a0
likhobory <grego@list.ru>
2014-01-22 10:48:26 +0400

    Added addidional ru_ru files to /custom/Extension

---
+++ custom/Extension/modules/Administration/Ext/Language/ru_ru.QuickCRM_Mobile_CE.php
@@ -0,0 +1,11 @@
+<?php
+
+$mod_strings['LBL_UPDATE_QUICKCRM_TITLE'] = "Настройка QuickCRM"; 
+$mod_strings['LBL_UPDATE_QUICKCRM'] = "Настройка приложения для работы через мобильные устройства";
+$mod_strings['LBL_QUICKCRM'] = 'QuickCRM Mobile';
+$mod_strings['LBL_CONFIG_QUICKCRM_TITLE'] = "Configuration of QuickCRM Mobile";
+$mod_strings['LBL_CONFIG_QUICKCRM'] = "Definition of visible modules and fields";
+$mod_strings['LBL_UPDATE_MSG'] = '<strong>Приложение настроено для работы через мобильные устройства.</strong><br/> Мобильная версия доступна по ссылке:';
+$mod_strings['LBL_ERR_DIR_MSG'] = 'Некоторые файлы не могут быть созданы. Проверьте права доступа для: ';  
+
+
---
+++ custom/Extension/modules/Administration/Ext/Language/ru_ru.Reschedule.php
@@ -0,0 +1,9 @@
+<?php
+    
+$mod_strings['LBL_RESCHEDULE_REBUILD'] = 'Восстановить отложенные звонки';
+$mod_strings['LBL_RESCHEDULE_REBUILD_DESC'] = 'Восстановление модуля отложенных звонков';
+$mod_strings['LBL_RESCHEDULE_ADMIN'] = 'Отложенные звонки';
+$mod_strings['LBL_RESCHEDULE_ADMIN_DESC'] = 'Настройка отложенных звонков';
+$mod_strings['LBL_REPAIR_RESCHEDULE_DONE'] = 'Отложенные звонки успешно восстановлены';
+$mod_strings['LBL_SALESAGILITY_ADMIN'] = 'OpenAdmin';
+
