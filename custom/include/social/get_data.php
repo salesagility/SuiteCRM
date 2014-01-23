ef230d29efa18b63284e46b24d34e956fcf78354
Lewis Jessett <lewis.jessett@salesagility.com>
2014-01-23 11:16:00 +0000

    Commenting code

--- custom/include/social/get_data.php
+++ custom/include/social/get_data.php
@@ -1,10 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ian
- * Date: 14/01/14
- * Time: 16:08
+
+/*
+ * @author Salesagility Ltd <support@salesagility.com>
+ * Date: 15/01/14
+ * Time: 09:14
  */
+
+
+//Grab the twitter and facebook values from the request.
 
 $social = $_REQUEST['social'];
 $twitter_user = $_REQUEST['twitter_user'];
@@ -14,12 +17,14 @@ if(!empty($_REQUEST['code'])){
     //nasty way of fixing the facebook login bug.
     header("Location: " . $sugar_config['site_url'] . "?module=" . $_REQUEST['module'] . "&action=DetailView&record=" . $_REQUEST['record'] . "&");
 }
+
+//If its facebook or twitter call the relevent file.
     switch($social){
 
         case "facebook":
             require("custom/include/social/facebook/facebookapi.php");
             break;
         case "twitter";
-            require("custom/include/social/twitter/twitter_process.php");
+            require("custom/include/social/twitter/twitterapi.php");
             break;
     }
