593b34da538cb9abd6efacf91fdaa938f1449f2d
Ian Davie <ian.davie@salesagility.com>
2014-01-31 16:11:04 +0000

    remove eapm from facebook connector.

--- custom/modules/Connectors/connectors/sources/ext/rest/facebook/language/en_us.lang.php
+++ custom/modules/Connectors/connectors/sources/ext/rest/facebook/language/en_us.lang.php
@@ -5,7 +5,7 @@
     $connector_strings = array (
         //Vardef labels
         'LBL_LICENSING_INFO' => '<table border="0" cellspacing="1"><tr><th valign="top" width="35%" class="dataLabel">Facebook Application Information </th></tr>
-                                    <tr><td width="35%" class="dataLabel">You will need to create a facebook application <a href=https://developers.facebook.com/?ref=pf">here</a> to aquire  </td></tr></table>',
+                                    <tr><td width="35%" class="dataLabel">You will need to create a facebook application, Get one <a href=https://developers.facebook.com/?ref=pf">here</a>  </td></tr></table>',
 
         //Configuration labels
         'appid' => 'Facebook App ID ',
