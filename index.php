f241809b8b6e33e500f9c39e37959fd3b966bbc9
Matthew <matthew.lorimer@salesagility.com>
2014-02-03 20:12:59 -0800

    7.1beta

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
-
 if(!defined('sugarEntry'))define('sugarEntry', true);
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
