d322b09a3d95ff4dae5709c45cdf29d8c4b60d78
Matthew <matthew.lorimer@salesagility.com>
2014-03-24 14:10:30 +0000

    install fixes

--- custom/Extension/modules/AOS_Invoices/Ext/Vardefs/sugarfield_twitter_user_c.php
+++
@@ -1,5 +0,0 @@
-<?php
- // created: 2014-01-29 15:05:14
-$dictionary['AOS_Invoices']['fields']['twitter_user_c']['duplicate_merge_dom_value']=0;
-
- ?>
--- custom/modules/logic_hooks.php
+++ custom/modules/logic_hooks.php
@@ -11,8 +11,6 @@ $hook_array['after_ui_frame'][] = Array(20, 'mass_assign', 'modules/SecurityGrou
 $hook_array['after_ui_frame'][] = Array(40, 'version_check', 'modules/SecurityGroups/VersionCheck.php','VersionCheck', 'version_check'); 
 $hook_array['after_ui_frame'][] = Array(1, 'Load Social JS', 'custom/include/social/hooks.php','hooks', 'load_js');
 $hook_array['after_save'] = Array();
-$hook_array['after_save'][] = Array(30, 'popup_select', 'modules/SecurityGroups/AssignGroups.php','AssignGroups', 'popup_select');
-$hook_array['after_save'] = Array(); 
 $hook_array['after_save'][] = Array(30, 'popup_select', 'modules/SecurityGroups/AssignGroups.php','AssignGroups', 'popup_select'); 
 $hook_array['after_save'][] = Array(1, 'AOD Index Changes', 'modules/AOD_Index/AOD_LogicHooks.php','AOD_LogicHooks', 'saveModuleChanges'); 
 
