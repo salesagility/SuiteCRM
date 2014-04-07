5797bb0e79550d7357dd6ad020861d8e23df3e91
Matthew <matthew.lorimer@salesagility.com>
2014-04-07 13:49:40 +0100

    Updated application data

--- custom/application/Ext/TableDictionary/tabledictionary.ext.php
+++ custom/application/Ext/TableDictionary/tabledictionary.ext.php
@@ -3,22 +3,7 @@
 
  
  //WARNING: The contents of this file are auto-generated
-include('custom/metadata/aos_contracts_documentsMetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/aos_quotes_aos_contractsMetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/aos_quotes_aos_invoicesMetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/aos_quotes_projectMetaData.php');
+include('custom/metadata/securitygroups_recordsMetaData.php');
 
 
  
@@ -28,27 +13,18 @@ include('custom/metadata/aow_processed_aow_actionsMetaData.php');
 
  
  //WARNING: The contents of this file are auto-generated
+include('custom/metadata/fp_events_contactsMetaData.php');
+
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/aos_contracts_documentsMetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 include('custom/metadata/fp_event_locations_fp_events_1MetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/fp_events_fp_event_delegates_1MetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/fp_events_fp_event_locations_1MetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/fp_events_prospects_1MetaData.php');
-
-
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/jjwg_maps_jjwg_areasMetaData.php');
 
 
  
@@ -63,17 +39,53 @@ include('custom/metadata/securitygroups_acl_rolesMetaData.php');
 
  
  //WARNING: The contents of this file are auto-generated
+include('custom/metadata/securitygroups_usersMetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/fp_events_prospects_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/aos_quotes_aos_contractsMetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/jjwg_maps_jjwg_areasMetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 include('custom/metadata/securitygroups_defaultsMetaData.php');
 
 
  
  //WARNING: The contents of this file are auto-generated
-include('custom/metadata/securitygroups_recordsMetaData.php');
+include('custom/metadata/fp_events_leads_1MetaData.php');
+
 
 
  
  //WARNING: The contents of this file are auto-generated
-include('custom/metadata/securitygroups_usersMetaData.php');
+include('custom/metadata/aos_quotes_projectMetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/fp_events_fp_event_locations_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/fp_events_fp_event_delegates_1MetaData.php');
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
+include('custom/metadata/aos_quotes_aos_invoicesMetaData.php');
 
 
 ?>
