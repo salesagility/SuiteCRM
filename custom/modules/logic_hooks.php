1721f25f88b3673bf453866d5733fe9169968b4d
Jim Mackin <jim.mackin@salesagility.com>
2014-06-11 10:27:36 +0100

    Update index on delete and restore.

--- custom/modules/logic_hooks.php
+++ custom/modules/logic_hooks.php
@@ -12,7 +12,11 @@ $hook_array['after_ui_frame'][] = Array(40, 'version_check', 'modules/SecurityGr
 $hook_array['after_ui_frame'][] = Array(1, 'Load Social JS', 'custom/include/social/hooks.php','hooks', 'load_js');
 $hook_array['after_save'] = Array();
 $hook_array['after_save'][] = Array(30, 'popup_select', 'modules/SecurityGroups/AssignGroups.php','AssignGroups', 'popup_select'); 
-$hook_array['after_save'][] = Array(1, 'AOD Index Changes', 'modules/AOD_Index/AOD_LogicHooks.php','AOD_LogicHooks', 'saveModuleChanges'); 
+$hook_array['after_save'][] = Array(1, 'AOD Index Changes', 'modules/AOD_Index/AOD_LogicHooks.php','AOD_LogicHooks', 'saveModuleChanges');
+$hook_array['after_delete'] = array();
+$hook_array['after_delete'][] = Array(1, 'AOD Index changes', 'modules/AOD_Index/AOD_LogicHooks.php','AOD_LogicHooks', 'saveModuleDelete');
+$hook_array['after_restore'] = array();
+$hook_array['after_restore'][] = Array(1, 'AOD Index changes', 'modules/AOD_Index/AOD_LogicHooks.php','AOD_LogicHooks', 'saveModuleRestore');
 
 
 
