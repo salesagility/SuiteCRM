36b9e491a991062ab4d2a4ba68fa0c496680cf64
Jim Mackin <jim@jsmackin.co.uk>
2014-07-29 11:36:54 +0100

    Prevent the portal creating duplicate sugar feed entries
    Prevent duplicate sugar feed entries when creating a case and populate the account name if it isn't already populated.

--- custom/modules/Cases/SugarFeeds/CaseFeed.php
+++ custom/modules/Cases/SugarFeeds/CaseFeed.php
@@ -45,8 +45,13 @@ class CaseFeed extends FeedLogicBase {
     var $module = 'Cases';
     function pushFeed($bean, $event, $arguments){
         $text = '';
-        if(empty($bean->fetched_row)){
-            $text =  '{SugarFeed.CREATED_CASE} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name.'] {SugarFeed.FOR} [Accounts:' . $bean->account_id . ':' . $bean->account_name . ']: '. $bean->description;
+	if(empty($bean->fetched_row) && $bean->in_save){
+            $accountName = $bean->account_name;
+            if(empty($accountName) && $bean->account_id){
+                $acc = BeanFactory::getBean('Accounts',$bean->account_id);
+                $accountName = $acc->name;
+            }
+            $text =  '{SugarFeed.CREATED_CASE} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name.'] {SugarFeed.FOR} [Accounts:' . $bean->account_id . ':' . $accountName . ']: '. $bean->description;
         }else{
             if(!empty($bean->fetched_row['status'] ) && $bean->fetched_row['status'] != $bean->status && strpos($bean->status, 'Closed') !== false){
                 $text =  '{SugarFeed.CLOSED_CASE} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name. '] {SugarFeed.FOR} [Accounts:' . $bean->account_id . ':' . $bean->account_name . ']';
