739338509485b94baf63fd405d443ae4d3f7556c
William Rennie <will.rennie@salesagility.com>
2014-10-29 23:14:17 +0000

    Tidying up installation page code structure.

--- install/dbConfig_a.php
+++ install/dbConfig_a.php
@@ -82,24 +82,14 @@ $out =<<<EOQ
     <meta http-equiv="Content-Style-Type" content="text/css">
     <title>{$mod_strings['LBL_WIZARD_TITLE']} {$mod_strings['LBL_DBCONF_TITLE']}</title>
     <link rel="stylesheet" href="install/install.css" type="text/css" />
+    <link rel='stylesheet' type='text/css' href='include/javascript/yui/build/container/assets/container.css' />
+    <link rel="stylesheet" href="themes/Suite7/css/fontello.css">
+    <link rel="stylesheet" href="themes/Suite7/css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
+    <link REL="SHORTCUT ICON" HREF="include/images/sugar_icon.ico">
     <script type="text/javascript" src="install/installCommon.js"></script>
     <script type="text/javascript" src="install/dbConfig.js"></script>
-    <link REL="SHORTCUT ICON" HREF="include/images/sugar_icon.ico">
     <script src="cache/include/javascript/sugar_grp1_yui.js?s={$sugar_version}&c={$js_custom_version}"></script>
     <script src="cache/include/javascript/sugar_grp1_jquery.js?s={$sugar_version}&c={$js_custom_version}"></script>
-    <script type="text/javascript">
-    <!--
-    if ( YAHOO.env.ua )
-        UA = YAHOO.env.ua;
-    -->
-    </script>
-    <link rel='stylesheet' type='text/css' href='include/javascript/yui/build/container/assets/container.css' />
-<link rel="stylesheet" href="themes/Suite7/css/responsiveslides.css" type="text/css">
-   <link rel="stylesheet" href="themes/Suite7/css/themes.css" type="text/css">
-   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="themes/Suite7/js/responsiveslides.min.js"></script>
-<link rel="stylesheet" href="themes/Suite7/css/fontello.css">
-    <link rel="stylesheet" href="themes/Suite7/css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/fontello-ie7.css"><![endif]-->
 </head>
 
 EOQ;
@@ -108,21 +98,19 @@ $out .= '<body onload="document.getElementById(\'button_next2\').focus();">';
 $out2 =<<<EOQ2
 <!--SuiteCRM installer-->
 <div id="install_container">
-<div id="install_box">
-<form action="install.php" method="post" name="setConfig" id="form">
-<header id="install_header">
-    <div id="steps"><p>{$mod_strings['LBL_STEP5']}</p><i class="icon-progress-0" id="complete"></i><i class="icon-progress-1" id="complete"></i><i class="icon-progress-2" id="complete"></i><i class="icon-progress-3" id="complete"></i><i class="icon-progress-4" id="complete"></i><i class="icon-progress-5"></i><i class="icon-progress-6"></i><i class="icon-progress-7"></i>
-    </div>
-    <div class="install_img"><a href="https://suitecrm.com" target="_blank"><img src="{$sugar_md}" alt="SuiteCRM"></a></div>
-</header>
-<input type='hidden' name='setup_db_drop_tables' id='setup_db_drop_tables' value=''>
-<input type="hidden" id="hidden_goto" name="goto" value="{$mod_strings['LBL_BACK']}" />
-
-<h2>{$mod_strings['LBL_DBCONF_TITLE']}</h2>
-<div id="errorMsgs" style="display:none"></div>
-<div class="required">{$mod_strings['LBL_REQUIRED']}</div>
-<hr>
-<h3>{$mod_strings['LBL_DBCONF_TITLE_NAME']}</h3>
+    <div id="install_box">
+        <form action="install.php" method="post" name="setConfig" id="form">
+            <header id="install_header">
+                <div id="steps"><p>{$mod_strings['LBL_STEP5']}</p><i class="icon-progress-0" id="complete"></i><i class="icon-progress-1" id="complete"></i><i class="icon-progress-2" id="complete"></i><i class="icon-progress-3" id="complete"></i><i class="icon-progress-4" id="complete"></i><i class="icon-progress-5"></i><i class="icon-progress-6"></i><i class="icon-progress-7"></i></div>
+                <div class="install_img"><a href="https://suitecrm.com" target="_blank"><img src="{$sugar_md}" alt="SuiteCRM"></a></div>
+            </header>
+            <input type='hidden' name='setup_db_drop_tables' id='setup_db_drop_tables' value=''>
+            <input type="hidden" id="hidden_goto" name="goto" value="{$mod_strings['LBL_BACK']}" />
+            <h2>{$mod_strings['LBL_DBCONF_TITLE']}</h2>
+            <div id="errorMsgs" style="display:none"></div>
+            <div class="required">{$mod_strings['LBL_REQUIRED']}</div>
+            <hr>
+            <h3>{$mod_strings['LBL_DBCONF_TITLE_NAME']}</h3>
 
 EOQ2;
 
@@ -201,22 +189,20 @@ if($db->supports("create_user")){
 
     $out2 .=<<<EOQ2
 <br>
-<hr>
+    <hr>
 <br>
 {$mod_strings['LBL_DBCONFIG_SECURITY']}
 <div class='install_block'><label><b>{$mod_strings['LBL_DBCONF_SUGAR_DB_USER']}</b></label>$dbUSRDD
-
-<span id='connection_user_div' style="display:none">
+    <span id='connection_user_div' style="display:none">
         <span class="required">*</span>
-        <label><b>{$mod_strings['LBL_DBCONF_SUGAR_DB_USER']}</b></label>
-        <input type="text" name="setup_db_sugarsales_user" maxlength="16" value="{$_SESSION['setup_db_sugarsales_user']}" />
-
-    <label><b>{$mod_strings['LBL_DBCONF_DB_PASSWORD']}</b></label>
-    <input type="password" name="setup_db_sugarsales_password_entry" value="{$setup_db_sugarsales_password}" /><input type="hidden" name="setup_db_sugarsales_password" value="{$setup_db_sugarsales_password}" />
-    <input type="hidden" name="setup_db_sugarsales_password" value="{$_SESSION['setup_db_sugarsales_password']}" />
-    <label><b>{$mod_strings['LBL_DBCONF_DB_PASSWORD2']}</b></label>
-    <input type="password" name="setup_db_sugarsales_password_retype_entry" value="{$setup_db_sugarsales_password_retype}"  /><input type="hidden" name="setup_db_sugarsales_password_retype" value="{$setup_db_sugarsales_password_retype}" />
-</span>
+            <label><b>{$mod_strings['LBL_DBCONF_SUGAR_DB_USER']}</b></label>
+            <input type="text" name="setup_db_sugarsales_user" maxlength="16" value="{$_SESSION['setup_db_sugarsales_user']}" />
+            <label><b>{$mod_strings['LBL_DBCONF_DB_PASSWORD']}</b></label>
+            <input type="password" name="setup_db_sugarsales_password_entry" value="{$setup_db_sugarsales_password}" /><input type="hidden" name="setup_db_sugarsales_password" value="{$setup_db_sugarsales_password}" />
+            <input type="hidden" name="setup_db_sugarsales_password" value="{$_SESSION['setup_db_sugarsales_password']}" />
+            <label><b>{$mod_strings['LBL_DBCONF_DB_PASSWORD2']}</b></label>
+            <input type="password" name="setup_db_sugarsales_password_retype_entry" value="{$setup_db_sugarsales_password_retype}"  /><input type="hidden" name="setup_db_sugarsales_password_retype" value="{$setup_db_sugarsales_password_retype}" />
+    </span>
 </div>
 
 EOQ2;
@@ -229,7 +215,7 @@ $demoDD .= "</select>";
 $out3 =<<<EOQ3
 <hr>
 <div class="install_block">
-<h2>{$mod_strings['LBL_DBCONF_DEMO_DATA_TITLE']}</h2>
+    <h2>{$mod_strings['LBL_DBCONF_DEMO_DATA_TITLE']}</h2>
     <label>{$mod_strings['LBL_DBCONF_DEMO_DATA']}</label>
         {$demoDD}
 </div>
@@ -434,21 +420,19 @@ function confirm_drop_tables(yes_no){
     <p id="footer_links"><a href="https://suitecrm.com" target="_blank">Visit suitecrm.com</a> | <a href="https://suitecrm.com/index.php?option=com_kunena&view=category&Itemid=1137&layout=list" target="_blank">Support Forums</a> | <a href="https://suitecrm.com/wiki/index.php/Installation" target="_blank">Installation Guide</a> | <a href="LICENSE.txt" target="_blank">License</a>
 </footer>
 </div>
+<script type="text/javascript">
+    <!--
+    if ( YAHOO.env.ua )
+        UA = YAHOO.env.ua;
+    -->
+    </script>
 </body>
 </html>
 
-
-
-
 EOQ5;
-
-
-
 
 ////	END PAGE OUTPUT
 ///////////////////////////////////////////////////////////////////////////////
-
-
 
 echo $out;
 echo $out2;
