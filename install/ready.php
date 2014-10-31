a949297f94227e1a00c6588df7f43b2141b53d77
William Rennie <will.rennie@salesagility.com>
2014-10-31 15:22:18 +0000

    Small fixes to installer CSS.

--- install/ready.php
+++ install/ready.php
@@ -69,12 +69,10 @@ $out = <<<EOQ
     <!--SuiteCRM installer-->
     <div id="install_container">
     <div id="install_box">
-        <form action="install.php" method="post" name="form" id="form">
-            <header id="install_header">
-                <div id="steps"><p>{$mod_strings['LBL_STEP1']}</p><i class="icon-progress-0"></i><i class="icon-progress-1"></i><i class="icon-progress-2"></i><i class="icon-progress-3"></i><i class="icon-progress-4"></i><i class="icon-progress-5"></i><i class="icon-progress-6"></i><i class="icon-progress-7"></i></div>
-                <div class="install_img"><a href="https://suitecrm.com" target="_blank"><img src="{$sugar_md}" alt="SuiteCRM"></a></div>
-            </header>
-
+        <header id="install_header">
+            <div id="steps"><p>{$mod_strings['LBL_STEP1']}</p><i class="icon-progress-0"></i><i class="icon-progress-1"></i><i class="icon-progress-2"></i><i class="icon-progress-3"></i><i class="icon-progress-4"></i><i class="icon-progress-5"></i><i class="icon-progress-6"></i><i class="icon-progress-7"></i></div>
+            <div class="install_img"><a href="https://suitecrm.com" target="_blank"><img src="{$sugar_md}" alt="SuiteCRM"></a></div>
+        </header>
 	        <form action="install.php" method="post" name="form" id="form">
 	        	<div id="install_content">
 			    {$mod_strings['LBL_TITLE_ARE_YOU_READY']}
