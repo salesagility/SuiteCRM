6ee464715bce395033728967f7788dbea294bdec
Jim Mackin <jim.mackin@salesagility.com>
2014-12-05 10:47:31 +0000

    Add ids to the report pagination. This will prevent conflicts if multiple reports share a page (as in the case with Dashlets).

--- modules/AOR_Reports/AOR_Report.js
+++ modules/AOR_Reports/AOR_Report.js
@@ -59,14 +59,12 @@ function setProspectReturn(popup_reply_data){
 }
 
 
-function changeReportPage(offset, group){
+function changeReportPage(record, offset, group){
     var callback = {
         success: function(result) {
            document.getElementById('report_table'+group).innerHTML = result.responseText;
         }
     }
-
-    var record = document.getElementsByName('record')[0].value;
 
     YAHOO.util.Connect.asyncRequest ("GET", "index.php?module=AOR_Reports&action=changeReportPage&record="+record+"&offset="+offset+"&group="+group,callback);
 }
