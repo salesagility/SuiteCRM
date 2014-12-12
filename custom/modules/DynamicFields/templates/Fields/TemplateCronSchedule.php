636c8225afa24bce272ab0b1f7caead1194b8137
Jim Mackin <jim.mackin@salesagility.com>
2014-12-12 11:41:05 +0000

    Add a custom CronSchedule field type

---
+++ custom/modules/DynamicFields/templates/Fields/TemplateCronSchedule.php
@@ -0,0 +1,17 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+require_once('modules/DynamicFields/templates/Fields/TemplateText.php');
+class TemplateCronSchedule extends TemplateText{
+    var $type='CronSchedule';
+
+    function TemplateCronSchedule(){
+        parent::__construct();
+    }
+
+    function get_field_def(){
+        $def = parent::get_field_def();
+        $def['dbType'] = 'varchar';
+        return $def;
+    }
+}
