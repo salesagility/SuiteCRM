cc6408c3e0ee51c294c7eb815cf88c6742427fd1
Jim Mackin <jim.mackin@salesagility.com>
2014-12-29 14:48:28 +0000

    Add a ViewDetail and update the report template to allow choosing the parameters for a report. Update AOR_Report to use these selected values.

--- modules/AOR_Reports/language/en_us.lang.php
+++ modules/AOR_Reports/language/en_us.lang.php
@@ -78,4 +78,5 @@ $mod_strings = array (
   'LBL_DASHLET_CHARTS' => 'Charts',
   'LBL_DASHLET_ONLY_CHARTS' => 'Only show charts',
   'LBL_AOR_SCHEDULED_REPORTS_AOR_REPORTS_FROM_AOR_SCHEDULED_REPORTS_TITLE' => 'Scheduled Reports',
+  'LBL_UPDATE_PARAMETERS' => 'Update',
 );
