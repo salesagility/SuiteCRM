687223d53f1634cd3436057444a440246932acb6
Matthew <matthew.lorimer@salesagility.com>
2015-02-09 16:29:14 +0000

    Updated version

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.5.0 Alpha';
-$suitecrm_timestamp    = '2015-01-01 00:00am';
+$suitecrm_version      = '7.2.0 Beta 3';
+$suitecrm_timestamp    = '2015-02-09 17:00';
