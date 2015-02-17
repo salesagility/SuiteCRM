ca805eed419a30543983ddbfc6a7a41b9d606f32
Ian Davie <ian.davie@salesagility.com>
2015-02-17 13:39:06 +0000

    New Image field type.

---
+++ custom/include/SugarFields/Fields/Image/deleteAttachment.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 17/02/15
+ * Time: 11:59
+ */
+
+    $field = $_REQUEST['field'];
+    $removeFile = "upload://{$_REQUEST['image_c_record_id']}_" . $field;
+    $bean = BeanFactory::getBean("Contacts", $_REQUEST[$field . "_record_id"]);
+
+
+if(file_exists($removeFile)) {
+    if(!unlink($removeFile)) {
+        $GLOBALS['log']->error("*** Could not unlink() file: [ {$removeFile} ]");
+    }else{
+        $bean->$field = '';
+        $bean->save();
+        echo "true";
+    }
+} else {
+    $bean->$field = '';
+    $bean->save();
+    echo 'true';
+}
