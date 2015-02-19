acf40571d2b6f0e766b5282940e45c5f54d70b68
Ian Davie <ian.davie@salesagility.com>
2015-02-19 13:42:01 +0000

    removed image_download entrypoint

--- custom/Extension/application/Ext/EntryPointRegistry/image_field.php
+++ custom/Extension/application/Ext/EntryPointRegistry/image_field.php
@@ -6,6 +6,4 @@
  * Time: 15:24
  */
 
-$entry_point_registry['deleteAttachment'] = array('file' => 'custom/include/SugarFields/Fields/Image/deleteAttachment.php' , 'auth' => '1');
-
-$entry_point_registry['image_download'] = array('file' => 'image_download.php' , 'auth' => '1');
+$entry_point_registry['deleteAttachment'] = array('file' => 'include/SugarFields/Fields/Image/deleteAttachment.php' , 'auth' => '1');
