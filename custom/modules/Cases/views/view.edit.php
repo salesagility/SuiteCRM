9e8d794b86b49660f6a5838330978be24e3ccd9a
Matthew <matthew.lorimer@salesagility.com>
2015-03-01 22:57:23 +0000

    Fix to hide update attachments on create

--- custom/modules/Cases/views/view.edit.php
+++ custom/modules/Cases/views/view.edit.php
@@ -34,10 +34,8 @@ class CasesViewEdit extends ViewEdit {
     function display(){
         parent::display();
         global $sugar_config;
-        $enabled = !empty($sugar_config['aop']['enable_aop']);
         $new = empty($this->bean->id);
-        $show = $enabled && !$new;
-        if(!$show){
+        if($new){
             ?>
             <script>
                 $(document).ready(function(){
@@ -45,6 +43,8 @@ class CasesViewEdit extends ViewEdit {
                     $('#update_text_label').closest('td').html('');
                     $('#internal').closest('td').html('');
                     $('#internal_label').closest('td').html('');
+                    $('#addFileButton').closest('td').html('');
+                    $('#case_update_form_label').closest('td').html('');
                 });
             </script>
         <?php
