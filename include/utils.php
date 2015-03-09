c4c4046805e869f7591229f9eadd15daf7697683
Matthew <matthew.lorimer@salesagility.com>
2015-03-09 13:40:39 +0000

    Updated PHP version check to check for php 5.3+

--- include/utils.php
+++ include/utils.php
@@ -2873,12 +2873,12 @@ function check_php_version($sys_php_version = '') {
 	// versions below $min_considered_php_version considered invalid by default,
 	// versions equal to or above this ver will be considered depending
 	// on the rules that follow
-	$min_considered_php_version = '5.2.2';
+	$min_considered_php_version = '5.3.0';
 
 	// only the supported versions,
 	// should be mutually exclusive with $invalid_php_versions
 	$supported_php_versions = array (
-    	'5.2.2', '5.2.3', '5.2.4', '5.2.5', '5.2.6', '5.2.8', '5.3.0'
+    	'5.3.0'
 	);
 
 	// invalid versions above the $min_considered_php_version,
