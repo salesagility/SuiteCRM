e9e2ae5b7fa14678f5d17a0fa8c844610ec4a777
JanSiero <jan@siero.de>
2015-05-01 23:30:13 +0200

    Fixed stray latin1 characters in language files

--- custom/Extension/application/Ext/Language/it_it.SecurityGroups.php
+++ custom/Extension/application/Ext/Language/it_it.SecurityGroups.php
@@ -1,8 +1,8 @@
 <?php
 
 $app_list_strings["moduleList"]["SecurityGroups"] = 'Gestione Gruppi di Sicurezza';
-$app_strings['LBL_LOGIN_AS'] = "Inizio attività come ";
-$app_strings['LBL_LOGOUT_AS'] = "Termine attività come ";
+$app_strings['LBL_LOGIN_AS'] = "Inizio attivitÃ  come ";
+$app_strings['LBL_LOGOUT_AS'] = "Termine attivitÃ  come ";
 $app_strings['LBL_SECURITYGROUP'] = 'Gestione Gruppi';
 
 ?>
