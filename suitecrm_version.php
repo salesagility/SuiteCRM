c2fc0b352b303d82bae16b012e416a0c8b98abed
William Rennie <will.rennie@salesagility.com>
2015-05-20 12:03:55 +0100

    Updating suitecrm version for 7.1.7 release.

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,7 +1,7 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.1.6';
-$suitecrm_timestamp    = '2015-03-11 14:00pm';
+$suitecrm_version      = '7.1.7';
+$suitecrm_timestamp    = '2015-05-20 17:00pm';
 
 ?>
