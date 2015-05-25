5a778b34ccd7533d243d4ff296571c04bd9846ba
Horus68 <pauloizidoro@gmail.com>
2015-05-25 18:51:00 +0100

    Revert "Remove hu_hu orphan language files"
    This reverts commit 7ecc8d921c75c642e9740a53fb9635427eeaa967.

---
+++ custom/Extension/application/Ext/Language/hu_hu.SecurityGroups.php
@@ -0,0 +1,8 @@
+<?php
+
+$app_list_strings["moduleList"]["SecurityGroups"] = 'Biztonsági Csoportok Kezelése';
+$app_strings['LBL_LOGIN_AS'] = "Belépés mint ";
+$app_strings['LBL_LOGOUT_AS'] = "Kilépés mint ";
+$app_strings['LBL_SECURITYGROUP'] = 'Biztonsági Csoportok';
+
+?>
---
+++ custom/Extension/modules/Administration/Ext/Language/hu_hu.SecurityGroups.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+$mod_strings['LBL_MANAGE_SECURITYGROUPS_TITLE'] = 'Biztonsági Csoportok Kezelése';
+$mod_strings['LBL_MANAGE_SECURITYGROUPS'] = 'Biztonsági Csoport Szerkesztő';
+$mod_strings['LBL_SECURITYGROUPS'] = 'Biztonsági Csoportok';
+$mod_strings['LBL_CONFIG_SECURITYGROUPS_TITLE'] = 'Biztonsági Csoportok Beállításai';
+$mod_strings['LBL_CONFIG_SECURITYGROUPS'] = 'Módosíthatók a Biztonsági Csoport beállításai, mint pl. az öröklődés, additív jogok, stb.';
+$mod_strings['LBL_SECURITYGROUPS'] = 'Biztonsági Csoportok';
+$mod_strings['LBL_SECURITYGROUPS_UPGRADE_INFO_TITLE'] = "Frissítés és Általános Információ";
+$mod_strings['LBL_SECURITYGROUPS_INFO_TITLE'] = "Információ a Biztonsági Csoportok-ról";
+$mod_strings['LBL_SECURITYGROUPS_INFO'] = "Általános Információ";
+$mod_strings['LBL_SECURITYGROUPS_DASHLETPUSH_TITLE'] = "Csoportos Üzenet Műszer telepítése";
+$mod_strings['LBL_SECURITYGROUPS_DASHLETPUSH'] = "Minden felhasználó kezdőlapjára telepíti a Csoportos Üzenet Műszert. Ez a művelet hoszabb ideig is eltarhat, a felhasználók számától függően.";
+$mod_strings['LBL_SECURITYGROUPS_HOOKUP_TITLE'] = "Modul Csatlakoztatása";
+$mod_strings['LBL_SECURITYGROUPS_HOOKUP'] = "Biztonsági Csoport képesség hozzáadása egyedi modulhoz";
+$mod_strings['LBL_SECURITYGROUPS_SUGAROUTFITTERS_TITLE'] = "SugarOutfitters";
+$mod_strings['LBL_SECURITYGROUPS_SUGAROUTFITTERS'] = "Fogd meg a legfrissebb SecuritySuite és talál más SugarCRM modul, témákat és integráció, értékelés, docs, támogatás, és a közösség ellenőrizni változatban.";
+?>
