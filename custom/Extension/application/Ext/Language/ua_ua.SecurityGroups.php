ee761155a4d17c8ef82c11e4ab5fb738bde3446b
Horus68 <pauloizidoro@gmail.com>
2015-05-25 17:49:19 +0100

    ua_ua orphan
    ua_ua orphan

--- custom/Extension/application/Ext/Language/ua_ua.SecurityGroups.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-
-$app_list_strings["moduleList"]["SecurityGroups"] = 'Управління Групами Користувачів';
-$app_strings['LBL_LOGIN_AS'] = "Ввійти як ";
-$app_strings['LBL_LOGOUT_AS'] = "Вийти як ";
-$app_strings['LBL_SECURITYGROUP'] = 'Управління Групами';
-?>
