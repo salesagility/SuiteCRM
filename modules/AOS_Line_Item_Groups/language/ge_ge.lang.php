5d927f10c92a965305fbc1e0679462f8570e2b49
Horus68 <pauloizidoro@gmail.com>
2015-05-25 11:43:09 +0100

    Revert "Remove ge_ge language files"
    This reverts commit e9978ae025f8290b254f0130ec9ebdfbf0757f02.

---
+++ modules/AOS_Line_Item_Groups/language/ge_ge.lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Products, Quotations & Invoices modules.
+ * Extensions to SugarCRM
+ * @package Advanced OpenSales for SugarCRM
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author Salesagility Ltd <support@salesagility.com>
+ */
+
+$mod_strings = array (
+  'LBL_ID' => 'ID',
+  'LBL_MODULE_NAME' => 'Positionsgruppen',
+  'LBL_DATE_ENTERED' => 'Erstellt am',
+  'LBL_DATE_MODIFIED' => 'Geändert',
+  'LBL_DESCRIPTION' => 'Beschreibung',
+  'LBL_DELETED' => 'Gelöscht',
+  'LBL_NAME' => 'Bezeichnung der Gruppe',
+  'LBL_TOTAL_AMT' => 'Brutto-Summe',
+  'LBL_DISCOUNT_AMOUNT' => 'Rabatt',
+  'LBL_SUBTOTAL_AMOUNT' => 'Netto-Summe',
+  'LBL_TAX_AMOUNT' => 'MwSt Betrag',
+  'LBL_SUBTOTAL_TAX_AMOUNT' => 'Summe + Steuer',
+  'LBL_GROUP_TOTAL' => 'Gruppensumme',
+  'LBL_LIST_NUM' => 'Nummer',
+);
+?>
---
+++ modules/AOS_PDF_Templates/language/ge_ge.lang.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Products, Quotations & Invoices modules.
+ * Extensions to SugarCRM
+ * @package Advanced OpenSales for SugarCRM
+ * @subpackage Products
+ * @copyright SalesAgility Ltd http://www.salesagility.com
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * along with this program; if not, see http://www.gnu.org/licenses
+ * or write to the Free Software Foundation,Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301  USA
+ *
+ * @author Salesagility Ltd <support@salesagility.com>
+ *
+ * German localisation by Claudia Haring, claudia.haring@kamux.de
+ *
+ */
+
+$mod_strings = array (
+  'LBL_ASSIGNED_TO_ID' => 'Zugewiesen an:',
+  'LBL_ASSIGNED_TO_NAME' => 'Zugewiesen an:',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Erstellt am:',
+  'LBL_DATE_MODIFIED' => 'Geändert',
+  'LBL_MODIFIED' => 'Geändert von:',
+  'LBL_MODIFIED_ID' => 'Geändert von ID',
+  'LBL_MODIFIED_NAME' => 'Geändert von Name',
+  'LBL_CREATED' => 'Erstellt von',
+  'LBL_CREATED_ID' => 'Erstellt von ID',
+  'LBL_DESCRIPTION' => 'Beschreibung',
+  'LBL_HEADER' => 'Kopfbereich',
+  'LBL_FOOTER' => 'Fußbereich',
+  'LBL_DELETED' => 'Gelöscht',
+  'LBL_NAME' => 'Name',
+  'LBL_CREATED_USER' => 'Erstellt von Benutzer',
+  'LBL_MODIFIED_USER' => 'Geändert von',
+  'LBL_LIST_FORM_TITLE' => 'PDF Vorlagen Liste',
+  'LBL_MODULE_NAME' => 'PDF Vorlagen',
+  'LBL_MODULE_TITLE' => 'PDF Vorlagen',
+  'LBL_HOMEPAGE_TITLE' => 'Meine PDF Vorlagen',
+  'LNK_NEW_RECORD' => 'Neue PDF Vorlage',
+  'LNK_LIST' => 'PDF Vorlagen',
+  'LBL_SEARCH_FORM_TITLE' => 'PDF Vorlagen Suche',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'änderungsprotokoll',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktivitäten',
+  'LBL_AOS_PDF_TEMPLATES_SUBPANEL_TITLE' => 'PDF Vorlagen',
+  'LBL_NEW_FORM_TITLE' => 'Neue PDF Vorlage',
+  'LBL_TYPE' => 'Typ',
+  'LBL_DEFAULT_TEMPLATE' => 'Standard Vorlage',
+  'LBL_INSERT_FIELDS' => 'Felder einfügen',
+  
+  'LBL_SAMPLE' => 'Beispiele laden',
+  'LBL_PAGE' => 'Seite',
+  'LBL_PREPARED_FOR' => 'Vorbereitet Für',
+  'LBL_PREPARED_BY' => 'Vorbereitet durch',
+  
+  'LBL_QUOTE_GROUP_SAMPLE' => 'Angebot Positionsgruppen Beispiel',
+  'LBL_INVOICE_GROUP_SAMPLE' => 'Rechnung Positionsgruppen Beispiel',
+  'LBL_MARGIN_LEFT' => 'Linker Rand',
+  'LBL_MARGIN_RIGHT' => 'Rechter Rand',
+  'LBL_MARGIN_TOP' => 'Oberer Rand',
+  'LBL_MARGIN_BOTTOM' => 'Unterer Rand',
+  'LBL_MARGIN_HEADER' => 'Rand Kopfzeile',
+  'LBL_MARGIN_FOOTER' => 'Rand Fußzeile',
+  'LBL_EDITVIEW_PANEL1' => 'Ränder',
+  'LBL_DETAILVIEW_PANEL1' => 'Ränder',
+);
+?>
