18e150986874a896ae24f3dfa13d097d3e2ea515
Matthew <matthew.lorimer@salesagility.com>
2015-06-02 21:47:12 +0100

    Fixed #269 AOR Scheduled Reports run despite being set to inactive

--- custom/Extension/modules/Schedulers/Ext/ScheduledTasks/AORScheduler.php
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/AORScheduler.php
@@ -29,7 +29,7 @@ function aorRunScheduledReports(){
     $date = new DateTime();//Ensure we check all schedules at the same instant
     foreach(BeanFactory::getBean('AOR_Scheduled_Reports')->get_full_list() as $scheduledReport){
 
-        if($scheduledReport->shouldRun($date)){
+        if($scheduledReport->status == 'active' && $scheduledReport->shouldRun($date)){
             if(empty($scheduledReport->aor_report_id)){
                 continue;
             }
