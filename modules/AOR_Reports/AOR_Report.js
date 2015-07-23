e16638d9e44036edd713c1a3bfc0f5c0893dd171
Richard Tang <richard.tang@salesagility.com>
2015-07-23 11:58:24 +0100

    missed a file

--- modules/AOR_Reports/AOR_Report.js
+++ modules/AOR_Reports/AOR_Report.js
@@ -75,9 +75,37 @@ function changeReportPage(record, offset, group){
         query += "&parameter_operator[]="+operator;
         var fieldType = $('#aor_conditions_value_type\\['+ln+'\\]').val();
         query += "&parameter_type[]="+fieldType;
-        var fieldInput = $('#aor_conditions_value\\['+ln+'\\]').val();
-        query += "&parameter_value[]="+fieldInput;
+        if (fieldType =='Date'){
+            for (i=0;i<4;i++){
+                var logged = $('#aor_conditions_value\\[0\\]\\['+ i +'\\]').val();
+                if(i == 0 ){
+                    var typeOfThing = logged ;
+                }
+                if(i == 1 ){
+                    var operatorForDate = logged;
+                }
+                if(i == 2 ){
+                    var numberForDateVariation = logged;
+                }
+                if(i == 3 ){
+                    var timeOfVariation = logged;
+                }
+            }
+            var fieldInput = typeOfThing +'|'+ operatorForDate + '|'+numberForDateVariation +'|' + timeOfVariation;
+            console.log (fieldInput);
+            _form.append('<input type="hidden" name="parameter_value[]" value="'+fieldInput+'">');
+        }
+        else{
+            var fieldInput = $('#aor_conditions_value\\['+ln+'\\]').val();
+            _form.append('<input type="hidden" name="parameter_value[]" value="'+fieldInput+'">');
+            console.log('in else ')
+            console.log(fieldInput);
+        }
     });
+    _form.submit();
+});
 
-    YAHOO.util.Connect.asyncRequest ("GET", "index.php"+query,callback);
+
+
+YAHOO.util.Connect.asyncRequest ("GET", "index.php"+query,callback);
 }
