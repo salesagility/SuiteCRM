60cd7649441dd329eb4af28d5b610d07aa5bb522
William Rennie <will.rennie@salesagility.com>
2015-08-04 16:44:57 +0100

    Changing suitecrm_version.php to 7.3 Beta 2.

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.3 Beta';
-$suitecrm_timestamp    = '2015-06-05 17:00pm';
+$suitecrm_version      = '7.3 Beta 2';
+$suitecrm_timestamp    = '2015-08-04 17:00pm';
