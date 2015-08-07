817f43482e32408a95a94e11e3ee3ce4e3bddcc9
William Rennie <will.rennie@salesagility.com>
2015-08-07 11:46:08 +0100

    Update version number to the latest release version.

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.2.2';
-$suitecrm_timestamp    = '2015-05-20 18:00pm';
+$suitecrm_version      = '7.2.3';
+$suitecrm_timestamp    = '2015-08-06 17:00pm';
