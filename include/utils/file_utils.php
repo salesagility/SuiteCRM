8833fae04cf9cc7fd2de9718446d56dba82bce77
JanSiero <jan@siero.de>
2015-08-08 18:22:00 +0200

    Fix op-cache issue when writing and re-reading vardefs

--- include/utils/file_utils.php
+++ include/utils/file_utils.php
@@ -130,7 +130,13 @@ function write_array_to_file( $the_name, $the_array, $the_file, $mode="w", $head
                     var_export_helper( $the_array ) .
                     ";";
 
-    return sugar_file_put_contents($the_file, $the_string, LOCK_EX) !== false;
+    $result = sugar_file_put_contents($the_file, $the_string, LOCK_EX) !== false;
+
+    if (function_exists('opcache_invalidate')) {
+        opcache_invalidate($the_file, true);
+    }
+
+    return $result;
 }
 
 function write_encoded_file( $soap_result, $write_to_dir, $write_to_file="" )
