0a28b8cc497989d76028aa8eb58dc7281b160e59
Richard Tang <richard.tang@salesagility.com>
2015-08-10 09:14:44 +0100

    Icon for location was missing

--- modules/Users/views/view.edit.php
+++ modules/Users/views/view.edit.php
@@ -210,7 +210,7 @@ EOD
 
         $action_button_footer[] = <<<EOD
                     <input type="button" id="SAVE_FOOTER" title="{$APP['LBL_SAVE_BUTTON_TITLE']}" accessKey="{$APP['LBL_SAVE_BUTTON_KEY']}"
-                          class="button primary" onclick="var _form = $('#EditView')[0]; if (!passwordCheck('{$hasMinumLength}',{$minpwdlength},{$hasMaximumLength},{$maxpwdlength},{$oneupper},{$nonalphaNumeric},{$onenumber},this.form)) return false; if (!Admin_check()) return false; _form.action.value='Save'; {$CHOOSER_SCRIPT} {$REASSIGN_JS} if(verify_data(EditView)) _form.submit();"
+                          class="button primary" onclick="var _form = $('#EditView')[0]; if (!passwordCheck({$hasMinumLength},{$minpwdlength},{$hasMaximumLength},{$maxpwdlength},{$oneupper},{$nonalphaNumeric},{$onenumber},this.form)) return false; if (!Admin_check()) return false; _form.action.value='Save'; {$CHOOSER_SCRIPT} {$REASSIGN_JS} if(verify_data(EditView)) _form.submit();"
                           name="button" value="{$APP['LBL_SAVE_BUTTON_LABEL']}">
 EOD
         ;
