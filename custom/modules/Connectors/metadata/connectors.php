3b8a63926f87c61301567d1c2ec46e09354956c0
Jim Mackin <jim.mackin@salesagility.com>
2015-08-24 15:18:44 +0100

    Remove the deprecated linkedin code.

--- custom/modules/Connectors/metadata/connectors.php
+++ custom/modules/Connectors/metadata/connectors.php
@@ -1,24 +1,6 @@
 <?php
 // created: 2014-02-03 12:55:32
 $connectors = array (
-  'ext_rest_linkedin' => 
-  array (
-    'id' => 'ext_rest_linkedin',
-    'name' => 'LinkedIn&#169;',
-    'enabled' => true,
-    'directory' => 'modules/Connectors/connectors/sources/ext/rest/linkedin',
-    'eapm' => 
-    array (
-      'enabled' => true,
-    ),
-    'modules' => 
-    array (
-      0 => 'Accounts',
-      1 => 'Contacts',
-      2 => 'Leads',
-      3 => 'Prospects',
-    ),
-  ),
   'ext_rest_insideview' => 
   array (
     'id' => 'ext_rest_insideview',
