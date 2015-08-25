e6d46505b0c08856f191e630b199e86795c6e3e1
William Rennie <will.rennie@salesagility.com>
2015-08-25 16:10:02 +0100

    Update SuiteCRM version to SuiteCRM 7.3.1.
    Fix for issue #399

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.3';
-$suitecrm_timestamp    = '2015-08-14 17:00pm';
+$suitecrm_version      = '7.3.1';
+$suitecrm_timestamp    = '2015-08-25 17:00pm';
