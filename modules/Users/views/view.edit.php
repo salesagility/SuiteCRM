a681de5cf967e16da84edebb5fd0668cb2487fdb
Richard Tang <richard.tang@salesagility.com>
2015-08-27 13:12:22 +0100

    forgot to change the the upper save button

--- modules/Users/views/view.edit.php
+++ modules/Users/views/view.edit.php
@@ -194,8 +194,8 @@ class UsersViewEdit extends ViewEdit {
         $onenumber =  !empty($PWDSETTINGS['onenumber']) ? $PWDSETTINGS['onenumber'] : 'false';
 
         $action_button_header[] = <<<EOD
-                    <input type="button" id="SAVE_HEADER" title="{$APP['LBL_SAVE_BUTTON_TITLE']}" accessKey="{$APP['LBL_SAVE_BUTTON_KEY']}"
-                          class="button primary" onclick="var _form = $('#EditView')[0]; if (!set_password(_form,newrules('{$minpwdlength}','{$maxpwdlength}','{$REGEX}'))) return false; if (!Admin_check()) return false; _form.action.value='Save'; {$CHOOSER_SCRIPT} {$REASSIGN_JS} if(verify_data(EditView)) _form.submit();"
+                     <input type="button" id="SAVE_HEADER" title="{$APP['LBL_SAVE_BUTTON_TITLE']}" accessKey="{$APP['LBL_SAVE_BUTTON_KEY']}"
+                          class="button primary" onclick="var _form = $('#EditView')[0]; if (!passwordCheck({$hasMinumLength},{$minpwdlength},{$hasMaximumLength},{$maxpwdlength},{$oneupper},{$nonalphaNumeric},{$onenumber},this.form)) return false; if (!Admin_check()) return false; _form.action.value='Save'; {$CHOOSER_SCRIPT} {$REASSIGN_JS} if(verify_data(EditView)) _form.submit();"
                           name="button" value="{$APP['LBL_SAVE_BUTTON_LABEL']}">
 EOD
         ;
