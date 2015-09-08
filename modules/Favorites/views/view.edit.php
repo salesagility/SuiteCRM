7a8cbd81dde38ee7949dff4ebfdd064352a906d5
Lewis Jessett <lewis.jessett@salesagility.com>
2015-09-08 15:25:52 +0100

    Added all of the favorites code.

---
+++ modules/Favorites/views/view.edit.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+require_once('include/MVC/View/views/view.edit.php');
+
+class FavoritesViewEdit extends ViewEdit {
+	function FavoritesViewEdit(){
+ 		parent::ViewEdit();
+ 	}
+	
+	function display(){
+		parent::display();
+	}
+
+}
+?>
