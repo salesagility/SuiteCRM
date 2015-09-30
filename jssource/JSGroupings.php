80b64b1bf562493a4a33d1f6122daf29ab4d7345
danieljsamson <danieljsamson@gmail.com>
2015-09-30 23:39:14 +0100

    removing alerts from $sugar_grp1

--- jssource/JSGroupings.php
+++ jssource/JSGroupings.php
@@ -51,7 +51,6 @@
        $js_groupings = array(
            $sugar_grp1 = array(
                 //scripts loaded on first page
-                'include/javascript/alerts.js'          => 'include/javascript/sugar_grp1.js',
                 'include/javascript/sugar_3.js'         => 'include/javascript/sugar_grp1.js',
                 'include/javascript/ajaxUI.js'          => 'include/javascript/sugar_grp1.js',
                 'include/javascript/cookie.js'          => 'include/javascript/sugar_grp1.js',
