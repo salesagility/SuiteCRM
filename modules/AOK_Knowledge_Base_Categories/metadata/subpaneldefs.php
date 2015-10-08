e9c26cea84770489d96b175582d5f67e651d368d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2015-10-08 09:03:57 +0100

    Long variable names to be removed and replaced with short variable names.

--- modules/AOK_Knowledge_Base_Categories/metadata/subpaneldefs.php
+++ modules/AOK_Knowledge_Base_Categories/metadata/subpaneldefs.php
@@ -38,14 +38,14 @@
  ********************************************************************************/
 
 // created: 2015-03-04 12:01:39
-$layout_defs["AOK_Knowledge_Base_Categories"]["subpanel_setup"]['aok_knowledgebase_aok_knowledge_base_categories'] = array (
+$layout_defs["AOK_Knowledge_Base_Categories"]["subpanel_setup"]['aok_knowledgebase_categories'] = array (
     'order' => 100,
     'module' => 'AOK_KnowledgeBase',
     'subpanel_name' => 'default',
     'sort_order' => 'asc',
     'sort_by' => 'id',
-    'title_key' => 'LBL_AOK_KNOWLEDGEBASE_AOK_KNOWLEDGE_BASE_CATEGORIES_FROM_AOK_KNOWLEDGEBASE_TITLE',
-    'get_subpanel_data' => 'aok_knowledgebase_aok_knowledge_base_categories',
+    'title_key' => 'LBL_AOK_KB_TITLE',
+    'get_subpanel_data' => 'aok_knowledgebase_categories',
     'top_buttons' =>
         array (
             0 =>
--- modules/AOK_Knowledge_Base_Categories/vardefs.php
+++ modules/AOK_Knowledge_Base_Categories/vardefs.php
@@ -50,14 +50,14 @@ $dictionary['AOK_Knowledge_Base_Categories'] = array(
 	);
 
 // created: 2015-03-04 12:01:39
-$dictionary["AOK_Knowledge_Base_Categories"]["fields"]["aok_knowledgebase_aok_knowledge_base_categories"] = array (
-	'name' => 'aok_knowledgebase_aok_knowledge_base_categories',
+$dictionary["AOK_Knowledge_Base_Categories"]["fields"]["aok_knowledgebase_categories"] = array (
+	'name' => 'aok_knowledgebase_categories',
 	'type' => 'link',
-	'relationship' => 'aok_knowledgebase_aok_knowledge_base_categories',
+	'relationship' => 'aok_knowledgebase_categories',
 	'source' => 'non-db',
 	'module' => 'AOK_KnowledgeBase',
 	'bean_name' => false,
-	'vname' => 'LBL_AOK_KNOWLEDGEBASE_AOK_KNOWLEDGE_BASE_CATEGORIES_FROM_AOK_KNOWLEDGEBASE_TITLE',
+	'vname' => 'LBL_AOK_KB_TITLE',
 );
 
 if (!class_exists('VardefManager')){
