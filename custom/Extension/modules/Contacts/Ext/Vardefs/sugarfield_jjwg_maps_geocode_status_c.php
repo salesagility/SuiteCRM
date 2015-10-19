b99f7034cb7bceb63ca228a2a159e537e7517ba7
Matthew <matthew.lorimer@salesagility.com>
2015-10-19 16:11:48 +0100

    revert changes

--- custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_jjwg_maps_geocode_status_c.php
+++
@@ -1,5 +0,0 @@
-<?php
- // created: 2015-08-23 20:20:17
-$dictionary['Contact']['fields']['jjwg_maps_geocode_status_c']['inline_edit']=1;
-
- ?>
--- custom/Extension/modules/Leads/Ext/Vardefs/sugarfield_jjwg_maps_address_c.php
+++
@@ -1,5 +0,0 @@
-<?php
- // created: 2015-08-23 20:20:17
-$dictionary['Lead']['fields']['jjwg_maps_address_c']['inline_edit']=1;
-
- ?>
