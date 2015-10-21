a817abe578c66d90ad5c83ba2f3125858a527677
Matthew Lorimer <matthew.lorimers@salesagility.com>
2015-10-21 23:29:23 +0200

    7.3.2

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.3.1';
-$suitecrm_timestamp    = '2015-08-25 17:00pm';
+$suitecrm_version      = '7.3.2';
+$suitecrm_timestamp    = '2015-10-22 12:00pm';
