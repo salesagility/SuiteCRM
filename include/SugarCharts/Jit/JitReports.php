e06e4484329b44de499d7ecb3d0833a1627839d0
Gyula Madarasz <gyula.madarasz@salesagility.com>
2015-11-04 12:33:37 +0000

    strict errors fixed

--- include/SugarCharts/Jit/JitReports.php
+++ include/SugarCharts/Jit/JitReports.php
@@ -249,8 +249,8 @@ class JitReports extends Jit {
 	 *			string $xmlFile	location of the XML file
 	 *			string $style	optional additional styles for the div
      * @return	string returns the html code through smarty
-     */					
-	function display($name, $xmlFile, $width='320', $height='480', $reportChartDivStyle, $resize=false){
+     */
+	function display($name, $xmlFile, $width='320', $height='480', $reportChartDivStyle = null, $resize=false){
 		if(empty($name)) {
 			$name = "unsavedReport";	
 		}
