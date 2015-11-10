680d4a06aeed2405fa54d35419aadf7e49e634f9
Matt Lorimer <matthew.lorimers@salesagility.com>
2015-11-10 17:03:21 +0100

    updated cases vardefs

--- modules/Cases/vardefs.php
+++ modules/Cases/vardefs.php
@@ -264,7 +264,18 @@ $dictionary['Case'] = array('table' => 'cases','audited'=>true, 'unified_search'
    ,'cases_created_by' =>
    array('lhs_module'=> 'Users', 'lhs_table'=> 'users', 'lhs_key' => 'id',
    'rhs_module'=> 'Cases', 'rhs_table'=> 'cases', 'rhs_key' => 'created_by',
-   'relationship_type'=>'one-to-many')
+   'relationship_type'=>'one-to-many'),
+
+	'cases_created_contact' =>
+	array (
+			'lhs_module' => 'Contacts',
+			'lhs_table' => 'contacts',
+			'lhs_key' => 'id',
+			'rhs_module' => 'Cases',
+			'rhs_table' => 'cases',
+			'rhs_key' => 'contact_created_by_id',
+			'relationship_type' => 'one-to-many',
+	),
 )
 //This enables optimistic locking for Saves From EditView
 	,'optimistic_locking'=>true,
