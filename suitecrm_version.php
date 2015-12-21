e833c7af1ec21c9e6ef0607bf7d1352808c4552c
William Rennie <will.rennie@salesagility.com>
2015-12-21 19:12:59 +0000

    Correct suitecrm_version.php typo

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -2,4 +2,4 @@
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $suitecrm_version      = '7.5beta2';
-$suitecrm_timestamp    = '2015-12-21 17:00am';
+$suitecrm_timestamp    = '2015-12-21 17:00';
