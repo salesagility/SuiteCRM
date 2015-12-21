59f6866aea2274f80ebf2c2af97320163758740c
Gyula Madarasz <gyula.madarasz@salesagility.com>
2015-12-21 17:34:24 +0000

    version 7.5beta2

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.5beta';
-$suitecrm_timestamp    = '2015-12-14 17:00am';
+$suitecrm_version      = '7.5beta2';
+$suitecrm_timestamp    = '2015-12-21 17:00am';
