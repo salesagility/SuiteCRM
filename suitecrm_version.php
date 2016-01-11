ef36527556026f1114a78ec939d90b129c7843dd
Horus68 <pauloizidoro@gmail.com>
2016-01-11 21:39:28 +0000

    2016 year update
    2016 year update

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -2,4 +2,4 @@
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $suitecrm_version      = '7.5RC';
-$suitecrm_timestamp    = '2015-01-11 17:00';
+$suitecrm_timestamp    = '2016-01-11 17:00';
