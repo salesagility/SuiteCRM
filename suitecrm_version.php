a75ecc22a3ae6a098cf7b2c597eede61d698fe6a
gyula <gyula.madarasz@salesagility.com>
2016-01-26 11:24:30 +0000

    update suitecrm version timestamp

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -2,4 +2,4 @@
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 $suitecrm_version      = '7.5.1';
-$suitecrm_timestamp    = '2015-01-25 17:00';
+$suitecrm_timestamp    = '2015-01-26 17:00';
