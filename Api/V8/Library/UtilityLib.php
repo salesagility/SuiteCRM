f25ccf4702210c0e2f9f266e1eaae7b74befaac6
paulgodley <paul.godley@salesagility.com>
2016-02-11 12:24:51 +0000

    Updated as per Matt's comments to remove the Sugar Version.

--- Api/V8/Library/UtilityLib.php
+++ Api/V8/Library/UtilityLib.php
@@ -8,20 +8,7 @@ class UtilityLib
     function getServerInfo()
     {
         require_once('suitecrm_version.php');
-        require_once('sugar_version.php');
-        require_once('modules/Administration/Administration.php');
-        global $sugar_flavor;
-        $admin = new \Administration();
-        $admin->retrieveSettings('info');
-        $ret = array();
-
-        if (isset($admin->settings['info_sugar_version'])) {
-            $sugar_version = $admin->settings['info_sugar_version'];
-        } else {
-            $sugar_version = '1.0';
-        }
-        $ret['server_info'] = array('suite_version' => $suitecrm_version, 'sugar_version' => $sugar_version);
-        return $ret;
+        return array('suite_version' => $suitecrm_version);
     }
 
     function login()
