5d0aab8386cf6aff8b510e007c2d81ae73da50cd
Jim Mackin <jim.mackin@salesagility.com>
2016-02-16 11:57:01 +0000

    Add a basic test which checks the config. Intended to test the TravisCI setup

---
+++ tests/tests/configTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class configTest extends PHPUnit_Framework_TestCase
+{
+
+	public function test_config() {
+	    global $sugar_config;
+		$this->assertNotEmpty($sugar_config,"SuiteCRM config is empty");
+	
+	}
+
+
+}
+
