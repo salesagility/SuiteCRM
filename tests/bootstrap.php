2035712e56fc645ed19b6ab4c6903d8ec48c35e5
Jim Mackin <jim.mackin@salesagility.com>
2016-02-24 13:53:43 +0000

    Add a flag to mark when we are testing and use this to avoid the query limits since this will be a limit for the overall tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,3 +33,4 @@ require_once 'include/modules.php';
 require_once 'include/entryPoint.php';
 //Oddly entry point loads app_strings but not app_list_strings, manually do this here.
 $GLOBALS['app_list_strings'] = return_app_list_strings_language($GLOBALS['current_language']);
+define('WITHIN_TESTS', true);
