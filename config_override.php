bf586a2ac1da108ca6c9615d49ac9327646563f2
jakabadambalazs <jakabadambalazs@gmail.com>
2016-03-30 10:00:41 +0200

    removed config_override.php from tracking

--- config_override.php
+++ config_override.php
@@ -1,3 +1,37 @@
 <?php
 /***CONFIGURATOR***/
+$sugar_config['addAjaxBannedModules'][0] = 'jjwg_Address_Cache';
+$sugar_config['addAjaxBannedModules'][1] = 'jjwg_Areas';
+$sugar_config['addAjaxBannedModules'][2] = 'SecurityGroups';
+$sugar_config['addAjaxBannedModules'][3] = 'jjwg_Markers';
+$sugar_config['addAjaxBannedModules'][4] = 'jjwg_Maps';
+$sugar_config['addAjaxBannedModules'][5] = 'AOS_Product_Categories';
+$sugar_config['addAjaxBannedModules'][6] = 'AOW_WorkFlow';
+$sugar_config['addAjaxBannedModules'][7] = 'AOS_Quotes';
+$sugar_config['addAjaxBannedModules'][8] = 'AOS_Products';
+$sugar_config['addAjaxBannedModules'][9] = 'AOS_Invoices';
+$sugar_config['addAjaxBannedModules'][10] = 'AOS_PDF_Templates';
+$sugar_config['addAjaxBannedModules'][11] = 'AOS_Contracts';
+$sugar_config['addAjaxBannedModules'][12] = 'AOR_Scheduled_Reports';
+$sugar_config['addAjaxBannedModules'][13] = 'AOR_Reports';
+$sugar_config['addAjaxBannedModules'][14] = 'AOK_KnowledgeBase';
+$sugar_config['addAjaxBannedModules'][15] = 'FP_Event_Locations';
+$sugar_config['addAjaxBannedModules'][16] = 'FP_events';
+$sugar_config['addAjaxBannedModules'][17] = 'Reminders';
+$sugar_config['addAjaxBannedModules'][18] = 'Reminders_Invitees';
+$sugar_config['addAjaxBannedModules'][19] = 'AOK_Knowledge_Base_Categories';
+$sugar_config['addAjaxBannedModules'][20] = 'AM_ProjectTemplates';
+$sugar_config['addAjaxBannedModules'][21] = 'Bugs';
+$sugar_config['addAjaxBannedModules'][22] = 'Cases';
+$sugar_config['addAjaxBannedModules'][23] = 'Opportunities';
+$sugar_config['addAjaxBannedModules'][24] = 'ProspectLists';
+$sugar_config['addAjaxBannedModules'][25] = 'Prospects';
+$sugar_config['addAjaxBannedModules'][26] = 'Accounts';
+$sugar_config['addAjaxBannedModules'][27] = 'Tasks';
+$sugar_config['addAjaxBannedModules'][28] = 'Contacts';
+$sugar_config['addAjaxBannedModules'][29] = 'Leads';
+$sugar_config['addAjaxBannedModules'][30] = 'Notes';
+$sugar_config['addAjaxBannedModules'][31] = 'Meetings';
+$sugar_config['addAjaxBannedModules'][32] = 'Calls';
+$sugar_config['addAjaxBannedModules'][33] = 'Home';
 /***CONFIGURATOR***/
