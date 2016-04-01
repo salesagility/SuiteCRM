4fc3e200bb5641c98c24bcd0a93163003261d97d
Matt Lorimer <matthew.lorimer@salesagility.com>
2016-04-01 16:12:23 +0100

    remove general logic_hooks from custom , move to install

--- install/suite_install/AdvancedOpenDiscovery.php
+++ install/suite_install/AdvancedOpenDiscovery.php
@@ -10,4 +10,46 @@ function install_aod() {
     ksort($sugar_config);
     write_array_to_file('sugar_config', $sugar_config, 'config.php');
 
+    installAODHooks();
+
+}
+
+function installAODHooks()
+{
+    require_once('ModuleInstall/ModuleInstaller.php');
+
+    $hooks = array(
+        array(
+            'module' => '',
+            'hook' => 'after_save',
+            'order' => 1,
+            'description' => 'popup_onload',
+            'file' => 'modules/AOD_Index/AOD_LogicHooks.php',
+            'class' => 'AOD_LogicHooks',
+            'function' => 'saveModuleChanges',
+        ),
+        array(
+            'module' => '',
+            'hook' => 'after_delete',
+            'order' => 1,
+            'description' => 'popup_onload',
+            'file' => 'modules/AOD_Index/AOD_LogicHooks.php',
+            'class' => 'AOD_LogicHooks',
+            'function' => 'saveModuleDelete',
+        ),
+        array(
+            'module' => '',
+            'hook' => 'after_restore',
+            'order' => 1,
+            'description' => 'popup_onload',
+            'file' => 'modules/AOD_Index/AOD_LogicHooks.php',
+            'class' => 'AOD_LogicHooks',
+            'function' => 'saveModuleRestore',
+        ),
+    );
+
+    foreach ($hooks as $hook) {
+        check_logic_hook_file($hook['module'], $hook['hook'], array($hook['order'], $hook['description'], $hook['file'], $hook['class'], $hook['function']));
+    }
+
 }
--- install/suite_install/suite_install.php
+++ install/suite_install/suite_install.php
@@ -40,6 +40,9 @@ install_ss();
 require_once('install/suite_install/GoogleMaps.php');
 install_gmaps();
 
+require_once('install/suite_install/Social.php');
+install_social();
+
 require_once('modules/Administration/QuickRepairAndRebuild.php');
 $actions = array('clearAll');
 $randc = new RepairAndClear();
