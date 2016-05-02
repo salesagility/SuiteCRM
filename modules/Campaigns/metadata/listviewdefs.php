3d799ccae6100d29620b2e0cd926ce6f62a090d4
Daniel Samson <daniel.samson@salesagility.com>
2016-05-02 11:46:21 +0100

    re-arranged list view columns

--- modules/Campaigns/metadata/listviewdefs.php
+++ modules/Campaigns/metadata/listviewdefs.php
@@ -59,7 +59,11 @@ $listViewDefs['Campaigns'] = array(
     'END_DATE' => array(
         'width' => '10', 
         'label' => 'LBL_LIST_END_DATE',
-        'default' => true),        
+        'default' => true),
+	'DATE_ENTERED' => array (
+		'width' => '10',
+		'label' => 'LBL_DATE_ENTERED',
+		'default' => true),
         
 	'ASSIGNED_USER_NAME' => array(
 		'width' => '8', 
@@ -68,26 +72,23 @@ $listViewDefs['Campaigns'] = array(
         'id' => 'ASSIGNED_USER_ID',
         'default' => true),
     'TRACK_CAMPAIGN' => array(
-        'width' => '1', 
+        'width' => '0.01',
         'label' => '&nbsp;',
         'link' => true,
-        'customCode' => ' <a title="{$TRACK_CAMPAIGN_TITLE}" href="index.php?action=TrackDetailView&module=Campaigns&record={$ID}"><!--not_in_theme!--><img border="0" src="{$TRACK_CAMPAIGN_IMAGE}" alt="{$TRACK_VIEW_ALT_TEXT}"></a> ',
+        'customCode' => ' <a title="{$TRACK_CAMPAIGN_TITLE}" href="index.php?action=TrackDetailView&module=Campaigns&record={$ID}"><!--not_in_theme!--><img border="0" src="{$TRACK_CAMPAIGN_IMAGE}" alt="{$TRACK_VIEW_ALT_TEXT}" width="16px" height="16px"></a> ',
         'default' => true,
         'studio' => false,
         'nowrap' => true,
         'sortable' => false),      
     'LAUNCH_WIZARD' => array(
-        'width' => '1', 
+        'width' => '0.01',
         'label' => '&nbsp;',
         'link' => true,
-        'customCode' => ' <a title="{$LAUNCH_WIZARD_TITLE}" href="index.php?action=WizardHome&module=Campaigns&record={$ID}"><!--not_in_theme!--><img border="0" src="{$LAUNCH_WIZARD_IMAGE}"  alt="{$LAUNCH_WIZ_ALT_TEXT}"></a>  ',
+        'customCode' => ' <a title="{$LAUNCH_WIZARD_TITLE}" href="index.php?action=WizardHome&module=Campaigns&record={$ID}"><!--not_in_theme!--><img border="0" src="{$LAUNCH_WIZARD_IMAGE}"  alt="{$LAUNCH_WIZ_ALT_TEXT}" width="16px" height="16px"></a>  ',
         'default' => true,
         'studio' => false,
         'nowrap' => true,
         'sortable' => false),
-	'DATE_ENTERED' => array (
-	    'width' => '10',
-	    'label' => 'LBL_DATE_ENTERED',
-	    'default' => true),              
+
 );
 ?>
