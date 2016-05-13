2e36cfd048077966812c50ad861701b56023230f
gymad <gyula.madarasz@salesagility.com>
2016-05-13 16:41:25 +0100

    move images to public folder for emails

--- modules/EmailTemplates/EmailTemplateFormBase.php
+++ modules/EmailTemplates/EmailTemplateFormBase.php
@@ -194,9 +194,12 @@ EOQ;
 						$GLOBALS['log']->debug("EMAIL Template could not copy attachment to $newFileLocation");
 					} else {
 						if($useUploadFolder) {
-							$secureLink = ($useSiteURL ? $sugar_config['site_url'] . '/' : '') . "upload/{$id}";
+							$secureLink = ($useSiteURL ? $sugar_config['site_url'] . '/' : '') . "public/{$id}";
 							// create a copy with correct extension by mime type
-							if(copy($file_location, "upload/{$id}.{$mime_type}")) {
+							if(!file_exists('public')) {
+								sugar_mkdir('public', 777);
+							}
+							if(copy($file_location, "public/{$id}.{$mime_type}")) {
 								$secureLink .= ".{$mime_type}";
 							}
 						}
