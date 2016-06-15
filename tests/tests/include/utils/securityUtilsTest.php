5f09c3571eff4101c94ed99928b898fe9fa78c01
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-06-15 16:05:04 +0100

    Updating Travis Tests for Beta1

--- tests/tests/include/utils/securityUtilsTest.php
+++ tests/tests/include/utils/securityUtilsTest.php
@@ -44,6 +44,7 @@ class security_utilsTest extends PHPUnit_Framework_TestCase
                 'AOW_WorkFlow' => 'AOW_WorkFlow',
                 'AOK_KnowledgeBase' => 'AOK_KnowledgeBase',
                 'AOK_Knowledge_Base_Categories' => 'AOK_Knowledge_Base_Categories',
+                'Spots' => 'Spots',
         );
 
         $actual = query_module_access_list($user);
