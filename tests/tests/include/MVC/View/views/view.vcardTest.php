6be40e010566de3deeeacb5473514a3b8eddea43
jakabadambalazs <jakabadambalazs@gmail.com>
2016-07-05 16:41:31 +0200

    removed (again) vcard display print output

--- tests/tests/include/MVC/View/views/view.vcardTest.php
+++ tests/tests/include/MVC/View/views/view.vcardTest.php
@@ -1,4 +1,41 @@
 <?php
+/**
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2016 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
+ * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
 /**
  * Class ViewVcardTest
@@ -8,15 +45,18 @@ class ViewVcardTest extends \SuiteCRM\Tests\SuiteCRMUnitTest
     public function testdisplay()
     {
         //execute the method with required child objects preset and check for the Object type and type attribute
-    
+
         $view = new ViewVcard();
         $view->module = 'Contacts';
         $view->bean = new Contact();
-    
+
         //execute the method and test if it works and does not throws an exception other than headers output exception.
         try
         {
+            //buffer and clean display printed output
+            ob_start();
             $view->display();
+            ob_end_clean();
         }
         catch(Exception $e)
         {
