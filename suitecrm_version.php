a6545196033a6ef3f9ddbf6a2babbbb351072798
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-07-18 16:51:45 +0100

    Updating Master Branch for Release 7.6.6

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.6.5';
-$suitecrm_timestamp    = '2016-07-04 17:00';
+$suitecrm_version      = '7.6.6';
+$suitecrm_timestamp    = '2016-07-18 17:00';
