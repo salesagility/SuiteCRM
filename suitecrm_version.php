8706c4960882fecf43eb4883d80eb82b8713917c
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-07-18 18:10:01 +0100

    Updating Develop Branch for Release 7.7RC

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.7RC';
+$suitecrm_version      = '7.7rc';
 $suitecrm_timestamp    = '2016-07-18 17:00';
