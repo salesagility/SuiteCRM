4ff381d8c3a725273dc8211685699deaa1cf08a0
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-07-26 18:17:19 +0100

    Preparing 7.7 RC 2 release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.7rc';
-$suitecrm_timestamp    = '2016-07-18 17:00';
+$suitecrm_version      = '7.7rc2';
+$suitecrm_timestamp    = '2016-07-26 17:00';
