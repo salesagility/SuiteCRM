6337bf41533451c8a82a7073107a2967fc8e27c1
Sathish Sengodan <sathish.sengodan@salesagility.com>
2016-07-26 15:03:24 +0100

    Fix #1757 - Notifications Emails being Sent on edit, sometimes multiple times after upgrade to 7.6.5

--- include/InlineEditing/InlineEditing.php
+++ include/InlineEditing/InlineEditing.php
@@ -304,6 +304,7 @@ function getEditFieldHTML($module, $fieldname, $aow_field, $view = 'EditView', $
 function saveField($field, $id, $module, $value)
 {
 
+    global $current_user;
     $bean = BeanFactory::getBean($module, $id);
 
     if (is_object($bean) && $bean->id != "") {
@@ -321,11 +322,13 @@ function saveField($field, $id, $module, $value)
             $bean->$field = $value;
         }
 
-        if (($bean->fetched_row['assigned_user_id'] != $value) && (!$bean->isOwner($bean->modified_user_id))) {
-            $check_notify = FALSE;
-        }
-        else {
-            $check_notify = TRUE;
+        $check_notify = FALSE;
+
+        if (isset( $bean->fetched_row['assigned_user_id'])) {
+            $old_assigned_user_id = $bean->fetched_row['assigned_user_id'];
+            if (!empty($value) && ($old_assigned_user_id != $value) && ($value != $current_user->id)) {
+                $check_notify = TRUE;
+            }
         }
 
         $bean->save($check_notify);
