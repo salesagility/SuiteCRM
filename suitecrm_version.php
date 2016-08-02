341d2c82acdbd2ef8293f25d7eee0f4c2ca22c11
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-08-02 13:10:29 +0100

    Updating Develop in prep for 7.7 Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.7rc2';
-$suitecrm_timestamp    = '2016-07-26 17:00';
+$suitecrm_version      = '7.7';
+$suitecrm_timestamp    = '2016-08-02 17:00';
