26b5aec84df9d26f4aa9f5809b9f9163767ab883
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2016-08-10 17:20:30 +0100

    Prep for 7.7.1 release

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##SuiteCRM 7.7
+##SuiteCRM 7.7.1
 
 [![Build Status](https://travis-ci.org/salesagility/SuiteCRM.svg?branch=hotfix)](https://travis-ci.org/salesagility/SuiteCRM)
 
--- suitecrm_version.php
+++ suitecrm_version.php
@@ -1,5 +1,5 @@
 <?php
  if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-$suitecrm_version      = '7.7';
-$suitecrm_timestamp    = '2016-08-02 17:00';
+$suitecrm_version      = '7.7.1';
+$suitecrm_timestamp    = '2016-08-10 17:00';
