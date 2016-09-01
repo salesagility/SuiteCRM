f2cfc9662d033a73ad8359e92605c5f07007f228
Jerzy Brzezinski <jerzy.brzezinski@salesagility.com>
2016-09-01 08:22:51 +0100

    Fixed #2094 iCal now export VTODO and no VEVENT

--- config_override.php
+++ config_override.php
@@ -1,3 +1,8 @@
 <?php
 /***CONFIGURATOR***/
+$sugar_config['enable_action_menu'] = true;
+$sugar_config['enable_line_editing_list'] = false;
+$sugar_config['hide_subpanels'] = true;
+$sugar_config['default_module_favicon'] = true;
+$sugar_config['developerMode'] = true;
 /***CONFIGURATOR***/
