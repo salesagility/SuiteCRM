1fd7c6efbfe15b6bc753063dd35fcd1ed8f78b20
Jerzy Brzezinski <jerzy.brzezinski@salesagility.com>
2016-09-14 09:16:11 +0100

    Fixed #146 now work for all themes

--- jssource/src_files/include/javascript/sugar_3.js
+++ jssource/src_files/include/javascript/sugar_3.js
@@ -3252,7 +3252,7 @@ SUGAR.util = function () {
 					var width = $dialog.dialog( "option", "width" );
 
 					if((pos.left + ofWidth) - 40 < width) {
-						$dialog.dialog("option","position",{my: 'left top',at: 'right top',of: $(el)})	;
+						$dialog.dialog("option","position",{my: 'right top',at: 'right top',of: $(el)})	;
 					}
 					$dialog.dialog('open');
 					$(".ui-dialog").appendTo("#content");
@@ -3274,7 +3274,7 @@ SUGAR.util = function () {
 				width: 300,
 				position: {
 				    my: 'right top',
-				    at: 'left top',
+				    at: 'right top',
 				    of: $(el)
 			  }
 			});
@@ -3335,7 +3335,7 @@ SUGAR.util = function () {
 					var ofWidth = $(el).width();
 
 					if((pos.left + ofWidth) - 40 < width) {
-						$dialog.dialog("option","position",{my: 'left top',at: 'right top',of: $(el)})	;
+						$dialog.dialog("option","position",{my: 'right top',at: 'right top',of: $(el)})	;
 					}
 
 					$dialog.dialog('open');
