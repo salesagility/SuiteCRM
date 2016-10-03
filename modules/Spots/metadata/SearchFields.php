dc33d210e826e53c51fb068666c216d31da218d8
Jerzy Brzezinski <jerzy.brzezinski@salesagility.com>
2016-10-03 14:24:24 +0100

    Fixed #2053 - Can't add Spots to Favorites

--- modules/Spots/metadata/SearchFields.php
+++ modules/Spots/metadata/SearchFields.php
@@ -46,7 +46,15 @@ $searchFields[$module_name] =
         'name' => array('query_type' => 'default'),
         'current_user_only' => array('query_type' => 'default', 'db_field' => array('assigned_user_id'), 'my_items' => true, 'vname' => 'LBL_CURRENT_USER_FILTER', 'type' => 'bool'),
         'assigned_user_id' => array('query_type' => 'default'),
-
+        'favorites_only' => array(
+            'query_type'=>'format',
+            'operator' => 'subquery',
+            'checked_only' => true,
+            'subquery' => "SELECT favorites.parent_id FROM favorites
+			                    WHERE favorites.deleted = 0
+			                        and favorites.parent_type = 'Spots'
+			                        and favorites.assigned_user_id = '{1}'",
+            'db_field'=>array('id')),
         //Range Search Support 
        'range_date_entered' => array('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
        'start_range_date_entered' => array('query_type' => 'default', 'enable_range_search' => true, 'is_date_field' => true),
--- modules/Spots/metadata/searchdefs.php
+++ modules/Spots/metadata/searchdefs.php
@@ -44,20 +44,41 @@
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
 $module_name = 'Spots';
-  $searchdefs[$module_name] = array(
-                    'templateMeta' => array(
-                            'maxColumns' => '3',
-                            'maxColumnsBasic' => '4',
-                            'widths' => array('label' => '10', 'field' => '30'),
-                           ),
-                    'layout' => array(
-                        'basic_search' => array(
-                            'name',
-                            array('name' => 'current_user_only', 'label' => 'LBL_CURRENT_USER_FILTER', 'type' => 'bool'),
-                            ),
-                        'advanced_search' => array(
-                            'name',
-                            array('name' => 'assigned_user_id', 'label' => 'LBL_ASSIGNED_TO', 'type' => 'enum', 'function' => array('name' => 'get_user_array', 'params' => array(false))),
-                        ),
-                    ),
-               );
+$searchdefs[$module_name] = array(
+    'templateMeta' => array(
+        'maxColumns' => '3',
+        'maxColumnsBasic' => '4',
+        'widths' => array(
+            'label' => '10',
+            'field' => '30'
+        ),
+    ),
+    'layout' => array(
+        'basic_search' => array(
+            'name' => array(
+                'name' => 'current_user_only',
+                'label' => 'LBL_CURRENT_USER_FILTER',
+                'type' => 'bool'
+            ),
+
+            'favorites_only' =>  array (
+                'name' => 'favorites_only',
+                'label' => 'LBL_FAVORITES_FILTER',
+                'type' => 'bool',
+            ),
+        ),
+        'advanced_search' => array(
+            'name' => array(
+                'name' => 'assigned_user_id',
+                'label' => 'LBL_ASSIGNED_TO',
+                'type' => 'enum',
+                'function' => array(
+                    'name' => 'get_user_array',
+                    'params' => array(
+                        false
+                    )
+                )
+            ),
+        ),
+    ),
+);
