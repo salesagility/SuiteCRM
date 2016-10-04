effa0c5b05bb1ec530688cab2e46cb45bd9673ff
Daniel Samson <daniel.samson@salesagility.com>
2016-10-04 12:01:23 +0100

    Making task modal dialog use additional details

--- jssource/src_files/modules/Calendar/Cal.js
+++ jssource/src_files/modules/Calendar/Cal.js
@@ -297,25 +297,17 @@ CAL.load_form = function (module_name, record, edit_all_recurrences, cal_event) 
 	if(module_name == "Tasks") {
 		var url = 'index.php?to_pdf=1&module=Home&action=AdditionalDetailsRetrieve&bean=' + cal_event.module + '&id=' + cal_event.record;
 		var body = '<span class="title"><strong>' + SUGAR.language.get('Calendar', 'LBL_DATE') + '</strong></span>: ' + (cal_event.start.format(global_datetime_format) ) + '<br><span class="title"><strong>' + SUGAR.language.get('Calendar', 'LBL_SUBJECT') + ': </strong></span>' + ( (cal_event.title) ? cal_event.title : '');
-		if(typeof cal_event.date_due !== "undefined") {
-			body = body
-				+ '<span class="title">' + SUGAR.language.get('Calendar', 'LBL_INFO_DUE_DT') + '</span>: ' + cal_event.date_due;
-		} else {
-			body = body
-				+ '<span class="title">' + SUGAR.language.get('Calendar', 'LBL_DATE') + '</span>: ' + (cal_event.start.format(global_datetime_format) )
-		}
 
-		body = body
-			+ '<br><span class="title">' + SUGAR.language.get('Calendar', 'LBL_STATUS') + ': </span>' + ( (cal_event.status) ? cal_event.status : '')
-			+ '<br><span class="title">' + SUGAR.language.get('Calendar', 'LBL_PRIORITY') + ': </span>' + ( (cal_event.priority) ? cal_event.priority : '');
-
-		if(cal_event.parent_name != "") {
-			body = body
-				+ '<br><span class="title">' + SUGAR.language.get('Calendar', 'LBL_INFO_RELATED_TO') + ': </span>' + '<a href="index.php?action=DetailView&module='+cal_event.parent_type+'&record='+cal_event.parent_id+'">' + cal_event.parent_name + '</a>';
-		} else {
-			body = body
-				+ '<br><span class="title">' + SUGAR.language.get('Calendar', 'LBL_INFO_RELATED_TO') + ': </span>' + '';
-		}
+		$.ajax(url)
+			.done(function (data) {
+				eval(data); // produces var result = {body:{}, caption:"", width:300}
+				$('.modal-cal-tasks-edit .modal-body .container-fluid').html(result.body);
+			})
+			.fail(function () {
+				$('.modal-cal-tasks-edit .modal-body .container-fluid').html(SUGAR.language.translate('app_strings', 'LBL_EMAIL_ERROR_GENERAL_TITLE'));
+			}).always(function () {
+					//console.log("complete");
+			});
 
 		$('.modal-cal-tasks-edit .modal-body .container-fluid').html(body);
 		$('.modal-cal-tasks-edit').modal('show');
@@ -999,8 +991,6 @@ $(document).ready(function () {
 										$(divBody).html(result.body);
 									})
 									.fail(function () {
-										//console.log("error");
-
 										$(divBody).html(SUGAR.language.translate('app_strings', 'LBL_EMAIL_ERROR_GENERAL_TITLE'));
 									})
 									.always(function () {
