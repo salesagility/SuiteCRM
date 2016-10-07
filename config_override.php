d900c9ce2b06feb873afbe31bf99e1335ba70101
Jerzy Brzezinski <jerzy.brzezinski@salesagility.com>
2016-10-07 08:14:36 +0100

    fixing - #164 clear config_override

--- config_override.php
+++ config_override.php
@@ -1,8 +1,3 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['default_module_favicon'] = false;
-$sugar_config['dashlet_auto_refresh_min'] = '30';
-$sugar_config['enable_action_menu'] = false;
-$sugar_config['stack_trace_errors'] = false;
-$sugar_config['developerMode'] = true;
 /***CONFIGURATOR***/
