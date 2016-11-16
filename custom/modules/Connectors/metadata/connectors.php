4324f64c8ec15acf9da90bdc46b5beef016aa1e9
Daniel Samson <daniel.samson@salesagility.com>
2016-11-16 10:27:09 +0000

    tabs and navbar

---
+++ custom/modules/Connectors/metadata/connectors.php
@@ -0,0 +1,47 @@
+<?php
+// created: 2016-11-15 16:06:40
+$connectors = array (
+  'ext_rest_facebook' => 
+  array (
+    'id' => 'ext_rest_facebook',
+    'name' => 'Facebook',
+    'enabled' => true,
+    'directory' => 'modules/Connectors/connectors/sources/ext/rest/facebook',
+    'eapm' => false,
+    'modules' => 
+    array (
+      0 => 'Accounts',
+      1 => 'Contacts',
+      2 => 'Leads',
+    ),
+  ),
+  'ext_rest_insideview' => 
+  array (
+    'id' => 'ext_rest_insideview',
+    'name' => 'InsideView&#169;',
+    'enabled' => true,
+    'directory' => 'modules/Connectors/connectors/sources/ext/rest/insideview',
+    'eapm' => false,
+    'modules' => 
+    array (
+      0 => 'Accounts',
+      1 => 'Contacts',
+      2 => 'Leads',
+      3 => 'Opportunities',
+    ),
+  ),
+  'ext_rest_twitter' => 
+  array (
+    'id' => 'ext_rest_twitter',
+    'name' => 'Twitter',
+    'enabled' => true,
+    'directory' => 'modules/Connectors/connectors/sources/ext/rest/twitter',
+    'eapm' => false,
+    'modules' => 
+    array (
+      0 => 'Accounts',
+      1 => 'Contacts',
+      2 => 'Leads',
+    ),
+  ),
+);
