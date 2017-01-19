837c8ed7f5b450ed7ec779d36421f54bc06569d5
Daniel Samson <daniel.samson@salesagility.com>
2017-01-19 12:21:38 +0000

    Making aow conditions more responsive.

--- modules/AOW_Conditions/conditionLines.js
+++ modules/AOW_Conditions/conditionLines.js
@@ -270,36 +270,36 @@ function insertConditionLine(){
     } else{
         a.innerHTML = condln +1;
     }
-    a.style.width = '5%';
+    a.style.width = ''; // 5%
 
     var b = x.insertCell(1);
-    b.style.width = '15%';
+    b.style.width = ''; // 15%
     var viewStyle = 'display:none';
     if(action_sugar_grp1 == 'EditView'){viewStyle = '';}
-    b.innerHTML = "<select style='width:178px;"+viewStyle+"' name='aow_conditions_module_path["+ condln +"][0]' id='aow_conditions_module_path" + condln + "' value='' title='' tabindex='116' onchange='showConditionCurrentModuleFields(" + condln + ");'>" + flow_rel_modules + "</select>";
+    b.innerHTML = "<select style='"+viewStyle+"' name='aow_conditions_module_path["+ condln +"][0]' id='aow_conditions_module_path" + condln + "' value='' title='' tabindex='116' onchange='showConditionCurrentModuleFields(" + condln + ");'>" + flow_rel_modules + "</select>";
     if(action_sugar_grp1 == 'EditView'){viewStyle = 'display:none';}else{viewStyle = '';}
-    b.innerHTML += "<span style='width:178px;"+viewStyle+"' id='aow_conditions_module_path_label" + condln + "' ></span>";
+    b.innerHTML += "<span style='"+viewStyle+"' id='aow_conditions_module_path_label" + condln + "' ></span>";
 
     var c = x.insertCell(2);
-    c.style.width = '15%';
+    c.style.width = ''; // 15%
     var viewStyle = 'display:none';
     if(action_sugar_grp1 == 'EditView'){viewStyle = '';}
-    c.innerHTML = "<select style='width:178px;"+viewStyle+"' name='aow_conditions_field["+ condln +"]' id='aow_conditions_field" + condln + "' value='' title='' tabindex='116' onchange='showModuleField(" + condln + ");'>" + flow_fields + "</select>";
+    c.innerHTML = "<select style='"+viewStyle+"' name='aow_conditions_field["+ condln +"]' id='aow_conditions_field" + condln + "' value='' title='' tabindex='116' onchange='showModuleField(" + condln + ");'>" + flow_fields + "</select>";
     if(action_sugar_grp1 == 'EditView'){viewStyle = 'display:none';}else{viewStyle = '';}
-    c.innerHTML += "<span style='width:178px;"+viewStyle+"' id='aow_conditions_field_label" + condln + "' ></span>";
+    c.innerHTML += "<span style='"+viewStyle+"' id='aow_conditions_field_label" + condln + "' ></span>";
 
 
     var d = x.insertCell(3);
     d.id='aow_conditions_operatorInput'+condln;
-    d.style.width = '15%';
+    d.style.width = ''; // 15%
 
     var e = x.insertCell(4);
     e.id='aow_conditions_fieldTypeInput'+condln;
-    e.style.width = '15%';
+    e.style.width = ''; // 15%
 
     var f = x.insertCell(5);
     f.id='aow_conditions_fieldInput'+condln;
-    f.style.width = '30%';
+    f.style.width = ''; // 30%
 
     condln++;
     condln_count++;
