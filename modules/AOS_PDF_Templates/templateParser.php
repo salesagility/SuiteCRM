a70d66a44ec1b8ad1c9cf07a981e466dcaca1dc2
sgaved <simeongaved@hotmail.com>
2017-02-13 11:03:05 +0000

    Fix for key instead of value

--- modules/AOS_PDF_Templates/templateParser.php
+++ modules/AOS_PDF_Templates/templateParser.php
@@ -63,7 +63,12 @@ class templateParser
                 } else if (($field_def['type'] == 'radioenum' || $field_def['type'] == 'enum') && isset($field_def['options'])) {
                     $repl_arr[$key . "_" . $fieldName] = translate($field_def['options'], $focus->module_dir, $focus->$fieldName);
                 } else if ($field_def['type'] == 'multienum' && isset($field_def['options'])) {
-                    $repl_arr[$key . "_" . $fieldName] = translate($field_def['options'], $focus->module_dir, implode(', ', unencodeMultienum($focus->$fieldName)));
+                    $mVals = unencodeMultienum($focus->$field_def['name']);
+                    $translatedVals = array();
+                    foreach($mVals as $mVal){
+                        $translatedVals[] = translate($field_def['options'], $focus->module_dir, $mVal);
+                    }
+                    $repl_arr[$key . "_" . $fieldName] = implode(", ", $translatedVals);
                 } //Fix for Windows Server as it needed to be converted to a string.
                 else if ($field_def['type'] == 'int') {
                     $repl_arr[$key . "_" . $fieldName] = strval($focus->$fieldName);
