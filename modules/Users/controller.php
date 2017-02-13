5ec9d78e38d3e3fa059ae964aafee8f8ec83d961
David Thomson <david.thomson@salesagility.com>
2017-02-13 13:46:22 +0000

    Fix #2695 - Reformat Code In Controller

--- modules/Users/controller.php
+++ modules/Users/controller.php
@@ -1,9 +1,10 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
  * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
  * Copyright (C) 2011 - 2014 Salesagility Ltd.
  *
@@ -39,7 +40,6 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  ********************************************************************************/
 
 /*********************************************************************************
-
  * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
  * All Rights Reserved.
  * Contributor(s): ______________________________________..
@@ -48,30 +48,31 @@ require_once("include/OutboundEmail/OutboundEmail.php");
 
 class UsersController extends SugarController
 {
-	/**
-	 * bug 48170
-	 * Action resetPreferences gets fired when user clicks on  'Reset User Preferences' button
-	 * This action is set in UserViewHelper.php
-	 */
-	protected function action_resetPreferences(){
-	    if($_REQUEST['record'] == $GLOBALS['current_user']->id || ($GLOBALS['current_user']->isAdminForModule('Users'))){
-	        $u = new User();
-	        $u->retrieve($_REQUEST['record']);
-	        $u->resetPreferences();
-	        if($u->id == $GLOBALS['current_user']->id) {
-	            SugarApplication::redirect('index.php');
-	        }
-	        else{
-	            SugarApplication::redirect("index.php?module=Users&record=".$_REQUEST['record']."&action=DetailView"); //bug 48170]
-	
-	        }
-	    }
-	}  
-	protected function action_delete()
-	{
-	    if($_REQUEST['record'] != $GLOBALS['current_user']->id && ($GLOBALS['current_user']->isAdminForModule('Users')
-            ))
-        {
+    /**
+     * bug 48170
+     * Action resetPreferences gets fired when user clicks on  'Reset User Preferences' button
+     * This action is set in UserViewHelper.php
+     */
+    protected function action_resetPreferences()
+    {
+        if ($_REQUEST['record'] == $GLOBALS['current_user']->id || ($GLOBALS['current_user']->isAdminForModule('Users'))) {
+            $u = new User();
+            $u->retrieve($_REQUEST['record']);
+            $u->resetPreferences();
+            if ($u->id == $GLOBALS['current_user']->id) {
+                SugarApplication::redirect('index.php');
+            } else {
+                SugarApplication::redirect("index.php?module=Users&record=" . $_REQUEST['record'] . "&action=DetailView"); //bug 48170]
+
+            }
+        }
+    }
+
+    protected function action_delete()
+    {
+        if ($_REQUEST['record'] != $GLOBALS['current_user']->id && ($GLOBALS['current_user']->isAdminForModule('Users')
+            )
+        ) {
             $u = new User();
             $u->retrieve($_REQUEST['record']);
             $u->status = 'Inactive';
@@ -83,38 +84,39 @@ class UsersController extends SugarController
             $eapm = loadBean('EAPM');
             $eapm->delete_user_accounts($_REQUEST['record']);
             $GLOBALS['log']->info("Removing user's External Accounts");
-            
-            SugarApplication::redirect("index.php?module=Users&action=index");
-        }
-        else 
-            sugar_die("Unauthorized access to administration.");
-	}
-	protected function action_wizard() 
-	{
-		$this->view = 'wizard';
-	}
 
-	protected function action_saveuserwizard() 
-	{
-	    global $current_user, $sugar_config;
-	    
-	    // set all of these default parameters since the Users save action will undo the defaults otherwise
-	    $_POST['record'] = $current_user->id;
-	    $_POST['is_admin'] = ( $current_user->is_admin ? 'on' : '' );
-	    $_POST['use_real_names'] = true;
-		$_POST['reminder_checked'] = '1';
-		$_POST['email_reminder_checked'] = '1';
-	    $_POST['reminder_time'] = 1800;
-		$_POST['email_reminder_time'] = 3600;
+            SugarApplication::redirect("index.php?module=Users&action=index");
+        } else {
+            sugar_die("Unauthorized access to administration.");
+        }
+    }
+
+    protected function action_wizard()
+    {
+        $this->view = 'wizard';
+    }
+
+    protected function action_saveuserwizard()
+    {
+        global $current_user, $sugar_config;
+
+        // set all of these default parameters since the Users save action will undo the defaults otherwise
+        $_POST['record'] = $current_user->id;
+        $_POST['is_admin'] = ($current_user->is_admin ? 'on' : '');
+        $_POST['use_real_names'] = true;
+        $_POST['reminder_checked'] = '1';
+        $_POST['email_reminder_checked'] = '1';
+        $_POST['reminder_time'] = 1800;
+        $_POST['email_reminder_time'] = 3600;
         $_POST['mailmerge_on'] = 'on';
         $_POST['receive_notifications'] = $current_user->receive_notifications;
-        $_POST['user_theme'] = (string) SugarThemeRegistry::getDefault();
-	    
-	    // save and redirect to new view
-	    $_REQUEST['return_module'] = 'Home';
-	    $_REQUEST['return_action'] = 'index';
-		require('modules/Users/Save.php');
-	}
+        $_POST['user_theme'] = (string)SugarThemeRegistry::getDefault();
+
+        // save and redirect to new view
+        $_REQUEST['return_module'] = 'Home';
+        $_REQUEST['return_action'] = 'index';
+        require('modules/Users/Save.php');
+    }
 
     protected function action_saveftsmodules()
     {
