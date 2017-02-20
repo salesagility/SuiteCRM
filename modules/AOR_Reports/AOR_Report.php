fa861ecb6340ed1639744c9576f265899c135b51
Daniel Samson <daniel.samson@salesagility.com>
2017-02-20 16:41:14 +0000

    Replaced "`" with db->quoteIdentifier method
    This ensures that it will work with all database managers.

--- modules/AOR_Reports/AOR_Report.php
+++ modules/AOR_Reports/AOR_Report.php
@@ -1378,7 +1378,7 @@ class AOR_Report extends Basic {
                                 $value = $condition_module->table_name . '_cstm.' . $condition->value;
                                 $query = $this->build_report_query_join($condition_module->table_name . '_cstm', $table_alias . '_cstm', $table_alias, $condition_module, 'custom', $query);
                             } else {
-                                $value = ($table_alias ? "`$table_alias`" : $condition_module->table_name) . '.' . $condition->value;
+                                $value = ($table_alias ? $this->db->quoteIdentifier($table_alias) : $condition_module->table_name) . '.' . $condition->value;
                             }
                             break;
 
