f27388ac6be68507d9ad76fd8a0e108e2e481df6
Dillon Brown <dillon.brown@salesagility.com>
2017-03-30 16:49:43 +0100

    Unused language strings removed

--- modules/SavedSearch/language/en_us.lang.php
+++ modules/SavedSearch/language/en_us.lang.php
@@ -1,5 +1,6 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry)
+    die('Not A Valid Entry Point');
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
@@ -47,26 +48,18 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  ********************************************************************************/
 
 $mod_strings = array (
-	'LBL_MODULE_TITLE' => 'My Saved Filters',
-	'LBL_SEARCH_FORM_TITLE' => 'My Saved Filters : Filter',
-	'LBL_LIST_FORM_TITLE' => 'My Saved Filters List',
-	'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
-	'LBL_UPDATE_BUTTON_TITLE' => 'Update this Saved Filter',
-	'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
-	'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
-	'LBL_LIST_NAME' => 'Name',
-	'LBL_LIST_MODULE' => 'Module',
+    'LBL_MODULE_TITLE' => 'My Saved Filters',
+    'LBL_SEARCH_FORM_TITLE' => 'My Saved Filters : Filter',
+    'LBL_LIST_FORM_TITLE' => 'My Saved Filters List',
+    'LBL_DELETE_CONFIRM' => 'Are you sure you want to delete the selected Saved Filter?',
+    'LBL_DELETE_BUTTON_TITLE' => 'Delete this Saved Filter',
+    'LBL_SAVE_BUTTON_TITLE' => 'Save the current filter',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_LIST_MODULE' => 'Module',
     'LBL_ORDER_BY_COLUMNS' => 'Order by column:',
     'LBL_DIRECTION' => 'Direction:',
     'LBL_SAVE_SEARCH_AS' => 'Save filter as:',
-    'LBL_SAVE_SEARCH_AS_HELP' => 'This saves your view settings and any filters on the Advanced Filter tab.',
-    'LBL_PREVIOUS_SAVED_SEARCH' => 'Previous Saved Filters:',
-    'LBL_PREVIOUS_SAVED_SEARCH_HELP' => 'Edit or Delete an existing Saved Filter.',
-    'LBL_ASCENDING' => 'Ascending', 
+    'LBL_ASCENDING' => 'Ascending',
     'LBL_DESCENDING' => 'Descending',
     'LBL_MODIFY_CURRENT_FILTER'=> 'Modify current filter',
-
 );
-
-
-?>
