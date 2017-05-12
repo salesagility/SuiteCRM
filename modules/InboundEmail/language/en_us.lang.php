d712415a8502597a0cd62969fd79086cc27465d9
Dillon Brown <dillon.brown@salesagility.com>
2017-05-12 15:33:35 +0100

    Fixed #3524 - Duplicated language strings (InboundEmail)

--- modules/InboundEmail/language/en_us.lang.php
+++ modules/InboundEmail/language/en_us.lang.php
@@ -195,7 +195,6 @@ $mod_strings = array(
     'LBL_ENABLE_AUTO_IMPORT' => 'Import Emails Automatically',
     'LBL_WARNING_CHANGING_AUTO_IMPORT' => 'Warning: You are modifying your automatic import setting which may result in loss of data.',
     'LBL_WARNING_CHANGING_AUTO_IMPORT_WITH_CREATE_CASE' => 'Warning: Auto import must be enabled when automatically creating cases.',
-    'LBL_EDIT_LAYOUT' => 'Edit Layout' /*for 508 compliance fix*/,
     'LBL_LIST_TITLE_MY_DRAFTS' => 'Drafts',
     'LBL_LIST_TITLE_MY_INBOX' => 'Inbox',
     'LBL_LIST_TITLE_MY_SENT' => 'Sent Email',
