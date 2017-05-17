139a1b86c9fd956ee7ad9098407e0d0dedd415e9
Dillon-Brown <dillon.brown@salesagility.com>
2017-05-17 12:24:00 +0100

    Fixed typo

--- modules/Calendar/language/en_us.lang.php
+++ modules/Calendar/language/en_us.lang.php
@@ -181,12 +181,12 @@ $mod_list_strings = array(
     'dom_cal_weekdays'=>
         array(
             '0' => "Sun",
-            '2' => "Mon",
-            '3' => "Tue",
-            '4' => "Wed",
-            '5' => "Thu",
-            '6' => "Fri",
-            '7' => "Sat",
+            '1' => "Mon",
+            '2' => "Tue",
+            '3' => "Wed",
+            '4' => "Thu",
+            '5' => "Fri",
+            '6' => "Sat",
         ),
     'dom_cal_weekdays_long'=>
         array(
