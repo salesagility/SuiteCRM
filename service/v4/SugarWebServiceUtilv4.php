ba48620ab785d4faca7c0683c655546c8f6a99d9
Jobst Schmalenbach <jobst@barrett.com.au>
2017-05-22 15:47:33 +1000

    named the pass parm to get_data_list correctly but also added the singleSelect to the list query.

--- service/v4/SugarWebServiceUtilv4.php
+++ service/v4/SugarWebServiceUtilv4.php
@@ -98,7 +98,7 @@ class SugarWebServiceUtilv4 extends SugarWebServiceUtilv3_1
 		  $params['favorites'] = true;
 		}
 
-		$query = $seed->create_new_list_query($order_by, $where,array(),$params, $show_deleted);
+		$query = $seed->create_new_list_query($order_by, $where,array(),$params, $show_deleted,'',false,null,$singleSelect);
 		return $seed->process_list_query($query, $row_offset, $limit, $max, $where);
 	}
 
