736caf683849ce3bbcbbfd02b4ee66e780542f5b
Dillon Brown <dillon.brown@salesagility.com>
2017-05-29 10:40:11 +0100

    Code reformat

--- modules/AOS_PDF_Templates/formLetter.php
+++ modules/AOS_PDF_Templates/formLetter.php
@@ -42,25 +42,29 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-class formLetter{
+class formLetter
+{
 
-	static function LVSmarty(){
-		global $app_strings, $sugar_config;
-		if (preg_match('/^6\./', $sugar_config['sugar_version'])) {
-			$script = '<a href="#" class="menuItem" onmouseover="hiliteItem(this,\'yes\');" onmouseout="unhiliteItem(this);" onclick="showPopup()">'.$app_strings['LBL_PRINT_AS_PDF'].'</a>';
-		}
-		else{
-			$script = ' <input class="button" type="button" value="'.$app_strings['LBL_PRINT_AS_PDF'].'" ' .'onClick="showPopup();">';
-    		}
-
-		return $script;
+    static function LVSmarty()
+    {
+        global $app_strings, $sugar_config;
+        if (preg_match('/^6\./', $sugar_config['sugar_version'])) {
+            $script = '<a href="#" class="menuItem" onmouseover="hiliteItem(this,\'yes\');
+" onmouseout="unhiliteItem(this);" onclick="showPopup()">' . $app_strings['LBL_PRINT_AS_PDF'] . '</a>';
+        } else {
+            $script = ' <input class="button" type="button" value="' .
+                $app_strings['LBL_PRINT_AS_PDF'] . '" ' . 'onClick="showPopup();">';
         }
 
-    static function getModuleTemplates($module){
+        return $script;
+    }
+
+    static function getModuleTemplates($module)
+    {
         global $db;
         $templates = array();
 
-        $sql = "SELECT id,name FROM aos_pdf_templates WHERE type = '".$module."' AND deleted = 0  AND active = 1 ORDER BY name";
+        $sql = "SELECT id,name FROM aos_pdf_templates WHERE type = '" . $module . "' AND deleted = 0  AND active = 1 ORDER BY name";
         $result = $db->query($sql);
         while ($row = $db->fetchByAssoc($result)) {
             $templates[$row['id']] = $row['name'];
@@ -69,28 +73,29 @@ class formLetter{
         return $templates;
     }
 
-	static function LVPopupHtml($module){
-		global $app_strings;
+    static function LVPopupHtml($module)
+    {
+        global $app_strings;
 
         $templates = formLetter::getModuleTemplates($module);
-		
-		if(!empty($templates)){
-		echo '	<div id="popupDiv_ara" style="display:none;position:fixed;top: 39%; left: 41%;opacity:1;z-index:9999;background:#FFFFFF;">
+
+        if (!empty($templates)) {
+            echo '	<div id="popupDiv_ara" style="display:none;position:fixed;top: 39%; left: 41%;opacity:1;z-index:9999;background:#FFFFFF;">
  				<table style="border: #000 solid 2px;padding-left:40px;padding-right:40px;padding-top:10px;padding-bottom:10px;font-size:110%;" >
 					<tr height="20">
 						<td colspan="2">
-						<b>'.$app_strings['LBL_SELECT_TEMPLATE'].':-</b>
+						<b>' . $app_strings['LBL_SELECT_TEMPLATE'] . ':-</b>
 						</td>
 					</tr>';
-			foreach ($templates as $templateid => $template ) {
-				$js = "document.getElementById('popupDivBack_ara').style.display='none';document.getElementById('popupDiv_ara').style.display='none';";
-				echo '<tr height="20">
-					<td width="17" valign="center"><a href="#" onclick="document.getElementById(\'popupDiv_ara\').style.display=\'none\';sListView.send_form(true, \''.$_REQUEST['module'].
-                                '\', \'index.php?templateID='.$templateid.'&entryPoint=formLetter\',\''.$app_strings['LBL_LISTVIEW_NO_SELECTED'].'\');document.getElementById(\'popupDivBack_ara\').style.display=\'none\';"><img src="themes/default/images/txt_image_inline.gif" width="16" height="16" /></a></td>
-					<td scope="row" align="left"><b><a href="#" onclick="document.getElementById(\'popupDiv_ara\').style.display=\'none\';sListView.send_form(true, \''.$_REQUEST['module'].
-                                '\', \'index.php?templateID='.$templateid.'&entryPoint=formLetter\',\''.$app_strings['LBL_LISTVIEW_NO_SELECTED'].'\');document.getElementById(\'popupDivBack_ara\').style.display=\'none\';">'.$template.'</a></b></td></tr>';
-			}
-		echo '<tr style="height:10px;"><tr><tr><td colspan="2"><button style=" display: block;margin-left: auto;margin-right: auto" onclick="document.getElementById(\'popupDivBack_ara\').style.display=\'none\';document.getElementById(\'popupDiv_ara\').style.display=\'none\';return false;">Cancel</button></td></tr>
+            foreach ($templates as $templateid => $template) {
+                $js = "document.getElementById('popupDivBack_ara').style.display='none';document.getElementById('popupDiv_ara').style.display='none';";
+                echo '<tr height="20">
+					<td width="17" valign="center"><a href="#" onclick="document.getElementById(\'popupDiv_ara\').style.display=\'none\';sListView.send_form(true, \'' . $_REQUEST['module'] .
+                    '\', \'index.php?templateID=' . $templateid . '&entryPoint=formLetter\',\'' . $app_strings['LBL_LISTVIEW_NO_SELECTED'] . '\');document.getElementById(\'popupDivBack_ara\').style.display=\'none\';"><img src="themes/default/images/txt_image_inline.gif" width="16" height="16" /></a></td>
+					<td scope="row" align="left"><b><a href="#" onclick="document.getElementById(\'popupDiv_ara\').style.display=\'none\';sListView.send_form(true, \'' . $_REQUEST['module'] .
+                    '\', \'index.php?templateID=' . $templateid . '&entryPoint=formLetter\',\'' . $app_strings['LBL_LISTVIEW_NO_SELECTED'] . '\');document.getElementById(\'popupDivBack_ara\').style.display=\'none\';">' . $template . '</a></b></td></tr>';
+            }
+            echo '<tr style="height:10px;"><tr><tr><td colspan="2"><button style=" display: block;margin-left: auto;margin-right: auto" onclick="document.getElementById(\'popupDivBack_ara\').style.display=\'none\';document.getElementById(\'popupDiv_ara\').style.display=\'none\';return false;">Cancel</button></td></tr>
 			</table>
 				</div>
 				<div id="popupDivBack_ara" onclick="this.style.display=\'none\';document.getElementById(\'popupDiv_ara\').style.display=\'none\';" style="top:0px;left:0px;position:fixed;height:100%;width:100%;background:#000000;opacity:0.5;display:none;vertical-align:middle;text-align:center;z-index:9998;">
@@ -99,7 +104,7 @@ class formLetter{
 					function showPopup(){
 					if(sugarListView.get_checks_count() < 1)
 					{
-						alert(\''.$app_strings['LBL_LISTVIEW_NO_SELECTED'].'\');
+						alert(\'' . $app_strings['LBL_LISTVIEW_NO_SELECTED'] . '\');
 					}
 					else
 					{
@@ -114,39 +119,39 @@ class formLetter{
 					}
 					}
 				</script>';
-			}
-		else{
-			echo '<script>
+        } else {
+            echo '<script>
 				function showPopup(){
-				alert(\''.$app_strings['LBL_NO_TEMPLATE'].'\');		
+				alert(\'' . $app_strings['LBL_NO_TEMPLATE'] . '\');		
 				}
 			</script>';
-		}
-	}
-	
-	static function DVPopupHtml($module){
-		global $app_strings;
+        }
+    }
+
+    static function DVPopupHtml($module)
+    {
+        global $app_strings;
 
         $templates = formLetter::getModuleTemplates($module);
 
-        if(!empty($templates)){
-		echo '	<div id="popupDiv_ara" style="display:none;position:fixed;top: 39%; left: 41%;opacity:1;z-index:9999;background:#FFFFFF;">
+        if (!empty($templates)) {
+            echo '	<div id="popupDiv_ara" style="display:none;position:fixed;top: 39%; left: 41%;opacity:1;z-index:9999;background:#FFFFFF;">
  				<form id="popupForm" action="index.php?entryPoint=formLetter" method="post">
  				<table style="border: #000 solid 2px;padding-left:40px;padding-right:40px;padding-top:10px;padding-bottom:10px;font-size:110%;" >
 					<tr height="20">
 						<td colspan="2">
-						<b>'.$app_strings['LBL_SELECT_TEMPLATE'].':-</b>
+						<b>' . $app_strings['LBL_SELECT_TEMPLATE'] . ':-</b>
 						</td>
 					</tr>';
-            foreach ($templates as $templateid => $template ) {
-				$js = "document.getElementById('popupDivBack_ara').style.display='none';document.getElementById('popupDiv_ara').style.display='none';var form=document.getElementById('popupForm');if(form!=null){form.templateID.value='".$templateid."';form.submit();}else{alert('Error!');}";
-					echo '<tr height="20">
-					<td width="17" valign="center"><a href="#" onclick="'.$js.'"><img src="themes/default/images/txt_image_inline.gif" width="16" height="16" /></a></td>
-					<td scope="row" align="left"><b><a href="#" onclick="'.$js.'">'.$template.'</a></b></td></tr>';
-			}
-		echo '		<input type="hidden" name="templateID" value="" />
-				<input type="hidden" name="module" value="'.$module.'" />
-				<input type="hidden" name="uid" value="'.$_REQUEST['record'].'" />
+            foreach ($templates as $templateid => $template) {
+                $js = "document.getElementById('popupDivBack_ara').style.display='none';document.getElementById('popupDiv_ara').style.display='none';var form=document.getElementById('popupForm');if(form!=null){form.templateID.value='" . $templateid . "';form.submit();}else{alert('Error!');}";
+                echo '<tr height="20">
+					<td width="17" valign="center"><a href="#" onclick="' . $js . '"><img src="themes/default/images/txt_image_inline.gif" width="16" height="16" /></a></td>
+					<td scope="row" align="left"><b><a href="#" onclick="' . $js . '">' . $template . '</a></b></td></tr>';
+            }
+            echo '		<input type="hidden" name="templateID" value="" />
+				<input type="hidden" name="module" value="' . $module . '" />
+				<input type="hidden" name="uid" value="' . $_REQUEST['record'] . '" />
 				</form>
 				<tr style="height:10px;"><tr><tr><td colspan="2"><button style=" display: block;margin-left: auto;margin-right: auto" onclick="document.getElementById(\'popupDivBack_ara\').style.display=\'none\';document.getElementById(\'popupDiv_ara\').style.display=\'none\';return false;">Cancel</button></td></tr>
 				</table>
@@ -166,15 +171,13 @@ class formLetter{
 						}
 					}
 				</script>';
-		}
-		else{
-			echo '<script>
+        } else {
+            echo '<script>
 				function showPopup(){
-				alert(\''.$app_strings['LBL_NO_TEMPLATE'].'\');		
+				alert(\'' . $app_strings['LBL_NO_TEMPLATE'] . '\');		
 				}
 			</script>';
-		}
-	}
+        }
+    }
 }
 
-?>
