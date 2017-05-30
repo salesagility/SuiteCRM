cb718a5f79ed093171373421dd20ccc66a9b45a5
Changez Khan <mynameischangezkhan@gmail.com>
2017-05-30 22:13:40 +0530

    Fixed #3592 - AOR Report- Date parameter doesn't work with any value of type 'Period'

--- modules/AOR_Reports/AOR_Report.php
+++ modules/AOR_Reports/AOR_Report.php
@@ -1560,17 +1560,17 @@ class AOR_Report extends Basic
                                 $params = base64_decode($condition->value);
                             }
                             $date = getPeriodEndDate($params)->format('Y-m-d H:i:s');
-                            $value = '\'' . getPeriodDate($params)->format('Y-m-d H:i:s') . '\'';
+                            $value = "'" . getPeriodDate($params)->format('Y-m-d H:i:s') . "'";
 
                             $query['where'][] = ($tiltLogicOp ? '' : ($condition->logic_op ? $condition->logic_op . ' ' : 'AND '));
                             $tiltLogicOp = false;
 
                             switch ($aor_sql_operator_list[$condition->operator]) {
                                 case "=":
-                                    $query['where'][] = $field . ' BETWEEN ' . $value . ' AND ' . '\'' . $date . '\'';
+                                    $query['where'][] = $field . ' BETWEEN ' . $value . ' AND ' . "'" . $date . "'";
                                     break;
                                 case "!=":
-                                    $query['where'][] = $field . ' NOT BETWEEN ' . $value . ' AND ' . '\'' . $date . '\'';
+                                    $query['where'][] = $field . ' NOT BETWEEN ' . $value . ' AND ' . "'" . $date . "'";
                                     break;
                                 case ">":
                                 case "<":
