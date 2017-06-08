7cb17d36bc9502458f74c0746696735c934cb404
Frank Sagurna <fgunni@01019freenet.de>
2017-06-08 16:30:14 +0200

    Create sugarsearch.php

--- sugarsearch.php
+++ sugarsearch.php
@@ -5,7 +5,7 @@ print <<<EOF
 <?xml version="1.0" encoding="UTF-8" ?> 
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"> 
     <ShortName>SuS</ShortName>  
-    <Description>Sugar Suche</Description> 
+    <Description>Sugar Search</Description> 
     <InputEncoding>UTF-8</InputEncoding> 
     <Image width="16" height="16">data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsNDSQPjkEj+QAAAfNJREFUWMPN2E1u1DAUwPH/c1KJdoOKkJDFHbqo2PU03XKGLrkG4hCskFggJLbTqheoKhZDWxWWVSnxYxFn6sm4ThOXmEhWMpqJ85v3MSNbAKy1AhigAoR5DwUawC2XSxVrrXx+c+hEBFVth4A0ev9aHTgFp0jTXTtEWV0DvD/YCe4JBu1ZIu99v/51dPLt6wfgJ3BrAJOLkUZ79wSQBEZVeb37/B2wD7wAjAGqXAzOgXPRqKQw3QD2PKiqAcnHaFsMEzAe9BLYAaQGsjHSeBDjMR601TVUDU+A8UUdPmQEhrCz6/WJJmJ6KRuDCVB90HSMBJOPxqx7OlAeJvyWYzHRCD0VZqP1E5hBUBam3/YjMFFQLqb/sDGYRMqmY4ZAKcwDoDxMCjSEiacsE6NaRUGPwaRTNhHTnu94++nWF7j2Cr3259+bre8qvjy7B5lVyjIx7eTjMf0Ime6/7H/ArEWoFEb1LgYqhxFlIGUzY6I1VBLT/WRspqwQJt5lBTGOSFGXxMRrqCTGPVBDpTDRCJXF/EmB5sckIlQG41cs2lt1/AtMtfrcAAaftwZQA+jix9lRKcxHuTwGroAbQMVaWwGv/JbInl/4b82wcdVV9BVwCiyAixpwfrNoAZwD237hP8fR+Mhce4OTyJaemXlLz4Vben8BBP/7vnr9w0kAAAAASUVORK5CYII=</Image> 
     <Url type="text/html" template="{$searchuri}"/>
