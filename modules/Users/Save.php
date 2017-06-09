6124b3ecc454d3167973674ecc745bcd80c25db5
Gyula Madarasz <gyula.madarasz@salesagility.com>
2017-06-09 10:15:11 +0100

    more php notices

--- modules/Users/Save.php
+++ modules/Users/Save.php
@@ -462,12 +462,12 @@ if(!$current_user->is_admin && !$GLOBALS['current_user']->isAdminForModule('User
         //$emailSettings['autoImport'] = isset($_REQUEST['autoImport']) ? '1' : '0';
         $emailSettings['alwaysSaveOutbound'] = '1';
         $emailSettings['sendPlainText'] = isset($_REQUEST['sendPlainText']) ? '1' : '0';
-        $emailSettings['showNumInList'] = $_REQUEST['showNumInList'];
-        $emailSettings['defaultOutboundCharset'] = $_REQUEST['default_charset'];
+        $emailSettings['showNumInList'] = isset($_REQUEST['showNumInList']) ? $_REQUEST['showNumInList'] : null;
+        $emailSettings['defaultOutboundCharset'] = isset($_REQUEST['default_charset']) ? $_REQUEST['default_charset'] : null;
         $focus->setPreference('emailSettings', $emailSettings, '', 'Emails');
 
         // signature
-        $focus->setPreference('signature_default', $_REQUEST['signature_id']);
+        $focus->setPreference('signature_default', isset($_REQUEST['signature_id']) ? $_REQUEST['signature_id'] : null);
         $focus->setPreference('signature_prepend', (isset($_REQUEST['signature_prepend'])) ? true : false);
 	}
 
