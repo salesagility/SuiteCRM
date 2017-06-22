3ae28fdef985f49e839de9dbaf8d3bf66183ae2d
pgorod <pgorod@users.noreply.github.com>
2017-06-22 17:16:56 +0100

    Fix #3753 no available themes
    This is the minimal fix, just a better message. It would be nice to handle this better in upgrades to 7.9 where SuiteP is disabled.

--- include/SugarTheme/SugarTheme.php
+++ include/SugarTheme/SugarTheme.php
@@ -1423,6 +1423,9 @@ class SugarThemeRegistry
             $themelist[$themeobject->dirName] = $themeobject->name;
         }
         asort($themelist, SORT_STRING);
+        if (count($themelist)==0) {
+		$GLOBALS['log']->fatal("availableThemes() is returning empty array! Check disabled_themes in config.php and config_override.php");
+        }
         return $themelist;
     }
 
