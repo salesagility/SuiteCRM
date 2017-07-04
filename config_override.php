6fc99f27cdda1d04280d59a4da23d7347593e5e1
Daniel Samson <daniel.samson@salesagility.com>
2017-07-04 12:42:29 +0100

    Removing settings from config_override.php

--- config_override.php
+++ config_override.php
@@ -1,7 +1,3 @@
 <?php
 /***CONFIGURATOR***/
-$sugar_config['developerMode'] = true;
-$sugar_config['default_module_favicon'] = false;
-$sugar_config['dashlet_auto_refresh_min'] = '30';
-$sugar_config['stack_trace_errors'] = false;
 /***CONFIGURATOR***/
