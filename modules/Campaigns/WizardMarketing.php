d938ae44263a8777ad88dc1b4c71a37e3ab0c366
Gyula Madarasz <gyula.madarasz@salesagility.com>
2017-07-05 10:59:24 +0100

    fix some php error

--- modules/Campaigns/WizardMarketing.php
+++ modules/Campaigns/WizardMarketing.php
@@ -271,8 +271,12 @@ if (empty($mrkt_focus->inbound_email_id)) {
 }
 
 $outboundEmailAccountLabels = array();
-foreach($outboundEmailAccounts = BeanFactory::getBean('OutboundEmailAccounts')->get_full_list() as $outboundEmailAccount) {
-    $outboundEmailLabels[$outboundEmailAccount->id] = $outboundEmailAccount->name;
+$outboundEmailLabels = array();
+$outboundEmailAccounts = BeanFactory::getBean('OutboundEmailAccounts')->get_full_list();
+if ($outboundEmailAccounts) {
+    foreach ($outboundEmailAccounts as $outboundEmailAccount) {
+        $outboundEmailLabels[$outboundEmailAccount->id] = $outboundEmailAccount->name;
+    }
 }
 
 $ss->assign('OUTBOUND_MAILBOXES', get_select_options_with_id($outboundEmailLabels, $mrkt_focus->outbound_email_id));
