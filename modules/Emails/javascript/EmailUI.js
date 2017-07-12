178365b885c5ede4e958082e78a2e466fbe0b389
Gyula Madarasz <gyula.madarasz@salesagility.com>
2017-07-12 09:30:35 +0100

    apply review change request

--- modules/Emails/javascript/EmailUI.js
+++ modules/Emails/javascript/EmailUI.js
@@ -2796,6 +2796,7 @@ SE.folders = {
     /**
      * Updates user's group folder subscriptsion (Sugar only)
      * @param ieID The group folder to add to the tree view
+     * @see SE.folders.setFolderSelection()
      */
     retrieveGroupFolderSubscriptions : function() {
 
@@ -2805,8 +2806,7 @@ SE.folders = {
     	for(i=0;i<a_rs.length;i++)
     	{
     		var t_record = a_rs[i];
-          // if it's not working try something like this:
-          // var is_active = $('#' + t_record._sId + ' input[type="checkbox"]').prop('checked');
+    		
     		var is_active = t_record.getData('is_active');
     		var isGroupFolder = t_record.getData('has_groupfolder');
     		var ieID = t_record.getData('id');
