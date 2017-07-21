d30688359a672205ec4382f0a37ad5cd51bcd9f1
Daniel Samson <daniel.samson@salesagility.com>
2017-07-21 17:23:46 +0100

    Adding comments and fixing up waiting for deploying module

--- tests/acceptance/Core/BasicModuleCest.php
+++ tests/acceptance/Core/BasicModuleCest.php
@@ -23,22 +23,27 @@ class BasicModuleCest
         $I->amOnUrl(
             $webDriverHelper->getInstanceURL()
         );
+
         $I->loginAsAdmin($webDriverHelper);
         $I->gotoAdministration();
+
+        // Go To Module Builder
         $I->click('#moduleBuilder');
+
+        // Create new package
         $I->click('#newPackageLink');
-
         $I->wait(3);
-
         $I->fillField(['name' => 'name'], \Page\BasicModule::$NAME);
         $I->fillField(['name' => 'author'], 'Acceptance Tester');
         $I->fillField(['name' => 'key'], 'Test');
         $I->fillField(['name' => 'description'], 'test module');
         $I->click('Save');
 
+        // Close confirmation window
         $I->wait(3);
         $I->click('.container-close');
 
+        // Create new basic module
         $I->click('New Module');
         $I->waitForElement('[name="label"]');
         $I->fillField(['name' => 'name'], \Page\BasicModule::$NAME);
@@ -47,16 +52,24 @@ class BasicModuleCest
         $I->click('#type_basic');
         $I->click('Save');
 
+        // Close popup
         $I->wait(3);
         $I->click('.container-close');
 
+        // Deploy module
         $I->wait(3);
         $I->click('Module Builder');
         $I->wait(3);
         $I->click(\Page\BasicModule::$NAME, '.bodywrapper');
         $I->wait(3);
         $I->click('Deploy');
-        $I->waitForElement('#newPackageLink');
+
+        // Close popup
+        $I->wait(3);
+        $I->click('.container-close');
+
+        // Wait for page to refresh and look for new package link
+        $I->waitForElement('#newPackageLink', 120);
     }
 
     // tests
