a39de26e35ba0a6c3c6827ea1e768670d26f9b68
Daniel Samson <daniel.samson@salesagility.com>
2017-07-28 16:28:19 +0100

    Adding browserstack support

--- tests/SuiteCRM/Test/Driver/WebDriver.php
+++ tests/SuiteCRM/Test/Driver/WebDriver.php
@@ -6,13 +6,63 @@ use Helper\WebDriverHelper;
 
 class WebDriver extends \Codeception\Module\WebDriver
 {
+    private $browserStackLocal;
+
     public function _initialize()
     {
-        parent::_initialize();
         $config = $this->_getConfig();
         $this->config['host'] = $config['host'];
         $this->config['port'] = $config['port'];
+
+        if($this->isBrowserStackEnabled()) {
+            $this->configureBrowserStack();
+        }
+
+        parent::_initialize();
+
     }
+
+    /**
+     * @return bool
+     */
+    protected function isBrowserStackEnabled()
+    {
+        $webDriverHelper = new WebDriverHelper($this->moduleContainer, $this->_getConfig());
+        if(empty($webDriverHelper->getBrowserStackUsername()) ||empty($webDriverHelper->getBrowserStackAccessKey())) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isBrowserStackLocalEnabled()
+    {
+        if(
+            array_key_exists('browserstack.local', $this->config['capabilities']) &&
+            $this->config['capabilities']['browserstack.local']
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected function configureBrowserStack()
+    {
+        $webDriverHelper = new WebDriverHelper($this->moduleContainer, $this->_getConfig());
+        $this->config['capabilities']['browserstack.user'] = $webDriverHelper->getBrowserStackUsername();
+        $this->config['capabilities']['browserstack.key'] = $webDriverHelper->getBrowserStackAccessKey();
+
+        if($this->isBrowserStackLocalEnabled()) {
+            $bs_local_args = array("key" => $this->config["capabilities"]["browserstack.key"]);
+            $this->browserStackLocal = new \BrowserStack\Local();
+            $this->browserStackLocal->start($bs_local_args);
+        }
+    }
+
 
     protected function initialWindowSize()
     {
@@ -20,5 +70,12 @@ class WebDriver extends \Codeception\Module\WebDriver
         $width =  $config['width'];
         $height = $config['height'];
         $this->resizeWindow($width, $height);
+    }
+
+    public function _afterSuite() {
+        parent::_afterSuite();
+        if ($this->browserStackLocal) {
+            $this->browserStackLocal->stop();
+        }
     }
 }
