2aef03698b0a5c3045a8be53b2867b19c33e1c45
Jerzy Brzezinski <jerzy.brzezski@salesagility.com>
2017-08-31 15:58:20 +0100

    fix issue with display tickboxes in csv, pdf works

--- modules/AOW_WorkFlow/aow_utils.php
+++ modules/AOW_WorkFlow/aow_utils.php
@@ -235,7 +235,7 @@ function getValidFieldsTypes($module, $field){
 }
 
 
-function getModuleField($module, $fieldname, $aow_field, $view='EditView',$value = '', $alt_type = '', $currency_id = '', $params= array()){
+function getModuleField($module, $fieldname, $aow_field, $view='EditView',$value = '', $alt_type = '', $currency_id = '', $params= array(), $report_csv){
     global $current_language, $app_strings, $app_list_strings, $current_user, $beanFiles, $beanList;
 
     // use the mod_strings for this module
@@ -512,6 +512,12 @@ function getModuleField($module, $fieldname, $aow_field, $view='EditView',$value
     // add in any additional strings
     $ss->assign("MOD", $mod_strings);
     $ss->assign("APP", $app_strings);
+
+    //In the case of a csv being generated check if the field type is a checkbox. if so just return the value
+    if($report_csv && $vardefFields[$fieldname]['type'] == 'bool'){
+        return $value;
+    }
+
 
     //$return = str_replace($fieldname,$ss->fetch($file));
 
