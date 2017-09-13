5fcd13ba0323b56fc07f277b8051c8203e4afd0a
Daniel Samson <daniel.samson@salesagility.com>
2017-09-13 13:05:10 +0100

    Renaming folder
    (cherry picked from commit 26ef53f)

--- json_server.php
+++ json_server.php
@@ -43,7 +43,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-require_once __DIR__ . '/service/JsonServerRPC/JsonRPCServer.php';
+require_once __DIR__ . '/service/JsonRPCServer/JsonRPCServer.php';
 
 $jsonServer = new JsonRPCServer();
 $jsonServer->run();
