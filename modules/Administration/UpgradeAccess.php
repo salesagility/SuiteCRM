6ff9791bad72f83d1a6d502b58f8031e7ba5b6d0
Dillon-Brown <dillon.brown@salesagility.com>
2017-09-15 22:59:23 +0000

    Reformat

--- modules/Administration/UpgradeAccess.php
+++ modules/Administration/UpgradeAccess.php
@@ -45,11 +45,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 global $mod_strings;
 global $sugar_config;
 
-$ignoreCase = (substr_count(strtolower($_SERVER['SERVER_SOFTWARE']), 'apache/2') > 0)?'(?i)':'';
-$htaccess_file   = getcwd() . "/.htaccess";
+$ignoreCase = (substr_count(strtolower($_SERVER['SERVER_SOFTWARE']), 'apache/2') > 0) ? '(?i)' : '';
+$htaccess_file = getcwd() . "/.htaccess";
 $contents = '';
 $basePath = parse_url($sugar_config['site_url'], PHP_URL_PATH);
-if(empty($basePath)) $basePath = '/';
+if (empty($basePath)) {
+    $basePath = '/';
+}
 
 $restrict_str = <<<EOQ
 # BEGIN SUGARCRM RESTRICTIONS
@@ -70,60 +72,65 @@ RedirectMatch 403 {$ignoreCase}/+files\.md5\$
 # END SUGARCRM RESTRICTIONS
 EOQ;
 
-if(file_exists($htaccess_file)){
+if (file_exists($htaccess_file)) {
     $fp = fopen($htaccess_file, 'r');
     $skip = false;
-    while($line = fgets($fp)){
+    while ($line = fgets($fp)) {
 
-    	if(preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = true;
-        if(!$skip)$contents .= $line;
-        if(preg_match('/\s*#\s*END\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = false;
+        if (preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line)) {
+            $skip = true;
+        }
+        if (!$skip) {
+            $contents .= $line;
+        }
+        if (preg_match('/\s*#\s*END\s*SUGARCRM\s*RESTRICTIONS/i', $line)) {
+            $skip = false;
+        }
     }
 }
-if(substr($contents, -1) != "\n") {
-    $restrict_str = "\n".$restrict_str;
+if (substr($contents, -1) != "\n") {
+    $restrict_str = "\n" . $restrict_str;
 }
-$status =  file_put_contents($htaccess_file, $contents . $restrict_str);
-if( !$status ){
+$status = file_put_contents($htaccess_file, $contents . $restrict_str);
+if (!$status) {
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE'] . "<span class=stop>{$htaccess_file}</span></p>\n";
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE_2'] . "</p>\n";
     echo "{$redirect_str}\n";
 }
 
 
-// cn: bug 9365 - security for filesystem
-$uploadDir='';
-$uploadHta='';
+$uploadDir = '';
+$uploadHta = '';
 
 if (empty($GLOBALS['sugar_config']['upload_dir'])) {
-    $GLOBALS['sugar_config']['upload_dir']='upload/';
+    $GLOBALS['sugar_config']['upload_dir'] = 'upload/';
 }
 
 $uploadHta = "upload://.htaccess";
 
-$denyAll =<<<eoq
+$denyAll = <<<eoq
 	Order Deny,Allow
 	Deny from all
 eoq;
 
-if(file_exists($uploadHta) && filesize($uploadHta)) {
-	// file exists, parse to make sure it is current
-	if(is_writable($uploadHta)) {
-		$oldHtaccess = file_get_contents($uploadHta);
-		// use a different regex boundary b/c .htaccess uses the typicals
-		if(strstr($oldHtaccess, $denyAll) === false) {
-		    $oldHtaccess .= "\n";
-			$oldHtaccess .= $denyAll;
-		}
-		if(!file_put_contents($uploadHta, $oldHtaccess)) {
-		    $htaccess_failed = true;
-		}
-	} else {
-		$htaccess_failed = true;
-	}
+if (file_exists($uploadHta) && filesize($uploadHta)) {
+    // File exists, parse to make sure it is current
+    if (is_writable($uploadHta)) {
+        $oldHtaccess = file_get_contents($uploadHta);
+        // Use a different regex boundary b/c .htaccess uses the typicals
+        if (strstr($oldHtaccess, $denyAll) === false) {
+            $oldHtaccess .= "\n";
+            $oldHtaccess .= $denyAll;
+        }
+        if (!file_put_contents($uploadHta, $oldHtaccess)) {
+            $htaccess_failed = true;
+        }
+    } else {
+        $htaccess_failed = true;
+    }
 } else {
-	// no .htaccess yet, create a fill
-	if(!file_put_contents($uploadHta, $denyAll)) {
-		$htaccess_failed = true;
-	}
+    // No .htaccess yet, create a fill
+    if (!file_put_contents($uploadHta, $denyAll)) {
+        $htaccess_failed = true;
+    }
 }
