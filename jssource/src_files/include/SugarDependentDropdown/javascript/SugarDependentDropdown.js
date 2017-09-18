b3968ed088f8ad1cddc56beda956c687dc3036a0
Gyula Madarasz <gyula.madarasz@salesagility.com>
2017-09-18 16:10:16 +0100

    remove eval

--- jssource/src_files/include/SugarDependentDropdown/javascript/SugarDependentDropdown.js
+++ jssource/src_files/include/SugarDependentDropdown/javascript/SugarDependentDropdown.js
@@ -1,9 +1,10 @@
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2014 Salesagility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2017 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -14,7 +15,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -32,9 +33,9 @@
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
 SUGAR.dependentDropdown = {
 	/*
@@ -141,8 +142,9 @@ SUGAR.dependentDropdown.generateElement = function(focusElement, elementRow, ind
 				 * focusElement.values can be lazy-loaded via JS call
 				 */
 				if(typeof(focusElement.values) == 'string') {
-					focusElement.values = eval(focusElement.values);
-				}
+					SUGAR.util.globalEval('retValue = ' + focusElement.values);
+					focusElement.values = retValue;
+                }
 				
 				/* Define the key-value that is to be used to pre-select a value in the dropdown */	
 				var preselect = SUGAR.dependentDropdown.utils.getPreselectKey(focusElement.name);
@@ -176,7 +178,8 @@ SUGAR.dependentDropdown.generateElement = function(focusElement, elementRow, ind
 				 * focusElement.values can be lazy-loaded via JS call
 				 */
 				if(typeof(focusElement.values) == 'string') {
-					focusElement.values = eval(focusElement.values);
+                  SUGAR.util.globalEval('retValue = ' + focusElement.values);
+                  focusElement.values = retValue;
 				}
 				
 				/* Define the key-value that is to be used to pre-select a value in the dropdown */
