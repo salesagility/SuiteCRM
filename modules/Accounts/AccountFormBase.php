436ac64473d482ca0b41b8e154a28a57b1df225e
Dillon Brown <dillon.brown@salesagility.com>
2017-09-21 15:16:13 +0100

    Code cleanup

--- modules/Accounts/AccountFormBase.php
+++ modules/Accounts/AccountFormBase.php
@@ -1,11 +1,11 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2014 Salesagility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2017 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -13,38 +13,34 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
-/*********************************************************************************
-
- * Description:  base form for account
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
- * All Rights Reserved.
- * Contributor(s): ______________________________________..
- ********************************************************************************/
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 class AccountFormBase{
 
@@ -392,163 +388,159 @@ $mod_strings = $temp_strings;
 }
 
 
-function handleSave($prefix,$redirect=true, $useRequired=false){
-	
-    
-	require_once('include/formbase.php');
-
-	$focus = new Account();
-
-	if($useRequired &&  !checkRequired($prefix, array_keys($focus->required_fields))){
-		return null;
-	}
-	$focus = populateFromPost($prefix, $focus);
-
-	if (isset($GLOBALS['check_notify'])) {
-		$check_notify = $GLOBALS['check_notify'];
-	}
-	else {
-		$check_notify = FALSE;
-	}
-
-	if (empty($_POST['record']) && empty($_POST['dup_checked'])) {
-		$duplicateAccounts = $this->checkForDuplicates($prefix);
-		if(isset($duplicateAccounts)){
-			$location='module=Accounts&action=ShowDuplicates';
-			$get = '';
-
-			// Bug 25311 - Add special handling for when the form specifies many-to-many relationships
-			if(isset($_POST['relate_to']) && !empty($_POST['relate_to'])) {
-				$get .= '&Accountsrelate_to='.$_POST['relate_to'];
-			}
-			if(isset($_POST['relate_id']) && !empty($_POST['relate_id'])) {
-				$get .= '&Accountsrelate_id='.$_POST['relate_id'];
-			}
-			
-			//add all of the post fields to redirect get string
-			foreach ($focus->column_fields as $field)
-			{
-				if (!empty($focus->$field) && !is_object($focus->$field))
-				{
-					$get .= "&Accounts$field=".urlencode($focus->$field);
-				}
-			}
-
-			foreach ($focus->additional_column_fields as $field)
-			{
-				if (!empty($focus->$field))
-				{
-					$get .= "&Accounts$field=".urlencode($focus->$field);
-				}
-			}
+    function handleSave($prefix, $redirect = true, $useRequired = false)
+    {
 
 
-            if ($focus->hasCustomFields()) {
-                foreach ($focus->field_defs as $name => $field) {
-                    if (!empty($field['source']) && $field['source'] === 'custom_fields') {
-                        $get .= "&Accounts$name=" . urlencode(isset($focus->$name));
+        require_once('include/formbase.php');
+
+        $focus = new Account();
+
+        if ($useRequired && !checkRequired($prefix, array_keys($focus->required_fields))) {
+            return null;
+        }
+        $focus = populateFromPost($prefix, $focus);
+
+        if (isset($GLOBALS['check_notify'])) {
+            $check_notify = $GLOBALS['check_notify'];
+        } else {
+            $check_notify = false;
+        }
+
+        if (empty($_POST['record']) && empty($_POST['dup_checked'])) {
+            $duplicateAccounts = $this->checkForDuplicates($prefix);
+            if (isset($duplicateAccounts)) {
+                $location = 'module=Accounts&action=ShowDuplicates';
+                $get = '';
+
+                if (isset($_POST['relate_to']) && !empty($_POST['relate_to'])) {
+                    $get .= '&Accountsrelate_to=' . $_POST['relate_to'];
+                }
+                if (isset($_POST['relate_id']) && !empty($_POST['relate_id'])) {
+                    $get .= '&Accountsrelate_id=' . $_POST['relate_id'];
+                }
+
+                // Add all of the post fields to redirect get string
+                foreach ($focus->column_fields as $field) {
+                    if (!empty($focus->$field) && !is_object($focus->$field)) {
+                        $get .= "&Accounts$field=" . urlencode($focus->$field);
                     }
                 }
+
+                foreach ($focus->additional_column_fields as $field) {
+                    if (!empty($focus->$field)) {
+                        $get .= "&Accounts$field=" . urlencode($focus->$field);
+                    }
+                }
+
+
+                if ($focus->hasCustomFields()) {
+                    foreach ($focus->field_defs as $name => $field) {
+                        if (!empty($field['source']) && $field['source'] === 'custom_fields') {
+                            $get .= "&Accounts$name=" . urlencode(isset($focus->$name));
+                        }
+                    }
+                }
+
+                $emailAddress = new SugarEmailAddress();
+                $get .= $emailAddress->getFormBaseURL($focus);
+
+
+                // Create list of suspected duplicate account id's in redirect get string
+                $i = 0;
+                foreach ($duplicateAccounts as $account) {
+                    $get .= "&duplicate[$i]=" . $account['id'];
+                    $i++;
+                }
+
+                // Add return_module, return_action, and return_id to redirect get string
+                $urlData = array('return_module' => 'Accounts', 'return_action' => '');
+                foreach (array('return_module', 'return_action', 'return_id', 'popup', 'create') as $var) {
+                    if (!empty($_POST[$var])) {
+                        $urlData[$var] = $_POST[$var];
+                    }
+                }
+                $get .= "&" . http_build_query($urlData);
+
+                $_SESSION['SHOW_DUPLICATES'] = $get;
+                // Now redirect the post to modules/Accounts/ShowDuplicates.php
+                if (!empty($_POST['is_ajax_call']) && $_POST['is_ajax_call'] == '1') {
+                    ob_clean();
+                    $json = getJSONobj();
+                    echo $json->encode(array('status' => 'dupe', 'get' => $location));
+                } else {
+                    if (!empty($_REQUEST['ajax_load'])) {
+                        echo "<script>SUGAR.ajaxUI.loadContent('index.php?$location');</script>";
+                    } else {
+                        if (!empty($_POST['to_pdf'])) {
+                            $location .= '&to_pdf=' . urlencode($_POST['to_pdf']);
+                        }
+                        header("Location: index.php?$location");
+                    }
+                }
+
+                return null;
+            }
+        }
+        if (!$focus->ACLAccess('Save')) {
+            ACLController::displayNoAccess(true);
+            sugar_cleanup(true);
+        }
+
+        $focus->save($check_notify);
+        $return_id = $focus->id;
+
+        $GLOBALS['log']->debug('Saved record with id of ' . $return_id);
+
+
+        if (!empty($_POST['is_ajax_call']) && $_POST['is_ajax_call'] == '1') {
+            $json = getJSONobj();
+            echo $json->encode(array(
+                'status' => 'success',
+                'get' => ''
+            ));
+            $trackerManager = TrackerManager::getInstance();
+            $timeStamp = TimeDate::getInstance()->nowDb();
+            if ($monitor = $trackerManager->getMonitor('tracker')) {
+                $monitor->setValue('action', 'detailview');
+                $monitor->setValue('user_id', $GLOBALS['current_user']->id);
+                $monitor->setValue('module_name', 'Accounts');
+                $monitor->setValue('date_modified', $timeStamp);
+                $monitor->setValue('visible', 1);
+
+                if (!empty($this->bean->id)) {
+                    $monitor->setValue('item_id', $return_id);
+                    $monitor->setValue('item_summary', $focus->get_summary_text());
+                }
+                $trackerManager->saveMonitor($monitor, true, true);
             }
 
-			$emailAddress = new SugarEmailAddress();
-			$get .= $emailAddress->getFormBaseURL($focus);
+            return null;
+        }
 
-			
-			
-			//create list of suspected duplicate account id's in redirect get string
-			$i=0;
-			foreach ($duplicateAccounts as $account)
-			{
-				$get .= "&duplicate[$i]=".$account['id'];
-				$i++;
-			}
-
-			//add return_module, return_action, and return_id to redirect get string
-			$urlData = array('return_module' => 'Accounts', 'return_action' => '');
-			foreach (array('return_module', 'return_action', 'return_id', 'popup', 'create') as $var) {
-			    if (!empty($_POST[$var])) {
-			        $urlData[$var] = $_POST[$var];
-			    }
-			}
-			$get .= "&".http_build_query($urlData);
-
-			$_SESSION['SHOW_DUPLICATES'] = $get;
-			//now redirect the post to modules/Accounts/ShowDuplicates.php
-            if (!empty($_POST['is_ajax_call']) && $_POST['is_ajax_call'] == '1')
-            {
-            	ob_clean();
-                $json = getJSONobj();
-                echo $json->encode(array('status' => 'dupe', 'get' => $location));
+        if (isset($_POST['popup']) && $_POST['popup'] === 'true') {
+            $urlData = array('query' => true, 'name' => $focus->name, 'module' => 'Accounts', 'action' => 'Popup');
+            if (!empty($_POST['return_module'])) {
+                $urlData['module'] = $_POST['return_module'];
             }
-            else if(!empty($_REQUEST['ajax_load']))
-            {
-                echo "<script>SUGAR.ajaxUI.loadContent('index.php?$location');</script>";
+            if (!empty($_POST['return_action'])) {
+                $urlData['action'] = $_POST['return_action'];
             }
-            else {
-                if(!empty($_POST['to_pdf']))
-                    $location .= '&to_pdf='.urlencode($_POST['to_pdf']);
-                header("Location: index.php?$location");
+            foreach (array('return_id', 'popup', 'create', 'to_pdf') as $var) {
+                if (!empty($_POST[$var])) {
+                    $urlData[$var] = $_POST[$var];
+                }
             }
-			return null;
-		}
-	}
-	if(!$focus->ACLAccess('Save')){
-		ACLController::displayNoAccess(true);
-		sugar_cleanup(true);
-	}
+            header('Location: index.php?' . http_build_query($urlData));
 
-	$focus->save($check_notify);
-    $return_id = $focus->id;
-    
-	$GLOBALS['log']->debug("Saved record with id of ".$return_id);
+            return null;
+        }
+        if ($redirect) {
+            handleRedirect($return_id, 'Accounts');
+        } else {
+            return $focus;
+        }
 
-
-    if (!empty($_POST['is_ajax_call']) && $_POST['is_ajax_call'] == '1') {
-        $json = getJSONobj();
-        echo $json->encode(array('status' => 'success',
-                                 'get' => ''));
-   	 	$trackerManager = TrackerManager::getInstance();
-        $timeStamp = TimeDate::getInstance()->nowDb();
-        if($monitor = $trackerManager->getMonitor('tracker')){ 
-	        $monitor->setValue('action', 'detailview');
-	        $monitor->setValue('user_id', $GLOBALS['current_user']->id);
-	        $monitor->setValue('module_name', 'Accounts');
-	        $monitor->setValue('date_modified', $timeStamp);
-	        $monitor->setValue('visible', 1);
-	
-	        if (!empty($this->bean->id)) {
-	            $monitor->setValue('item_id', $return_id);
-	            $monitor->setValue('item_summary', $focus->get_summary_text());
-	        }
-			$trackerManager->saveMonitor($monitor, true, true);
-		}
         return null;
     }
-
-    if (isset($_POST['popup']) && $_POST['popup'] == 'true') {
-	    $urlData = array("query" => true, "name" => $focus->name, "module" => 'Accounts', 'action' => 'Popup');
-    	if (!empty($_POST['return_module'])) {
-    	    $urlData['module'] = $_POST['return_module'];
-    	}
-        if (!empty($_POST['return_action'])) {
-    	    $urlData['action'] = $_POST['return_action'];
-    	}
-    	foreach (array('return_id', 'popup', 'create', 'to_pdf') as $var) {
-    	    if (!empty($_POST[$var])) {
-    	        $urlData[$var] = $_POST[$var];
-    	    }
-    	}
-		header("Location: index.php?".http_build_query($urlData));
-		return;
-	}
-	if($redirect){
-		handleRedirect($return_id,'Accounts');
-	}else{
-		return $focus;
-	}
 }
-
-
-}
-?>
