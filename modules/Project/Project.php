5078fbbaca82004e4e95cd9cbceea66c6f0ab5d4
Dillon Brown <dillon.brown@salesagility.com>
2017-10-02 13:11:30 +0100

    Add popup tpls, current user

--- modules/Project/Project.php
+++ modules/Project/Project.php
@@ -1,11 +1,11 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2014 Salesagility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2017 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -16,7 +16,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -34,59 +34,63 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 class Project extends SugarBean {
 	// database table columns
-	var $id;
-	var $date_entered;
-	var $date_modified;
-	var $assigned_user_id;
-	var $modified_user_id;
-	var $created_by;
-	var $name;
-	var $description;
-	var $deleted;
+    public $id;
+    public $date_entered;
+    public $date_modified;
+    public $assigned_user_id;
+    public $modified_user_id;
+    public $created_by;
+    public $name;
+    public $description;
+    public $deleted;
 
 
 	// related information
-	var $assigned_user_name;
-	var $modified_by_name;
-	var $created_by_name;
+    public $assigned_user_name;
+    public $modified_by_name;
+    public $created_by_name;
 
-	var $account_id;
+    public $account_id;
 	var $contact_id;
-	var $opportunity_id;
-	var $email_id;
-    var $estimated_start_date;
+    public $opportunity_id;
+    public $email_id;
+    public $estimated_start_date;
+    public $override_business_hours;
 
 	// calculated information
-	var $total_estimated_effort;
-	var $total_actual_effort;
+    public $total_estimated_effort;
+    public $total_actual_effort;
 
-	var $object_name = 'Project';
-	var $module_dir = 'Project';
-	var $new_schema = true;
-	var $table_name = 'project';
+    public $object_name = 'Project';
+    public $module_dir = 'Project';
+    public $new_schema = true;
+    public $table_name = 'project';
 
 	// This is used to retrieve related fields from form posts.
-	var $additional_column_fields = array(
+    public $additional_column_fields = array(
 		'account_id',
 		'contact_id',
 		'opportunity_id',
 	);
 
-	var $relationship_fields = array(
+    public $relationship_fields = array(
 		'account_id' => 'accounts',
 		'contact_id'=>'contacts',
 		'opportunity_id'=>'opportunities',
 		'email_id' => 'emails',
 	);
 
-	//////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////
 	// METHODS
 	//////////////////////////////////////////////////////////////////
 
@@ -660,4 +664,3 @@ class Project extends SugarBean {
 	}
 
 }
-?>
