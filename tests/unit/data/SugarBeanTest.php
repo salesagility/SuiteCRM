591a68d723d90627bb1dcec33fde1bf0d9333e3d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2017-11-13 17:32:48 +0000

    fix a test

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -291,6 +291,8 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
             70 => 'securitygroup_noninherit_id',
             71 => 'securitygroup_noninheritable',
             72 => 'securitygroup_primary_group',
+            73 => 'factor_auth',
+            74 => 'factor_auth_interface',
         ), $bean->column_fields);
 
         $keys = array_keys($bean->field_name_map);
