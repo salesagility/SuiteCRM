7275e7979e7fe364454f723ef67d9918d84b6671
Jim Mackin <jim.mackin@salesagility.com>
2017-11-23 16:21:35 +0000

    Add initial import of the Survey module.

---
+++ modules/Surveys/controller.php
@@ -0,0 +1,11 @@
+<?php
+
+if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
+
+class SurveysController extends SugarController {
+
+    function action_Reports() {
+        $this->view = 'Reports';
+    }
+
+}
