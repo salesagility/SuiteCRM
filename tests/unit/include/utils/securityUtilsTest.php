cc2debce2203edec826c0e4661b04e30d508dab3
Dillon Brown <dillon.brown@salesagility.com>
2017-12-01 14:17:25 +0000

    Fix test

--- tests/unit/include/utils/securityUtilsTest.php
+++ tests/unit/include/utils/securityUtilsTest.php
@@ -52,7 +52,6 @@ class security_utilsTest extends PHPUnit_Framework_TestCase
                 'AOW_WorkFlow' => 'AOW_WorkFlow',
                 'AOK_KnowledgeBase' => 'AOK_KnowledgeBase',
                 'AOK_Knowledge_Base_Categories' => 'AOK_Knowledge_Base_Categories',
-            'Surveys' => 'Surveys',
                 'EmailTemplates' => 'EmailTemplates'
 
         );
