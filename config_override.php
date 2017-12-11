955060380db151e4f76527db6e5fd7c19c2dcbe6
Daniel Samson <daniel.samson@salesagility.com>
2017-12-11 12:09:52 +0000

    Reverting config_override.php

--- config_override.php
+++
@@ -1,7 +0,0 @@
-<?php
-/***CONFIGURATOR***/
-$sugar_config['developerMode'] = true;
-$sugar_config['default_module_favicon'] = false;
-$sugar_config['dashlet_auto_refresh_min'] = '30';
-$sugar_config['stack_trace_errors'] = false;
-/***CONFIGURATOR***/
