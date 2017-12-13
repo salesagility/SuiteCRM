4cae21c4f2fd1925e1621e7b965a9076a7a8cf49
Dillon Brown <dillon.brown@salesagility.com>
2017-12-13 12:00:31 +0000

    Update JS + Style.css

--- include/javascript/yui3/build/tabview/tabview-plugin.js
+++ include/javascript/yui3/build/tabview/tabview-plugin.js
@@ -1,8 +1,8 @@
 /*
- Copyright (c) 2010, Yahoo! Inc. All rights reserved.
- Code licensed under the BSD License:
- http://developer.yahoo.com/yui/license.html
- version: 3.3.0
- build: 3167
- */
-YUI.add('tabview-plugin',function(Y){function TabviewPlugin(){TabviewPlugin.superclass.constructor.apply(this,arguments);};TabviewPlugin.NAME='tabviewPlugin';TabviewPlugin.NS='tabs';Y.extend(TabviewPlugin,Y.TabviewBase);Y.namespace('Plugin');Y.Plugin.Tabview=TabviewPlugin;},'3.3.0',{requires:['node-pluginhost','tabview-base']});
+Copyright (c) 2010, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://developer.yahoo.com/yui/license.html
+version: 3.3.0
+build: 3167
+*/
+YUI.add("tabview-plugin",function(b){function a(){a.superclass.constructor.apply(this,arguments);}a.NAME="tabviewPlugin";a.NS="tabs";b.extend(a,b.TabviewBase);b.namespace("Plugin");b.Plugin.Tabview=a;},"3.3.0",{requires:["node-pluginhost","tabview-base"]});
--- modules/UpgradeWizard/upgradeWizard.js
+++ modules/UpgradeWizard/upgradeWizard.js
@@ -1,6 +1,9 @@
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ 
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
+ * Copyright (C) 2011 - 2014 Salesagility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -28,9 +31,9 @@
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
- * SugarCRM" logo. If the display of the logo is not reasonably feasible for
- * technical reasons, the Appropriate Legal Notices must display the words
- * "Powered by SugarCRM".
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
+ * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  ********************************************************************************/
 var req;var uw_check_msg="";var find_done=false;function loadXMLDoc(url){req=false;if(window.XMLHttpRequest){try{req=new XMLHttpRequest();}catch(e){req=false;}}else if(window.ActiveXObject){try{req=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{req=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){req=false;}}}
 if(req){req.onreadystatechange=processReqChange;req.open("GET",url,true);req.send("");}}
