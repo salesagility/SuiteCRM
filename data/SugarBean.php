bc6310a939cb8544ed0c7787376596a14a085a13
p.ernst <peter.ernst@salesagility.com>
2017-12-19 15:05:57 +0000

    Explicit typecasting

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -1447,7 +1447,7 @@ class SugarBean
 
         $result = $this->db->query($count_query, true, "Error running count query for $this->object_name List: ");
         while ($row = $this->db->fetchByAssoc($result, true)) {
-            $num_rows_in_query += current($row);
+            $num_rows_in_query += (int)current($row);
         }
 
         return $num_rows_in_query;
