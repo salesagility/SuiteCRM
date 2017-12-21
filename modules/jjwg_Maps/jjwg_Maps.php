32891a221a1d509bc012592e772a3f17a6d208cd
Frank Sagurna <fgunni@01019freenet.de>
2017-12-21 11:00:26 +0100

    If there is no google maps API key don't throw php notice
    If there is no google maps API key don't throw php notice

--- modules/jjwg_Maps/jjwg_Maps.php
+++ modules/jjwg_Maps/jjwg_Maps.php
@@ -353,7 +353,7 @@ class jjwg_Maps extends jjwg_Maps_sugar {
                 $this->settings['geocoding_api_secret'] = $rev['geocoding_api_secret'];
             }
             // Set Google Maps API Key
-            $this->settings['google_maps_api_key'] = $rev['google_maps_api_key'];
+            $this->settings['google_maps_api_key'] = !empty($rev['google_maps_api_key'])?$rev['google_maps_api_key']:"";
         }
 
         // Set for Global Use
