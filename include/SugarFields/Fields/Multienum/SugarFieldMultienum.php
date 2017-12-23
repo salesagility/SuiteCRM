bdeb94db0ebdd5c8fa850f6e679037409c0b8141
pgorod <pgorod@users.noreply.github.com>
2017-12-23 11:58:03 +0000

    Fix PHP Warning

--- include/SugarFields/Fields/Multienum/SugarFieldMultienum.php
+++ include/SugarFields/Fields/Multienum/SugarFieldMultienum.php
@@ -58,7 +58,7 @@ class SugarFieldMultienum extends SugarFieldEnum
     	}
     }
 
-    function displayFromFunc( $displayType, $parentFieldArray, $vardef, $displayParams, $tabindex ) {
+    function displayFromFunc( $displayType, $parentFieldArray, $vardef, $displayParams, $tabindex = 0) {
         if ( isset($vardef['function']['returns']) && $vardef['function']['returns'] == 'html' ) {
             return parent::displayFromFunc($displayType, $parentFieldArray, $vardef, $displayParams, $tabindex);
         }
