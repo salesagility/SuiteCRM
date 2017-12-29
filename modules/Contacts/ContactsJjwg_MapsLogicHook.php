922177b92b5fd999fe37a342fb79a9d40728ee60
p.ernst <peter.ernst@salesagility.com>
2017-12-29 14:38:52 +0000

    ContactsJjwg_MapsLogicHook.php

--- modules/Contacts/ContactsJjwg_MapsLogicHook.php
+++ modules/Contacts/ContactsJjwg_MapsLogicHook.php
@@ -2,43 +2,68 @@
 
 // custom/modules/Contacts/ContactsJjwg_MapsLogicHook.php
 
-if (!defined('sugarEntry') || !sugarEntry)
+if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
+}
 
-class ContactsJjwg_MapsLogicHook {
-
-    var $jjwg_Maps;
-    function __construct() {
-        $this->jjwg_Maps = get_module_info('jjwg_Maps');
-    }
+/**
+ * Class ContactsJjwg_MapsLogicHook
+ */
+class ContactsJjwg_MapsLogicHook
+{
 
     /**
-     * @deprecated deprecated since version 7.6, PHP4 Style Constructors are deprecated and will be remove in 7.8, please update your code, use __construct instead
+     * @var void
      */
-    function ContactsJjwg_MapsLogicHook(){
-        $deprecatedMessage = 'PHP4 Style Constructors are deprecated and will be remove in 7.8, please update your code';
-        if(isset($GLOBALS['log'])) {
+    public $jjwg_Maps;
+
+    /**
+     * @deprecated deprecated since version 7.6, PHP4 Style Constructors are deprecated and will be remove in 7.8,
+     *     please update your code, use __construct instead
+     */
+    public function ContactsJjwg_MapsLogicHook()
+    {
+        $deprecatedMessage = 'PHP4 Style Constructors are deprecated and will be remove in 7.8, ' .
+            'please update your code';
+        if (isset($GLOBALS['log'])) {
             $GLOBALS['log']->deprecated($deprecatedMessage);
-        }
-        else {
+        } else {
             trigger_error($deprecatedMessage, E_USER_DEPRECATED);
         }
         self::__construct();
     }
 
+    /**
+     * ContactsJjwg_MapsLogicHook constructor.
+     */
+    public function __construct()
+    {
+        $this->jjwg_Maps = get_module_info('jjwg_Maps');
+    }
 
-    function updateGeocodeInfo(&$bean, $event, $arguments) {
+    /**
+     * @param $bean
+     * @param $event
+     * @param $arguments
+     */
+    public function updateGeocodeInfo(&$bean, $event, $arguments)
+    {
         // before_save
         if ($this->jjwg_Maps->settings['logic_hooks_enabled']) {
             $this->jjwg_Maps->updateGeocodeInfo($bean);
         }
     }
 
-    function updateRelatedMeetingsGeocodeInfo(&$bean, $event, $arguments) {
+    /**
+     * @param $bean
+     * @param $event
+     * @param $arguments
+     */
+    public function updateRelatedMeetingsGeocodeInfo(&$bean, $event, $arguments)
+    {
         // after_save
         if ($this->jjwg_Maps->settings['logic_hooks_enabled']) {
             $this->jjwg_Maps->updateRelatedMeetingsGeocodeInfo($bean);
         }
     }
-
 }
