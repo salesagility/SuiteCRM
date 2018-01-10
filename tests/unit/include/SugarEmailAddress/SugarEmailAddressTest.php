b8b4b410408240054276fc5cfadba65190a7927b
Daniel Samson <daniel.samson@salesagility.com>
2018-01-10 14:16:45 +0000

    updating testUpdateFlags

--- tests/unit/include/SugarEmailAddress/SugarEmailAddressTest.php
+++ tests/unit/include/SugarEmailAddress/SugarEmailAddressTest.php
@@ -841,7 +841,6 @@ class SugarEmailAddressTest extends PHPUnit_Framework_TestCase
             'email_address_caps' => 'TEST@EMAIL.COM',
             'invalid_email' => '0',
             'opt_out' => '0',
-            'opt_in_email_created' => null,
             'confirm_opt_in' => '0',
             'date_created' => $a['date_created'],
             'date_modified' => $a['date_modified'],
