1dd5a6d266b7389203a32f3fa73341886a378146
Daniel Samson <daniel.samson@salesagility.com>
2018-01-11 10:52:40 +0000

    updating license header confirm_opt_in.php

--- include/SugarObjects/templates/basic/entrypoints/confirm_opt_in.php
+++ include/SugarObjects/templates/basic/entrypoints/confirm_opt_in.php
@@ -6,7 +6,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2017 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -38,6 +38,7 @@
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
  * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
+
 
 /**
  * Class confirm_opt_in
