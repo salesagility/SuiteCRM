7bf422ced1f2aa19d45bef0447a0b40d3522687e
Dillon Brown <dillon.brown@salesagility.com>
2018-01-12 10:54:59 +0000

    Fixed #3252 - rest interface problem, works in 7.6.10 with PHP 5.6.30 but not with PHP 7.1.2

--- service/v4/SugarWebServiceImplv4.php
+++ service/v4/SugarWebServiceImplv4.php
@@ -208,9 +208,16 @@ class SugarWebServiceImplv4 extends SugarWebServiceImplv3_1 {
 	 *	     'relationship_list' -- Array - The records link field data. The example is if asked about accounts email address then return data would look like Array ( [0] => Array ( [name] => email_addresses [records] => Array ( [0] => Array ( [0] => Array ( [name] => id [value] => 3fb16797-8d90-0a94-ac12-490b63a6be67 ) [1] => Array ( [name] => email_address [value] => hr.kid.qa@example.com ) [2] => Array ( [name] => opt_out [value] => 0 ) [3] => Array ( [name] => primary_address [value] => 1 ) ) [1] => Array ( [0] => Array ( [name] => id [value] => 403f8da1-214b-6a88-9cef-490b63d43566 ) [1] => Array ( [name] => email_address [value] => kid.hr@example.name ) [2] => Array ( [name] => opt_out [value] => 0 ) [3] => Array ( [name] => primary_address [value] => 0 ) ) ) ) )
 	 * @exception 'SoapFault' -- The SOAP error, if any
 	 */
-	public function get_entries($session, $module_name, $ids, $select_fields, $link_name_to_fields_array)
+    public function get_entries(
+        $session,
+        $module_name,
+        $ids,
+        $select_fields,
+        $link_name_to_fields_array,
+        $track_view = false
+    )
 	{
-	    $result = parent::get_entries($session, $module_name, $ids, $select_fields, $link_name_to_fields_array);
+	    $result = parent::get_entries($session, $module_name, $ids, $select_fields, $link_name_to_fields_array, $track_view);
 		$relationshipList = $result['relationship_list'];
 		$returnRelationshipList = array();
 		foreach($relationshipList as $rel){
