de4ba7f4c1c5c77d1b2567ed608ebc96f5305289
p.ernst <peter.ernst@salesagility.com>
2018-01-17 11:24:28 +0000

    Fix a false assumption that browser is Safari

--- jssource/src_files/include/javascript/sugar_3.js
+++ jssource/src_files/include/javascript/sugar_3.js
@@ -2962,7 +2962,7 @@ SUGAR.util = function () {
       }
     },
     evalScript: function (text) {
-      if (isSafari) {
+      if (UA.webkit && !UA.chrome) {
         var waitUntilLoaded = function () {
           SUGAR.evalScript_waitCount--;
           if (SUGAR.evalScript_waitCount == 0) {
