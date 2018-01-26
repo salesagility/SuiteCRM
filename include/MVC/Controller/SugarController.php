67257cf3b678444c1a85e3cc7fd7002f11a192b3
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-01-26 10:55:49 +0000

    Test for class instead module name

--- include/MVC/Controller/SugarController.php
+++ include/MVC/Controller/SugarController.php
@@ -1110,7 +1110,8 @@ class SugarController
     public function action_sendConfirmOptInEmail() {
         global $app_strings;
 
-        if (!in_array($this->bean->module_name, array('Accounts', 'Contacts', 'Leads', 'Prospects'))) {
+        
+        if ($this->bean instanceof Company || $this->bean instanceof Person) {
             $msg = $app_strings['LBL_CONFIRM_OPT_IN_ONLY_FOR_PERSON'];
             SugarApplication::appendErrorMessage($msg);
         } else {
