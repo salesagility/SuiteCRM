c7f09ffbd0195f5827a3a6bc17fb96a8e4492c02
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-01-26 15:49:48 +0000

    typo fix

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -3936,4 +3936,4 @@ $app_strings['RESPONSE_SEND_CONFIRM_OPT_IN_EMAIL_NOT_OPT_IN'] = 'Unable to send 
 $app_strings['RESPONSE_SEND_CONFIRM_OPT_IN_EMAIL_MISSING_EMAIL_ADDRESS_ID'] = '%s email address do not have a valid id. ';
 
 $app_strings['ERR_TWO_FACTOR_FAILED'] = 'Two Factor Authentication failed';
-$app_strings['ERR_TWO_FACTOR_CODE_SENT'] = 'Two factor authentication code sent.';
+$app_strings['ERR_TWO_FACTOR_CODE_SENT'] = 'Two Factor Authentication code sent.';
