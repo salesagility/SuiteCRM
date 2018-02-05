c56e48cf4023f54c5071444cf52c78c3836396f3
Daniel Samson <daniel.samson@salesagility.com>
2018-02-05 10:29:53 +0000

    Updating PHP Doc SugarEmailAddress::AddUpdateEmailAddress

--- include/SugarEmailAddress/SugarEmailAddress.php
+++ include/SugarEmailAddress/SugarEmailAddress.php
@@ -1204,14 +1204,14 @@ class SugarEmailAddress extends SugarBean
     /**
      * Creates or Updates an entry in the email_addresses table, depending
      * on if the email address submitted matches a previous entry (case-insensitive)
-     * @param String $addr - email address
+     * @param string $addr - email address
      * @param int $invalid - is the email address marked as Invalid?
      * @param int $opt_out - is the email address marked as Opt-Out?
-     * @param String $id - the GUID of the original SugarEmailAddress bean,
+     * @param string $id - the GUID of the original SugarEmailAddress bean,
      *        in case a "email has changed" WorkFlow has triggered - hack to allow workflow-induced changes
      *        to propagate to the new SugarEmailAddress - see bug 39188
      * @param int|null $optInFlag
-     * @return String GUID of Email Address or '' if cleaned address was empty.
+     * @return string GUID of Email Address or '' if cleaned address was empty.
      */
     public function AddUpdateEmailAddress($addr, $invalid = 0, $opt_out = 0, $id = null, $optInFlag = null)
     {
