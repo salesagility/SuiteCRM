375d5cc05a7ce37e2e17463e8dffa161e3d5f942
p.ernst <peter.ernst@salesagility.com>
2018-02-07 08:55:48 +0000

    Remove empty line

--- data/Link.php
+++ data/Link.php
@@ -1,5 +1,4 @@
 <?php
-
 /*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
