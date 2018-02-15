baa36503e6b4d7afaa0569b7dcb4d7cfa4dae89b
Daniel Samson <daniel.samson@salesagility.com>
2018-02-15 17:17:28 +0000

    Fixing htaccess file to prepend new rules instead of append

--- modules/Administration/UpgradeAccess.php
+++ modules/Administration/UpgradeAccess.php
@@ -1,11 +1,11 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2014 Salesagility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -13,33 +13,34 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
  * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
  * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with
  * this program; if not, see http://www.gnu.org/licenses or write to the Free
  * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA.
- * 
+ *
  * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
  * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- * 
+ *
  * The interactive user interfaces in modified source and object code versions
  * of this program must display Appropriate Legal Notices, as required under
  * Section 5 of the GNU Affero General Public License version 3.
- * 
+ *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
-
-
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 global $mod_strings;
 global $sugar_config;
@@ -78,21 +79,24 @@ if(file_exists($htaccess_file)){
     $skip = false;
     while($line = fgets($fp)){
 
-    	if(preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = true;
-        if(!$skip)$contents .= $line;
+        if(preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = true;
+        if(!$skip)$oldcontents .= $line;
         if(preg_match('/\s*#\s*END\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = false;
     }
 }
+
 if(substr($contents, -1) != "\n") {
     $restrict_str = "\n".$restrict_str;
 }
 $status =  file_put_contents($htaccess_file, $contents . $restrict_str);
+
 if( !$status ){
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE'] . "<span class=stop>{$htaccess_file}</span></p>\n";
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE_2'] . "</p>\n";
     echo "{$redirect_str}\n";
 }
 
+$status2 =  file_put_contents($htaccess_file, $oldcontents, FILE_APPEND);
 
 // cn: bug 9365 - security for filesystem
 $uploadDir='';
@@ -110,27 +114,30 @@ $denyAll =<<<eoq
 eoq;
 
 if(file_exists($uploadHta) && filesize($uploadHta)) {
-	// file exists, parse to make sure it is current
-	if(is_writable($uploadHta)) {
-		$oldHtaccess = file_get_contents($uploadHta);
-		// use a different regex boundary b/c .htaccess uses the typicals
-		if(strstr($oldHtaccess, $denyAll) === false) {
-		    $oldHtaccess .= "\n";
-			$oldHtaccess .= $denyAll;
-		}
-		if(!file_put_contents($uploadHta, $oldHtaccess)) {
-		    $htaccess_failed = true;
-		}
-	} else {
-		$htaccess_failed = true;
-	}
+    // file exists, parse to make sure it is current
+    if(is_writable($uploadHta)) {
+        $oldHtaccess = file_get_contents($uploadHta);
+        // use a different regex boundary b/c .htaccess uses the typicals
+        if(strstr($oldHtaccess, $denyAll) === false) {
+            $oldHtaccess .= "\n";
+            $oldHtaccess .= $denyAll;
+        }
+        if(!file_put_contents($uploadHta, $oldHtaccess)) {
+            $htaccess_failed = true;
+        }
+    } else {
+        $htaccess_failed = true;
+    }
 } else {
-	// no .htaccess yet, create a fill
-	if(!file_put_contents($uploadHta, $denyAll)) {
-		$htaccess_failed = true;
-	}
+    // no .htaccess yet, create a fill
+    if(!file_put_contents($uploadHta, $denyAll)) {
+        $htaccess_failed = true;
+    }
 }
 
-/* Commenting out as this shows on upgrade screen
- * echo "\n" . $mod_strings['LBL_HT_DONE']. "<br />\n";
-*/
+
+if(isset($_REQUEST['action']) && $_REQUEST['action'] === 'UpgradeAccess')
+{
+    // only display message in the repair tool and not during the upgrade process
+    echo "\n" . $mod_strings['LBL_HT_DONE']. "<br />\n";
+}
