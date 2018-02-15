6aa81ac07184ce23489becec79429ea220083e2b
Daniel Samson <daniel.samson@salesagility.com>
2018-02-15 17:21:00 +0000

    Reformatting UpgradeAccess.php

--- modules/Administration/UpgradeAccess.php
+++ modules/Administration/UpgradeAccess.php
@@ -45,11 +45,13 @@ if (!defined('sugarEntry') || !sugarEntry) {
 global $mod_strings;
 global $sugar_config;
 
-$ignoreCase = (substr_count(strtolower($_SERVER['SERVER_SOFTWARE']), 'apache/2') > 0)?'(?i)':'';
-$htaccess_file   = getcwd() . "/.htaccess";
+$ignoreCase = (substr_count(strtolower($_SERVER['SERVER_SOFTWARE']), 'apache/2') > 0) ? '(?i)' : '';
+$htaccess_file = getcwd() . "/.htaccess";
 $contents = '';
 $basePath = parse_url($sugar_config['site_url'], PHP_URL_PATH);
-if(empty($basePath)) $basePath = '/';
+if (empty($basePath)) {
+    $basePath = '/';
+}
 
 $restrict_str = <<<EOQ
 # BEGIN SUGARCRM RESTRICTIONS
@@ -74,55 +76,61 @@ RedirectMatch 403 {$ignoreCase}/+files\.md5\$
 # END SUGARCRM RESTRICTIONS
 EOQ;
 
-if(file_exists($htaccess_file)){
+if (file_exists($htaccess_file)) {
     $fp = fopen($htaccess_file, 'r');
     $skip = false;
-    while($line = fgets($fp)){
+    while ($line = fgets($fp)) {
 
-        if(preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = true;
-        if(!$skip)$oldcontents .= $line;
-        if(preg_match('/\s*#\s*END\s*SUGARCRM\s*RESTRICTIONS/i', $line))$skip = false;
+        if (preg_match('/\s*#\s*BEGIN\s*SUGARCRM\s*RESTRICTIONS/i', $line)) {
+            $skip = true;
+        }
+        if (!$skip) {
+            $oldcontents .= $line;
+        }
+        if (preg_match('/\s*#\s*END\s*SUGARCRM\s*RESTRICTIONS/i', $line)) {
+            $skip = false;
+        }
     }
 }
 
-if(substr($contents, -1) != "\n") {
-    $restrict_str = "\n".$restrict_str;
+if (substr($contents, -1) != "\n") {
+    $restrict_str = "\n" . $restrict_str;
 }
-$status =  file_put_contents($htaccess_file, $contents . $restrict_str);
+$status = file_put_contents($htaccess_file, $contents . $restrict_str);
 
-if( !$status ){
+if (!$status) {
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE'] . "<span class=stop>{$htaccess_file}</span></p>\n";
     echo '<p>' . $mod_strings['LBL_HT_NO_WRITE_2'] . "</p>\n";
     echo "{$redirect_str}\n";
 }
 
-$status2 =  file_put_contents($htaccess_file, $oldcontents, FILE_APPEND);
+$status2 = file_put_contents($htaccess_file, $oldcontents, FILE_APPEND);
 
 // cn: bug 9365 - security for filesystem
-$uploadDir='';
-$uploadHta='';
+$uploadDir = '';
+$uploadHta = '';
 
 if (empty($GLOBALS['sugar_config']['upload_dir'])) {
-    $GLOBALS['sugar_config']['upload_dir']='upload/';
+    $GLOBALS['sugar_config']['upload_dir'] = 'upload/';
 }
 
 $uploadHta = "upload://.htaccess";
 
-$denyAll =<<<eoq
+$denyAll = <<<eoq
 	Order Deny,Allow
 	Deny from all
 eoq;
 
-if(file_exists($uploadHta) && filesize($uploadHta)) {
+if (file_exists($uploadHta) && filesize($uploadHta)) {
     // file exists, parse to make sure it is current
-    if(is_writable($uploadHta)) {
+    if (is_writable($uploadHta)) {
         $oldHtaccess = file_get_contents($uploadHta);
         // use a different regex boundary b/c .htaccess uses the typicals
-        if(strstr($oldHtaccess, $denyAll) === false) {
+        if (strstr($oldHtaccess, $denyAll) === false) {
             $oldHtaccess .= "\n";
             $oldHtaccess .= $denyAll;
         }
-        if(!file_put_contents($uploadHta, $oldHtaccess)) {
+        if (!file_put_contents($uploadHta, $oldHtaccess)) {
             $htaccess_failed = true;
         }
     } else {
@@ -130,14 +138,13 @@ if(file_exists($uploadHta) && filesize($uploadHta)) {
     }
 } else {
     // no .htaccess yet, create a fill
-    if(!file_put_contents($uploadHta, $denyAll)) {
+    if (!file_put_contents($uploadHta, $denyAll)) {
         $htaccess_failed = true;
     }
 }
 
 
-if(isset($_REQUEST['action']) && $_REQUEST['action'] === 'UpgradeAccess')
-{
+if (isset($_REQUEST['action']) && $_REQUEST['action'] === 'UpgradeAccess') {
     // only display message in the repair tool and not during the upgrade process
-    echo "\n" . $mod_strings['LBL_HT_DONE']. "<br />\n";
+    echo "\n" . $mod_strings['LBL_HT_DONE'] . "<br />\n";
 }
