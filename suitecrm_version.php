8f21be19eb89f4f0cb64ebb0263910297471eaf7
Dillon Brown <dillon.brown@salesagility.com>
2018-02-16 17:22:51 +0000

    Increments suitecrm_version.php

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version      = '7.8.14';
+$suitecrm_version      = '7.8.15';
 $suitecrm_timestamp    = '2018-02-16 17:00';
