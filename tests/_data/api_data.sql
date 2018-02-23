91aedddacc2c81c35dabc0c295bb9442407c9633
p.ernst <peter.ernst@salesagility.com>
2018-02-23 12:31:54 +0000

    Update testing API sql dump

--- tests/_data/api_data.sql
+++ tests/_data/api_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Feb 21, 2018 at 04:36 PM
+-- Generation Time: Feb 23, 2018 at 12:31 PM
 -- Server version: 10.2.10-MariaDB-10.2.10+maria~jessie
 -- PHP Version: 7.1.9
 
@@ -40,21 +40,22 @@ CREATE TABLE `oauth2clients` (
   `secret` varchar(4098) DEFAULT NULL,
   `redirect_url` varchar(255) DEFAULT NULL,
   `is_confidential` tinyint(1) DEFAULT NULL,
-  `allowed_grant_type` varchar(255) DEFAULT 'Password Grant',
+  `allowed_grant_type` varchar(255) DEFAULT 'password',
   `duration_value` int(11) DEFAULT NULL,
   `duration_amount` int(11) DEFAULT NULL,
-  `duration_unit` varchar(255) DEFAULT 'Duration Unit'
+  `duration_unit` varchar(255) DEFAULT 'Duration Unit',
+  `assigned_user_id` char(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `oauth2clients`
 --
 
-INSERT INTO `oauth2clients` (`id`, `name`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `description`, `deleted`, `secret`, `redirect_url`, `is_confidential`, `allowed_grant_type`, `duration_value`, `duration_amount`, `duration_unit`) VALUES
-('API-4c59-f678-cecc-6594-5a8d9c704473', 'Password Grant API Client', '2018-02-21 16:20:43', '2018-02-21 16:20:43', '1', '1', NULL, 0, '$1$LWHSyJNo$Dg3KssRJcfw85uRJcF/py.', 'https://test.com', 1, 'password', 8640000, 100, 'day'),
-('API-6d34-6c4c-59be-9fb5-5a8d9cda918f', 'Implicit API Client', '2018-02-21 16:22:07', '2018-02-21 16:22:17', '1', '1', NULL, 0, '', 'https://test.com', 1, 'implicit', 8640000, 100, 'day'),
-('API-b95b-19cd-0229-a3ed-5a8d9cc0d3eb', 'Authorization Code API Client', '2018-02-21 16:22:46', '2018-02-21 16:22:46', '1', '1', NULL, 0, '', 'https://test.com', 1, 'authorization-code', 8640000, 100, 'day'),
-('API-ea74-c352-badd-c2be-5a8d9c9d4351', 'Client Credentials API Client', '2018-02-21 16:21:42', '2018-02-21 16:21:42', '1', '1', NULL, 0, '', 'https://test.com', 1, 'client-credentials', 8640000, 100, 'day');
+INSERT INTO `oauth2clients` (`id`, `name`, `date_entered`, `date_modified`, `modified_user_id`, `created_by`, `description`, `deleted`, `secret`, `redirect_url`, `is_confidential`, `allowed_grant_type`, `duration_value`, `duration_amount`, `duration_unit`, `assigned_user_id`) VALUES
+('API-4c59-f678-cecc-6594-5a8d9c704473', 'Password Grant API Client', '2018-02-21 16:20:43', '2018-02-21 16:20:43', '1', '1', NULL, 0, '$1$LWHSyJNo$Dg3KssRJcfw85uRJcF/py.', 'https://test.com', 1, 'password', 8640000, 1, 'day', NULL),
+('API-6d34-6c4c-59be-9fb5-5a8d9cda918f', 'Implicit API Client', '2018-02-21 16:22:07', '2018-02-21 16:22:17', '1', '1', NULL, 0, '$1$LWHSyJNo$Dg3KssRJcfw85uRJcF/py.', 'https://test.com', 1, 'implicit', 8640000, 1, 'day', NULL),
+('API-b95b-19cd-0229-a3ed-5a8d9cc0d3eb', 'Authorization Code API Client', '2018-02-21 16:22:46', '2018-02-21 16:22:46', '1', '1', NULL, 0, '$1$LWHSyJNo$Dg3KssRJcfw85uRJcF/py.', 'https://test.com', 1, 'authorization_code', 8640000, 1, 'day', NULL),
+('API-ea74-c352-badd-c2be-5a8d9c9d4351', 'Client Credentials API Client', '2018-02-21 16:21:42', '2018-02-22 17:03:17', '1', '1', NULL, 0, '$1$LWHSyJNo$Dg3KssRJcfw85uRJcF/py.', 'https://test.com', 1, 'client_credentials', 1, 1, 'day', '1');
 
 --
 -- Indexes for dumped tables
