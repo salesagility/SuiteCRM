66cc89a2db096bc0dff3169d1e1fb52c68788a63
Dillon Brown <dillon.brown@salesagility.com>
2018-02-26 11:05:10 +0000

    Adding opt out search to accounts

--- modules/Accounts/metadata/searchdefs.php
+++ modules/Accounts/metadata/searchdefs.php
@@ -181,6 +181,11 @@ $searchdefs ['Accounts'] =
                                 'default' => true,
                                 'width' => '10%',
                             ),
+                        'open_opt_out' => array(
+                            'name' => 'open_opt_out',
+                            'label' => 'LBL_EMAIL_OPT_OUT',
+                            'type' => 'bool',
+                        ),
                     ),
             ),
     );
