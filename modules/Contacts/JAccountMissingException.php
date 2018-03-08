386f56f272852bcaa699a268636e1d48d9cb64bd
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-03-08 10:43:46 +0000

    set a default message with a default status code

--- modules/Contacts/JAccountMissingException.php
+++ modules/Contacts/JAccountMissingException.php
@@ -1,5 +1,13 @@
 <?php
 
 
-class JAccountMissingException extends Exception {}
+class JAccountMissingException extends Exception {
+  
+    const JACCOUNT_MISSING = 100;
+
+    public function __construct($message = '', $code = JAccountMissingException::JACCOUNT_MISSING, $previous = null)
+    {
+        parent::__construct('[JAccount missing] '.$message.'', $code, $previous);
+    }
+}
 
