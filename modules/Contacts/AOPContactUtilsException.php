579c4010fe410b83c72c8e4177f6be485b2fbcdd
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-03-08 10:41:45 +0000

    set a default message with a default status code

--- modules/Contacts/AOPContactUtilsException.php
+++ modules/Contacts/AOPContactUtilsException.php
@@ -8,5 +8,10 @@ class AOPContactUtilsException extends Exception {
 
     const UNABLE_READ_PORTAL_VERSION = 100;
 
+    public function __construct($message = '', $code = AOPContactUtilsException::UNABLE_READ_PORTAL_VERSION, $previous = null)
+    {
+        parent::__construct('[Unable to Read Portal Version] '.$message.'', $code, $previous);
+    }
+    
 }
 
