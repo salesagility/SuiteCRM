870cedf9372645e4b7b012b0d28aa5eecbc1f4b1
Dillon Brown <dillon.brown@salesagility.com>
2018-03-09 16:32:34 +0000

    Fixed #2049 - 7.7.2 - Calendar Activities are off by 1 day

--- jssource/src_files/modules/Calendar/Cal.js
+++ jssource/src_files/modules/Calendar/Cal.js
@@ -4,7 +4,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2017 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -36,6 +36,7 @@
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
  * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
+
 
 var CAL = {};
 CAL.slot_height = 14;
@@ -661,8 +662,8 @@ CAL.dialog_save = function () {
           newEvent.borderColor = '#' + global_colorList[res.module_name].border;
           newEvent.textColor = '#' + global_colorList[res.module_name].text;
         }
-        newEvent.start = new Date(moment.unix(res['ts_start']).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"));
-        newEvent.end = moment(new Date(moment.unix(res['ts_start']).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"))).add(res['duration_hours'], 'hours').add(res['duration_minutes'], 'minutes');
+        newEvent.start = new Date(moment.utc(moment.unix(res['ts_start'])).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"));
+        newEvent.end = moment(new Date(moment.utc(moment.unix(res['ts_start'])).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"))).add(res['duration_hours'], 'hours').add(res['duration_minutes'], 'minutes');(res['duration_hours'], 'hours').add(res['duration_minutes'], 'minutes');
         if ((res['duration_hours'] % 24 === 0) && (res['time_start'] == "12:00am")) {
           newEvent.allDay = "true";
         }
@@ -675,8 +676,8 @@ CAL.dialog_save = function () {
             newEvent.title = res['name'];
             newEvent.record = value['id'];
             newEvent.id = value['id'];
-            newEvent.start = new Date(moment.unix(value['ts_start']).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"));
-            newEvent.end = moment(new Date(moment.unix(value['ts_start']).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"))).add(res['duration_hours'], 'hours').add(res['duration_minutes'], 'minutes');
+            newEvent.start = new Date(moment.utc(moment.unix(res['ts_start'])).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"));
+            newEvent.end = moment(new Date(moment.utc(moment.unix(res['ts_start'])).format("MM/DD/YYYY") + " " + moment(res['time_start'], 'hh:mma').format("HH:mm"))).add(res['duration_hours'], 'hours').add(res['duration_minutes'], 'minutes');
             if ((res['duration_hours'] % 24 === 0) && (res['time_start'] == "12:00am")) {
               newEvent.allDay = "true";
             }
