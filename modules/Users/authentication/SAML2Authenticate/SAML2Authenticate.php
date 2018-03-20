abb424a6a426428dd75ff0212719c1804ef2cb97
Daniel Samson <daniel.samson@salesagility.com>
2018-03-20 17:30:55 +0000

    Moving SAML onelogin library to composer

--- modules/Users/authentication/SAML2Authenticate/SAML2Authenticate.php
+++ modules/Users/authentication/SAML2Authenticate/SAML2Authenticate.php
@@ -42,7 +42,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-require_once dirname(dirname(__FILE__)).'/SAML2Authenticate/lib/onelogin/php-saml/_toolkit_loader.php';
 
 require_once('modules/Users/authentication/SugarAuthenticate/SugarAuthenticate.php');
 
@@ -90,8 +89,7 @@ class SAML2Authenticate extends SugarAuthenticate {
             }
 
             if (!$auth->isAuthenticated()) {
-                echo "<p>Not authenticated</p>";
-                exit();
+                SugarApplication::redirect($auth->getSSOurl());
             }
 
             $_SESSION['samlUserdata'] = $auth->getAttributes();
@@ -99,14 +97,19 @@ class SAML2Authenticate extends SugarAuthenticate {
             $_SESSION['samlNameIdFormat'] = $auth->getNameIdFormat();
             $_SESSION['samlSessionIndex'] = $auth->getSessionIndex();
             unset($_SESSION['AuthNRequestID']);
-            if (isset($_POST['RelayState']) && OneLogin_Saml2_Utils::getSelfURL() != $_POST['RelayState']) {
-                $this->redirectToLogin($GLOBALS['app']);
+
+            if (isset($_POST['RelayState'])) {
+                $relayStateUrl = $_POST['RelayState'] . '?action=Login&module=Users';
+                $selfurl = OneLogin_Saml2_Utils::getSelfURL();
+                if($selfurl === $relayStateUrl) {
+                    // Authenticate with suitecrm
+                    $this->redirectToLogin($GLOBALS['app']);
+                }
             }
         } else {
             $auth->login();
             exit;
         }
-
     }
 
     /**
