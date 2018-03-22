a08314276703f85fd742e5ab2f52543bf139c8e8
p.ernst <peter.ernst@salesagility.com>
2018-03-22 16:28:33 +0000

    Rebuild JS

--- include/javascript/alerts.js
+++ include/javascript/alerts.js
@@ -46,12 +46,11 @@ else{AlertObj.options.type='info';}}
 var notification=new Notification(AlertObj.title,AlertObj.options);if(typeof AlertObj.options!=="undefined"){if(typeof AlertObj.options.url_redirect!=="undefined"){notification.onclick=function(){window.open(AlertObj.options.url_redirect);}}}}
 else{var message=AlertObj.title;if(typeof AlertObj.options!=="undefined"){if(typeof AlertObj.options.body!=="undefined"){message+='\n'+AlertObj.options.body;}
 message+=SUGAR.language.translate('app_strings','MSG_JS_ALERT_MTG_REMINDER_CALL_MSG')+"\n\n";if(confirm(message)){if(typeof AlertObj.options!=="undefined"){if(typeof AlertObj.options.url_redirect!=="undefined"){window.location=AlertObj.options.url_redirect;}}}}}}}
-Alerts.prototype.addToManager=function(AlertObj){console.log(AlertObj);var url='index.php',name=AlertObj.title,description,url_redirect,is_read=0,target_module,type='info';if(typeof AlertObj.options!=="undefined"){if(typeof AlertObj.options.url_redirect!=="undefined"){url_redirect=AlertObj.options.url_redirect}
+Alerts.prototype.addToManager=function(AlertObj){var url='index.php',name=AlertObj.title,description,url_redirect,is_read=0,target_module,type='info';if(typeof AlertObj.options!=="undefined"){if(typeof AlertObj.options.url_redirect!=="undefined"){url_redirect=AlertObj.options.url_redirect}
 if(typeof AlertObj.options.body!=="undefined"){description=AlertObj.options.body}
 if(typeof AlertObj.options.target_module!=="undefined"){target_module=AlertObj.options.target_module}
 if(typeof AlertObj.options.type!=="undefined"){type=AlertObj.options.type}}
-$.post(url,{module:'Alerts',action:'add',name:name,description:description,url_redirect:url_redirect,is_read:is_read,target_module:target_module,type:type}).done(function(jsonData){data=JSON.parse(jsonData);if(typeof data!=='undefined'&&typeof data.result!=='undefined'&&data.result===1){console.log('showing alert');Alerts.prototype.show(AlertObj);}
-console.log(data);}).fail(function(data){}).always(function(){Alerts.prototype.updateManager();});}
+$.post(url,{module:'Alerts',action:'add',name:name,description:description,url_redirect:url_redirect,is_read:is_read,target_module:target_module,type:type}).done(function(jsonData){data=JSON.parse(jsonData);if(typeof data!=='undefined'&&typeof data.result!=='undefined'&&data.result===1){Alerts.prototype.show(AlertObj);}}).fail(function(data){}).always(function(){Alerts.prototype.updateManager();});}
 Alerts.prototype.redirectToLogin=function(){var getQueryParams=function(qs){qs=qs.split('+').join(' ');var params={},tokens,re=/[?&]?([^=]+)=([^&]*)/g;while(tokens=re.exec(qs)){params[decodeURIComponent(tokens[1])]=decodeURIComponent(tokens[2]);}
 return params;};var params=getQueryParams(document.location.search);if(params.entryPoint!='Changenewpassword'&&params.module!='Users'&&params.action!='Login'){document.location.href='index.php?module=Users&action=Login&loginErrorMessage=LBL_SESSION_EXPIRED';return true;}
 return false;}
