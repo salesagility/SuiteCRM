81a0321fcbbb9a8275cea9556f582172599e5355
Ashley Nicolson <ashjayne.nicolson@gmail.com>
2018-04-03 12:20:17 +0100

    7.10.3 Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version      = '7.10.2';
-$suitecrm_timestamp    = '2018-03-15 17:00';
+$suitecrm_version      = '7.10.3';
+$suitecrm_timestamp    = '2018-04-03 17:00';
