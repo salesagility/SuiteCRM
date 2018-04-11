84762c660509d04aa102044e9bfacedb1e228e6d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-04-11 13:33:23 +0100

    fixing AOR_Scheduled_ReportsTest

--- tests/unit/modules/AOR_Scheduled_Reports/AOR_Scheduled_ReportsTest.php
+++ tests/unit/modules/AOR_Scheduled_Reports/AOR_Scheduled_ReportsTest.php
@@ -11,6 +11,47 @@ class AOR_Scheduled_ReportsTest extends SuiteCRM\StateChecker_PHPUnit_Framework_
         $current_user = new User();
     }
 
+	public function testSaveAndGet_email_recipients(){
+            
+            $state = new SuiteCRM\StateSaver();
+            $state->pushTable('aor_scheduled_reports');
+            $state->pushTable('tracker');
+            $state->pushTable('aod_index');
+            $state->pushGlobals();
+
+		$aorScheduledReports = new AOR_Scheduled_Reports();
+		$aorScheduledReports->name = "test";
+		$aorScheduledReports->description = "test description";
+		$_POST['email_recipients']= Array('email_target_type'=> array('Email Address','all','Specify User')  ,'email' =>array('test@test.com','','1') );
+
+
+		//test save and test for record ID to verify that record is saved
+		$aorScheduledReports->save();
+		$this->assertTrue(isset($aorScheduledReports->id));
+		$this->assertEquals(36, strlen($aorScheduledReports->id));
+
+
+
+		//test get_email_recipients
+		$expected = array('test@test.com','','1');
+		$aorScheduledReports->retrieve($aorScheduledReports->id);
+		$emails = $aorScheduledReports->get_email_recipients();
+
+		$this->assertTrue(is_array($emails));
+		$this->assertEquals('test@test.com',$emails[0]);
+
+
+		$aorScheduledReports->mark_deleted($aorScheduledReports->id);
+		unset($aorScheduledReports);
+
+            // clean up
+                
+            $state->popGlobals();
+            $state->popTable('tracker');
+            $state->popTable('aod_index');
+            $state->popTable('aor_scheduled_reports');
+    }
+    
 	public function testAOR_Scheduled_Reports(){
 
 		//execute the contructor and check for the Object type and  attributes
@@ -47,42 +88,6 @@ class AOR_Scheduled_ReportsTest extends SuiteCRM\StateChecker_PHPUnit_Framework_
 
     }
 
-	public function testSaveAndGet_email_recipients(){
-            
-            $state = new SuiteCRM\StateSaver();
-            $state->pushTable('aor_scheduled_reports');
-            $state->pushGlobals();
-
-		$aorScheduledReports = new AOR_Scheduled_Reports();
-		$aorScheduledReports->name = "test";
-		$aorScheduledReports->description = "test description";
-		$_POST['email_recipients']= Array('email_target_type'=> array('Email Address','all','Specify User')  ,'email' =>array('test@test.com','','1') );
-
-
-		//test save and test for record ID to verify that record is saved
-		$aorScheduledReports->save();
-		$this->assertTrue(isset($aorScheduledReports->id));
-		$this->assertEquals(36, strlen($aorScheduledReports->id));
-
-
-
-		//test get_email_recipients
-		$expected = array('test@test.com','','1');
-		$aorScheduledReports->retrieve($aorScheduledReports->id);
-		$emails = $aorScheduledReports->get_email_recipients();
-
-		$this->assertTrue(is_array($emails));
-		$this->assertEquals('test@test.com',$emails[0]);
-
-
-		$aorScheduledReports->mark_deleted($aorScheduledReports->id);
-		unset($aorScheduledReports);
-
-                // clean up
-                
-                $state->popGlobals();
-                $state->popTable('aor_scheduled_reports');
-    }
 
 
 
