18974506f388faa5b889add06c41a99b7f403781
Daniel Samson <daniel.samson@salesagility.com>
2018-04-27 15:03:49 +0100

    formatting screenshot output

--- build/push_output.sh
+++ build/push_output.sh
@@ -4,11 +4,20 @@ SCRIPT=$(readlink -f $0);
 SCRIPTPATH=`dirname $SCRIPT`;
 ROOTPATH=`dirname $SCRIPTPATH`;
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;36m'
+NC='\033[0m' # No Color
+
 function push_output {
 FILE=$1;
-echo $FILE;
+echo -n "Error: ScreenShot $FILE: open ";
+echo -ne $BLUE;
 curl -F "files[]=@$FILE" https://rokket.space/upload\?output\=text;
-echo " ";
+echo -en $NC;
+echo " in your favorite web browser";
+echo " "
 }
 
 #
