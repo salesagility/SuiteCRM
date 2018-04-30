ed42084dd1a74b45a313b6428bc1b67a330f22c5
Dillon-Brown <dillon.brown@salesagility.com>
2018-04-30 15:47:09 +0100

    Report output test

--- tests/acceptance/modules/AOR_Reports/AOR_ReportsCest.php
+++ tests/acceptance/modules/AOR_Reports/AOR_ReportsCest.php
@@ -19,7 +19,7 @@ class AOR_ReportsCest
      */
     public function _before(AcceptanceTester $I)
     {
-        if(!$this->fakeData) {
+        if (!$this->fakeData) {
             $this->fakeData = Faker\Factory::create();
             $this->fakeDataSeed = rand(0, 2048);
         }
@@ -34,18 +34,19 @@ class AOR_ReportsCest
 
     }
 
-    // Tests
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
+     * @param \Step\Acceptance\Reports $reports
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As an administrator I want to view the reports module.
      */
     public function testScenarioViewReportsModule(
-       \AcceptanceTester $I,
-       \Step\Acceptance\NavigationBar $navigationBar,
-       \Helper\WebDriverHelper $webDriverHelper
+        \AcceptanceTester $I,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Reports $reports,
+        \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('View the reports module for testing');
 
@@ -55,18 +56,19 @@ class AOR_ReportsCest
 
         // Navigate to reports list-view
         $I->loginAsAdmin();
-        $navigationBar->clickAllMenuItem('Reports');
+        $listView->waitForListViewVisible();
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
 
         $I->see('Reports', '.module-title-text');
     }
 
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
-     * @param \Step\Acceptance\ListView $listView
      * @param \Step\Acceptance\EditView $editView
-     * @param \Step\Acceptance\DetailView $detailView,
-     * @param \Step\Acceptance\Step\Acceptance\SideBar $sidebar,
+     * @param \Step\Acceptance\DetailView $detailView ,
+     * @param \Step\Acceptance\Step\Acceptance\SideBar $sidebar ,
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As administrative user I want to create a report with the reports module so that I can test
@@ -74,11 +76,10 @@ class AOR_ReportsCest
      */
     public function testScenarioCreateReport(
         \AcceptanceTester $I,
-        \Step\Acceptance\NavigationBar $navigationBar,
-        \Step\Acceptance\ListView $listView,
         \Step\Acceptance\EditView $editView,
         \Step\Acceptance\DetailView $detailView,
         \Step\Acceptance\SideBar $sidebar,
+        \Step\Acceptance\Reports $reports,
         \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('Create a Report');
@@ -90,7 +91,8 @@ class AOR_ReportsCest
         // Navigate to reports list-view
         $I->loginAsAdmin();
         $listView->waitForListViewVisible();
-        $navigationBar->clickAllMenuItem('Reports');
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
 
         // Select create report from sidebar
         $I->see('Create Report', '.actionmenulink');
@@ -106,18 +108,18 @@ class AOR_ReportsCest
 
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
      * @param \Step\Acceptance\ListView $listView
-     * @param \Step\Acceptance\DetailView $detailView,
+     * @param \Step\Acceptance\DetailView $detailView ,
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As administrative user I want to view the report by selecting it in the list view
      */
     public function testScenarioViewReportFromListView(
         \AcceptanceTester $I,
-        \Step\Acceptance\NavigationBar $navigationBar,
         \Step\Acceptance\ListView $listView,
         \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\Reports $reports,
         \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('Select Report from list view');
@@ -129,7 +131,8 @@ class AOR_ReportsCest
         // Navigate to reports list-view
         $I->loginAsAdmin();
         $listView->waitForListViewVisible();
-        $navigationBar->clickAllMenuItem('Reports');
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
 
         $listView->clickNameLink('Report_Test');
         $detailView->waitForDetailViewVisible();
@@ -137,20 +140,20 @@ class AOR_ReportsCest
 
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
      * @param \Step\Acceptance\ListView $listView
      * @param \Step\Acceptance\DetailView $detailView
      * @param \Step\Acceptance\EditView $editView
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As administrative user I want to edit the record by selecting it in the detail view
      */
     public function testScenarioEditReportFromDetailView(
-        \AcceptanceTester$I,
-        \Step\Acceptance\NavigationBar $navigationBar,
+        \AcceptanceTester $I,
         \Step\Acceptance\ListView $listView,
         \Step\Acceptance\DetailView $detailView,
         \Step\Acceptance\EditView $editView,
+        \Step\Acceptance\Reports $reports,
         \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('Edit a Report from the detail view');
@@ -159,10 +162,11 @@ class AOR_ReportsCest
             $webDriverHelper->getInstanceURL()
         );
 
-        // Navigate to reports list-view
+        // Navigate to a report
         $I->loginAsAdmin();
         $listView->waitForListViewVisible();
-        $navigationBar->clickAllMenuItem('Reports');
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
         $listView->clickNameLink('Report_Test');
         $detailView->waitForDetailViewVisible();
 
@@ -177,20 +181,20 @@ class AOR_ReportsCest
 
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
      * @param \Step\Acceptance\ListView $listView
      * @param \Step\Acceptance\DetailView $detailView
      * @param \Step\Acceptance\EditView $editView
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As administrative user I want to duplicate the report
      */
     public function testScenarioDuplicateReportFromDetailView(
         \AcceptanceTester $I,
-        \Step\Acceptance\NavigationBar $navigationBar,
         \Step\Acceptance\ListView $listView,
         \Step\Acceptance\DetailView $detailView,
         \Step\Acceptance\EditView $editView,
+        \Step\Acceptance\Reports $reports,
         \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('Duplicate Report from detail view');
@@ -199,10 +203,11 @@ class AOR_ReportsCest
             $webDriverHelper->getInstanceURL()
         );
 
-        // Navigate to reports list-view
+        // Navigate to a report
         $I->loginAsAdmin();
         $listView->waitForListViewVisible();
-        $navigationBar->clickAllMenuItem('Reports');
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
         $listView->clickNameLink('Report_Test');
         $detailView->waitForDetailViewVisible();
 
@@ -222,26 +227,31 @@ class AOR_ReportsCest
 
     /**
      * @param \AcceptanceTester $I
-     * @param \Step\Acceptance\NavigationBar $navigationBar
      * @param \Step\Acceptance\ListView $listView
      * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\Reports $reports
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
      * As administrative user I want to delete the report by selecting it in the detail view
      */
     public function testScenarioDeleteRecordFromDetailView(
         \AcceptanceTester $I,
-        \Step\Acceptance\NavigationBar $navigationBar,
         \Step\Acceptance\ListView $listView,
         \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\Reports $reports,
         \Helper\WebDriverHelper $webDriverHelper
     ) {
         $I->wantTo('Delete Report from detail view');
 
-        // Navigate to reports list-view
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to a report
         $I->loginAsAdmin();
         $listView->waitForListViewVisible();
-        $navigationBar->clickAllMenuItem('Reports');
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
         $listView->clickNameLink('Report_Test');
         $detailView->waitForDetailViewVisible();
 
@@ -250,5 +260,75 @@ class AOR_ReportsCest
         $detailView->acceptPopup();
 
         $listView->waitForListViewVisible();
+    }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\SideBar $sidebar
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\EditView $editView
+     * @param \Step\Acceptance\NavigationBar $navigationBar,
+     * @param \Step\Acceptance\Reports $reports
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to verify the output of a report using text fields
+     */
+    public function testScenarioTextFieldReportOutput(
+        \AcceptanceTester $I,
+        \Step\Acceptance\SideBar $sidebar,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\EditView $editView,
+        \Step\Acceptance\NavigationBar $navigationBar,
+        \Step\Acceptance\Reports $reports,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Verify the output of a report based on text fields');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to Accounts
+        $I->loginAsAdmin();
+        $listView->waitForListViewVisible();
+        $navigationBar->clickAllMenuItem('Accounts');
+        $listView->waitForListViewVisible();
+
+        // Create Account
+        $I->see('Create Account', '.actionmenulink');
+        $sidebar->clickSideBarAction('Create');
+        $editView->waitForEditViewVisible();
+        $editView->fillField('#name', 'Test_Account');
+        $editView->clickSaveButton();
+        $detailView->waitForDetailViewVisible();
+
+        // Navigate to reports list-view
+        $reports->gotoReports();
+        $listView->waitForListViewVisible();
+
+        // Select create report from sidebar
+        $I->see('Create Report', '.actionmenulink');
+        $sidebar->clickSideBarAction('Create');
+
+        // Create a report
+        $editView->waitForEditViewVisible();
+        $editView->fillField('#name', 'Report_Test_Text');
+        $editView->fillField('#report_module', 'Accounts');
+
+        // Add field
+        $editView->click('Fields', 'tab-toggler');
+        $editView->click('Accounts', 'jqtree_common jqtree-title jqtree-title-folder');
+        $editView->click('Name', 'jqtree-title jqtree_common');
+
+        // Add condition
+        $editView->click('Conditions', 'tab-toggler');
+        $editView->click('Accounts', 'jqtree_common jqtree-title jqtree-title-folder');
+        $editView->click('Name', 'jqtree-title jqtree_common');
+        $editView->fillField('#aor_conditions_value[0]', 'Test_Account');
+        $editView->clickSaveButton();
+        $detailView->waitForDetailViewVisible();
+
+        // Check Output
+        $I->see('Test_Account', '.sugar_field');
     }
 }
