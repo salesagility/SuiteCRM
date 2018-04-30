ab73b98531258593372f9249b2469ebeb579a526
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-04-30 10:29:24 +0100

    fixing view detail test

--- tests/tests/include/MVC/View/views/view.detailTest.php
+++ tests/tests/include/MVC/View/views/view.detailTest.php
@@ -4,15 +4,35 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 {
     public function testViewDetail()
     {
+        
+        // save state
+        
+        $state = new SuiteCRM\StateSaver();
+        $state->pushGlobals();
+        
+        // test 
+        
         //execute the contructor and check for the Object type and type attribute
         $view = new ViewDetail();
         $this->assertInstanceOf('ViewDetail', $view);
         $this->assertInstanceOf('SugarView', $view);
         $this->assertAttributeEquals('detail', 'type', $view);
+        
+        // clean up
+        
+        $state->popGlobals();
     }
 
     public function testpreDisplay()
     {
+        
+        // save state
+        
+        $state = new SuiteCRM\StateSaver();
+        $state->pushGlobals();
+        
+        // test 
+        
         //execute the method with required attributes preset, it will initialize the dv(detail view) attribute. 
         $view = new ViewDetail();
         $view->module = 'Users';
@@ -30,10 +50,21 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $view->preDisplay();
         $this->assertInstanceOf('DetailView2', $view->dv);
         $this->asserttrue(is_array($view->dv->defs));
+        
+        // clean up
+        
+        $state->popGlobals();
     }
 
     public function testdisplay()
     {
+        
+        // save state
+        
+        $state = new SuiteCRM\StateSaver();
+        $state->pushGlobals();
+        
+        // test 
 
         //execute the method with essential parameters set. it should return some html.
         $view = new ViewDetail();
@@ -48,5 +79,9 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $renderedContent = ob_get_contents();
         ob_end_clean();
         $this->assertGreaterThan(0, strlen($renderedContent));
+        
+        // clean up
+        
+        $state->popGlobals();
     }
 }
