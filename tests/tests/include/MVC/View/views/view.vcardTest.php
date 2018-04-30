6f314e0efc9eab46afa74ddd4b6b3fce738eaf08
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-04-30 12:33:16 +0100

    fixing output of view vcard test

--- tests/tests/include/MVC/View/views/view.vcardTest.php
+++ tests/tests/include/MVC/View/views/view.vcardTest.php
@@ -12,11 +12,29 @@ class ViewVcardTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $view->bean = new Contact();
 
         //execute the method and test if it works and does not throws an exception other than headers output exception.
+        
+        ob_start();
+        
         try {
             $view->display();
         } catch (Exception $e) {
             $this->assertStringStartsWith('Cannot modify header information', $e->getMessage());
         }
+        
+        $contents = ob_get_clean();
+        $this->assertEquals($contents, 'BEGIN:VCARD
+N;CHARSET=utf-8:;;;
+FN;CHARSET=utf-8:  
+TEL;WORK;FAX:
+TEL;HOME:
+TEL;CELL:
+TEL;WORK:
+EMAIL;INTERNET:
+ADR;WORK;CHARSET=utf-8:;;;;;;
+ORG;CHARSET=utf-8:;
+TITLE:
+END:VCARD
+');
 
         $this->assertInstanceOf('ViewVcard', $view);
         $this->assertInstanceOf('SugarView', $view);
