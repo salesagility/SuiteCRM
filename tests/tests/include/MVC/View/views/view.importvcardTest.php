d7e924353e7bebe598b304c603552c2abb6c9b79
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-04-30 10:29:47 +0100

    fixing view import vcard test

--- tests/tests/include/MVC/View/views/view.importvcardTest.php
+++ tests/tests/include/MVC/View/views/view.importvcardTest.php
@@ -4,15 +4,34 @@ class ViewImportvcardTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 {
     public function test__construct()
     {
+        
+        // save state
+        
+        $state = new SuiteCRM\StateSaver();
+        $state->pushGlobals();
+        
+        // test 
+        
         //execute the contructor and check for the Object type and type attribute
         $view = new ViewImportvcard();
         $this->assertInstanceOf('ViewImportvcard', $view);
         $this->assertInstanceOf('SugarView', $view);
         $this->assertAttributeEquals('edit', 'type', $view);
+        
+        // clean up
+        
+        $state->popGlobals();
     }
 
     public function testdisplay()
     {
+        // save state
+        
+        $state = new SuiteCRM\StateSaver();
+        $state->pushGlobals();
+        
+        // test 
+        
 
         //execute the method with essential parameters set. it should return some html.
         $view = new ViewImportvcard();
@@ -24,5 +43,9 @@ class ViewImportvcardTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $renderedContent = ob_get_contents();
         ob_end_clean();
         $this->assertGreaterThan(0, strlen($renderedContent));
+        
+        // clean up
+        
+        $state->popGlobals();
     }
 }
