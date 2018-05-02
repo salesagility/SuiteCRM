c48a45125f01962f8727bbcbd4c85de729dc095f
Daniel Samson <daniel.samson@salesagility.com>
2018-05-02 13:41:52 +0100

    reverting change

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -698,7 +698,7 @@ class SugarBean
      *
      *  Internal function, do not override.
      */
-    public static function createRelationshipMeta($key, $db, $tablename, $dictionary, $module_dir, $is_custom = false)
+    public static function createRelationshipMeta($key, $db, $tablename, array $dictionary, $module_dir, $is_custom = false)
     {
         //load the module dictionary if not supplied.
         if (empty($dictionary) && !empty($module_dir)) {
@@ -716,11 +716,11 @@ class SugarBean
             }
 
             if (file_exists($filename)) {
+                include($filename);
                 // cn: bug 7679 - dictionary entries defined as $GLOBALS['name'] not found
                 if (empty($dictionary) || !empty($GLOBALS['dictionary'][$key])) {
                     $dictionary = $GLOBALS['dictionary'];
                 }
-                include($filename);
             } else {
                 $GLOBALS['log']->debug('createRelationshipMeta: no metadata file found' . $filename);
 
