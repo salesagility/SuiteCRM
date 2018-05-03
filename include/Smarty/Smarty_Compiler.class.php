56f86a75faf0788ad0b784161f3ef2152c352c68
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-05-03 10:35:17 +0100

    smarty upgrade

--- include/Smarty/Smarty_Compiler.class.php
+++ include/Smarty/Smarty_Compiler.class.php
@@ -21,7 +21,7 @@
  * @link http://smarty.php.net/
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
- * @version 2.6.29
+ * @version 2.6.25-dev
  * @copyright 2001-2005 New Digital Group, Inc.
  * @package Smarty
  */
@@ -259,14 +259,9 @@ class Smarty_Compiler extends Smarty {
 
         preg_match_all($search, $source_content, $match,  PREG_SET_ORDER);
         $this->_folded_blocks = $match;
-        reset($this->_folded_blocks);
 
         /* replace special blocks by "{php}" */
-        $source_content = preg_replace_callback($search, create_function ('$matches', "return '"
-                                       . $this->_quote_replace($this->left_delimiter) . 'php'
-                                       . "' . str_repeat(\"\n\", substr_count('\$matches[1]', \"\n\")) .'"
-                                       . $this->_quote_replace($this->right_delimiter)
-                                       . "';")
+        $source_content = preg_replace_callback($search, array($this,'_preg_callback')
                                        , $source_content);
 
         /* Gather all template tags. */
@@ -556,7 +551,7 @@ class Smarty_Compiler extends Smarty {
 
             case 'php':
                 /* handle folded tags replaced by {php} */
-                list(, $block) = each($this->_folded_blocks);
+                $block = array_shift($this->_folded_blocks);
                 $this->_current_line_no += substr_count($block[0], "\n");
                 /* the number of matched elements in the regexp in _compile_file()
                    determins the type of folded tag that was found */
@@ -754,7 +749,12 @@ class Smarty_Compiler extends Smarty {
         return true;
     }
 
-
+    function _preg_callback ($matches) {
+    return $this->_quote_replace($this->left_delimiter)
+           . 'php'
+           . str_repeat("\n", substr_count($matches[1], "\n"))
+           . $this->_quote_replace($this->right_delimiter);
+    }
     /**
      * compile custom function tag
      *
@@ -975,7 +975,6 @@ class Smarty_Compiler extends Smarty {
             $this->_syntax_error("missing 'file' attribute in include tag", E_USER_ERROR, __FILE__, __LINE__);
         }
 
-        $theme_template = 'false';
         foreach ($attrs as $arg_name => $arg_value) {
             if ($arg_name == 'file') {
                 $include_file = $arg_value;
@@ -983,17 +982,11 @@ class Smarty_Compiler extends Smarty {
             } else if ($arg_name == 'assign') {
                 $assign_var = $arg_value;
                 continue;
-            } else if ($arg_name == 'theme_template') {
-                $theme_template = $arg_value;
-                continue;
             }
             if (is_bool($arg_value))
                 $arg_value = $arg_value ? 'true' : 'false';
             $arg_list[] = "'$arg_name' => $arg_value";
         }
-
-        if ( $theme_template == 'true' )
-            $include_file = '"'.SugarThemeRegistry::current()->getTemplate(str_replace(array('"',"'"),'',$include_file)).'"';
 
         $output = '<?php ';
 
