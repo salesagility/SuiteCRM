09bc2b1cc81b4a94fdfa3d67911c983704ee1e73
p.ernst <peter.ernst@salesagility.com>
2018-05-08 11:20:44 +0100

    Indentation

--- modules/AOW_Actions/actions/templateParser.php
+++ modules/AOW_Actions/actions/templateParser.php
@@ -37,13 +37,13 @@ class aowTemplateParser extends templateParser {
 
 				$focus = BeanFactory::getBean($bean_name, $bean_id);
 
-        if (!$focus->fetched_row) {
+            if (!$focus->fetched_row) {
 
-            // We do not want the cached version for a newly created bean, as some data such as date fields and
-            // auto increment fields will only be correct after a retrieve operation
-            BeanFactory::unregisterBean($bean_name, $bean_id);
-            $focus = BeanFactory::getBean($bean_name, $bean_id);
-        }
+                // We do not want the cached version for a newly created bean, as some data such as date fields and
+                // auto increment fields will only be correct after a retrieve operation
+                BeanFactory::unregisterBean($bean_name, $bean_id);
+                $focus = BeanFactory::getBean($bean_name, $bean_id);
+            }
 
 				$string = aowTemplateParser::parse_template_bean($string, strtolower($beanList[$bean_name]), $focus);
 
