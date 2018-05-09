bc150fc75c7b8ab83499054650a2877a144c2bd6
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-05-09 18:02:38 +0100

    fixing ContactTest

--- tests/tests/modules/Contacts/ContactTest.php
+++ tests/tests/modules/Contacts/ContactTest.php
@@ -119,7 +119,7 @@ class ContactTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract {
                                 contacts.*,
                                 email_addresses.email_address email_address,
                                 '' email_addresses_non_primary, accounts.name as account_name,
-                                users.user_name as assigned_user_name ,contacts_cstm.jjwg_maps_lat_c,contacts_cstm.jjwg_maps_address_c,contacts_cstm.jjwg_maps_geocode_status_c,contacts_cstm.jjwg_maps_lng_c FROM contacts LEFT JOIN users
+                                users.user_name as assigned_user_name ,contacts_cstm.jjwg_maps_address_c,contacts_cstm.jjwg_maps_geocode_status_c,contacts_cstm.jjwg_maps_lat_c,contacts_cstm.jjwg_maps_lng_c FROM contacts LEFT JOIN users
 	                                ON contacts.assigned_user_id=users.id LEFT JOIN accounts_contacts
 	                                ON ( contacts.id=accounts_contacts.contact_id and (accounts_contacts.deleted is null or accounts_contacts.deleted = 0))
 	                                LEFT JOIN accounts
@@ -134,12 +134,12 @@ class ContactTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract {
                                 contacts.*,
                                 email_addresses.email_address email_address,
                                 '' email_addresses_non_primary, accounts.name as account_name,
-                                users.user_name as assigned_user_name ,contacts_cstm.jjwg_maps_lat_c,contacts_cstm.jjwg_maps_address_c,contacts_cstm.jjwg_maps_geocode_status_c,contacts_cstm.jjwg_maps_lng_c FROM contacts LEFT JOIN users
+                                users.user_name as assigned_user_name ,contacts_cstm.jjwg_maps_address_c,contacts_cstm.jjwg_maps_geocode_status_c,contacts_cstm.jjwg_maps_lat_c,contacts_cstm.jjwg_maps_lng_c FROM contacts LEFT JOIN users
 	                                ON contacts.assigned_user_id=users.id LEFT JOIN accounts_contacts
 	                                ON ( contacts.id=accounts_contacts.contact_id and (accounts_contacts.deleted is null or accounts_contacts.deleted = 0))
 	                                LEFT JOIN accounts
 	                                ON accounts_contacts.account_id=accounts.id  LEFT JOIN  email_addr_bean_rel on contacts.id = email_addr_bean_rel.bean_id and email_addr_bean_rel.bean_module='Contacts' and email_addr_bean_rel.deleted=0 and email_addr_bean_rel.primary_address=1  LEFT JOIN email_addresses on email_addresses.id = email_addr_bean_rel.email_address_id  LEFT JOIN contacts_cstm ON contacts.id = contacts_cstm.id_c where (contacts.name=\"\") AND ( accounts.deleted IS NULL OR accounts.deleted=0 )
-                       AND contacts.deleted=0 ";
+                      AND contacts.deleted=0 ";
 		$actual = $contact->create_export_query('contacts.id','contacts.name=""');
 		$this->assertSame($expected,$actual);
 
