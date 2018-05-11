88e3cd02ebd9c3cc255862e6ae1c49b4eaa68552
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-05-11 17:10:02 +0100

    StateSaver: show table if it is not pushed

--- include/StateSaver.php
+++ include/StateSaver.php
@@ -328,7 +328,7 @@ class StateSaver
         DBManagerFactory::getInstance()->query("TRUNCATE TABLE " . DBManagerFactory::getInstance()->quote($table));
         
         if(!is_array($rows)) {
-            throw new StateSaverException('Table information is not an array. Are you sure you pushed this table previously?');
+            throw new StateSaverException('Table information is not an array. Are you sure you pushed this table "' . $table . '" previously?');
         }
         foreach ($rows as $row) {
             $query = "INSERT INTO $table (";
