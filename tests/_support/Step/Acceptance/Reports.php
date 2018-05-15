53ea71cf80ee9cfc941fc549b9378a095b080f6b
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-15 13:23:27 +0100

    Fix JS

--- tests/_support/Step/Acceptance/Reports.php
+++ tests/_support/Step/Acceptance/Reports.php
@@ -67,7 +67,108 @@ class Reports extends \AcceptanceTester
     {
         $I = new EditView($this->getScenario());
         $I->executeJS('var node = $(\'span.jqtree_common.jqtree-title.jqtree-title-folder\').closest(\'li.jqtree_common\').data(\'node\');
-$(\'#fieldTree\').tree(\'addToSelection\', node);');
+
+var module = "Accounts";
+var module_name = "Accounts";
+var module_path_display = "Accounts";
+
+        $.getJSON(\'index.php\',
+          {
+            \'module\': \'AOR_Reports\',
+            \'action\': \'getModuleFields\',
+            \'aor_module\': node.module,
+            \'view\': \'JSON\'
+          },
+          function (fieldData) {
+            var treeData = [];
+
+            for (var field in fieldData) {
+              if (field) {
+                treeData.push(
+                  {
+                    id: field,
+                    label: fieldData[field],
+                    \'load_on_demand\': false,
+                    type: \'field\',
+                    module: node.module,
+                    module_path: node.module_path,
+                    module_path_display: node.module_path_display
+                  });
+              }
+            }
+            var treeDataLeafs = [];
+            treeDataLeafs[module] = treeData;
+
+                var showTreeDataLeafs = function(treeDataLeafs, module, module_name, module_path_display) {
+      if (typeof module_name == \'undefined\' || !module_name) {
+        module_name = module;
+      }
+      if (typeof module_path_display == \'undefined\' || !module_path_display) {
+        module_path_display = module_name;
+      }
+      $(\'#module-name\').html(\'(<span title="\' + module_path_display + \'">\' + module_name + \'</span>)\');
+      $(\'#fieldTreeLeafs\').remove();
+      $(\'#detailpanel_fields_select\').append(
+        \'<div id="fieldTreeLeafs" class="dragbox aor_dragbox" title="\'
+        + SUGAR.language.translate(\'AOR_Reports\', \'LBL_TOOLTIP_DRAG_DROP_ELEMS\')
+        + \'"></div>\'
+      );
+      $(\'#fieldTreeLeafs\').tree({
+        data: treeDataLeafs,
+        dragAndDrop: true,
+        selectable: true,
+        onCanSelectNode: function(node) {
+          if($(\'#report-editview-footer .toggle-detailpanel_fields\').hasClass(\'active\')) {
+            dropFieldLine(node);
+          }
+          else if($(\'#report-editview-footer .toggle-detailpanel_conditions\').hasClass(\'active\')) {
+            dropConditionLine(node);
+          }
+        },
+        onDragMove: function() {
+          $(\'.drop-area\').addClass(\'highlighted\');
+        },
+        onDragStop: function(node, e,thing){
+          $(\'.drop-area\').removeClass(\'highlighted\');
+          var target = $(document.elementFromPoint(e.pageX - window.pageXOffset, e.pageY - window.pageYOffset));
+          if(node.type != \'field\'){
+            return;
+          }
+          if(target.closest(\'#fieldLines\').length > 0){
+            dropFieldLine(node);
+          }else if(target.closest(\'#aor_conditionLines\').length > 0){
+            var conditionLineTarget = ConditionOrderHandler.getConditionLineByPageEvent(e);
+            var conditionLineNew = dropConditionLine(node);
+            if(conditionLineTarget) {
+              ConditionOrderHandler.putPositionedConditionLines(conditionLineTarget, conditionLineNew);
+              ConditionOrderHandler.setConditionOrders();
+            }
+            ParenthesisHandler.addParenthesisLineIdent();
+          }
+          else if(target.closest(\'.tab-toggler\').length > 0) {
+            target.closest(\'.tab-toggler\').click();
+            if(target.closest(\'.tab-toggler\').hasClass(\'toggle-detailpanel_fields\')) {
+              dropFieldLine(node);
+            }
+            else if (target.closest(\'.tab-toggler\').hasClass(\'toggle-detailpanel_conditions\')) {
+              dropConditionLine(node);
+            }
+          }
+
+        },
+        onCanMoveTo: function(){
+          return false;
+        }
+      });
+    };
+    
+            showTreeDataLeafs(treeDataLeafs[module], module, module_name, module_path_display);
+          }
+        );
+
+$(\'.jqtree-selected\').removeClass(\'jqtree-selected\');
+$(\'#fieldTree\').tree(\'addToSelection\', node);
+');
         $I->click($module, '.jqtree_common jqtree-title jqtree-title-folder');
         $I->click($name, '.jqtree-title jqtree_common');
     }
