4a40a4803bf59c2503283aa9a269ac31a8639c1d
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-22 03:50:20 +0100

    Create Contracts test

--- tests/acceptance/modules/Contracts/ContractsCest.php
+++ tests/acceptance/modules/Contracts/ContractsCest.php
@@ -54,4 +54,66 @@ class ContractsCest
 
         $I->see('Contracts', '.module-title-text');
     }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\ListView $listView
+     * @param \Step\Acceptance\Contracts $contract
+     * @param \Step\Acceptance\Accounts $account
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to create a contract so that I can test
+     * the standard fields.
+     */
+    public function testScenarioCreateContract(
+        \AcceptanceTester $I,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Contracts $contract,
+        \Step\Acceptance\Accounts $account,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Create a Contract');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to accounts list-view
+        $I->loginAsAdmin();
+        $account->gotoAccounts();
+        $listView->waitForListViewVisible();
+
+        // Create account
+        $this->fakeData->seed($this->fakeDataSeed);
+        $account_name = 'Test_'. $this->fakeData->company();
+        $account->createAccount($account_name);
+
+        // Navigate to contracts list-view
+        $contract->gotoContracts();
+        $listView->waitForListViewVisible();
+
+        // Create contract
+        $this->fakeData->seed($this->fakeDataSeed);
+        $contract->createContract('Test_'. $this->fakeData->company(), $account_name);
+
+        // Delete contract
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+
+        // Delete account
+        $account->gotoAccounts();
+        $listView->waitForListViewVisible();
+        $listView->clickFilterButton();
+        $I->fillField('#name_advanced', $account_name);
+        $I->click('#search_form_submit_advanced');
+        $listView->waitForListViewVisible();
+        $I->click($account_name);
+        $detailView->waitForDetailViewVisible();
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+    }
 }
