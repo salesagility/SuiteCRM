5a773d33fe66b7e10ffb7f9428fee7eb13d20537
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-22 10:43:36 +0100

    Create Events test

--- tests/acceptance/modules/FP_Events/FP_EventsCest.php
+++ tests/acceptance/modules/FP_Events/FP_EventsCest.php
@@ -54,4 +54,66 @@ class EventsCest
 
         $I->see('Events', '.module-title-text');
     }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\ListView $listView
+     * @param \Step\Acceptance\Events $event
+     * @param \Step\Acceptance\Locations $location
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to create an event so that I can test
+     * the standard fields.
+     */
+    public function testScenarioCreateEvent(
+        \AcceptanceTester $I,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Events $event,
+        \Step\Acceptance\Locations $location,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Create an Event');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to locations list-view
+        $I->loginAsAdmin();
+        $location->gotoLocations();
+        $listView->waitForListViewVisible();
+
+        // Create location
+        $this->fakeData->seed($this->fakeDataSeed);
+        $location_name = 'Test_'. $this->fakeData->company();
+        $location->createEventLocation($location_name);
+
+        // Navigate to events list-view
+        $event->gotoEvents();
+        $listView->waitForListViewVisible();
+
+        // Create event
+        $this->fakeData->seed($this->fakeDataSeed);
+        $event->createEvent('Test_'. $this->fakeData->company(), $location_name);
+
+        // Delete event
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+
+        // Delete location
+        $location->gotoLocations();
+        $listView->waitForListViewVisible();
+        $listView->clickFilterButton();
+        $I->fillField('#name_basic', $location_name);
+        $I->click('#search_form_submit');
+        $listView->waitForListViewVisible();
+        $I->click($location_name);
+        $detailView->waitForDetailViewVisible();
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+    }
 }
