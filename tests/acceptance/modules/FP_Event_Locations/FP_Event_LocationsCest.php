ee8462aec1432cb0738bfd44d18b1d5827463a15
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-22 10:29:37 +0100

    Create Location test

--- tests/acceptance/modules/FP_Event_Locations/FP_Event_LocationsCest.php
+++ tests/acceptance/modules/FP_Event_Locations/FP_Event_LocationsCest.php
@@ -54,4 +54,42 @@ class LocationsCest
 
         $I->see('Locations', '.module-title-text');
     }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\ListView $listView
+     * @param \Step\Acceptance\Locations $location
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to create a locations with the so that I can test
+     * the standard fields.
+     */
+    public function testScenarioCreateEventLocation(
+        \AcceptanceTester $I,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Locations $location,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Create a Location');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to locations list-view
+        $I->loginAsAdmin();
+        $location->gotoLocations();
+        $listView->waitForListViewVisible();
+
+        // Create location
+        $this->fakeData->seed($this->fakeDataSeed);
+        $location->createEventLocation('Test_'. $this->fakeData->company());
+
+        // Delete location
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+    }
 }
