de29c21f787c36296e994593e469488ffb61d708
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-22 16:01:30 +0100

    Create maps area test

--- tests/_support/Step/Acceptance/MapsAreas.php
+++ tests/_support/Step/Acceptance/MapsAreas.php
@@ -12,4 +12,32 @@ class MapsAreas extends \AcceptanceTester
         $I = new NavigationBar($this->getScenario());
         $I->clickAllMenuItem('Maps - Areas');
     }
+
+    /**
+     * Create maps area
+     *
+     * @param $name
+     */
+    public function createMapsArea($name)
+    {
+        $I = new EditView($this->getScenario());
+        $ListView = new ListView($this->getScenario());
+        $Sidebar = new SideBar($this->getScenario());
+        $faker = $this->getFaker();
+
+        $I->see('Create Areas', '.actionmenulink');
+        $Sidebar->clickSideBarAction('Create');
+        $I->waitForEditViewVisible();
+        $I->fillField('#name', $name);
+        $I->fillField('#city', $faker->city());
+        $I->fillField('#state', $faker->city());
+        $I->fillField('#country', $faker->country());
+        $I->fillField('#description', $faker->text());
+        $I->fillField('#coordinates', $faker->latitude() . ' ' . $faker->longitude());
+
+        $I->seeElement('#assigned_user_name');
+
+        $I->clickSaveButton();
+        $ListView->waitForListViewVisible();
+    }
 }
