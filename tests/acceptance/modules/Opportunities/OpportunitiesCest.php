29f966d550de62111474d5d6b207fb8895b38945
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-23 09:33:24 +0100

    Create opportunities test

--- tests/acceptance/modules/Opportunities/OpportunitiesCest.php
+++ tests/acceptance/modules/Opportunities/OpportunitiesCest.php
@@ -54,4 +54,66 @@ class OpportunitiesCest
 
         $I->see('Opportunities', '.module-title-text');
     }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\ListView $listView
+     * @param \Step\Acceptance\Opportunities $opportunities
+     * @param \Step\Acceptance\Accounts $account
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to create an opportunity so that I can test
+     * the standard fields.
+     */
+    public function testScenarioCreateOpportunity(
+        \AcceptanceTester $I,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Opportunities $opportunities,
+        \Step\Acceptance\Accounts $account,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Create an opportunity');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to accounts list-view
+        $I->loginAsAdmin();
+        $account->gotoAccounts();
+        $listView->waitForListViewVisible();
+
+        // Create account
+        $this->fakeData->seed($this->fakeDataSeed);
+        $account_name = 'Test_'. $this->fakeData->company();
+        $account->createAccount($account_name);
+
+        // Navigate to opportunities list-view
+        $opportunities->gotoOpportunities();
+        $listView->waitForListViewVisible();
+
+        // Create opportunity
+        $this->fakeData->seed($this->fakeDataSeed);
+        $opportunities->createOpportunity('Test_'. $this->fakeData->company(), $account_name);
+
+        // Delete opportunity
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+
+        // Delete account
+        $account->gotoAccounts();
+        $listView->waitForListViewVisible();
+        $listView->clickFilterButton();
+        $I->fillField('#name_basic', $account_name);
+        $I->click('#search_form_submit');
+        $listView->waitForListViewVisible();
+        $listView->clickNameLink($account_name);
+        $detailView->waitForDetailViewVisible();
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+    }
 }
