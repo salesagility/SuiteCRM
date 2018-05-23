3ae9e8389138430cccba5283ad24434ea51f2f7e
Dillon-Brown <dillon.brown@salesagility.com>
2018-05-23 08:51:18 +0100

    Create leads test

--- tests/acceptance/modules/Leads/LeadsCest.php
+++ tests/acceptance/modules/Leads/LeadsCest.php
@@ -54,4 +54,42 @@ class LeadsCest
 
         $I->see('Leads', '.module-title-text');
     }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param \Step\Acceptance\DetailView $detailView
+     * @param \Step\Acceptance\ListView $listView
+     * @param \Step\Acceptance\Leads $lead
+     * @param \Helper\WebDriverHelper $webDriverHelper
+     *
+     * As administrative user I want to create a laed so that I can test
+     * the standard fields.
+     */
+    public function testScenarioCreateLead(
+        \AcceptanceTester $I,
+        \Step\Acceptance\DetailView $detailView,
+        \Step\Acceptance\ListView $listView,
+        \Step\Acceptance\Leads $lead,
+        \Helper\WebDriverHelper $webDriverHelper
+    ) {
+        $I->wantTo('Create a Lead');
+
+        $I->amOnUrl(
+            $webDriverHelper->getInstanceURL()
+        );
+
+        // Navigate to leads list-view
+        $I->loginAsAdmin();
+        $lead->gotoLeads();
+        $listView->waitForListViewVisible();
+
+        // Create lead
+        $this->fakeData->seed($this->fakeDataSeed);
+        $lead->createLead('Test_'. $this->fakeData->company());
+
+        // Delete lead
+        $detailView->clickActionMenuItem('Delete');
+        $detailView->acceptPopup();
+        $listView->waitForListViewVisible();
+    }
 }
