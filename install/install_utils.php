5fe26da54d7bd51cdcb1e0dccbc3339e2881733b
zoltan kocsardi <zoltan.kocsardi@gmail.com>
2018-05-29 17:04:52 +0100

    revert back everything

--- install/install_utils.php
+++ install/install_utils.php
@@ -987,7 +987,8 @@ EOQ;
     RewriteBase {$basePath}
     RewriteRule ^cache/jsLanguage/(.._..).js$ index.php?entryPoint=jslang&modulename=app_strings&lang=$1 [L,QSA]
     RewriteRule ^cache/jsLanguage/(\w*)/(.._..).js$ index.php?entryPoint=jslang&modulename=$1&lang=$2 [L,QSA]
-    RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L,QSA]
+    RewriteRule ^api/(.*?)$ lib/API/public/index.php/$1 [L]
+    RewriteRule ^api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 </IfModule>
 <FilesMatch "\.(jpg|png|gif|js|css|ico)$">
         <IfModule mod_headers.c>
