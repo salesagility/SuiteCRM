329215c65de83128d596c158da2a3c37f108f1f6
zoltan kocsardi <zoltan.kocsardi@gmail.com>
2018-05-29 16:27:52 +0100

    test travis

--- modules/Administration/UpgradeAccess.php
+++ modules/Administration/UpgradeAccess.php
@@ -71,8 +71,6 @@ RedirectMatch 403 {$ignoreCase}/+files\.md5\$
     RewriteRule ^cache/jsLanguage/(.._..).js$ index.php?entryPoint=jslang&module=app_strings&lang=$1 [L,QSA]
     RewriteRule ^cache/jsLanguage/(\w*)/(.._..).js$ index.php?entryPoint=jslang&module=$1&lang=$2 [L,QSA]
     RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L,QSA]
-    RewriteRule ^api/(.*?)$ lib/API/public/index.php/$1 [L]
-    RewriteRule ^api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
 </IfModule>
 # END SUGARCRM RESTRICTIONS
 EOQ;
