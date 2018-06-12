02ad65ebe96328079bbd5aeae5bea58d2ebc44b2
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-06-12 17:52:19 +0100

    update htaccess for travis

--- install/install_utils.php
+++ install/install_utils.php
@@ -989,8 +989,11 @@ EOQ;
     RewriteRule ^cache/jsLanguage/(\w*)/(.._..).js$ index.php?entryPoint=jslang&modulename=$1&lang=$2 [L,QSA]
     # RewriteRule ^api/(.*?)$ lib/API/public/index.php/$1 [L]
     # RewriteRule ^api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-    RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L]
-    RewriteRule ^Api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+    # RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L]
+    # RewriteRule ^Api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteRule ^Api/(.*)$ Api/index.php/$1 [QSA,L]
 </IfModule>
 <FilesMatch "\.(jpg|png|gif|js|css|ico)$">
         <IfModule mod_headers.c>
--- modules/Administration/UpgradeAccess.php
+++ modules/Administration/UpgradeAccess.php
@@ -72,8 +72,11 @@ RedirectMatch 403 {$ignoreCase}/+files\.md5\$
     RewriteRule ^cache/jsLanguage/(\w*)/(.._..).js$ index.php?entryPoint=jslang&module=$1&lang=$2 [L,QSA]
     # RewriteRule ^api/(.*?)$ lib/API/public/index.php/$1 [L]
     # RewriteRule ^api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-    RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L]
-    RewriteRule ^Api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+    # RewriteRule ^Api/(.*?)$ Api/index.php/$1 [L]
+    # RewriteRule ^Api/(.*)$ - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteRule ^Api/(.*)$ Api/index.php/$1 [QSA,L]
 </IfModule>
 # END SUGARCRM RESTRICTIONS
 EOQ;
