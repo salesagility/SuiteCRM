02c3e0bb2efb82e7d9dbb97ff4b26cf38d529060
Marc Sánchez <sanchezfauste@gmail.com>
2018-06-12 19:56:59 +0200

    Fix #6022 - API v4_1 get_module_fields method does not return parentenum

--- service/v4/SugarWebServiceUtilv4.php
+++ service/v4/SugarWebServiceUtilv4.php
@@ -211,6 +211,9 @@ class SugarWebServiceUtilv4 extends SugarWebServiceUtilv3_1
 	            $entry['type'] = $var['type'];
 	            $entry['group'] = isset($var['group']) ? $var['group'] : '';
 	            $entry['id_name'] = isset($var['id_name']) ? $var['id_name'] : '';
+                if (isset($var['parentenum'])) {
+                    $entry['parentenum'] = $var['parentenum'];
+                }
 
 	            if ($var['type'] == 'link') {
 		            $entry['relationship'] = (isset($var['relationship']) ? $var['relationship'] : '');
