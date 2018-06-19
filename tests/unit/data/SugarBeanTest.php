b9c51feff44631b6e5ad4528e024e1fd66cef9dd
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:36:17 +0100

    removing a comment

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -2428,7 +2428,7 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
         self::assertEquals(null, $bean->in_save);
         
         self::assertEquals($current_user->id, $bean->modified_user_id);
-        //self::assertEquals('testing', $bean->modified_by_name);
+        
         self::assertEquals($current_user->user_name, null);
         self::assertEquals(0, $bean->deleted);
         /** @noinspection UnSafeIsSetOverArrayInspection */
