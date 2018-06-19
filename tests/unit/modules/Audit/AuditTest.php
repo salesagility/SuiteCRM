919ac3c42a4dbdc9c3f47407426d6d43536681de
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:50:25 +0100

    removing some comment

--- tests/unit/modules/Audit/AuditTest.php
+++ tests/unit/modules/Audit/AuditTest.php
@@ -17,16 +17,16 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
-        //execute the contructor and check for the Object type and  attributes
+        
         $audit = new Audit();
         $this->assertInstanceOf('Audit', $audit);
         $this->assertInstanceOf('SugarBean', $audit);
         $this->assertAttributeEquals('Audit', 'module_dir', $audit);
         $this->assertAttributeEquals('Audit', 'object_name', $audit);
         
-        // clean up
+        
         
         
     }
@@ -35,10 +35,10 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $audit = new Audit();
 
-        //test without setting name
+        
         $this->assertEquals(null, $audit->get_summary_text());
 
-        //test with name set
+        
         $audit->name = 'test';
         $this->assertEquals('test', $audit->get_summary_text());
     }
@@ -48,12 +48,12 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->create_export_query('', '');
             $this->assertTrue(true);
@@ -63,7 +63,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -73,11 +73,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->fill_in_additional_list_fields();
             $this->assertTrue(true);
@@ -87,7 +87,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -97,11 +97,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->fill_in_additional_detail_fields();
             $this->assertTrue(true);
@@ -111,7 +111,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -121,11 +121,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->fill_in_additional_parent_fields();
             $this->assertTrue(true);
@@ -135,7 +135,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -145,11 +145,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->get_list_view_data();
             $this->assertTrue(true);
@@ -159,7 +159,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -169,11 +169,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $audit = new Audit();
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $audit->get_audit_link();
             $this->assertTrue(true);
@@ -183,7 +183,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $this->markTestIncomplete('method has no implementation');
         
-        // clean up
+        
         
         
     }
@@ -195,7 +195,7 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $audit = new Audit();
 
-        //execute the method and verify that it returns an array
+        
         $result = $audit->get_audit_list();
         $this->assertTrue(is_array($result));
     }
@@ -207,11 +207,11 @@ class AuditTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $audit = new Audit();
 
-        //test with name field
+        
         $result = $audit->getAssociatedFieldName('name', '1');
         $this->assertEquals('1', $result);
 
-        //test with parent_id field
+        
         $result = $audit->getAssociatedFieldName('parent_id', '1');
         $this->assertEquals(null, $result);
     }
