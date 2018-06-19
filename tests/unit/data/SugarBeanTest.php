4efc69a57a9af4405440dc0897dd900c3b22fdf7
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:36:02 +0100

    removing a comment

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -2317,7 +2317,7 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
 
         self::assertEquals($bean, $bean->custom_fields->bean);
         self::assertEquals(false, $bean->new_with_id);
-        //self::assertEquals(null, $bean->old_modified_by_name);
+        
 
 
         // test
