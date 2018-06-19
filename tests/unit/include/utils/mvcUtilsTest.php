5fedda6e962f2d0c6d37b029e622a870a314e7c1
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:49:39 +0100

    removing some comment

--- tests/unit/include/utils/mvcUtilsTest.php
+++ tests/unit/include/utils/mvcUtilsTest.php
@@ -8,10 +8,10 @@ class mvc_utilsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
         
         
-        //execute the method and test if it doesn't throws an exception
+        
+        
         try {
             loadParentView('classic');
             $this->assertTrue(true);
@@ -19,7 +19,7 @@ class mvc_utilsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -30,14 +30,14 @@ class mvc_utilsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushGlobals();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
-        //test without setting REQUEST param
+        
         $expected = "javascript:void window.open('index.php?','printwin','menubar=1,status=0,resizable=1,scrollbars=1,toolbar=0,location=1')";
         $actual = getPrintLink();
         $this->assertSame($expected, $actual);
 
-        //test with required REQUEST param set
+        
         $_REQUEST['action'] = 'ajaxui';
         $expected = 'javascript:SUGAR.ajaxUI.print();';
         $actual = getPrintLink();
@@ -50,7 +50,7 @@ class mvc_utilsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testajaxBannedModules()
     {
-        //execute the method and test verify it returns true
+        
         $result = ajaxBannedModules();
         $this->assertTrue(is_array($result));
     }
