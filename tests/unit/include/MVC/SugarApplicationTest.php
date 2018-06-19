fc9d1cd63691f1d7c101f0827a6df1f347ea02ed
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:38:28 +0100

    removing a comment

--- tests/unit/include/MVC/SugarApplicationTest.php
+++ tests/unit/include/MVC/SugarApplicationTest.php
@@ -184,7 +184,7 @@ class SugarApplicationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $SugarApplication = new SugarApplication();
