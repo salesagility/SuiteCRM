9146411d5941a8fcabd6d7f52a2ff61d4aa5274b
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:49:52 +0100

    removing some comment

--- tests/unit/modules/Administration/AdministrationTest.php
+++ tests/unit/modules/Administration/AdministrationTest.php
@@ -15,7 +15,7 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testAdministration()
     {
 
-        //execute the contructor and check for the Object type and type attribute
+        
         $admin = new Administration();
         $this->assertInstanceOf('Administration', $admin);
         $this->assertInstanceOf('SugarBean', $admin);
@@ -33,23 +33,23 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $admin = new Administration();
 
-        //execute with default parameters and test if it returns object itself
+        
         $result = $admin->retrieveSettings();
         $this->assertInstanceOf('Administration', $result);
         $this->assertSame($admin, $result);
 
-        //execute with a invalid category and test if it returns object itself.
+        
         $result = $admin->retrieveSettings('test');
         $this->assertInstanceOf('Administration', $result);
         $this->assertSame($admin, $result);
         $this->assertEquals(true, $admin->settings['test']);
 
-        //execute with a valid category and test if it returns object itself.
+        
         $result = $admin->retrieveSettings('notify');
         $this->assertInstanceOf('Administration', $result);
         $this->assertSame($admin, $result);
 
-        //execute with a valid category and clean=true and test if it returns object itself.
+        
         $admin = new Administration();
         $result = $admin->retrieveSettings('notify', true);
         $this->assertInstanceOf('Administration', $result);
@@ -60,24 +60,24 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         self::markTestIncomplete('environment dependency');
         
-	// save state
+	
 
         $state = new \SuiteCRM\StateSaver();
         $state->pushTable('config');
         $state->pushGlobals();
 
-	// test
+	
         
         $admin = new Administration();
 
         $_POST['proxy_test'] = 'test value';
 
-        //execute the method and verify that it sets the correct config key
+        
         $admin->saveConfig();
         $actual = $admin->settings['proxy_test'];
         $this->assertEquals($actual, 'test value');
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('config');
@@ -85,7 +85,7 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testsaveSetting()
     {
-	// save state
+	
         
         self::markTestIncomplete('environment dependency');
 
@@ -93,17 +93,17 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushTable('config');
         $state->pushGlobals();
 
-	// test
+	
         
         $admin = new Administration();
 
-        //execute the method and verify that sets the correct config key
+        
         $result = $admin->saveSetting('category', 'key', 'test value');
         $admin->retrieveSettings('category');
         $actual = $admin->settings['category_key'];
         $this->assertEquals($actual, 'test value');
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('config');
@@ -113,12 +113,12 @@ class AdministrationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $admin = new Administration();
 
-        //test with empty string
+        
         $expected = array(false, false);
         $actual = $admin->get_config_prefix('');
         $this->assertSame($expected, $actual);
 
-        //test with a valid string
+        
         $expected = array('category', 'test');
         $actual = $admin->get_config_prefix('category_test');
         $this->assertSame($expected, $actual);
