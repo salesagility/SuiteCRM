cbbe5b4507b554f2daaa219e1fbde57ee6f8eeb1
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:50:31 +0100

    removing some comment

--- tests/unit/modules/Campaigns/CampaignTest.php
+++ tests/unit/modules/Campaigns/CampaignTest.php
@@ -15,7 +15,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testCampaign()
     {
 
-        //execute the contructor and check for the Object type and  attributes
+        
         $campaign = new Campaign();
         $this->assertInstanceOf('Campaign', $campaign);
         $this->assertInstanceOf('SugarBean', $campaign);
@@ -32,16 +32,16 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
         $campaign = new Campaign();
 
-        //test with attributes preset and verify template variables are set accordingly
+        
         $tpl = new Sugar_Smarty();
         $campaign->list_view_parse_additional_sections($tpl);
         $this->assertEquals('', isset($tpl->_tpl_vars['ASSIGNED_USER_NAME']) ? $tpl->_tpl_vars['ASSIGNED_USER_NAME'] : null);
         
-        // clean up
+        
         
         
     }
@@ -50,10 +50,10 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $campaign = new Campaign();
 
-        //test without setting name
+        
         $this->assertEquals(null, $campaign->get_summary_text());
 
-        //test with name set
+        
         $campaign->name = 'test';
         $this->assertEquals('test', $campaign->get_summary_text());
     }
@@ -63,12 +63,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         self::markTestIncomplete('#Warning: Strings contain different line endings!');
         $campaign = new Campaign();
 
-        //test with empty string params
+        
         $expected = "SELECT\n            campaigns.*,\n            users.user_name as assigned_user_name  FROM campaigns LEFT JOIN users\n                      ON campaigns.assigned_user_id=users.id where  campaigns.deleted=0 ORDER BY campaigns.name";
         $actual = $campaign->create_export_query('', '');
         $this->assertSame($expected, $actual);
 
-        //test with valid string params
+        
         $expected = "SELECT\n            campaigns.*,\n            users.user_name as assigned_user_name  FROM campaigns LEFT JOIN users\n                      ON campaigns.assigned_user_id=users.id where campaigns.name=\"\" AND  campaigns.deleted=0 ORDER BY campaigns.id";
         $actual = $campaign->create_export_query('campaigns.id', 'campaigns.name=""');
         $this->assertSame($expected, $actual);
@@ -79,12 +79,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $campaign = new Campaign();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $campaign->clear_campaign_prospect_list_relationship('');
             $campaign->clear_campaign_prospect_list_relationship('1');
@@ -93,7 +93,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -103,12 +103,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $campaign = new Campaign();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $campaign->mark_relationships_deleted('');
             $campaign->mark_relationships_deleted('1');
@@ -117,7 +117,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -127,12 +127,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $campaign = new Campaign();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $campaign->fill_in_additional_list_fields();
             $this->assertTrue(true);
@@ -140,7 +140,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -150,12 +150,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $campaign = new Campaign();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $campaign->fill_in_additional_detail_fields();
             $this->assertTrue(true);
@@ -163,7 +163,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -173,12 +173,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $campaign = new Campaign();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $campaign->update_currency_id('', '');
             $this->assertTrue(true);
@@ -186,7 +186,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -196,7 +196,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $campaign = new Campaign();
 
         $current_theme = SugarThemeRegistry::current();
-        //execute the method and verify that it returns expected results
+        
 
         $expected = array(
                 'DELETED' => 0,
@@ -205,10 +205,10 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
                 'IMPRESSIONS' => '0',
                 'OPTIONAL_LINK' => 'display:none',
                 'TRACK_CAMPAIGN_TITLE' => 'View Status',
-            // The theme may fallback to default so we only care that the icon is the same.
+            
                 'TRACK_CAMPAIGN_IMAGE' => '~images/view_status~',
                 'LAUNCH_WIZARD_TITLE' => 'Launch Wizard',
-            // The theme may fallback to default so we only care that the icon is the same.
+            
                 'LAUNCH_WIZARD_IMAGE' => '~images/edit_wizard~',
                 'TRACK_VIEW_ALT_TEXT' => 'View Status',
                 'LAUNCH_WIZ_ALT_TEXT' => 'Launch Wizard',
@@ -228,12 +228,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $campaign = new Campaign();
 
-        //test with blank parameter
+        
         $expected = "campaigns.name like '%'";
         $actual = $campaign->build_generic_where_clause('');
         $this->assertSame($expected, $actual);
 
-        //test with valid parameter
+        
         $expected = "campaigns.name like '1%'";
         $actual = $campaign->build_generic_where_clause(1);
         $this->assertSame($expected, $actual);
@@ -242,7 +242,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testSaveAndMarkDeleted()
     {
         
-	// save state
+	
 
         $state = new \SuiteCRM\StateSaver();
         $state->pushTable('aod_index');
@@ -251,7 +251,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushTable('tracker');
         $state->pushGlobals();
 
-	// test
+	
         
         $campaign = new Campaign();
         $campaign->name = 'test';
@@ -259,16 +259,16 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $campaign->save();
 
-        //test for record ID to verify that record is saved
+        
         $this->assertTrue(isset($campaign->id));
         $this->assertEquals(36, strlen($campaign->id));
 
-        //mark the record as deleted and verify that this record cannot be retrieved anymore.
+        
         $campaign->mark_deleted($campaign->id);
         $result = $campaign->retrieve($campaign->id);
         $this->assertEquals(null, $result);
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('tracker');
@@ -281,7 +281,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $campaign = new Campaign();
 
-        //test with attributes preset and verify template variables are set accordingly
+        
         $campaign->name = 'test';
         $campaign->budget = '1000';
         $campaign->end_date = '10/01/2015';
@@ -310,12 +310,12 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $campaign = new Campaign();
 
-        //test without parameters
+        
         $expected = "SELECT campaign_log.*  FROM campaign_log WHERE campaign_log.campaign_id = '' AND campaign_log.deleted=0 AND activity_type='targeted' AND archived=0 ";
         $actual = $campaign->track_log_entries();
         $this->assertSame($expected, $actual);
 
-        //test with parameters
+        
         $expected = "SELECT campaign_log.*  FROM campaign_log WHERE campaign_log.campaign_id = '' AND campaign_log.deleted=0 AND activity_type='test1' AND archived=0 ";
         $actual = $campaign->track_log_entries(array('test1', 'test2'));
         $this->assertSame($expected, $actual);
@@ -325,7 +325,7 @@ class CampaignTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $campaign = new Campaign();
 
-        //without parameters
+        
         $expected = " SELECT emailman.*,
        campaigns.NAME       AS campaign_name,
        email_marketing.NAME AS message_name,
@@ -395,7 +395,7 @@ WHERE  emailman.campaign_id = ''
 
         $this->assertSame($expected, $actual);
 
-        //with parameters
+        
         $expected = "SELECT emailman.*,
        campaigns.NAME       AS campaign_name,
        email_marketing.NAME AS message_name,
@@ -483,17 +483,17 @@ WHERE  emailman.campaign_id = ''
     {
         $campaign = new Campaign();
 
-        //test without parameters
+        
         $expected = '';
         $actual = $campaign->create_list_count_query('');
         $this->assertSame($expected, $actual);
 
-        //test with query parameters
+        
         $expected = 'SELECT count(*) c FROM campaigns';
         $actual = $campaign->create_list_count_query('select * from campaigns');
         $this->assertSame($expected, $actual);
 
-        //test with distinct
+        
         $expected = 'SELECT count(DISTINCT campaigns.id) c FROM campaigns';
         $actual = $campaign->create_list_count_query('SELECT distinct marketing_id FROM campaigns');
         $this->assertSame($expected, $actual);
