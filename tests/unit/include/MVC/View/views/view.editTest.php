a46ebcb8563e4337f4a6feaf9c6c135ff93e8349
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:35:14 +0100

    removing a commented code

--- tests/unit/include/MVC/View/views/view.editTest.php
+++ tests/unit/include/MVC/View/views/view.editTest.php
@@ -35,7 +35,7 @@
         $state = new SuiteCRM\StateSaver();
         
         
-         ////error_reporting(E_ERROR | E_PARSE);
+         
 
         //execute the method with required attributes preset, it will initialize the ev(edit view) attribute.
         $view = new ViewEdit();
@@ -68,7 +68,7 @@
          $state = new SuiteCRM\StateSaver();
          
          
-         ////error_reporting(E_ERROR | E_PARSE);
+         
          
         //execute the method with essential parameters set. it should return some html.
         $view = new ViewEdit();
