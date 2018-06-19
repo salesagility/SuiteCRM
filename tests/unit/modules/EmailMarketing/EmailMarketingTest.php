cde3c62db8fc7b52ba753cdcbca56355a76bebe7
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:50:43 +0100

    removing some comment

--- tests/unit/modules/EmailMarketing/EmailMarketingTest.php
+++ tests/unit/modules/EmailMarketing/EmailMarketingTest.php
@@ -6,7 +6,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testEmailMarketing()
     {
 
-        //execute the contructor and check for the Object type and  attributes
+        
         $emailMarketing = new EmailMarketing();
 
         $this->assertInstanceOf('EmailMarketing', $emailMarketing);
@@ -24,14 +24,14 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
         $emailMarketing = new EmailMarketing();
 
         $result = $emailMarketing->retrieve();
         $this->assertInstanceOf('EmailMarketing', $result);
         
-        // clean up
+        
         
         
     }
@@ -40,17 +40,17 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $emailMarketing = new EmailMarketing();
 
-        //test without setting name
+        
         $this->assertEquals(null, $emailMarketing->get_summary_text());
 
-        //test with name set
+        
         $emailMarketing->name = 'test';
         $this->assertEquals('test', $emailMarketing->get_summary_text());
     }
 
     public function testcreate_export_query()
     {
-	// save state
+	
 
         $state = new \SuiteCRM\StateSaver();
         $state->pushGlobals();
@@ -58,18 +58,18 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         $emailMarketing = new EmailMarketing();
 
-        //test with empty string params
+        
         $expected = " SELECT  email_marketing.*  , jt0.name template_name , jt0.assigned_user_id template_name_owner  , 'EmailTemplates' template_name_mod FROM email_marketing   LEFT JOIN  email_templates jt0 ON email_marketing.template_id=jt0.id AND jt0.deleted=0\n\n AND jt0.deleted=0 where email_marketing.deleted=0";
         $actual = $emailMarketing->create_export_query('', '');
         $this->assertSame($expected, $actual);
 
-        //test with valid string params
+        
         $expected = " SELECT  email_marketing.*  , jt0.name template_name , jt0.assigned_user_id template_name_owner  , 'EmailTemplates' template_name_mod FROM email_marketing   LEFT JOIN  email_templates jt0 ON email_marketing.template_id=jt0.id AND jt0.deleted=0\n\n AND jt0.deleted=0 where (email_marketing.name=\"\") AND email_marketing.deleted=0";
         $actual = $emailMarketing->create_export_query('email_marketing.id', 'email_marketing.name=""');
         $this->assertSame($expected, $actual);
 
 
-        // clean up
+        
         
         $state->popGlobals();
     }
@@ -78,7 +78,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $emailMarketing = new EmailMarketing();
 
-        //execute the method and verify that it retunrs expected results
+        
         $expected = array(
                 'ALL_PROSPECT_LISTS' => '0',
         );
@@ -99,7 +99,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $emailMarketing = new EmailMarketing();
 
-        //execute the method and verify that it retunrs expected results
+        
         $expected = "select prospect_lists.* from prospect_lists  left join prospect_list_campaigns on prospect_list_campaigns.prospect_list_id=prospect_lists.id where prospect_list_campaigns.deleted=0 and prospect_list_campaigns.campaign_id='' and prospect_lists.deleted=0 and prospect_lists.list_type not like 'exempt%'";
         $actual = $emailMarketing->get_all_prospect_lists();
         $this->assertSame($expected, $actual);
