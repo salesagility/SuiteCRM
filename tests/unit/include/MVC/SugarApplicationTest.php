50c4196d769082bec70655db4f145c0bd54dcb16
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:38:29 +0100

    removing a comment

--- tests/unit/include/MVC/SugarApplicationTest.php
+++ tests/unit/include/MVC/SugarApplicationTest.php
@@ -189,7 +189,7 @@ class SugarApplicationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         $SugarApplication = new SugarApplication();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $SugarApplication->handleOfflineClient();
         } catch (Exception $e) {
