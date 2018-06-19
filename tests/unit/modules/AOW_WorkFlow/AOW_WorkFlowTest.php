6781b9cd14dd1142d28d2bca3c2d8b10f87e4877
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:50:24 +0100

    removing some comment

--- tests/unit/modules/AOW_WorkFlow/AOW_WorkFlowTest.php
+++ tests/unit/modules/AOW_WorkFlow/AOW_WorkFlowTest.php
@@ -14,7 +14,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testAOW_WorkFlow()
     {
 
-        //execute the contructor and check for the Object type and  attributes
+        
         $aowWorkFlow = new AOW_WorkFlow();
         $this->assertInstanceOf('AOW_WorkFlow', $aowWorkFlow);
         $this->assertInstanceOf('Basic', $aowWorkFlow);
@@ -33,14 +33,14 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
         $aowWorkFlow = new AOW_WorkFlow();
         $this->assertEquals(false, $aowWorkFlow->bean_implements('')); //test with blank value
         $this->assertEquals(false, $aowWorkFlow->bean_implements('test')); //test with invalid value
         $this->assertEquals(true, $aowWorkFlow->bean_implements('ACL')); //test with valid value
         
-        // clean up
+        
         
         
     }
@@ -63,16 +63,16 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $aowWorkFlow->save();
 
-        //test for record ID to verify that record is saved
+        
         $this->assertTrue(isset($aowWorkFlow->id));
         $this->assertEquals(36, strlen($aowWorkFlow->id));
 
-        //mark the record as deleted and verify that this record cannot be retrieved anymore.
+        
         $aowWorkFlow->mark_deleted($aowWorkFlow->id);
         $result = $aowWorkFlow->retrieve($aowWorkFlow->id);
         $this->assertEquals(null, $result);
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('tracker');
@@ -87,12 +87,12 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $aowWorkFlow->load_flow_beans();
             $this->assertTrue(true);
@@ -100,7 +100,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -116,7 +116,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $result = $aowWorkFlow->run_flows();
         $this->assertTrue($result);
         
-        // clean up
+        
         
         $state->popGlobals();
     }
@@ -126,12 +126,12 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state = new SuiteCRM\StateSaver();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
         
         
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $aowWorkFlow->run_flow();
             $this->assertTrue(true);
@@ -139,7 +139,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
             $this->fail($e->getMessage() . "\nTrace:\n" . $e->getTraceAsString());
         }
         
-        // clean up
+        
         
         
     }
@@ -148,7 +148,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //test with different modules. it always returns true
+        
 
         $result = $aowWorkFlow->run_bean_flows(new AOS_Quotes());
         $this->assertTrue($result);
@@ -162,7 +162,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         $aowWorkFlow = new AOW_WorkFlow();
         
-        //test for AOS_Quotes. it will return null as no test data is available
+        
         $aowWorkFlow->flow_module = 'AOS_Quotes';
         $result = $aowWorkFlow->get_flow_beans();
         $this->assertGreaterThanOrEqual(0, count((array)$result));
@@ -173,16 +173,16 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $aowWorkFlow = new AOW_WorkFlow();
         $query = array();
 
-        //test without type param
+        
         $result = $aowWorkFlow->build_flow_query_join('aos_products_quotes', new AOS_Quotes(), '', array());
         $this->assertSame(array(), $result);
 
-        //test with type custom
+        
         $expected = array('join' => array('c' => 'LEFT JOIN calls_cstm c ON calls.id = c.id_c '));
         $result = $aowWorkFlow->build_flow_query_join('c', new Call(), 'custom', array());
         $this->assertSame($expected, $result);
 
-        //test with type relationship
+        
         $expected = array(
                 'join' => array('aos_products_quotes' => "LEFT JOIN aos_products_quotes aos_products_quotes ON aos_quotes.id=aos_products_quotes.parent_id AND aos_products_quotes.deleted=0\n\n"),
                 'select' => array("aos_products_quotes.id AS 'aos_products_quotes_id'"),
@@ -195,12 +195,12 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //test without presetting required object attributes
+        
         $expected = array();
         $query = $aowWorkFlow->build_flow_query_where();
         $this->assertSame($expected, $query);
 
-        //test with module required attributes set
+        
         $aowWorkFlow->id = '1';
         $aowWorkFlow->flow_module = 'Calls';
         $expected = array(
@@ -210,7 +210,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $query = $aowWorkFlow->build_flow_query_where();
         $this->assertSame($expected, $query);
 
-        //test with flow_run_on and multiple_runs attributes set
+        
         $expected = array(
                         'where' => array('calls.date_entered > \'\'', 'calls.deleted = 0 '),
                     );
@@ -225,7 +225,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         self::markTestIncomplete('[PHPUnit_Framework_Exception] unserialize(): Error at offset 0 of 5 bytes');
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //populate required values
+        
         $call = new Call();
         $aowCondition = new AOW_Condition();
         $aowCondition->name = 'test';
@@ -233,7 +233,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $aowCondition->field = 'name';
         $aowCondition->value = 'testval';
 
-        //test with contains operator
+        
         $aowCondition->operator = 'Contains';
         $aowCondition->value_type = 'Value';
         $expected = array(
@@ -242,7 +242,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $query = $aowWorkFlow->build_query_where($aowCondition, $call);
         $this->assertEquals($expected, $query);
 
-        //test for starts with operator
+        
         $aowCondition->operator = 'Starts_With';
         $aowCondition->value_type = 'Value';
 
@@ -252,7 +252,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $query = $aowWorkFlow->build_query_where($aowCondition, $call);
         $this->assertEquals($expected, $query);
 
-        //test for Equal_To operator
+        
         $aowCondition->operator = 'Equal_To';
         $aowCondition->value_type = 'Value';
 
@@ -262,7 +262,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $query = $aowWorkFlow->build_query_where($aowCondition, $call);
         $this->assertEquals($expected, $query);
 
-        //test with value type Date
+        
         $aowCondition->operator = 'Equal_To';
         $aowCondition->value_type = 'Date';
 
@@ -271,15 +271,15 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         );
         
         
-//        $tmpstate = new SuiteCRM\StateSaver();
-//        $tmpstate->pushErrorLevel();
-//        error_reporting(E_ERROR | E_PARSE);
+
+
+
         $query = $aowWorkFlow->build_query_where($aowCondition, $call);
-//        $tmpstate->popErrorLevel();
+
         
         $this->assertEquals($expected, $query);
 
-        //test with value type Field
+        
         $aowCondition->operator = 'Equal_To';
         $aowCondition->value_type = 'Field';
 
@@ -306,7 +306,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //execute the method and verify that it returns valid values for all operators
+        
 
         $this->assertTrue($aowWorkFlow->compare_condition(1, 1));
         $this->assertTrue($aowWorkFlow->compare_condition(1, 2, 'Not_Equal_To'));
@@ -318,14 +318,14 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $this->assertTrue($aowWorkFlow->compare_condition('test2', array('test1', 'test2'), 'One_of'));
         $this->assertTrue($aowWorkFlow->compare_condition('test', array('test1', 'test2'), 'Not_One_of'));
 
-        //These do not return bool but 'strpos' result
-        //$this->assertNotFalse($aowWorkFlow->compare_condition('test1', 'test', 'Contains'));
+        
+        
         $this->assertEquals(0, $aowWorkFlow->compare_condition('test1', 'test', 'Contains'));
 
-        //$this->assertNotFalse($aowWorkFlow->compare_condition('test1', 'test', 'Starts_With'));
+        
         $this->assertEquals(0, $aowWorkFlow->compare_condition('test1', 'test', 'Starts_With'));
 
-        //$this->assertNotFalse($aowWorkFlow->compare_condition('test1', '1', 'Ends_With'));
+        
         $this->assertEquals(4, $aowWorkFlow->compare_condition('test1', '1', 'Ends_With'));
     }
 
@@ -333,7 +333,7 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //test with two different modules
+        
         $this->assertFalse($aowWorkFlow->check_in_group(1, 'Users', 1));
         $this->assertFalse($aowWorkFlow->check_in_group(1, 'Calls', 1));
     }
@@ -347,29 +347,29 @@ class AOW_WorkFlowTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
                 
         $aowWorkFlow = new AOW_WorkFlow();
 
-        //prepare the required objects and variables
+        
         $aowWorkFlow->id = 1;
 
         $call = new Call();
         $call->id = 1;
 
-        //execute the method and verify if it creates records in processed table
+        
         $result = $aowWorkFlow->run_actions($call);
 
-        //test for a entry in AOW_Processed table.
+        
         $processed = new AOW_Processed();
         $processed->retrieve_by_string_fields(array('aow_workflow_id' => 1, 'parent_id' => 1));
 
-        //test for record ID to verify that record is saved
+        
         $this->assertTrue(isset($processed->id));
         $this->assertEquals(36, strlen($processed->id));
 
-        //mark the record as deleted and verify that this record cannot be retrieved anymore.
+        
         $processed->mark_deleted($processed->id);
         $result = $processed->retrieve($processed->id);
         $this->assertEquals(null, $result);
         
-        // clean up
+        
         
         $state->popTable('tracker');
         $state->popTable('aow_processed');
