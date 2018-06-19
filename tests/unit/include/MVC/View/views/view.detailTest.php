710c0f88298b6fc27d89ad9ac76a39e33f1f03bd
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:49:03 +0100

    removing some comment

--- tests/unit/include/MVC/View/views/view.detailTest.php
+++ tests/unit/include/MVC/View/views/view.detailTest.php
@@ -27,21 +27,21 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testViewDetail()
     {
         
-        // store state
+        
         
         $state = new SuiteCRM\StateSaver();
         $state->pushGlobals();
         $state->pushTable('email_addresses');
         
-        // test
         
-        //execute the contructor and check for the Object type and type attribute
+        
+        
         $view = new ViewDetail();
         $this->assertInstanceOf('ViewDetail', $view);
         $this->assertInstanceOf('SugarView', $view);
         $this->assertAttributeEquals('detail', 'type', $view);
         
-        // clean up
+        
         
         $state->popTable('email_addresses');
         $state->popGlobals();
@@ -51,16 +51,16 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testpreDisplay()
     {
         
-        // store state
+        
         
         $state = new SuiteCRM\StateSaver();
         $state->pushGlobals();
         $state->pushTable('email_addresses');
         
-        // test
+        
         
 
-        //execute the method with required attributes preset, it will initialize the dv(detail view) attribute. 
+        
         $view = new ViewDetail();
         $view->module = 'Users';
         $view->bean = new User();
@@ -69,7 +69,7 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $this->assertInstanceOf('DetailView2', $view->dv);
         $this->asserttrue(is_array($view->dv->defs));
 
-        //execute the method again for a different module with required attributes preset, it will initialize the dv(detail view) attribute.
+        
         $view = new ViewDetail();
         $view->module = 'Meetings';
         $view->bean = new Meeting();
@@ -78,7 +78,7 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $this->assertInstanceOf('DetailView2', $view->dv);
         $this->asserttrue(is_array($view->dv->defs));
         
-        // clean up
+        
         
         $state->popTable('email_addresses');
         $state->popGlobals();
@@ -88,18 +88,18 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testdisplay()
     {
         
-        // store state
+        
         
         $state = new SuiteCRM\StateSaver();
         $state->pushGlobals();
         $state->pushTable('email_addresses');
         
-        // test
+        
         
 
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
-        //execute the method with essential parameters set. it should return some html.
+        
         $view = new ViewDetail();
         $view->module = 'Users';
         $view->bean = new User();
@@ -113,7 +113,7 @@ class ViewDetailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         ob_end_clean();
         $this->assertGreaterThan(0, strlen($renderedContent));
         
-        // clean up
+        
         
         $state->popTable('email_addresses');
         $state->popGlobals();
