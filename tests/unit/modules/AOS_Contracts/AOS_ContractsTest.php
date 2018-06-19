3d458eeb2ec4aefdd723c44f9852b9297a8be3ac
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:50:10 +0100

    removing some comment

--- tests/unit/modules/AOS_Contracts/AOS_ContractsTest.php
+++ tests/unit/modules/AOS_Contracts/AOS_ContractsTest.php
@@ -14,7 +14,7 @@ class AOS_ContractsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     public function testAOS_Contracts()
     {
 
-        //execute the contructor and check for the Object type and attributes
+        
         $aosContracts = new AOS_Contracts();
         $this->assertInstanceOf('AOS_Contracts', $aosContracts);
         $this->assertInstanceOf('Basic', $aosContracts);
@@ -40,23 +40,23 @@ class AOS_ContractsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushTable('aod_index');
         $state->pushGlobals();
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
         $aosContracts = new AOS_Contracts();
         $aosContracts->name = 'test';
 
         $aosContracts->save();
 
-        //test for record ID to verify that record is saved 
+        
         $this->assertTrue(isset($aosContracts->id));
         $this->assertEquals(36, strlen($aosContracts->id));
 
-        //mark the record as deleted and verify that this record cannot be retrieved anymore.
+        
         $aosContracts->mark_deleted($aosContracts->id);
         $result = $aosContracts->retrieve($aosContracts->id);
         $this->assertEquals(null, $result);
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('aod_index');
@@ -80,28 +80,28 @@ class AOS_ContractsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $aosContracts = new AOS_Contracts();
         $aosContracts->name = 'test';
 
-        //test createReminder() 
+        
         $aosContracts->createReminder();
 
-        //verify record ID to check that record is saved
+        
         $this->assertTrue(isset($aosContracts->call_id));
         $this->assertEquals(36, strlen($aosContracts->call_id));
 
-        //verify the parent_type value set by createReminder()
+        
         $call->retrieve($aosContracts->call_id);
         $this->assertAttributeEquals('AOS_Contracts', 'parent_type', $call);
 
-        //test createLink() and verify the parent_type value
+        
         $aosContracts->createLink();
         $call->retrieve($aosContracts->call_id);
         $this->assertAttributeEquals('Accounts', 'parent_type', $call);
 
-        //delete the call and verify that this record cannot be retrieved anymore.		
+        
         $aosContracts->deleteCall();
         $result = $call->retrieve($aosContracts->call_id);
         $this->assertEquals(null, $result);
         
-        // clean up
+        
         
         $state->popGlobals();
         $state->popTable('tracker');
