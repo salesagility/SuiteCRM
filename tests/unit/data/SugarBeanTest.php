9a5bdfb434c0a64df739aef15388237b6d12ea3f
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:36:47 +0100

    removing a comment

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -2737,7 +2737,7 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
         /** @noinspection PhpUndefinedFieldInspection */
         self::assertNotNull($bean->testField1);
 
-        // test
+        
         $bean = new Contact();
         $bean->id = 'test_contact_10';
         $bean->fetched_row['id'] = 'test_contact_10';
