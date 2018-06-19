aa5f4cc29f95df30eb25ad05b39ae0fa8e14464f
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:35:46 +0100

    removing a comment

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -2185,7 +2185,7 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
      */
     public function testGetImportableFields()
     {
-        // test
+        
         $GLOBALS['log']->reset();
         $GLOBALS['log']->fatal('test');
         $bean = new Contact();
