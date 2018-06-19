434f86ce03281a254d47d48e133291346b4f9c3a
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:49:25 +0100

    removing some comment

--- tests/unit/include/MVC/View/views/view.vcardTest.php
+++ tests/unit/include/MVC/View/views/view.vcardTest.php
@@ -14,13 +14,13 @@ class ViewVcardTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testdisplay()
     {
-        //execute the method with required child objects preset and check for the Object type and type attribute
+        
 
         $view = new ViewVcard();
         $view->module = 'Contacts';
         $view->bean = new Contact();
 
-        //execute the method and test if it works and does not throws an exception other than headers output exception.
+        
         try {
             $view->display();
         } catch (Exception $e) {
