7557f26b8d0c9f65644991a6545ab5b5cc222ae0
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:38:50 +0100

    removing a comment

--- tests/unit/include/MVC/SugarApplicationTest.php
+++ tests/unit/include/MVC/SugarApplicationTest.php
@@ -298,7 +298,7 @@ class SugarApplicationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
         
-        //
+        
     }
 
     public function testloadDisplaySettings()
