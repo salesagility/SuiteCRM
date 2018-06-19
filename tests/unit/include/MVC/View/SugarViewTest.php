becfd001205e799f78c1af2744ab722940e65122
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:39:38 +0100

    removing a comment

--- tests/unit/include/MVC/View/SugarViewTest.php
+++ tests/unit/include/MVC/View/SugarViewTest.php
@@ -17,7 +17,7 @@ class SugarViewTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
         
-        //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+        
 
         $SugarView = new SugarView();
 
