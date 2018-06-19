f019013b775590d623e71bbcc3957e91c51b17d4
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:49:30 +0100

    removing some comment

--- tests/unit/include/utils/activityUtilsTest.php
+++ tests/unit/include/utils/activityUtilsTest.php
@@ -20,22 +20,22 @@ class activity_utilsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushGlobals();
         
         
-        //error_reporting(E_ERROR | E_PARSE);
+        
 
-        //execute the method and test if it returns true
+        
 
-        //with rel_users_table manually set
+        
         $bean = new User();
         $bean->rel_users_table = 'users_signatures';
         $list = build_related_list_by_user_id($bean, '1', '');
         $this->assertTrue(is_array($list));
 
-        //with rel_users_table set by default 
+        
         $bean = new Meeting();
         $list = build_related_list_by_user_id($bean, '1', '');
         $this->assertTrue(is_array($list));
         
-        // clean up
+        
         $state->popGlobals();
         
     }
