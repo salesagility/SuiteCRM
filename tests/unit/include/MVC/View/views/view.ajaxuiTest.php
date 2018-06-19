ae1edb499996871c62ef978fd2efae2473cc0c00
gymad <gyula.madarasz@salesagility.com>
2018-06-19 20:35:12 +0100

    removing a commented code

--- tests/unit/include/MVC/View/views/view.ajaxuiTest.php
+++ tests/unit/include/MVC/View/views/view.ajaxuiTest.php
@@ -17,9 +17,9 @@ class ViewAjaxUITest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
 //        //execute the method and test if it works and does not throws an exception other than headers output exception.
 //        try {
-//            $view->display();
-//        } catch (Exception $e) {
-//            $this->assertStringStartsWith('Cannot modify header information', $e->getMessage());
+
+
+
 //        }
         $this->markTestIncomplete('Can Not be implemented');
     }
