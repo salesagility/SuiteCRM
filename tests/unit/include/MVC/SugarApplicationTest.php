5affd4130cd9e8c7f01970c652940d40a220436e
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:38:54 +0100

    removing a comment

--- tests/unit/include/MVC/SugarApplicationTest.php
+++ tests/unit/include/MVC/SugarApplicationTest.php
@@ -311,7 +311,7 @@ class SugarApplicationTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         $SugarApplication = new SugarApplication();
 
-        //execute the method and test if it works and does not throws an exception.
+        
         try {
             $SugarApplication->loadDisplaySettings();
         } catch (Exception $e) {
