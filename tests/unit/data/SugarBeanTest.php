6fe8fbb55de07dbca511e683d1f41d67bd33f3bc
gymadarasz <gyula.madarasz@salesagility.com>
2018-06-19 18:35:35 +0100

    removing a comment

--- tests/unit/data/SugarBeanTest.php
+++ tests/unit/data/SugarBeanTest.php
@@ -2034,7 +2034,7 @@ class SugarBeanTest extends SuitePHPUnit_Framework_TestCase
         self::assertEquals(true, $results);
         self::assertCount(2, $GLOBALS['log']->calls['fatal']);
 
-        // test
+        
         $GLOBALS['log']->reset();
         $GLOBALS['log']->fatal('test');
         $bean = new Contact();
