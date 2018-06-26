c1c420c2d1f2fa4f347b9e0fe468ec1e8a4d3984
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-06-26 16:22:25 +0100

    Implement ElasticSearchEngine

--- modules/ElasticSearch/ElasticSearchEngine.php
+++ modules/ElasticSearch/ElasticSearchEngine.php
@@ -1,23 +1,152 @@
 <?php
 /**
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+/**
  * Created by PhpStorm.
  * User: viocolano
  * Date: 21/06/18
  * Time: 16:25
  */
 
+use SuiteCRM\Search\MasterSearchInvalidRequestException;
 use SuiteCRM\Search\SearchEngine;
 use SuiteCRM\Search\SearchQuery;
+
+require_once 'modules/ElasticSearch/ElasticSearchClientBuilder.php';
 
 class ElasticSearchEngine extends SearchEngine
 {
 
     /**
      * @param $query SearchQuery
-     * @return \SugarView
+     * @param Elasticsearch\Client|null $client
+     * @return array[] ids
      */
-    public function searchAndView($query)
+    public function search($query, $client = null)
     {
-        // TODO: Implement searchAndView() method.
+        $this->validateQuery($query);
+        $params = $this->createSearchParams($query);
+        $hits = $this->runElasticSearch($params, $client);
+        $results = $this->parseHits($hits);
+
+        return $results;
+    }
+
+    /**
+     * Generates the parameter array for the Elasticsearch API from a SearchQuery.
+     * @param $query SearchQuery
+     * @return array
+     */
+    private function createSearchParams($query)
+    {
+        $params = [
+            'index' => 'main',
+            'body' => [
+                'stored_fields' => [],
+                'from' => $query->getFrom(),
+                'size' => $query->getSize(),
+                'query' => ['bool' => ['must' => []]]
+            ]
+        ];
+
+        $boom = explode(' ', $query->getSearchString());
+
+        foreach ($boom as $word) {
+            $params['body']['query']['bool']['must'][] = ["regexp" => ["_all" => ".*$word.*"]];
+        }
+
+        return $params;
+    }
+
+    /**
+     * @param $query
+     */
+    protected function validateQuery(&$query)
+    {
+        $query->toLowerCase();
+        $query->trim();
+        $query->replace('-', ' ');
+
+        $string = $query->getSearchString();
+
+        if (empty($string)) {
+            throw new MasterSearchInvalidRequestException("Search string not provided.");
+        }
+    }
+
+    /**
+     * Calls the Elasticsearch API.
+     *
+     * The `$client` parameter is only used for testing purposes.
+     *
+     * @param $params array
+     * @param Elasticsearch\Client|null $client
+     * @return array
+     */
+    private function runElasticSearch($params, $client = null)
+    {
+        if ($client === null) {
+            $client = ElasticSearchClientBuilder::getClient();
+        }
+
+        $results = $client->search($params);
+
+        return $results;
+    }
+
+    /**
+     * Reads the array returned from the Elasticsearch API
+     * and converts it into an associative array of ids, grouped by Module.
+     *
+     * @param $hits array
+     * @return array
+     */
+    private function parseHits($hits)
+    {
+        $hitsArray = $hits['hits']['hits'];
+
+        $results = [];
+
+        foreach ($hitsArray as $hit) {
+            $results[$hit['_type']][] = $hit['_id'];
+        }
+
+        return $results;
     }
 }
---
+++ tests/unit/modules/ElasticSearch/ElasticSearchEngineTest.php
@@ -0,0 +1,280 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+/**
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: viocolano
+ * Date: 26/06/18
+ * Time: 09:22
+ */
+
+use SuiteCRM\Search\MasterSearchInvalidRequestException;
+use SuiteCRM\Search\SearchQuery;
+
+require_once 'modules/ElasticSearch/ElasticSearchEngine.php';
+require_once 'modules/ElasticSearch/ElasticSearchClientBuilder.php';
+
+class ElasticSearchEngineTest extends \SuiteCRM\Search\SearchTestAbstract
+{
+    public function testValidateQuery()
+    {
+        $engine = new ElasticSearchEngine();
+        $str = " HeLlO Search ";
+        $exp = "hello search";
+        $query = SearchQuery::fromString($str);
+
+        $this->invokeMethod($engine, 'validateQuery', [&$query]);
+
+        self::assertEquals($exp, $query->getSearchString());
+    }
+
+    public function testValidateQuery2()
+    {
+        $engine = new ElasticSearchEngine();
+        $query = SearchQuery::fromString("");
+
+        try {
+            $this->invokeMethod($engine, 'validateQuery', [&$query]);
+        } catch (MasterSearchInvalidRequestException $e) {
+            return; // all good!
+        }
+
+        $this->fail("An exception should have been thrown here. The string was null.");
+    }
+
+    public function testCreateSearchParams1()
+    {
+        $engine = new ElasticSearchEngine();
+        $query = SearchQuery::fromString("hello search", 30, 5);
+
+        $expectedParams = [
+            'index' => 'main',
+            'body' => [
+                'size' => 30,
+                'from' => 5,
+                'stored_fields' => [],
+                'query' => ["bool" => ["must" => [
+                    ["regexp" => ["_all" => ".*hello.*"]],
+                    ["regexp" => ["_all" => ".*search.*"]]
+                ]]]
+            ]
+        ];
+
+        $params = $this->invokeMethod($engine, 'createSearchParams', [$query]);
+
+        $this->assertEquals($expectedParams, $params);
+    }
+
+    public function testCreateSearchParams2()
+    {
+        $engine = new ElasticSearchEngine();
+        $query = SearchQuery::fromString("test", 5, 0);
+
+        $expectedParams = [
+            'index' => 'main',
+            'body' => [
+                'size' => 5,
+                'from' => 0,
+                'stored_fields' => [],
+                'query' => ["bool" => ["must" => [
+                    ["regexp" => ["_all" => ".*test.*"]],
+                ]]]
+            ]
+        ];
+
+        $params = $this->invokeMethod($engine, 'createSearchParams', [$query]);
+
+        $this->assertEquals($expectedParams, $params);
+    }
+
+    public function testRunElasticSearch()
+    {
+        $engine = new ElasticSearchEngine();
+        $query = SearchQuery::fromString("a");
+
+        $mockedResults = $this->getMockedHits();
+
+        $client = $this->getMockedClient($mockedResults);
+
+        $results = $params = $this->invokeMethod($engine, 'runElasticSearch', [$query, $client]);
+
+        self::assertEquals($mockedResults, $results);
+    }
+
+    /**
+     * @return array
+     */
+    private function getMockedHits()
+    {
+        $mockedResults = [
+            'took' => 5,
+            'timed_out' => false,
+            '_shards' =>
+                [
+                    'total' => 3,
+                    'successful' => 2,
+                    'skipped' => 0,
+                    'failed' => 0,
+                ],
+            'hits' =>
+                [
+                    'total' => 258,
+                    'max_score' => 1.0,
+                    'hits' =>
+                        [
+                            0 =>
+                                [
+                                    '_index' => 'main',
+                                    '_type' => 'Accounts',
+                                    '_id' => 'id1',
+                                    '_score' => 1.0,
+                                ],
+                            1 =>
+                                [
+                                    '_index' => 'main',
+                                    '_type' => 'Accounts',
+                                    '_id' => 'id2',
+                                    '_score' => 1.0,
+                                ],
+                            2 =>
+                                [
+                                    '_index' => 'main',
+                                    '_type' => 'Contacts',
+                                    '_id' => 'id3',
+                                    '_score' => 0.5,
+                                ],
+                        ]
+                ],
+        ];
+        return $mockedResults;
+    }
+
+    /**
+     * @param $mockedResults
+     * @return \Mockery\MockInterface
+     */
+    private function getMockedClient($mockedResults)
+    {
+        $client = Mockery::mock('Elasticsearch\Client');
+
+        $client
+            ->shouldReceive('search')
+            ->once()
+            ->andReturn($mockedResults);
+
+        return $client;
+    }
+
+    public function testParseHits()
+    {
+        $engine = new ElasticSearchEngine();
+
+        $mockedHits = $this->getMockedHits();
+
+        $expectedResults = $this->getExpectedResultsForMockedHits();
+
+        $results = $params = $this->invokeMethod($engine, 'parseHits', [$mockedHits]);
+
+        self::assertEquals($expectedResults, $results);
+    }
+
+    /**
+     * @return array
+     */
+    private function getExpectedResultsForMockedHits()
+    {
+        $expectedResults = [
+            'Accounts' => [
+                'id1',
+                'id2'
+            ],
+            'Contacts' => [
+                'id3',
+            ]
+        ];
+
+        return $expectedResults;
+    }
+
+    public function testParseEmptyHits()
+    {
+        $engine = new ElasticSearchEngine();
+
+        $mockedHits = $this->getMockedHitsEmpty();
+
+        $expectedResults = [];
+
+        $results = $params = $this->invokeMethod($engine, 'parseHits', [$mockedHits]);
+
+        self::assertEquals($expectedResults, $results);
+    }
+
+    private function getMockedHitsEmpty()
+    {
+        return [
+            'took' => 1,
+            'timed_out' => false,
+            '_shards' =>
+                [
+                    'total' => 5,
+                    'successful' => 5,
+                    'skipped' => 0,
+                    'failed' => 0,
+                ],
+            'hits' => [
+                "total" => 0,
+                "max_score" => null,
+                "hits" => [],
+            ]
+        ];
+    }
+
+    public function testSearch()
+    {
+        $engine = new ElasticSearchEngine();
+        $mockedClient = $this->getMockedClient($this->getMockedHits());
+        $expectedResults = $this->getExpectedResultsForMockedHits();
+        $query = SearchQuery::fromString("test");
+
+        $results = $engine->search($query, $mockedClient);
+
+        self::assertEquals($expectedResults, $results);
+    }
+}
