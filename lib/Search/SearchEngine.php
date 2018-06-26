5907284100d565756ad77f5a65e9e9c4d974f2ba
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-06-26 09:20:38 +0100

    Improve search classes method and documentation
    Also added a new type of exception for an invalid query

--- lib/Search/SearchEngine.php
+++ lib/Search/SearchEngine.php
@@ -12,8 +12,54 @@ namespace SuiteCRM\Search;
 abstract class SearchEngine
 {
     /**
+     * Performs a search using the given query and returns a view to be displayed.
+     *
      * @param $query SearchQuery
      * @return \SugarView
      */
-    public abstract function searchAndView($query);
+    public function searchAndView($query)
+    {
+        $this->validateQuery($query);
+
+        $this->search($query);
+
+        return $this->makeView($this->search($query));
+    }
+
+    /**
+     * Makes a default result view given a set of results.
+     *
+     * @param $results array
+     * @return \SugarView
+     */
+    public function makeView($results)
+    {
+        return new \SugarView();
+    }
+
+    /**
+     * Performs a search using the search engine and returns a list of ids in the following format:
+     *
+     * <code>
+     * [
+     *  'module1' => ['id1', 'id2', 'id3'],
+     *  'module2' => ['id4', 'id5', 'id5'],
+     * ]
+     * </code>
+     *
+     * @param SearchQuery
+     * @return array[] ids
+     */
+    public abstract function search($query);
+
+    /**
+     * This method should be extend to sanitize and standardise the request to fill all the values as they are expected
+     * to be by the `search()` method.
+     *
+     * If it is impossible to validate the query a `MasterSearchInvalidRequestException` should be thrown.
+     *
+     * @param $query SearchQuery the query to validate
+     * @throws MasterSearchInvalidRequestException if the query is not valid
+     */
+    protected abstract function validateQuery(&$query);
 }
--- lib/Search/SearchQuery.php
+++ lib/Search/SearchQuery.php
@@ -8,7 +8,21 @@
 
 namespace SuiteCRM\Search;
 
-
+/**
+ * Class SearchQuery
+ *
+ * The current format is the following:
+ *
+ * <code>
+ * [
+ *  'from' => 0,
+ *  'size' => 100,
+ *  'query' => 'search this'
+ * ]
+ * </code>
+ *
+ * @package SuiteCRM\Search
+ */
 class SearchQuery
 {
     public $query = [];
@@ -22,5 +36,60 @@ class SearchQuery
         $this->query = $query;
     }
 
+    /**
+     * The offset of the search results.
+     *
+     * @return int
+     */
+    public function getFrom()
+    {
+        return $this->query['from'];
+    }
 
+    /**
+     * The size of the search results.
+     *
+     * @return int
+     */
+    public function getSize()
+    {
+        return $this->query['size'];
+    }
+
+    /**
+     * The query string if available.
+     *
+     * If a query object is present `null` is returned.
+     *
+     * @return string|null
+     */
+    public function getSearchString()
+    {
+        if (is_string($this->query['query'])) {
+            return $this->query['query'];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Creates a query object from a query string, i.e. from a search from.
+     *
+     * `$size` and `$from` are for pagination.
+     *
+     * @param $searchString string a search string, as it would appear on a search bar
+     * @param int $size the number of results
+     * @param int $from
+     * @return SearchQuery a fully built query
+     */
+    public static function fromString($searchString, $size = 50, $from = 0)
+    {
+        $query = [
+            'from' => $from,
+            'size' => $size,
+            'query' => $searchString
+        ];
+
+        return new self($query);
+    }
 }
