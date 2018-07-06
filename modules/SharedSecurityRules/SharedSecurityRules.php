058c5b7636bd7e49f89df3bca49b0918e16f351d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-06 11:14:15 +0100

    removing duplicated definitions

--- modules/SharedSecurityRules/SharedSecurityRules.php
+++ modules/SharedSecurityRules/SharedSecurityRules.php
@@ -71,19 +71,9 @@ class SharedSecurityRules extends Basic
      */
     public $importable = false;
 
-    public $id;
-    public $name;
-    public $date_entered;
-    public $date_modified;
-    public $modified_user_id;
-    public $modified_by_name;
-    public $created_by;
-    public $created_by_name;
-    public $description;
-    public $deleted;
+    
     public $created_by_link;
     public $modified_user_link;
-    public $assigned_user_id;
     public $assigned_user_name;
     public $assigned_user_link;
     public $SecurityGroups;
