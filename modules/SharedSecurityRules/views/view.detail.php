bde25ed08148acb0f4050de419a94dd3a2379ec9
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-12 12:11:16 +0100

    applying changes

--- modules/SharedSecurityRules/views/view.detail.php
+++ modules/SharedSecurityRules/views/view.detail.php
@@ -1,88 +1,91 @@
 <?php
- /**
- * 
- * 
- * @package 
- * @copyright SalesAgility Ltd http://www.salesagility.com
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+/**
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
- * You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
- * along with this program; if not, see http://www.gnu.org/licenses
- * or write to the Free Software Foundation,Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA 02110-1301  USA
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
- * @author Salesagility Ltd <support@salesagility.com>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
 require_once 'include/MVC/View/views/view.detail.php';
 require_once 'modules/AOW_WorkFlow/aow_utils.php';
 require_once 'modules/AOR_Reports/aor_utils.php';
-class SharedSecurityRulesViewDetail extends ViewDetail {
 
-    public function __construct()
+class SharedSecurityRulesViewDetail extends ViewDetail
+{
+    private function getConditionLines()
     {
-        parent::__construct();
-    }
-
-    function display()
-    {
-        //   global $app_list_strings;
-        //   $matrix = new SharedSecurityRules();
-        //   $bean = BeanFactory::getBean($this->bean->report_module);
-        //    $app_list_strings['aomr_field_list'] = $matrix->getFieldDefs($bean->field_defs, $this->bean->report_module);
-        parent::display();
-    }
-    private function getConditionLines(){
-        if(!$this->bean->id){
+        if (!$this->bean->id) {
             return array();
         }
-        $sql = "SELECT id FROM sharedsecurityrulesconditions WHERE sa_shared_sec_rules_id = '".$this->bean->id."' AND deleted = 0 ORDER BY condition_order ASC";
+        $sql = "SELECT id FROM sharedsecurityrulesconditions WHERE sa_shared_sec_rules_id = '" . $this->bean->id . "' AND deleted = 0 ORDER BY condition_order ASC";
         $result = $this->bean->db->query($sql);
         $conditions = array();
         while ($row = $this->bean->db->fetchByAssoc($result)) {
-            //$condition_name = new AOMR_Condition();
             $condition_name = new SharedSecurityRulesConditions();
 
             $condition_name->retrieve($row['id']);
-            //      if(!$condition_name->parenthesis) {
-            //          $condition_name->module_path = implode(":", unserialize(base64_decode($condition_name->module_path)));
-            //      }
-            if($condition_name->value_type == 'Date'){
+            if ($condition_name->value_type == 'Date') {
                 $condition_name->value = unserialize(base64_decode($condition_name->value));
             }
             $condition_item = $condition_name->toArray();
 
-            if(!$condition_name->parenthesis) {
+            if (!$condition_name->parenthesis) {
                 $display = $this->getDisplayForField($condition_name->module_path, $condition_name->field, $this->bean->flow_module);
                 $condition_item['module_path_display'] = $display['module'];
                 $condition_item['field_label'] = $display['field'];
             }
-            if(isset($conditions[$condition_item['condition_order']])) {
+            if (isset($conditions[$condition_item['condition_order']])) {
                 $conditions[] = $condition_item;
-            }
-            else {
+            } else {
                 $conditions[$condition_item['condition_order']] = $condition_item;
             }
         }
         return $conditions;
     }
+
     public function preDisplay()
     {
         $conditions = $this->getConditionLines();
-        echo "<script>var conditionLines = ".json_encode($conditions)."</script>";
+        echo "<script>var conditionLines = " . json_encode($conditions) . "</script>";
 
 
         parent::preDisplay();
-
-
     }
 
     private function getDisplayForField($modulePath, $field, $reportModule)
@@ -118,13 +121,11 @@ class SharedSecurityRulesViewDetail extends ViewDetail {
         $fieldDisplay = $currentBean->field_name_map[$field]['vname'];
         $fieldDisplay = translate($fieldDisplay, $currentBean->module_dir);
         $fieldDisplay = trim($fieldDisplay, ':');
-        foreach($modulePathDisplay as &$module) {
+        foreach ($modulePathDisplay as &$module) {
             $module = isset($app_list_strings['aor_moduleList'][$module]) ? $app_list_strings['aor_moduleList'][$module] : (
-            isset($app_list_strings['moduleList'][$module]) ? $app_list_strings['moduleList'][$module] : $module
-            );
+                    isset($app_list_strings['moduleList'][$module]) ? $app_list_strings['moduleList'][$module] : $module
+                    );
         }
         return array('field' => $fieldDisplay, 'module' => str_replace(' ', '&nbsp;', implode(' : ', $modulePathDisplay)));
     }
-
-
 }
