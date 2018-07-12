d0f1b5508b019332c930f5521c9ffcaba5164b27
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-12 12:17:10 +0100

    applying changes

--- modules/SharedSecurityRulesActions/vardefs.php
+++ modules/SharedSecurityRulesActions/vardefs.php
@@ -5,7 +5,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2017 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -16,7 +16,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -34,92 +34,96 @@
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 $dictionary['SharedSecurityRulesActions'] = array(
     'table' => 'sharedsecurityrulesactions',
     'audited' => true,
     'inline_edit' => true,
     'duplicate_merge' => true,
-    'fields' => array (
+    'fields' => array(
         'sa_shared_security_rules_id' =>
-            array (
-                'required' => false,
-                'name' => 'sa_shared_security_rules_id',
-                'vname' => 'LBL_WORKFLOW_ID',
-                'type' => 'id',
-                'massupdate' => 0,
-                'comments' => '',
-                'help' => '',
-                'importable' => 'true',
-                'duplicate_merge' => 'disabled',
-                'duplicate_merge_dom_value' => 0,
-                'audited' => false,
-                'reportable' => false,
-                'unified_search' => false,
-                'merge_filter' => 'disabled',
-                'len' => 36,
-                'size' => '20',
-            ),
+        array(
+            'required' => false,
+            'name' => 'sa_shared_security_rules_id',
+            'vname' => 'LBL_WORKFLOW_ID',
+            'type' => 'id',
+            'massupdate' => 0,
+            'comments' => '',
+            'help' => '',
+            'importable' => 'true',
+            'duplicate_merge' => 'disabled',
+            'duplicate_merge_dom_value' => 0,
+            'audited' => false,
+            'reportable' => false,
+            'unified_search' => false,
+            'merge_filter' => 'disabled',
+            'len' => 36,
+            'size' => '20',
+        ),
         'action_order' =>
-            array (
-                'required' => false,
-                'name' => 'action_order',
-                'vname' => 'LBL_ORDER',
-                'type' => 'int',
-                'massupdate' => 0,
-                'comments' => '',
-                'help' => '',
-                'importable' => 'true',
-                'duplicate_merge' => 'disabled',
-                'duplicate_merge_dom_value' => '0',
-                'audited' => false,
-                'reportable' => true,
-                'unified_search' => false,
-                'merge_filter' => 'disabled',
-                'len' => '255',
-                'size' => '20',
-                'enable_range_search' => false,
-                'disable_num_format' => '',
-            ),
+        array(
+            'required' => false,
+            'name' => 'action_order',
+            'vname' => 'LBL_ORDER',
+            'type' => 'int',
+            'massupdate' => 0,
+            'comments' => '',
+            'help' => '',
+            'importable' => 'true',
+            'duplicate_merge' => 'disabled',
+            'duplicate_merge_dom_value' => '0',
+            'audited' => false,
+            'reportable' => true,
+            'unified_search' => false,
+            'merge_filter' => 'disabled',
+            'len' => '255',
+            'size' => '20',
+            'enable_range_search' => false,
+            'disable_num_format' => '',
+        ),
         'action' =>
-            array (
-                'required' => false,
-                'name' => 'action',
-                'vname' => 'LBL_ACTION',
-                'type' => 'enum',
-                'massupdate' => 0,
-                'comments' => '',
-                'help' => '',
-                'importable' => 'true',
-                'duplicate_merge' => 'disabled',
-                'duplicate_merge_dom_value' => '0',
-                'audited' => false,
-                'reportable' => true,
-                'unified_search' => false,
-                'merge_filter' => 'disabled',
-                'len' => 100,
-                'size' => '20',
-                'options' => 'aow_actions_list',
-                'studio' => 'visible',
-                'dependency' => false,
-            ),
+        array(
+            'required' => false,
+            'name' => 'action',
+            'vname' => 'LBL_ACTION',
+            'type' => 'enum',
+            'massupdate' => 0,
+            'comments' => '',
+            'help' => '',
+            'importable' => 'true',
+            'duplicate_merge' => 'disabled',
+            'duplicate_merge_dom_value' => '0',
+            'audited' => false,
+            'reportable' => true,
+            'unified_search' => false,
+            'merge_filter' => 'disabled',
+            'len' => 100,
+            'size' => '20',
+            'options' => 'aow_actions_list',
+            'studio' => 'visible',
+            'dependency' => false,
+        ),
         'parameters' =>
-            array (
-                'name' => 'parameters',
-                'type' => 'longtext',
-                'vname' => 'LBL_PARAMETERS',
-                'isnull' => true,
-            ),
-),
-    'relationships' => array (
-),
+        array(
+            'name' => 'parameters',
+            'type' => 'longtext',
+            'vname' => 'LBL_PARAMETERS',
+            'isnull' => true,
+        ),
+    ),
+    'relationships' => array(
+    ),
     'optimistic_locking' => true,
     'unified_search' => true,
 );
 if (!class_exists('VardefManager')) {
-        require_once('include/SugarObjects/VardefManager.php');
+    require_once('include/SugarObjects/VardefManager.php');
 }
-VardefManager::createVardef('SharedSecurityRulesActions', 'SharedSecurityRulesActions', array('basic','assignable','security_groups'));
+VardefManager::createVardef('SharedSecurityRulesActions', 'SharedSecurityRulesActions', array('basic', 'assignable', 'security_groups'));
