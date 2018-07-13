7ee80acbdcfd6e363555dc05df5f2af679f59f57
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-13 03:39:18 +0100

    fixing a test

--- tests/unit/modules/InboundEmail/InboundEmailTest.php
+++ tests/unit/modules/InboundEmail/InboundEmailTest.php
@@ -544,7 +544,7 @@ class InboundEmailTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         //test for default/imap
         $result = $inboundEmail->getFormattedHeaders(1);
-        $this->assertSame("<table cellspacing='0' cellpadding='2' border='0' width='100%'></table>", $result);
+        $this->assertSame(null, $result);
 
         //test for pop3
         $inboundEmail->protocol = 'pop3';
