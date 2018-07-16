936f1ef4fd92ebb3e11e50f258fe95a67450ab0c
Dillon-Brown <dillon.brown@salesagility.com>
2018-07-16 07:28:33 +0100

    Conflict

--- include/InlineEditing/inlineEditing.js
+++ include/InlineEditing/inlineEditing.js
@@ -1,10 +1,9 @@
-/**
- *
+/*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- *
- * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
+ * Copyright (C) 2011 - 2015 Salesagility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -15,7 +14,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -33,9 +32,9 @@
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- */
+ * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
+ * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ ********************************************************************************/
 
 buildEditField();
 
@@ -187,7 +186,6 @@ function buildEditField(){
 
     var touchtime = 0;
     $(".inlineEdit").dblclick(function(e) {
-    //$('.inlineEdit').on('click', function(e) {
         if(touchtime == 0) {
             //set first click
             touchtime = new Date().getTime();
@@ -267,13 +265,12 @@ function clickedawayclose(field,id,module, type){
 }
 
 $(document).on('click', function (e) {
-    if(clickListenerActive) {
+    if (clickListenerActive) {
         var field = ie_field;
         var id = ie_id;
         var module = ie_module;
         var type = ie_type;
         var message_field = ie_message_field;
-        var alertFlag = true;
 
         if (!$(e.target).parents().is(".inlineEditActive, .cal_panel") && !$(e.target).hasClass("inlineEditActive")) {
             var output_value = loadFieldHTMLValue(field, id, module);
@@ -309,11 +306,18 @@ $(document).on('click', function (e) {
                 }
             }
 
-            if (user_value == outputValueParse || user_value == output_value) {
-                var alertFlag = false;
+            var date_compare = false;
+            var output_value_compare = '';
+            if (type == 'datetimecombo' || type == 'datetime' || type == 'date') {
+                if (output_value == user_value) {
+                    output_value_compare = user_value;
+                    date_compare = true;
+                }
+            } else {
+                output_value_compare = $(output_value).text();
             }
-
-            if (alertFlag) {
+            if (user_value != output_value_compare) {
+                message_field = message_field != 'undefined' ? message_field : '';
                 var r = confirm(SUGAR.language.translate('app_strings', 'LBL_CONFIRM_CANCEL_INLINE_EDITING') + ' ' + message_field);
                 if (r == true) {
                     var output = setValueClose(output_value);
@@ -494,18 +498,18 @@ function saveFieldHTML(field,module,id,value, parent_type) {
  * @returns {*}
  */
 
-function loadFieldHTML(field,module,id) {
+function loadFieldHTML(field, module, id) {
     $.ajaxSetup({"async": false});
     var result = $.getJSON('index.php',
-      {
-          'module': 'Home',
-          'action': 'getEditFieldHTML',
-          'field': field,
-          'current_module': module,
-          'id': id,
-          'view' : view,
-          'to_pdf': true
-      }
+        {
+            'module': 'Home',
+            'action': 'getEditFieldHTML',
+            'field': field,
+            'current_module': module,
+            'id': id,
+            'view': view,
+            'to_pdf': true
+        }
     );
     $.ajaxSetup({"async": true});
     if(result.responseText){
@@ -532,22 +536,22 @@ function loadFieldHTML(field,module,id) {
  * @returns {*}
  */
 
-function loadFieldHTMLValue(field,id,module) {
+function loadFieldHTMLValue(field, id, module) {
     $.ajaxSetup({"async": false});
     var result = $.getJSON('index.php',
-      {
-          'module': 'Home',
-          'action': 'getDisplayValue',
-          'field': field,
-          'current_module': module,
-          'view': view,
-          'id': id,
-          'to_pdf': true
-      }
+        {
+            'module': 'Home',
+            'action': 'getDisplayValue',
+            'field': field,
+            'current_module': module,
+            'view': view,
+            'id': id,
+            'to_pdf': true
+        }
     );
     $.ajaxSetup({"async": true});
 
-    return(result.responseText);
+    return (result.responseText);
 }
 
 /**
@@ -560,17 +564,17 @@ function loadFieldHTMLValue(field,id,module) {
  * @returns {*}
  */
 
-function getValidationRules(field,module,id){
+function getValidationRules(field, module, id) {
     $.ajaxSetup({"async": false});
     var result = $.getJSON('index.php',
-      {
-          'module': 'Home',
-          'action': 'getValidationRules',
-          'field': field,
-          'current_module': module,
-          'id': id,
-          'to_pdf': true
-      }
+        {
+            'module': 'Home',
+            'action': 'getValidationRules',
+            'field': field,
+            'current_module': module,
+            'id': id,
+            'to_pdf': true
+        }
     );
     $.ajaxSetup({"async": true});
 
@@ -594,17 +598,17 @@ function getValidationRules(field,module,id){
  * @returns {*}
  */
 
-function getRelateFieldJS(field, module, id){
+function getRelateFieldJS(field, module, id) {
     $.ajaxSetup({"async": false});
     var result = $.getJSON('index.php',
-      {
-          'module': 'Home',
-          'action': 'getRelateFieldJS',
-          'field': field,
-          'current_module': module,
-          'id': id,
-          'to_pdf': true
-      }
+        {
+            'module': 'Home',
+            'action': 'getRelateFieldJS',
+            'field': field,
+            'current_module': module,
+            'id': id,
+            'to_pdf': true
+        }
     );
     $.ajaxSetup({"async": true});
 
