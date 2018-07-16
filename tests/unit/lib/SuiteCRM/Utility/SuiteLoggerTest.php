28b6bf02206045aad544919855d5c37f34e6709d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-16 14:25:41 +0100

    Revert "mark wrong test as incomplete"
    This reverts commit 6b53332e64276a31a4d5e89d4c9129a8033f25c1.

--- tests/unit/lib/SuiteCRM/Utility/SuiteLoggerTest.php
+++ tests/unit/lib/SuiteCRM/Utility/SuiteLoggerTest.php
@@ -69,7 +69,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogError()
     {
-        $this->markTestIncomplete('Call to a member function error() on null');
         self::$logger->error('test');
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[ERROR\] test/', $lastLine, $matches);
@@ -78,7 +77,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogWarning()
     {
-        $this->markTestIncomplete('Call to a member function warning() on null');
         self::$logger->warning('test');
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[WARNING\] test/', $lastLine, $matches);
@@ -87,7 +85,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogNotice()
     {
-        $this->markTestIncomplete('Call to a member function notice() on null');
         self::$logger->notice('test');
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[NOTICE\] test/', $lastLine, $matches);
@@ -96,7 +93,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogInfo()
     {
-        $this->markTestIncomplete('Call to a member function info() on null');
         self::$logger->info('test');
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[INFO\] test/', $lastLine, $matches);
@@ -105,7 +101,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogDebug()
     {
-        $this->markTestIncomplete('Call to a member function debug() on null');
         self::$logger->debug('test');
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[DEBUG\] test/', $lastLine, $matches);
@@ -114,7 +109,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testLogInvalid()
     {
-        $this->markTestIncomplete('Call to a member function expectException() on null');
         $this->tester->expectException(
             new \Psr\Log\InvalidArgumentException(),
             function() {
@@ -125,7 +119,6 @@ class SuiteLoggerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testInterpolate()
     {
-        $this->markTestIncomplete('Call to a member function error() on null');
         self::$logger->error('test {a}', array('a' => 'apple'));
         $lastLine = $this->getLastLogMessage();
         preg_match('/\[ERROR\] test apple/', $lastLine, $matches);
