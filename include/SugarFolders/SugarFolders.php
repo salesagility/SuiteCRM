47d41de976bbbd0d167ee91281c843579d04d21f
p.ernst <peter.ernst@salesagility.com>
2018-07-20 08:52:18 +0100

    Fix bug introduced in previous PR

--- include/SugarFolders/SugarFolders.php
+++ include/SugarFolders/SugarFolders.php
@@ -609,7 +609,7 @@ class SugarFolder
 
         $grp = array();
 
-        $folders = $this->retrieveFoldersForProcessing($focusUser);
+        $folders = $this->retrieveFoldersForProcessing($focusUser, false);
         $subscriptions = $this->getSubscriptions($focusUser);
 
         foreach ($folders as $a) {
