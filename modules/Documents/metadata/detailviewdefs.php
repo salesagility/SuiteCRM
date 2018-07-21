de1cecce28fe236a4736b9f15537188908fc62ea
jsamelko <j.samelko@globalsm.com.au>
2018-07-21 20:21:13 +0800

    Additional code tidy

--- modules/Documents/metadata/detailviewdefs.php
+++ modules/Documents/metadata/detailviewdefs.php
@@ -43,48 +43,48 @@ array(
   array(
     'templateMeta' =>
     array(
-      'maxColumns' =>'2',
+      'maxColumns' => '2',
       'form' =>
       array(
         'buttons' =>
         array(
-          0 =>'EDIT',
-          1 =>'DUPLICATE',
-          2 =>'DELETE',
+          0 => 'EDIT',
+          1 => 'DUPLICATE',
+          2 => 'DELETE',
         ),
-        'headerTpl' =>'modules/Documents/tpls/detailHeader.tpl',
+        'headerTpl' => 'modules/Documents/tpls/detailHeader.tpl',
       ),
       array(
         'hidden' =>
         array(
-          0 =>'<input type="hidden" name="old_id" value="{$fields.document_revision_id.value}">',
+          0 => '<input type="hidden" name="old_id" value="{$fields.document_revision_id.value}">',
         ),
       ),
       'widths' =>
       array(
         0 =>
         array(
-          'label' =>'10',
-          'field' =>'30',
+          'label' => '10',
+          'field' => '30',
         ),
         1 =>
         array(
-          'label' =>'10',
-          'field' =>'30',
+          'label' => '10',
+          'field' => '30',
         ),
       ),
-      'useTabs' =>true,
+      'useTabs' => true,
       'tabDefs' =>
       array(
         'LBL_DOCUMENT_INFORMATION' =>
         array(
-          'newTab' =>true,
-          'panelDefault' =>'expanded',
+          'newTab' => true,
+          'panelDefault' => 'expanded',
         ),
         'LBL_PANEL_ASSIGNMENT' =>
         array(
-          'newTab' =>true,
-          'panelDefault' =>'expanded',
+          'newTab' => true,
+          'panelDefault' => 'expanded',
         ),
       ),
     ),
@@ -96,71 +96,71 @@ array(
         array(
           0 =>
           array(
-            'name' =>'filename',
+            'name' => 'filename',
             'displayParams' =>
             array(
-              'link' =>'filename',
-              'id' =>'document_revision_id',
+              'link' => 'filename',
+              'id' => 'document_revision_id',
             ),
           ),
           1 =>
           array(
-            'name' =>'status_id',
-            'label' =>'LBL_DOC_STATUS',
+            'name' => 'status_id',
+            'label' => 'LBL_DOC_STATUS',
           ),
         ),
         1 =>
         array(
           0 =>
           array(
-            'name' =>'document_name',
-            'label' =>'LBL_DOC_NAME',
+            'name' => 'document_name',
+            'label' => 'LBL_DOC_NAME',
           ),
           1 =>
           array(
-            'name' =>'revision',
-            'label' =>'LBL_DOC_VERSION',
+            'name' => 'revision',
+            'label' => 'LBL_DOC_VERSION',
           ),
         ),
         2 =>
         array(
           0 =>
           array(
-            'name' =>'template_type',
-            'label' =>'LBL_DET_TEMPLATE_TYPE',
+            'name' => 'template_type',
+            'label' => 'LBL_DET_TEMPLATE_TYPE',
           ),
           1 =>
           array(
-            'name' =>'is_template',
-            'label' =>'LBL_DET_IS_TEMPLATE',
+            'name' => 'is_template',
+            'label' => 'LBL_DET_IS_TEMPLATE',
           ),
         ),
         3 =>
         array(
-          0 =>'active_date',
-          1 =>'exp_date',
+          0 => 'active_date',
+          1 => 'exp_date',
         ),
         4 =>
         array(
-          0 =>'category_id',
-          1 =>'subcategory_id',
+          0 => 'category_id',
+          1 => 'subcategory_id',
         ),
         5 =>
         array(
-          0 =>'',
-          1 =>'',
+          0 => '',
+          1 => '',
         ),
         6 =>
         array(
-          0 =>'related_doc_name',
-          1 =>'related_doc_rev_number',
+          0 => 'related_doc_name',
+          1 => 'related_doc_rev_number',
         ),
         7 =>
         array(
-          0 =>
+          0 => 
           array(
-            'name' =>'assigned_user_name',
-            'label' =>'LBL_ASSIGNED_TO_NAME',
+            'name' => 'assigned_user_name',
+            'label' => 'LBL_ASSIGNED_TO_NAME',
           ),
         ),
       ),
@@ -170,14 +170,14 @@ array(
         array(
           0 =>
           array(
-              'name' =>'date_entered',
-              'customCode' =>'{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
+              'name' => 'date_entered',
+              'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
           ),
           1 =>
           array(
-              'name' =>'date_modified',
-              'label' =>'LBL_DATE_MODIFIED',
-              'customCode' =>'{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
+              'name' => 'date_modified',
+              'label' => 'LBL_DATE_MODIFIED',
+              'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
           ),
         ),
       ),
