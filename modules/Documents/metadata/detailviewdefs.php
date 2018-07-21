16e0170e404b7559d3e4971249ba813f2a40a03d
jsamelko <j.samelko@globalsm.com.au>
2018-07-21 20:18:28 +0800

    Enable 'Change Log' within Document Detail View
    Enables 'View Change Log' button from actions menu in Detail view when auditing enabled + additional small code-tidying changes

--- modules/Documents/metadata/detailviewdefs.php
+++ modules/Documents/metadata/detailviewdefs.php
@@ -38,150 +38,149 @@
  ********************************************************************************/
 
 $viewdefs ['Documents'] = 
-array (
-  'DetailView' => 
-  array (
-    'templateMeta' => 
-    array (
-      'maxColumns' => '2',
-      'form' => 
-      array (
-        'buttons' => 
-        array (
-          0 => 'EDIT',
-          1 => 'DUPLICATE',
-          2 => 'DELETE',
+array(
+  'DetailView' =>
+  array(
+    'templateMeta' =>
+    array(
+      'maxColumns' =>'2',
+      'form' =>
+      array(
+        'buttons' =>
+        array(
+          0 =>'EDIT',
+          1 =>'DUPLICATE',
+          2 =>'DELETE',
         ),
-        'headerTpl' => 'modules/Documents/tpls/detailHeader.tpl',
+        'headerTpl' =>'modules/Documents/tpls/detailHeader.tpl',
       ),
-      array (
-        'hidden' => 
-        array (
-          0 => '<input type="hidden" name="old_id" value="{$fields.document_revision_id.value}">',
-        ),
-      ),
-      'widths' => 
-      array (
-        0 => 
-        array (
-          'label' => '10',
-          'field' => '30',
-        ),
-        1 => 
-        array (
-          'label' => '10',
-          'field' => '30',
+      array(
+        'hidden' =>
+        array(
+          0 =>'<input type="hidden" name="old_id" value="{$fields.document_revision_id.value}">',
         ),
       ),
-      'useTabs' => true,
-      'tabDefs' => 
-      array (
-        'LBL_DOCUMENT_INFORMATION' => 
-        array (
-          'newTab' => true,
-          'panelDefault' => 'expanded',
+      'widths' =>
+      array(
+        0 =>
+        array(
+          'label' =>'10',
+          'field' =>'30',
+        ),
+        1 =>
+        array(
+          'label' =>'10',
+          'field' =>'30',
+        ),
+      ),
+      'useTabs' =>true,
+      'tabDefs' =>
+      array(
+        'LBL_DOCUMENT_INFORMATION' =>
+        array(
+          'newTab' =>true,
+          'panelDefault' =>'expanded',
         ),
         'LBL_PANEL_ASSIGNMENT' =>
-        array (
-          'newTab' => true,
-          'panelDefault' => 'expanded',
+        array(
+          'newTab' =>true,
+          'panelDefault' =>'expanded',
         ),
       ),
     ),
-    'panels' => 
-    array (
-      'lbl_document_information' => 
-      array (
-        0 => 
-        array (
-          0 => 
-          array (
-            'name' => 'filename',
-            'displayParams' => 
-            array (
-              'link' => 'filename',
-              'id' => 'document_revision_id',
+    'panels' =>
+    array(
+      'lbl_document_information' =>
+      array(
+        0 =>
+        array(
+          0 =>
+          array(
+            'name' =>'filename',
+            'displayParams' =>
+            array(
+              'link' =>'filename',
+              'id' =>'document_revision_id',
             ),
           ),
-          1 => 
-          array (
-            'name' => 'status_id',
-            'label' => 'LBL_DOC_STATUS',
+          1 =>
+          array(
+            'name' =>'status_id',
+            'label' =>'LBL_DOC_STATUS',
           ),
         ),
-        1 => 
-        array (
-          0 => 
-          array (
-            'name' => 'document_name',
-            'label' => 'LBL_DOC_NAME',
+        1 =>
+        array(
+          0 =>
+          array(
+            'name' =>'document_name',
+            'label' =>'LBL_DOC_NAME',
           ),
-          1 => 
-          array (
-            'name' => 'revision',
-            'label' => 'LBL_DOC_VERSION',
-          ),
-        ),
-        2 => 
-        array (
-          0 => 
-          array (
-            'name' => 'template_type',
-            'label' => 'LBL_DET_TEMPLATE_TYPE',
-          ),
-          1 => 
-          array (
-            'name' => 'is_template',
-            'label' => 'LBL_DET_IS_TEMPLATE',
+          1 =>
+          array(
+            'name' =>'revision',
+            'label' =>'LBL_DOC_VERSION',
           ),
         ),
-        3 => 
-        array (
-          0 => 'active_date',
-          1 => 'exp_date',
+        2 =>
+        array(
+          0 =>
+          array(
+            'name' =>'template_type',
+            'label' =>'LBL_DET_TEMPLATE_TYPE',
+          ),
+          1 =>
+          array(
+            'name' =>'is_template',
+            'label' =>'LBL_DET_IS_TEMPLATE',
+          ),
         ),
-        4 => 
-        array (
-          0 => 'category_id',
-          1 => 'subcategory_id',
+        3 =>
+        array(
+          0 =>'active_date',
+          1 =>'exp_date',
         ),
-        5 => 
-        array (
-          0 => '',
-          1 => '',
+        4 =>
+        array(
+          0 =>'category_id',
+          1 =>'subcategory_id',
         ),
-        6 => 
-        array (
-          0 => 'related_doc_name',
-          1 => 'related_doc_rev_number',
+        5 =>
+        array(
+          0 =>'',
+          1 =>'',
         ),
-        7 => 
-        array (
-          0 => 
-          array (
-            'name' => 'assigned_user_name',
-            'label' => 'LBL_ASSIGNED_TO_NAME',
+        6 =>
+        array(
+          0 =>'related_doc_name',
+          1 =>'related_doc_rev_number',
+        ),
+        7 =>
+        array(
+          0 =>
+          array(
+            'name' =>'assigned_user_name',
+            'label' =>'LBL_ASSIGNED_TO_NAME',
           ),
         ),
       ),
       'LBL_PANEL_ASSIGNMENT' =>
-      array (
-        0 => 
-        array (
-          0 => 
-          array (
-              'name' => 'date_entered',
-              'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
+      array(
+        0 =>
+        array(
+          0 =>
+          array(
+              'name' =>'date_entered',
+              'customCode' =>'{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
           ),
-          1 => 
-          array (
-              'name' => 'date_modified',
-              'label' => 'LBL_DATE_MODIFIED',
-              'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
+          1 =>
+          array(
+              'name' =>'date_modified',
+              'label' =>'LBL_DATE_MODIFIED',
+              'customCode' =>'{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
           ),
         ),
       ),
     ),
   ),
 );
-
