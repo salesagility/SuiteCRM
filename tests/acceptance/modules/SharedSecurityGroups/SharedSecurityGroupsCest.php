830f4246a61b171c932b2ccb81af3e303fcfb2a9
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-23 10:41:23 +0100

    trying to fix test in travis

--- tests/acceptance/modules/SharedSecurityGroups/SharedSecurityGroupsCest.php
+++ tests/acceptance/modules/SharedSecurityGroups/SharedSecurityGroupsCest.php
@@ -152,6 +152,7 @@ class SharedSecurityGroupsCest {
         $I->waitForElementVisible('#EditView_tabs > div.panel-content', self::WAITING_DELAY); // waiting fo create page
         $I->fillField('#name', 'test ssg1');
         $I->selectOption('#flow_module', 'Accounts');
+        $I->wait(1);
         $I->click('#fieldTreeLeafs > ul > li:nth-child(23) > div > span'); // it should be the Name field of Account module
         $I->selectOption('#aor_conditions_operator\5b 0\5d', 'Contains');
         $I->fillField('#aor_conditions_value\5b 0\5d', 'foo');
