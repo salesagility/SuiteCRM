e7366f01b290557c444a70eb3f85225d8dbcd57e
Vittorio Iocolano <pryderi.mail@gmail.com>
2018-07-27 08:55:27 +0100

    Add support for custom search engines

--- lib/Search/MasterSearch.php
+++ lib/Search/MasterSearch.php
@@ -60,6 +60,9 @@ class MasterSearch
     /** @var float|null the number of seconds it took to perform the previous search */
     private static $searchTime = null;
 
+    /** @var string Path to the folder where to load custom engines from */
+    const CUSTOM_ENGINES_PATH = __DIR__ . '/../../custom/Extension/SearchEngines/';
+
     /**
      * Perform a search with the given query and engine.
      *
@@ -67,13 +70,22 @@ class MasterSearch
      */
     public static function searchAndView(SearchQuery $query)
     {
-        $engine = self::fetchEngine($query->getEngine());
+        if (empty($query->getEngine())) {
+            // TODO use configurable default engine instead
+            $engine = key(self::$engines);
+        } else {
+            $engine = $query->getEngine();
+        }
 
+        $engine = self::fetchEngine($engine);
         $engine->searchAndView($query);
     }
 
     /**
      * Performs various validation and retrieves an instance of a given search engine.
+     *
+     * It first searches in the default definitions array `self::$engines`,
+     * then attempts to find a matching engine in the folder `self::CUSTOM_ENGINES_PATH`.
      *
      * @param $engineName string|SearchEngine
      * @return SearchEngine
@@ -83,24 +95,31 @@ class MasterSearch
         if (is_subclass_of($engineName, SearchEngine::class, false)) {
             return $engineName;
         } elseif (!is_string($engineName)) {
-            throw new \InvalidArgumentException("\$engine should either be a string or a SearchEngine");
+            throw new \InvalidArgumentException('$engineName should either be a string or a SearchEngine');
         }
 
-        if (!isset(self::$engines[$engineName])) {
-            throw new \RuntimeException("Unable to find search engine $engineName.");
+        if (isset(self::$engines[$engineName])) {
+            // Look in the $engines definitions first
+            $filename = self::$engines[$engineName];
+        } else {
+            // Then look in the extension folder
+            $filename = self::CUSTOM_ENGINES_PATH . $engineName . '.php';
         }
-
-        $filename = self::$engines[$engineName];
 
         if (!file_exists($filename)) {
             throw new \RuntimeException("Unable to find search file '$filename'' for engine '$engineName''.");
         }
 
         /** @noinspection PhpIncludeInspection */
-        require_once self::$engines[$engineName];
+        require_once $filename;
+
+        if (!is_subclass_of($engineName, SearchEngine::class)) {
+            throw new \RuntimeException("The provided class '$engineName' is not an instance of SearchEngine");
+        }
 
         /** @var SearchEngine $engineName */
         $engineName = new $engineName();
+
         return $engineName;
     }
 
@@ -143,7 +162,13 @@ class MasterSearch
      */
     public static function getEngines()
     {
-        return array_keys(self::$engines);
+        $default = array_keys(self::$engines);
+        $custom = [];
+        foreach (glob(self::CUSTOM_ENGINES_PATH . '*.php') as $file) {
+            $file = pathinfo($file);
+            $custom[] = $file['filename'];
+        }
+        return array_merge($default, $custom);
     }
 
     /*  @return float|null the number of seconds it took to perform the previous search */
