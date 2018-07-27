dbde618c5f9aaf25082e9c281ed841a3651b0c14
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-07-27 11:16:33 +0100

    Revert "fixing a test"
    This reverts commit 35e9826b32dc953dc8145e820655923d5e93ace7.

--- tests/tests/modules/Schedulers/SchedulerTest.php
+++ tests/tests/modules/Schedulers/SchedulerTest.php
@@ -162,7 +162,7 @@ class SchedulerTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
         $scheduler->job_interval = '*::*::*::*::3';
         $result = $scheduler->deriveDBDateTimes($scheduler);
-        $this->assertTrue(!empty($result));
+        $this->assertEquals(false, $result);
 
         $scheduler->job_interval = '0::*::3::*::*';
         $result = $scheduler->deriveDBDateTimes($scheduler);
