c1e486d675b96d137f42d372c3e428092fb9cd19
Horus68 <pauloizidoro@gmail.com>
2018-08-02 18:33:00 +0100

    fix part 1
    fixing typos

--- include/database/MysqliManager.php
+++ include/database/MysqliManager.php
@@ -6,7 +6,8 @@ if (!defined('sugarEntry') || !sugarEntry) {
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
