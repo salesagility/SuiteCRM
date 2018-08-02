7a8c3afa486ffc5a7925f3067d0fc4113917773d
Horus68 <pauloizidoro@gmail.com>
2018-08-02 19:01:56 +0100

    fix part 2
    fixing typos

--- modules/Project/views/view.list.php
+++ modules/Project/views/view.list.php
@@ -2,12 +2,11 @@
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
-
 /**
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * *
+ *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
--- modules/ProjectTask/updateProject.php
+++ modules/ProjectTask/updateProject.php
@@ -2,12 +2,12 @@
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
-
 /**
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- * * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
