34d274258b5161ba043252fa45ca947c4876da9d
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-08-08 15:14:36 +0100

    codacy: refact complexity

--- modules/SharedSecurityRules/views/view.detail.php
+++ modules/SharedSecurityRules/views/view.detail.php
@@ -113,6 +113,41 @@ class SharedSecurityRulesViewDetail extends ViewDetail
 
         parent::preDisplay();
     }
+    
+    /**
+     *
+     * @param SugarBean $currentBean
+     * @param string $field
+     */
+    protected function getCurrentBeanFieldNameMapVNameLog(SugarBean $currentBean, $field)
+    {
+        if (!isset($currentBean->field_name_map[$field])) {
+            if (!isset($field)) {
+                LoggerManager::getLogger()->warn('detailview: current bean field map index is not set');
+            } else {
+                LoggerManager::getLogger()->warn('detailview: current bean field map is not set at index: ' . $field);
+            }
+        } else {
+            LoggerManager::getLogger()->warn('detailview: current bean field map index error');
+        }
+    }
+    
+    /**
+     *
+     * @param SugarBean $currentBean
+     * @param string $field
+     * @return string
+     */
+    protected function getCurrentBeanFieldNameMapVName(SugarBean $currentBean, $field)
+    {
+        $currentBeanFieldNameMapVName = null;
+        if (!isset($currentBean->field_name_map[$field]['vname'])) {
+            $this->getCurrentBeanFieldNameMapVNameLog();
+        } else {
+            $currentBeanFieldNameMapVName = $currentBean->field_name_map[$field]['vname'];
+        }
+        return $currentBeanFieldNameMapVName;
+    }
 
     /**
      *
@@ -157,22 +192,9 @@ class SharedSecurityRulesViewDetail extends ViewDetail
                 }
             }
             
-            $currentBeanFieldNamMapVName = null;
-            if (!isset($currentBean->field_name_map[$field]['vname'])) {
-                if (!isset($currentBean->field_name_map[$field])) {
-                    if (!isset($field)) {
-                        LoggerManager::getLogger()->warn('detailview: current bean field map index is not set');
-                    } else {
-                        LoggerManager::getLogger()->warn('detailview: current bean field map is not set at index: ' . $field);
-                    }
-                } else {
-                    LoggerManager::getLogger()->warn('detailview: current bean field map index error');
-                }
-            } else {
-                $currentBeanFieldNamMapVName = $currentBean->field_name_map[$field]['vname'];
-            }
+            $currentBeanFieldNameMapVName = $this->getCurrentBeanFieldNameMapVName($currentBean, $field);
             
-            $fieldDisplay = $currentBeanFieldNamMapVName;
+            $fieldDisplay = $currentBeanFieldNameMapVName;
             $fieldDisplay = translate($fieldDisplay, $currentBean->module_dir);
             $fieldDisplay = trim($fieldDisplay, ':');
             foreach ($modulePathDisplay as &$module) {
