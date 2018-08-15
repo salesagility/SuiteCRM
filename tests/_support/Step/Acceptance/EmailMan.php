8e5815adb0c2c1d0e5e1981571fba13123a6ede8
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-08-15 12:35:50 +0100

    Revert "travis? are you here?"
    This reverts commit a5a924a2df9ba4a47a4b3998a1495f8801668092.

--- tests/_support/Step/Acceptance/EmailMan.php
+++ tests/_support/Step/Acceptance/EmailMan.php
@@ -29,7 +29,6 @@ class EmailMan extends \AcceptanceTester
         $I->click('#mass_Email_config');
 
         $I->fillField('#notify_fromname', $faker->name());
-        
         $I->fillField('#notify_fromaddress', $faker->email);
         $I->click('#gmail-button');
 
