e750d38610377b864bc6d7b6888cde34a9b55672
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-21 12:09:23 +0100

    Hotfix -> hotfix-7.8.x

--- modules/AOR_Reports/AOR_Report.js
+++ modules/AOR_Reports/AOR_Report.js
@@ -68,17 +68,16 @@ function updateTimeDateFields(fieldInput, ln) {
 
 function updateHiddenReportFields(ln, _form) {
 // Fix for issue #1272 - AOR_Report module cannot update Date type parameter.
-  if ($('#aor_conditions_value' + ln).length) {
-      var conditionsValue = $('#aor_conditions_value' + ln).val();
-      var fieldValue = conditionsValue;
-      var fieldSign = conditionsValue;
-      var fieldNumber = conditionsValue;
-      var fieldTime = conditionsValue;
+  if ($("#aor_conditions_value\\["+ln+"\\]\\[0\\]").length) {
+    var fieldValue = $("#aor_conditions_value\\["+ln+"\\]\\[0\\]").val();
+    var fieldSign = $("#aor_conditions_value\\["+ln+"\\]\\[1\\]").val();
+    var fieldNumber = $("#aor_conditions_value\\["+ln+"\\]\\[2\\]").val();
+    var fieldTime = $("#aor_conditions_value\\["+ln+"\\]\\[3\\]").val();
 
-      _form.append('<input type="hidden" name="parameter_date_value['+ ln + '] value="' + fieldValue + '">');
-      _form.append('<input type="hidden" name="parameter_date_sign['+ ln + ']" value="' + fieldSign + '">');
-      _form.append('<input type="hidden" name="parameter_date_number['+ ln + ']" value="' + fieldNumber + '">');
-      _form.append('<input type="hidden" name="parameter_date_time['+ ln + ']" value="' + fieldTime + '">');
+    _form.append('<input type="hidden" name="parameter_date_value['+ ln + ']" value="' + fieldValue + '">');
+    _form.append('<input type="hidden" name="parameter_date_sign['+ ln + ']" value="' + fieldSign + '">');
+    _form.append('<input type="hidden" name="parameter_date_number['+ ln + ']" value="' + fieldNumber + '">');
+    _form.append('<input type="hidden" name="parameter_date_time['+ ln + ']" value="' + fieldTime + '">');
   }
 }
 
@@ -93,23 +92,23 @@ function localToDbFormat(index, ln, fieldInput) {
 }
 
 function appendHiddenFields(_form, ln, id) {
-    _form.append('<input type="hidden" name="parameter_id\[' + ln + '\]" value="' + id + '">');
-    var operator = $("#aor_conditions_operator\\[" + ln + "\\]").val();
-    _form.append('<input type="hidden" name="parameter_operator\[' + ln + '\]" value="' + operator + '">');
-    var fieldType = $('#aor_conditions_value_type\\[' + ln + '\\]').val();
-    _form.append('<input type="hidden" name="parameter_type[' + ln + ']" value="' + fieldType + '">');
+  _form.append('<input type="hidden" name="parameter_id\[' + ln + '\]" value="' + id + '">');
+  var operator = $("#aor_conditions_operator\\[" + ln + "\\]").val();
+  _form.append('<input type="hidden" name="parameter_operator\[' + ln + '\]" value="' + operator + '">');
+  var fieldType = $("#aor_conditions_value_type\\[" + ln + "\\]").val();
+  _form.append('<input type="hidden" name="parameter_type[' + ln + ']" value="' + fieldType + '">');
 
-    // values can be #aor_conditions_value3 or #aor_conditions_value[3]
-    var fieldInput = '';
-    if ($('#aor_conditions_value' + ln).length > 0) {
-        fieldInput = $('#aor_conditions_value' + ln).val();
-        fieldInput = updateTimeDateFields(fieldInput, ln);
-    } else {
-        fieldInput = $('#aor_conditions_value\\[' + ln + '\\]').val();
-    }
+  // values can be #aor_conditions_value3 or #aor_conditions_value[3]
+  var fieldInput = '';
+  if ($("#aor_conditions_value\\["+ln+"\\]\\[0\\]").length > 0) {
+    fieldInput = $("#aor_conditions_value\\["+ln+"\\]\\[0\\]").val();
+    fieldInput = updateTimeDateFields(fieldInput, ln);
+  } else {
+    fieldInput = $("#aor_conditions_value\\[" + ln + "\\]").val();
+  }
 
-    _form.append('<input type="hidden" name="parameter_value[' + ln + ']" value="' + fieldInput + '">');
-    updateHiddenReportFields(ln, _form);
+  _form.append('<input type="hidden" name="parameter_value[' + ln + ']" value="' + fieldInput + '">');
+  updateHiddenReportFields(ln, _form);
 }
 
 function addParametersToForm(action) {
@@ -172,18 +171,26 @@ function changeReportPage(record, offset, group_value, table_id) {
     var fieldType = $('#aor_conditions_value_type\\[' + ln + '\\]').val();
     query += "&parameter_type[]=" + fieldType;
     var fieldInput = '';
-    if ($('#aor_conditions_value' + ln).length > 0) {
-        fieldInput = $('#aor_conditions_value' + ln).val();
-        fieldInput = updateTimeDateFields(fieldInput, ln);
+    if ($("#aor_conditions_value\\["+ln+"\\]\\[0\\]").length > 0) {
+      var fieldValue = $("#aor_conditions_value\\["+ln+"\\]\\[0\\]").val();
+      query += "&parameter_date_value[]=" + fieldValue;
+      var fieldSign = $("#aor_conditions_value\\["+ln+"\\]\\[1\\]").val();
+      query += "&parameter_date_sign[]=" + fieldSign;
+      var fieldNumber = $("#aor_conditions_value\\["+ln+"\\]\\[2\\]").val();
+      query += "&parameter_date_number[]=" + fieldNumber;
+      var fieldTime = $("#aor_conditions_value\\["+ln+"\\]\\[3\\]").val();
+      query += "&parameter_date_time[]=" + fieldTime;
+      fieldInput = $("#aor_conditions_value\\["+ln+"\\]\\[0\\]").val();
+      fieldInput = updateTimeDateFields(fieldInput, ln);
     } else {
-        fieldInput = $('#aor_conditions_value\\[' + ln + '\\]').val();
+      fieldInput = $('#aor_conditions_value\\[' + ln + '\\]').val();
     }
     query += "&parameter_value[]=" + fieldInput;
   });
 
   $.get(query).done(
     function (data) {
-      $('#report_table_' + table_id).replaceWith(data);
+      $('#report_table_' + table_id + group_value).replaceWith(data);
     }
   );
 }
