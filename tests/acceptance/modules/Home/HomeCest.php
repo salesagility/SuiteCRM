43700f2963e81ea42f101db1a2a5ac4d71ed3e1a
samus-aran <ashjayne.nicolson@gmail.com>
2018-09-17 14:58:33 +0100

    Fixing Acceptances test for adding and removing dashlets

--- tests/acceptance/modules/Home/HomeCest.php
+++ tests/acceptance/modules/Home/HomeCest.php
@@ -33,7 +33,7 @@ class HomeCest
      * @param \Step\Acceptance\Dashboard $dashboard
      * @param \Helper\WebDriverHelper $webDriverHelper
      *
-     * As a user I want to see the due date on the activities module
+     * As a user I want to see a chart added to my dashboard
      */
     public function testCreateChartsDashlet(
         \AcceptanceTester $I,
@@ -51,11 +51,22 @@ class HomeCest
         $I->loginAsAdmin();
         $dashboard->waitForDashboardVisible();
         $detailView->clickActionMenuItem('Add Dashlets');
-        $I->waitForText('Charts');
-        $I->click('Charts');
+        $I->wait(1);
+        $I->click('#chartCategory');
         $I->click('All Opportunities By Lead Source By Outcome');
         $I->click('Close');
         $dashboard->waitForDashboardVisible();
+        $I->wait(1);
         $I->see('All Opportunities By Lead Source By Outcome');
+        $dashboardID = $I->grabAttributeFrom('descendant-or-self::div[@class="dashletPanel"]', 'id');
+        if ($dashboardID != "") {
+            $dashboardID = str_replace("dashlet_entire_", "", $dashboardID);
+        }
+        $I->comment("The All Opportunities By Lead Source By Outcome dashboard ID is: ".$dashboardID);
+        $I->click('//*[@id="dashlet_header_'.$dashboardID.'"]/div[2]/table/tbody/tr/td[2]/div/a[3]/span');
+        $I->wait(1);
+        $detailView->acceptPopup();
+        $I->wait(1);
+        $I->dontSee('All Opportunities By Lead Source By Outcome');
     }
 }
