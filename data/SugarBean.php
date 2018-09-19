83dc281c103d414f9a1cb39346f6ddad1c588a7e
Hieu Hoang <32474581+hieuhoanghd@users.noreply.github.com>
2018-09-19 18:12:47 +0700

    Fixed 6150
    https://github.com/salesagility/SuiteCRM/issues/6150

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -1595,6 +1595,10 @@ class SugarBean
                                 " FROM " . $templates[$child_info['parent_type']]->table_name .
                                 " WHERE id IN ('$childInfoParentId'";
                         }
+                    } elseif(isset($parent_child_map[$child_info['parent_id']])) {
+                        if (empty($parent_child_map[$child_info['parent_id']])) {
+                            $queries[$child_info['parent_type']] .= " ,'{$child_info['parent_id']}'";
+                        }
                     }
 
                     if (isset($child_info['parent_id'])) {
