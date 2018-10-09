7038268ebde97cc75518ca95ed6ccbcb84408fa9
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-10-09 10:18:17 +0100

    adding a comment explanation

--- metadata/emails_beansMetaData.php
+++ metadata/emails_beansMetaData.php
@@ -291,6 +291,7 @@ $dictionary['emails_beans'] = array('table' => 'emails_beans',
 $dictionary['emails_text'] = array(
     'table' => 'emails_text',
     'comment' => 'Large email text fields',
+    // email text using InnoDB as default. Add following code to use MyISAM:
 //    'mysqlengine' => 'MyISAM',
 //    'engine' => 'MyISAM',
     'fields' => array(
