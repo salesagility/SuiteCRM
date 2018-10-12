8f28d64f90dc28561c5a2c75cc7a4b1db0fbeb80
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-10-12 15:06:46 +0100

    removing incomplete test (should complete)

--- tests/unit/codecept/modules/InboundEmail/InboundEmailTest.php
+++ tests/unit/codecept/modules/InboundEmail/InboundEmailTest.php
@@ -44,32 +44,33 @@ class InboundEmailTest extends SuiteCRM\StateCheckerUnitAbstract
     
     public function testFindOptimumSettingsOk()
     {
-        
-//        $state = new SuiteCRM\StateSaver();
-//        $state->pushErrorLevel('errs');
-        
-        $fakes = new ImapHandlerFakeData();
-        $fakes->add('isAvailable', null, true);  // <-- when the code calls ImapHandlerInterface::isAvailable([null]), it will return true
-        $fakes->add('setTimeout', [1, 60], true);
-        $fakes->add('setTimeout', [2, 60], true);
-        $fakes->add('setTimeout', [3, 60], true);
-        $fakes->add('getErrors', null, false);
-        $fakes->add('open', ["{:/service=/notls/novalidate-cert/secure}", null, null, 0, 0, []], function(){
-            return fopen('fakeImapResource', 'w+');
-        });
-        $fakes->add('getLastError', null, false);
-        $fakes->add('getAlerts', null, false);
-        $fakes->add('getConnection', null, function(){
-            return fopen('fakeImapResource', 'w+');
-        });
-        //$fakes->add('getMailboxes', [], []); // TODO: !@#
-        
-        $imap = new ImapHandlerFake($fakes);
-        $inboundEmail = new InboundEmail($imap);
-        $ret = $inboundEmail->findOptimumSettings();
-        $this->assertEquals(null, $ret);
-        
-//        $state->popErrorLevel('errs');
+        // TODO:
+//        
+////        $state = new SuiteCRM\StateSaver();
+////        $state->pushErrorLevel('errs');
+//        
+//        $fakes = new ImapHandlerFakeData();
+//        $fakes->add('isAvailable', null, true);  // <-- when the code calls ImapHandlerInterface::isAvailable([null]), it will return true
+//        $fakes->add('setTimeout', [1, 60], true);
+//        $fakes->add('setTimeout', [2, 60], true);
+//        $fakes->add('setTimeout', [3, 60], true);
+//        $fakes->add('getErrors', null, false);
+//        $fakes->add('open', ["{:/service=/notls/novalidate-cert/secure}", null, null, 0, 0, []], function(){
+//            return fopen('fakeImapResource', 'w+');
+//        });
+//        $fakes->add('getLastError', null, false);
+//        $fakes->add('getAlerts', null, false);
+//        $fakes->add('getConnection', null, function(){
+//            return fopen('fakeImapResource', 'w+');
+//        });
+//        //$fakes->add('getMailboxes', [], []); // TODO: !@#
+//        
+//        $imap = new ImapHandlerFake($fakes);
+//        $inboundEmail = new InboundEmail($imap);
+//        $ret = $inboundEmail->findOptimumSettings();
+//        $this->assertEquals(null, $ret);
+//        
+////        $state->popErrorLevel('errs');
     }
     
     // ------------------------------------------------------------
