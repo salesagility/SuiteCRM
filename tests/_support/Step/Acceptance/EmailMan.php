9d406f21c50afc598951f381ea86a6879ce87799
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-10-18 11:05:12 +0100

    removing old idiotic stuff

--- tests/_support/Step/Acceptance/EmailMan.php
+++ tests/_support/Step/Acceptance/EmailMan.php
@@ -21,23 +21,24 @@ class EmailMan extends \AcceptanceTester
      */
     public function createEmailSettings()
     {
-        $I = new NavigationBar($this->getScenario());
-        $EditView = new EditView($this->getScenario());
-        $faker = $this->getFaker();
-
-        $I->clickUserMenuItem('#admin_link');
-        $I->click('#mass_Email_config');
-
-        $I->fillField('#notify_fromname', $faker->name());
-        $I->fillField('#notify_fromaddress', $faker->email);
-        $I->click('#gmail-button');
-
-        $I->checkOption('#mail_smtpauth_req');
-        $I->fillField('#mail_smtpuser', $faker->email);
-        $I->executeJS('SUGAR.util.setEmailPasswordEdit(\'mail_smtppass\')');
-        $I->fillField('#mail_smtppass', $faker->email);
-        $I->checkOption('#notify_allow_default_outbound');
-
-        $EditView->clickSaveButton();
+        // TODO: DO NOT, __NEVER__ USE NON-WORKING FUNCIOTNS LIKE IMAP IN TESTS (Even if it's using random fake data!!!)
+//        $I = new NavigationBar($this->getScenario());
+//        $EditView = new EditView($this->getScenario());
+//        $faker = $this->getFaker();
+//
+//        $I->clickUserMenuItem('#admin_link');
+//        $I->click('#mass_Email_config');
+//
+//        $I->fillField('#notify_fromname', $faker->name());
+//        $I->fillField('#notify_fromaddress', $faker->email);
+//        $I->click('#gmail-button');
+//
+//        $I->checkOption('#mail_smtpauth_req');
+//        $I->fillField('#mail_smtpuser', $faker->email);
+//        $I->executeJS('SUGAR.util.setEmailPasswordEdit(\'mail_smtppass\')');
+//        $I->fillField('#mail_smtppass', $faker->email);
+//        $I->checkOption('#notify_allow_default_outbound');
+//
+//        $EditView->clickSaveButton();
     }
 }
--- tests/_support/Step/Acceptance/InboundEmailTester.php
+++ tests/_support/Step/Acceptance/InboundEmailTester.php
@@ -20,19 +20,20 @@ class InboundEmailTester extends \AcceptanceTester
      */
     public function createBounceEmail()
     {
-        $I = new NavigationBar($this->getScenario());
-        $EditView = new EditView($this->getScenario());
-        $sideBar = new SideBar($this->getScenario());
-        $faker = $this->getFaker();
-
-        $I->clickUserMenuItem('#admin_link');
-        $I->click('#mailboxes');
-        $sideBar->clickSideBarAction('New Bounce Handling Account');
-
-        $I->click('#prefill_gmail_defaults_link');
-        $I->fillField('#name', 'Test_BounceHandling');
-        $I->fillField('#email_user', $faker->name);
-        $I->fillField('#email_password', $faker->name);
-        $EditView->clickSaveButton();
+        // TODO: DO NOT, __NEVER__ USE NON-WORKING FUNCIOTNS LIKE IMAP IN TESTS (Even if it's using random fake data!!!)
+//        $I = new NavigationBar($this->getScenario());
+//        $EditView = new EditView($this->getScenario());
+//        $sideBar = new SideBar($this->getScenario());
+//        $faker = $this->getFaker();
+//
+//        $I->clickUserMenuItem('#admin_link');
+//        $I->click('#mailboxes');
+//        $sideBar->clickSideBarAction('New Bounce Handling Account');
+//
+//        $I->click('#prefill_gmail_defaults_link');
+//        $I->fillField('#name', 'Test_BounceHandling');
+//        $I->fillField('#email_user', $faker->name);
+//        $I->fillField('#email_password', $faker->name);
+//        $EditView->clickSaveButton();
     }
 }
