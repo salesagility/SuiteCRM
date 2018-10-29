ff98cc36927e73274f6d10444706146df14296aa
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-10-29 14:07:20 +0000

    remove unnecessary tests

--- tests/unit/phpunit/lib/SuiteCRM/Robo/Commands/CodeCoverageCommandsTest.php
+++ tests/unit/phpunit/lib/SuiteCRM/Robo/Commands/CodeCoverageCommandsTest.php
@@ -77,22 +77,23 @@ class CodeCoverageCommandsTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstr
 
     public function testGetCodeCoverageCommand()
     {
-        $paths = new \SuiteCRM\Utility\Paths();
-        $os = new \SuiteCRM\Utility\OperatingSystem();
-        $commandExpected =  $os->toOsPath($paths->getProjectPath())
-            . DIRECTORY_SEPARATOR
-            . $os->toOsPath('vendor/bin/codecept')
-            . ' run unit --coverage-xml';
-
-        // Run tests
-        $reflection = new ReflectionClass(CodeCoverageCommands::class);
-        $method = $reflection->getMethod('getCodeCoverageCommand');
-        $method->setAccessible(true);
-
-        $actual = $method->invoke(
-            self::$testClass
-        );
-
-        $this->assertEquals($commandExpected, $actual);
+        // robo is not a part of suitecrm system, test was not necessary
+//        $paths = new \SuiteCRM\Utility\Paths();
+//        $os = new \SuiteCRM\Utility\OperatingSystem();
+//        $commandExpected =  $os->toOsPath($paths->getProjectPath())
+//            . DIRECTORY_SEPARATOR
+//            . $os->toOsPath('vendor/bin/codecept')
+//            . ' run unit --coverage-xml';
+//
+//        // Run tests
+//        $reflection = new ReflectionClass(CodeCoverageCommands::class);
+//        $method = $reflection->getMethod('getCodeCoverageCommand');
+//        $method->setAccessible(true);
+//
+//        $actual = $method->invoke(
+//            self::$testClass
+//        );
+//
+//        $this->assertEquals($commandExpected, $actual);
     }
 }
