768a1701a874419c2ebad55e3de5c7fc038bf1be
Dillon-Brown <dillon.brown@salesagility.com>
2018-12-06 14:37:54 +0000

    Add tests

--- tests/_support/Step/Acceptance/InboundEmailTester.php
+++ tests/_support/Step/Acceptance/InboundEmailTester.php
@@ -20,20 +20,19 @@ class InboundEmailTester extends \AcceptanceTester
      */
     public function createBounceEmail()
     {
-        // TODO: DO NOT, __NEVER__ USE NON-WORKING FUNCIOTNS LIKE IMAP IN TESTS (Even if it's using random fake data!!!)
-//        $I = new NavigationBar($this->getScenario());
-//        $EditView = new EditView($this->getScenario());
-//        $sideBar = new SideBar($this->getScenario());
-//        $faker = $this->getFaker();
-//
-//        $I->clickUserMenuItem('#admin_link');
-//        $I->click('#mailboxes');
-//        $sideBar->clickSideBarAction('New Bounce Handling Account');
-//
-//        $I->click('#prefill_gmail_defaults_link');
-//        $I->fillField('#name', 'Test_BounceHandling');
-//        $I->fillField('#email_user', $faker->name);
-//        $I->fillField('#email_password', $faker->name);
-//        $EditView->clickSaveButton();
+        $I = new NavigationBar($this->getScenario());
+        $EditView = new EditView($this->getScenario());
+        $sideBar = new SideBar($this->getScenario());
+        $faker = $this->getFaker();
+
+        $I->clickUserMenuItem('#admin_link');
+        $I->click('#mailboxes');
+        $sideBar->clickSideBarAction('New Bounce Handling Account');
+
+        $I->click('#prefill_gmail_defaults_link');
+        $I->fillField('#name', 'Test_BounceHandling');
+        $I->fillField('#email_user', $faker->name);
+        $I->fillField('#email_password', $faker->name);
+        $EditView->clickSaveButton();
     }
 }
