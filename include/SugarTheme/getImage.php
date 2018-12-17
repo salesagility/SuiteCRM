4e6ea5a37d9bb2e5d471c9b7609a569b0b08d75e
Dillon Brown <26431166+Dillon-Brown@users.noreply.github.com>
2018-12-17 16:04:31 +0000

    Review

--- include/SugarTheme/getImage.php
+++ include/SugarTheme/getImage.php
@@ -1,11 +1,11 @@
 <?php
-if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
-
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2014 Salesagility Ltd.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -16,7 +16,7 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -34,11 +34,15 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
-if(stristr($_REQUEST['imageName'], 'attachment') !== FALSE) {
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+if(stristr($_REQUEST['imageName'], 'attachment') !== false) {
     $break = true;
 }
 
@@ -111,12 +115,15 @@ if (($ifmod || $iftag) && ($ifmod !== false && $iftag !== false)) {
 header("Last-Modified: ".gmdate('D, d M Y H:i:s \G\M\T', $last_modified_time));
 
 // now send the content
-if ( substr($filename,-3) == 'gif' )
+if (substr($filename, -3) === 'gif') {
     header("Content-Type: image/gif");
-elseif ( substr($filename,-3) == 'svg' )
+}
+elseif (substr($filename, -3) === 'svg') {
     header("Content-Type: image/svg+xml");
-elseif ( substr($filename,-3) == 'png' )
+}
+elseif (substr($filename, -3) === 'png') {
     header("Content-Type: image/png");
+}
 
 if(!defined('TEMPLATE_URL')) {
     if(!file_exists($filename)) {
