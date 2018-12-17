8ad334cc2248d8b36181292e7722f35e5edab840
Gyula Madarasz <gyula.madarasz@salesagility.com>
2018-12-17 10:46:14 +0000

    typo fix

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -3661,7 +3661,7 @@ $app_strings['MSG_BROWSER_NOTIFICATIONS_UNSUPPORTED'] = 'This browser does not s
 
 $app_strings['IMAP_HANDLER_ERROR'] = 'ERROR: {error}; key was: "{key}".';
 $app_strings['IMAP_HANDLER_SUCCESS'] = 'OK: test settings changed to "{key}"';
-$app_strings['IMAP_HANDLER_ERROR_INVALID_REQUEST'] = 'Invalud request, use "{var}" value.';
+$app_strings['IMAP_HANDLER_ERROR_INVALID_REQUEST'] = 'Invalid request, use "{var}" value.';
 $app_strings['IMAP_HANDLER_ERROR_UNKNOWN_BY_KEY'] = 'Unknown error occured, key "{key}" not saved.';
 $app_strings['IMAP_HANDLER_ERROR_NO_TEST_SET'] = 'Test settings does not exists.';
 $app_strings['IMAP_HANDLER_ERROR_NO_KEY'] = 'Key not found.';
