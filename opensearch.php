56c09d88b33d90a1d6c77029f8bb365886f1f66e
Frank Sagurna <fgunni@01019freenet.de>
2018-12-18 16:35:22 +0100

    File for opensearch xml creation

---
+++ opensearch.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined('sugarEntry')) {
+    define('sugarEntry', true);
+}
+require_once 'include/entryPoint.php';
+
+$sugar_view=new SugarView();
+$favicon = $sugar_view->getFavicon();
+$shortname = $GLOBALS['system_config']->settings['system_name'];
+$searchuri="{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['SERVER_NAME']}" . dirname($_SERVER['REQUEST_URI']). "/index.php?action=UnifiedSearch&amp;module=Home&amp;search_form=false&amp;advanced=false&amp;query_string={searchTerms}";
+
+header("Content-type: text/xml");
+print <<<EOF
+<?xml version="1.0" encoding="UTF-8" ?> 
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/"> 
+    <ShortName>{$shortname}</ShortName>  
+    <Description>SuiteCRM Search Provider</Description> 
+    <InputEncoding>UTF-8</InputEncoding> 
+    <Image width="16" height="16" type="image/x-icon">{$favicon['url']}</Image> 
+    <Url type="text/html" template="{$searchuri}"/>
+</OpenSearchDescription>
+EOF;
