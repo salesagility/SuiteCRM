67b645271bcf314fa1a6a3ae9e9053cd5708db66
Jyoti Singh Raghav <jyoti.raghav@livpure.in>
2018-12-18 11:23:27 +0530

    #3727 fix

---
+++ custom/modules/Emails/delEmailCls.php
@@ -0,0 +1,12 @@
+<?php
+class delEmailCls{
+	function delEmailFunc(&$bean,$event,$arguments){
+		require_once('modules/InboundEmail/InboundEmail.php');
+		$inbouneEmail = new InboundEmail();
+		$inbouneEmail->retrieve($bean->mailbox_id);
+        $uid = $bean->uid;
+		//Deleting the email on mail server also
+        $inbouneEmail->deleteMessageOnMailServer($uid);
+	}
+}
+?>
---
+++ custom/modules/Emails/logic_hooks.php
@@ -0,0 +1,14 @@
+<?php
+// Do not store anything in this file that is not part of the array or the hook version.  This file will	
+// be automatically rebuilt in the future. 
+ $hook_version = 1; 
+$hook_array = Array(); 
+// position, file, function 
+$hook_array['after_save'] = Array(); 
+$hook_array['after_save'][] = Array(10, 'Save email case updates', 'modules/AOP_Case_Updates/CaseUpdatesHook.php','CaseUpdatesHook', 'saveEmailUpdate'); 
+
+$hook_array['before_delete'] = Array(); 
+$hook_array['before_delete'][] = Array(30, 'Delete the emails from crm to imap server', 'custom/modules/Emails/delEmailCls.php','delEmailCls', 'delEmailFunc'); 
+
+
+?>
