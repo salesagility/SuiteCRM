a2dfbe22c54647aa2c0b89d2be0130c9f448a91c
Jose C. Massón <jose@gcoop.coop>
2018-12-21 15:45:41 -0300

    FIX #6648 - refactor and delete the right directories

--- RoboFile.php
+++ RoboFile.php
@@ -15,20 +15,39 @@ class RoboFile extends \Robo\Tasks
      */
     public function cleanCache()
     {
-        $toDelete = array('cache/xml',
-                          'cache/import',
-                          'cache/dashlets',
-                          'cache/modules',
-                          'cache/smarty',
-                          'cache/jsLanguage',
-                          'cache/themes',
-                          'cache/Relationships',
-                          'cache/include/javascript',);
+        $toDelete = array();
+        $doNotDelete = array('Emails', 'emails', '.', '..');
+        $cacheToDelete = array('cache/Relationships',
+                               'cache/csv',
+                               'cache/dashlets',
+                               'cache/diagnostics',
+                               'cache/dynamic_fields',
+                               'cache/feeds',
+                               'cache/import',
+                               'cache/include/javascript',
+                               'cache/jsLanguage',
+                               'cache/pdf',
+                               'cache/themes',
+                               'cache/xml',
+        );
 
-        foreach ($toDelete as  $dir) {
+        foreach ($cacheToDelete as  $dir) {
             if ((file_exists($dir) && is_dir($dir))) {
-                $this->_cleanDir([$dir]);
+                $toDelete[] = $dir;
             }
         }
+
+        $cacheModules = 'cache/modules';
+        $modules = scandir($cacheModules);
+
+        foreach ($modules as $module) {
+            if (file_exists($cacheModules.'/'.$module)
+                && is_dir($cacheModules.'/'.$module)
+                && !in_array($module, $doNotDelete)
+            ) {
+                $toDelete[] = $cacheModules.'/'.$module;
+            }
+        }
+        $this->_cleanDir($toDelete);
     }
 }
