0d1899597dea67d3b7a1f2fff2a452dd272d2a79
Jack Anderson <44227185+jack7anderson7@users.noreply.github.com>
2019-01-24 15:47:56 +0000

    Update SimpleList.js

--- modules/ModuleBuilder/javascript/SimpleList.js
+++ modules/ModuleBuilder/javascript/SimpleList.js
@@ -290,7 +290,6 @@ if(typeof(SimpleList) == 'undefined'){
         let dispSpan = document.getElementById('span_'+id);
         let editSpan = document.getElementById('span_edit_'+id);
 
-
         let value = encodeURI(val);
         dispSpan.style.display = 'inline';
         editSpan.style.display = 'none';
