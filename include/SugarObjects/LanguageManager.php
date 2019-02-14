fb3bffd4d844ca766a8247982b7d5809491daa47
QuickCRM <quickcrm@ns-team.fr>
2019-02-14 15:08:54 +0100

    Fix priorities when loading language files
    Labels defined in the Extension framework (for example after exporting customizations from Studio and loading the module with Module Loader on another instance) can not be changed with Studio.
    Steps to reproduce:
    - Add a field to Calls with Studio
    - Export customizations
    - Load the generated module in another instance
    - Go to Studio and change the label of that field.
    - The change is not taken into account.
    
    The changes is properly saved in custom/modules/Calls/language/en_us.php
    But it is overriden when LanguageManager loads custom/modules/Calls/Ext/Language/en_us.lang.ext.php which includes the original field label
    
    Context : Any version of SuiteCRM.

--- include/SugarObjects/LanguageManager.php
+++ include/SugarObjects/LanguageManager.php
@@ -198,8 +198,8 @@ class LanguageManager
         $lang_paths = array(
                     'modules/'.$module.'/language/'.$lang.'.lang.php',
                     'modules/'.$module.'/language/'.$lang.'.lang.override.php',
-                    'custom/modules/'.$module.'/language/'.$lang.'.lang.php',
                     'custom/modules/'.$module.'/Ext/Language/'.$lang.'.lang.ext.php',
+                    'custom/modules/'.$module.'/language/'.$lang.'.lang.php',
                  );
 
         #27023, if this module template language file was not attached , get the template from this module vardef cache file if exsits and load the template language files.
