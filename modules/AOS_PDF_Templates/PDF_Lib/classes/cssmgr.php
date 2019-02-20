840364e85433cfe954017d2b8d0ade44db8d582e
g.fantini <gabriele.fantini@salesagility.com>
2019-02-20 16:27:23 +0000

    Fix for checkbox in PDF

--- modules/AOS_PDF_Templates/PDF_Lib/classes/cssmgr.php
+++ modules/AOS_PDF_Templates/PDF_Lib/classes/cssmgr.php
@@ -1251,13 +1251,16 @@ function MergeCSS($inherit,$tag,$attr) {
 /*-- END TABLES --*/
 	//===============================================
 	// STYLESHEET TAG e.g. h1  p  div  table
-	if (isset($this->CSS[$tag]) && $this->CSS[$tag]) { 
-			$zp = $this->CSS[$tag];
-			if ($tag=='TD' || $tag=='TH')  { $this->setBorderDominance($zp, 9); }	// *TABLES*	// *TABLES-ADVANCED-BORDERS*
-			if (is_array($zp)) { 
-				$p = array_merge($p,$zp); 
-				$this->_mergeBorders($p,$zp);
-			}
+	if (isset($this->CSS[$tag]) && $this->CSS[$tag]) {
+
+		$zp = $this->CSS[$tag];
+		if ($tag=='TD' || $tag=='TH')  { $this->setBorderDominance($zp, 9); }	// *TABLES*	// *TABLES-ADVANCED-BORDERS*
+        //Necessary in order to display the checkbox correctly
+        if($tag == 'INPUT') { unset($zp['FONT-SIZE']); }
+        if (is_array($zp)) {
+		    $p = array_merge($p,$zp);
+			$this->_mergeBorders($p,$zp);
+		}
 	}
 	//===============================================
 	// STYLESHEET CLASS e.g. .smallone{}  .redletter{}
