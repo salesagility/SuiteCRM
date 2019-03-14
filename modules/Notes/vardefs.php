442cf0136dcefd25bd3d3052776092d4576df47f
Dillon-Brown <dillon.brown@salesagility.com>
2019-03-14 11:31:34 +0000

    Reformat

--- modules/Notes/vardefs.php
+++ modules/Notes/vardefs.php
@@ -1,11 +1,11 @@
 <?php
-if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-/*********************************************************************************
+/**
+ *
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
- * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
- * Copyright (C) 2011 - 2016 Salesagility Ltd.
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -16,7 +16,7 @@ if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -34,14 +34,20 @@ if (!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
- * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- ********************************************************************************/
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
 
 $dictionary['Note'] = array(
 
     'table' => 'notes',
-    'unified_search' => true, 'full_text_search' => true, 'unified_search_default_enabled' => true,
+    'unified_search' => true,
+    'full_text_search' => true,
+    'unified_search_default_enabled' => true,
 
     'comment' => 'Notes and Attachments',
     'fields' => array(
@@ -462,13 +468,25 @@ $dictionary['Note'] = array(
     ),
     'relationships' => array(
         'notes_modified_user' =>
-            array('lhs_module' => 'Users', 'lhs_table' => 'users', 'lhs_key' => 'id',
-                'rhs_module' => 'Notes', 'rhs_table' => 'notes', 'rhs_key' => 'modified_user_id',
-                'relationship_type' => 'one-to-many'),
+            array(
+                'lhs_module' => 'Users',
+                'lhs_table' => 'users',
+                'lhs_key' => 'id',
+                'rhs_module' => 'Notes',
+                'rhs_table' => 'notes',
+                'rhs_key' => 'modified_user_id',
+                'relationship_type' => 'one-to-many'
+            ),
         'notes_created_by' =>
-            array('lhs_module' => 'Users', 'lhs_table' => 'users', 'lhs_key' => 'id',
-                'rhs_module' => 'Notes', 'rhs_table' => 'notes', 'rhs_key' => 'created_by',
-                'relationship_type' => 'one-to-many')
+            array(
+                'lhs_module' => 'Users',
+                'lhs_table' => 'users',
+                'lhs_key' => 'id',
+                'rhs_module' => 'Notes',
+                'rhs_table' => 'notes',
+                'rhs_key' => 'created_by',
+                'relationship_type' => 'one-to-many'
+            )
     ),
     'indices' => array(
         array('name' => 'notespk', 'type' => 'primary', 'fields' => array('id')),
@@ -482,5 +500,7 @@ $dictionary['Note'] = array(
     'optimistic_locking' => true,
 );
 
-VardefManager::createVardef('Notes', 'Note', array('assignable', 'security_groups',
+VardefManager::createVardef('Notes', 'Note', array(
+    'assignable',
+    'security_groups',
 ));
