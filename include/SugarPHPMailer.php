68cb45b585806e06d12b5d2d3990ff5f4417e75c
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2019-04-05 18:42:17 +0200

    Fixes SugarPHPMailer encountered an error: Could not access file (SugarPHPMailer.php)

--- include/SugarPHPMailer.php
+++ include/SugarPHPMailer.php
@@ -303,7 +303,7 @@ eoq;
         //Replace any embeded images using the secure entryPoint for src url.
         $this->replaceImageByRegex(
             "(?:{$sugar_config['site_url']})?index.php[?]entryPoint=download&(?:amp;)?[^\"]+?id=",
-            'upload://',
+            'upload/',
             true
         );
 
@@ -322,13 +322,13 @@ eoq;
                     $filename = $note->file->original_file_name;
                     $mime_type = $note->file->mime_type;
                 } else {
-                    $file_location = "upload://{$note->id}";
+                    $file_location = "upload/{$note->id}";
                     $filename = $note->id . $note->filename;
                     $mime_type = $note->file_mime_type;
                 }
             } elseif ($note->object_name === 'DocumentRevision') { // from Documents
                 $filename = $note->id . $note->filename;
-                $file_location = "upload://$filename";
+                $file_location = "upload/$filename";
                 $mime_type = $note->file_mime_type;
             }
 
