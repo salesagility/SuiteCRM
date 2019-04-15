2f082cb5f71267b017cc34308a3eac75d2d0d942
Jose C. Massón <jose@gcoop.coop>
2019-04-15 16:51:14 -0300

    FIX #7189 - Remove redundant require_once

--- modules/AOS_Contracts/views/view.detail.php
+++ modules/AOS_Contracts/views/view.detail.php
@@ -3,7 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-require_once('include/MVC/View/views/view.detail.php');
 
 class AOS_ContractsViewDetail extends ViewDetail
 {
