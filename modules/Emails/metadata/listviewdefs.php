0a85ca01605840151394611cf0ded45c431bc1d4
Dillon-Brown <dillon.brown@salesagility.com>
2019-05-17 09:32:01 +0100

    Update email listviewdefs

--- modules/Emails/metadata/listviewdefs.php
+++ modules/Emails/metadata/listviewdefs.php
@@ -82,7 +82,7 @@ $viewdefs['Emails']['ListView'] = array(
         ),
         'includes' => array(
             array(
-              'file' => 'include/javascript/jstree/dist/jstree.js',
+              'file' => 'vendor/vakata/jstree/dist/jstree.js',
             ),
             array(
                 'file' => 'modules/Emails/include/ListView/ComposeViewModal.js',
