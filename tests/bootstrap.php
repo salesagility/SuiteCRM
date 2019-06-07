c0a26d6b32723f3a51e94c7d88b305b929462bfa
Connor Shea <connor.james.shea@gmail.com>
2019-06-07 13:46:44 -0600

    Run PHPUnit anywhere.
    This changes the PHPUnit configuration to allow PHPUnit to be run from
    outside the tests directory.
    
    There's not really a good reason to require it be run from ./tests/

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -38,17 +38,15 @@
  */
 
 /* bootstrap composer's autoloader */
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 /* bootstrap sugarcrm */
-//echo "CWD:" . getcwd() . "\n";
 error_reporting(E_ALL);
-chdir('../');
 define('sugarEntry', true);
 global $sugar_config, $db;
-require_once 'include/utils.php';
-require_once 'include/modules.php';
-require_once 'include/entryPoint.php';
+require_once dirname(__FILE__) . '/../include/utils.php';
+require_once dirname(__FILE__) . '/../include/modules.php';
+require_once dirname(__FILE__) . '/../include/entryPoint.php';
 //Oddly entry point loads app_strings but not app_list_strings, manually do this here.
 $GLOBALS['app_list_strings'] = return_app_list_strings_language($GLOBALS['current_language']);
 
