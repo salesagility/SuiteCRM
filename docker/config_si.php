fca79437a8127e1b7385377b9c5453ca47c14d0d
Mark N Carpenter Jr <mcarpenter@smtcorp.com>
2019-06-27 15:50:52 -0400

    CI-REFACTOR: match config to working travis_ci

--- docker/config_si.php
+++ docker/config_si.php
@@ -1,6 +1,31 @@
 <?php
 // Look for ENVs first, then use the last possible case.
 $sugar_config_si  = array (
+
+  'dbUSRData' => isset($_ENV['DATABASE_USR_DATA']) ? $_ENV['DATABASE_USR_DATA'] : 'create',
+  // ---------- Database Options ----------
+
+  'setup_db_admin_password' => isset($_ENV['DATABASE_ROOT_PASSWORD']) ? $_ENV['DATABASE_ROOT_PASSWORD'] : 'automated_tests',
+  'setup_db_admin_user_name' => isset($_ENV['DATABASE_ROOT_USERNAME']) ? $_ENV['DATABASE_ROOT_USERNAME'] : 'automated_tests',
+  'setup_db_create_database' => isset($_ENV['DATABASE_CREATE_NEW']) ? $_ENV['DATABASE_CREATE_NEW'] : 1,
+  'setup_db_database_name' => isset($_ENV['DATABASE_NAME']) ? $_ENV['DATABASE_NAME'] : 'automated_tests',
+  'setup_db_drop_tables' => isset($_ENV['DATABASE_DROP_TABLES']) ? $_ENV['DATABASE_DROP_TABLES'] : 0,
+  'setup_db_host_name' => isset($_ENV['DATABASE_HOST_NAME']) ? $_ENV['DATABASE_HOST_NAME'] : 'localhost',
+  'setup_db_pop_demo_data' => isset($_ENV['DATABASE_DEMO_DATA']) ? $_ENV['DATABASE_DEMO_DATA'] : true,
+  'setup_db_type' => isset($_ENV['DATABASE_TYPE']) ? $_ENV['DATABASE_TYPE'] : 'mysql',
+  'setup_db_username_is_privileged' => isset($_ENV['DATABASE_USER_PRIVILEDGED']) ? $_ENV['DATABASE_USER_PRIVILEDGED'] : true,
+
+
+  // ---------- Suite Site Options ----------
+  
+  'setup_site_admin_password' => isset($_ENV['SITE_ADMIN_PASSWORD']) ? $_ENV['SITE_ADMIN_PASSWORD'] : 'admin',
+  'setup_site_admin_user_name' => isset($_ENV['SITE_ADMIN_USER']) ? $_ENV['SITE_ADMIN_USER'] : 'admin',
+  'setup_site_url' => isset($_ENV['SITE_URL']) ? $_ENV['SITE_URL'] : 'http://localhost',
+  'setup_system_name' => isset($_ENV['SITE_SYSTEM_NAME']) ? $_ENV['SITE_SYSTEM_NAME'] : 'SuiteCRM Docker Build',
+  'setup_site_sugarbeet_automatic_checks' => true,
+  'show_log_trace' => false,
+
+
   // ---------- Installation Defaults ----------
 
   'default_currency_iso4217' => isset($_ENV['DEFAULT_CURRENCY']) ? $_ENV['DEFAULT_CURRENCY'] : 'USD',
@@ -15,37 +40,4 @@ $sugar_config_si  = array (
   'default_number_grouping_seperator' => isset($_ENV['DEFAULT_NUMBER_GROUPING']) ? $_ENV['DEFAULT_NUMBER_GROUPING'] : ',',
   'default_time_format' => isset($_ENV['DEFAULT_TIME_FORMAT']) ? $_ENV['DEFAULT_TIME_FORMAT'] : 'H:i',
   'export_delimiter' => isset($_ENV['EXPORT_DEILIMITER']) ? $_ENV['EXPORT_DEILIMITER'] : ',',
-
-
-  // ---------- Database Options ----------
-
-  'dbconfig' => array(
-    'db_host_name' => '172.21.0.3:3306',
-    'db_host_instance' => '',
-    'db_user_name' => 'suitecrm',
-    'db_password' => '',
-    'db_name' => 'suitecrm',
-    'db_type' => 'mysql',
-    'db_port' => '',
-    'db_manager' => 'MysqliManager',
-  ),
-
-  // 'setup_db_host_name' => isset($_ENV['DATABASE_HOST_NAME']) ? $_ENV['DATABASE_HOST_NAME'] : 'DB Host',
-  // 'setup_db_type' => isset($_ENV['DATABASE_TYPE']) ? $_ENV['DATABASE_TYPE'] : 'mysql',
-  // 'setup_db_admin_user_name' => isset($_ENV['DATABASE_ROOT_USERNAME']) ? $_ENV['DATABASE_ROOT_USERNAME'] : 'DB Username',
-  // 'setup_db_admin_password' => isset($_ENV['DATABASE_ROOT_PASSWORD']) ? $_ENV['DATABASE_ROOT_PASSWORD'] : 'DB Password',
-  // 'setup_db_database_name' => isset($_ENV['DATABASE_NAME']) ? $_ENV['DATABASE_NAME'] : 'DB Name',
-  // 'setup_db_create_database' => isset($_ENV['DATABASE_CREATE_NEW']) ? $_ENV['DATABASE_CREATE_NEW'] : 1,
-  // 'dbUSRData' => isset($_ENV['DATABASE_USR_DATA']) ? $_ENV['DATABASE_USR_DATA'] : 'create',
-  // 'setup_db_drop_tables' => isset($_ENV['DATABASE_DROP_TABLES']) ? $_ENV['DATABASE_DROP_TABLES'] : 0,
-  // 'setup_db_pop_demo_data' => isset($_ENV['DATABASE_DEMO_DATA']) ? $_ENV['DATABASE_DEMO_DATA'] : false,
-  // 'setup_db_username_is_privileged' => isset($_ENV['DATABASE_USER_PRIVILEDGED']) ? $_ENV['DATABASE_USER_PRIVILEDGED'] : true,
-
-  
-  // ---------- Suite Site Options ----------
-
-  'setup_site_admin_password' => isset($_ENV['SITE_ADMIN_PASSWORD']) ? $_ENV['SITE_ADMIN_PASSWORD'] : 'Admin Password',
-  'setup_site_admin_user_name' => isset($_ENV['SITE_ADMIN_USER']) ? $_ENV['SITE_ADMIN_USER'] : 'Admin username',
-  'setup_site_url' => isset($_ENV['SITE_URL']) ? $_ENV['SITE_URL'] : 'Site URL',
-  'setup_system_name' => isset($_ENV['SITE_SYSTEM_NAME']) ? $_ENV['SITE_SYSTEM_NAME'] : 'Site Name',
   );
