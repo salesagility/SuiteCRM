23ffedceb1e2971fcdd4b7b0eb6a9a3314ecc4ef
Matjaž Godec <matjaz.godec@gmail.com>
2019-07-26 09:48:32 +0200

    Fix for Oportunities with assigned tasks not displayed #7634  On branch TaskSubpanels/ForActivites_fix  Changes to be committed: 	modified:   modules/Tasks/metadata/subpanels/ForActivities.php

--- modules/Tasks/metadata/subpanels/ForActivities.php
+++ modules/Tasks/metadata/subpanels/ForActivities.php
@@ -76,8 +76,8 @@ $subpanel_layout = array(
         'date_due' => array(
             'vname' => 'LBL_LIST_DUE_DATE',
             'width' => '10%',
-            'alias' => 'date_end',
-            'sort_by' => 'date_end',
+            'alias' => 'date_due',
+            'sort_by' => 'date_due',
         ),
         'assigned_user_name' => array(
             'name' => 'assigned_user_name',
