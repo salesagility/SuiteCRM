19fe22d64be60b1f6f891a933f0d4c1c130b2842
j.dang <jason.dang@salesagility.com>
2019-08-21 16:57:56 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/include/ListView/ListView.php.
    
    - Administration

--- include/ListView/ListView.php
+++ include/ListView/ListView.php
@@ -1435,7 +1435,7 @@ class ListView
                 $delete_link = '&nbsp;';
             }
 
-            $admin = new Administration();
+            $admin = BeanFactory::newBean('Administration');
             $admin->retrieveSettings('system');
 
             $user_merge = $current_user->getPreference('mailmerge_on');
