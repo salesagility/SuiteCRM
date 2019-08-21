08b547ecd8fbc33af2282bffe6fddaa9be1d5816
j.dang <jason.dang@salesagility.com>
2019-08-21 17:04:27 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Opportunities/SugarFeeds/OppFeed.php.
    
    - Currency

--- modules/Opportunities/SugarFeeds/OppFeed.php
+++ modules/Opportunities/SugarFeeds/OppFeed.php
@@ -51,12 +51,12 @@ class OppFeed extends FeedLogicBase
     {
         $text = '';
         if (empty($bean->fetched_row)) {
-            $currency = new Currency();
+            $currency = BeanFactory::newBean('Currencies');
             $currency->retrieve($bean->currency_id);
             $text = '{SugarFeed.CREATED_OPPORTUNITY} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name . '] {SugarFeed.WITH} [Accounts:' . $bean->account_id . ':' . $bean->account_name . '] {SugarFeed.FOR_AMOUNT} ' . $currency->symbol. format_number($bean->amount);
         } else {
             if (!empty($bean->fetched_row['sales_stage']) && $bean->fetched_row['sales_stage'] != $bean->sales_stage && $bean->sales_stage == 'Closed Won') {
-                $currency = new Currency();
+                $currency = BeanFactory::newBean('Currencies');
                 $currency->retrieve($bean->currency_id);
                 $text = '{SugarFeed.WON_OPPORTUNITY} [' . $bean->module_dir . ':' . $bean->id . ':' . $bean->name . '] {SugarFeed.WITH} [Accounts:' . $bean->account_id . ':' . $bean->account_name . '] {SugarFeed.FOR_AMOUNT} '. $currency->symbol . format_number($bean->amount);
             }
