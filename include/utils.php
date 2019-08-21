cd37c3c821fac2d3613108b63c38cbffce88d707
j.dang <jason.dang@salesagility.com>
2019-08-21 16:57:40 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/include/utils.php.
    
    - User

--- include/utils.php
+++ include/utils.php
@@ -4475,7 +4475,7 @@ function array_depth($array, $depth_count = -1, $depth_array = array())
  */
 function createGroupUser($name)
 {
-    $group = new User();
+    $group = BeanFactory::newBean('Users');
     $group->user_name = $name;
     $group->last_name = $name;
     $group->is_group = 1;
