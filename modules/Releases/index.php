5ae9f4e977695966ee43c55cb76d32e5a787712e
www-data <jason.dang@salesagility.com>
2019-08-21 13:49:49 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Releases/index.php.
    
    - Release

--- modules/Releases/index.php
+++ modules/Releases/index.php
@@ -57,7 +57,7 @@ if ((!is_admin($GLOBALS['current_user']) && (!is_admin_for_module($GLOBALS['curr
     sugar_die("Unauthorized access to administration.");
 }
 
-$focus = new Release();
+$focus = BeanFactory::newBean('Releases');
 echo getClassicModuleTitle($mod_strings['LBL_MODULE_NAME'], array($mod_strings['LBL_MODULE_TITLE']), true);
 $is_edit = false;
 if (!empty($_REQUEST['record'])) {
