df36a9498b4f28b387408f4e163a6d8b2bd4a852
www-data <jason.dang@salesagility.com>
2019-08-21 13:52:52 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Campaigns/PopupCampaignRoi.php.
    
    - Campaign
    
    - Currency

--- modules/Campaigns/PopupCampaignRoi.php
+++ modules/Campaigns/PopupCampaignRoi.php
@@ -72,7 +72,7 @@ $xtpl=new XTemplate('modules/Campaigns/PopupCampaignRoi.html');
 
 //_pp($_REQUEST['id']);
 $campaign_id=$_REQUEST['id'];
-$campaign = new Campaign();
+$campaign = BeanFactory::newBean('Campaigns');
 $opp_query1  = "select camp.name, camp.actual_cost,camp.budget,camp.expected_revenue,count(*) opp_count,SUM(opp.amount) as Revenue, SUM(camp.actual_cost) as Investment,
                             ROUND((SUM(opp.amount) - SUM(camp.actual_cost))/(SUM(camp.actual_cost)), 2)*100 as ROI";
             $opp_query1 .= " from opportunities opp";
@@ -105,7 +105,7 @@ $opp_query1  = "select camp.name, camp.actual_cost,camp.budget,camp.expected_rev
 
 
 
-    $currency  = new Currency();
+    $currency  = BeanFactory::newBean('Currencies');
 if (isset($focus->currency_id) && !empty($focus->currency_id)) {
     $currency->retrieve($focus->currency_id);
     if ($currency->deleted != 1) {
