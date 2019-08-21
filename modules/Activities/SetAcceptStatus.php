5f2e608396a32a322179729e6d1f45dfaf35a771
www-data <jason.dang@salesagility.com>
2019-08-21 13:50:22 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Activities/SetAcceptStatus.php.
    
    - Call
    
    - Meeting

--- modules/Activities/SetAcceptStatus.php
+++ modules/Activities/SetAcceptStatus.php
@@ -49,12 +49,12 @@ if (!defined('sugarEntry') || !sugarEntry) {
     
     
     if ($_REQUEST['object_type'] == "Meeting") {
-        $focus = new Meeting();
+        $focus = BeanFactory::newBean('Meetings');
         $focus->id = $_REQUEST['object_id'];
         $test = $focus->set_accept_status($current_user, $_REQUEST['accept_status']);
     } else {
         if ($_REQUEST['object_type'] == "Call") {
-            $focus = new Call();
+            $focus = BeanFactory::newBean('Calls');
             $focus->id = $_REQUEST['object_id'];
             $test = $focus->set_accept_status($current_user, $_REQUEST['accept_status']);
         }
