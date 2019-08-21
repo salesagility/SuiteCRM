686655821b529aefbbca77d2eb25b554a294d35c
www-data <jason.dang@salesagility.com>
2019-08-21 13:50:33 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Leads/Dashlets/MyLeadsDashlet/MyLeadsDashlet.php.
    
    - Lead

--- modules/Leads/Dashlets/MyLeadsDashlet/MyLeadsDashlet.php
+++ modules/Leads/Dashlets/MyLeadsDashlet/MyLeadsDashlet.php
@@ -63,7 +63,7 @@ class MyLeadsDashlet extends DashletGeneric
 
         $this->searchFields = $dashletData['MyLeadsDashlet']['searchFields'];
         $this->columns = $dashletData['MyLeadsDashlet']['columns'];
-        $this->seedBean = new Lead();
+        $this->seedBean = BeanFactory::newBean('Leads');
     }
 
     /**
