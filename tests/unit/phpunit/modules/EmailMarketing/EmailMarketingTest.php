7ebe1693ef5857bdfda014cad8ce799b6f03d5d9
www-data <jason.dang@salesagility.com>
2019-08-21 13:47:11 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/tests/unit/phpunit/modules/EmailMarketing/EmailMarketingTest.php.
    
    - EmailMarketing

--- tests/unit/phpunit/modules/EmailMarketing/EmailMarketingTest.php
+++ tests/unit/phpunit/modules/EmailMarketing/EmailMarketingTest.php
@@ -7,7 +7,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
     {
 
         //execute the contructor and check for the Object type and  attributes
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         $this->assertInstanceOf('EmailMarketing', $emailMarketing);
         $this->assertInstanceOf('SugarBean', $emailMarketing);
@@ -26,7 +26,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
 
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         $result = $emailMarketing->retrieve();
         $this->assertInstanceOf('EmailMarketing', $result);
@@ -36,7 +36,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testget_summary_text()
     {
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         //test without setting name
         $this->assertEquals(null, $emailMarketing->get_summary_text());
@@ -54,7 +54,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         $state->pushGlobals();
         
         
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         //test with empty string params
         $expected = " SELECT  email_marketing.*  , jt0.name template_name , jt0.assigned_user_id template_name_owner  , 'EmailTemplates' template_name_mod FROM email_marketing   LEFT JOIN  email_templates jt0 ON email_marketing.template_id=jt0.id AND jt0.deleted=0\n\n AND jt0.deleted=0 where email_marketing.deleted=0";
@@ -74,7 +74,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testget_list_view_data()
     {
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         //execute the method and verify that it retunrs expected results
         $expected = array(
@@ -87,7 +87,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testbean_implements()
     {
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
         $this->assertEquals(false, $emailMarketing->bean_implements('')); //test with blank value
         $this->assertEquals(false, $emailMarketing->bean_implements('test')); //test with invalid value
         $this->assertEquals(true, $emailMarketing->bean_implements('ACL')); //test with valid value
@@ -95,7 +95,7 @@ class EmailMarketingTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
 
     public function testget_all_prospect_lists()
     {
-        $emailMarketing = new EmailMarketing();
+        $emailMarketing = BeanFactory::newBean('EmailMarketing');
 
         //execute the method and verify that it retunrs expected results
         $expected = "select prospect_lists.* from prospect_lists  left join prospect_list_campaigns on prospect_list_campaigns.prospect_list_id=prospect_lists.id where prospect_list_campaigns.deleted=0 and prospect_list_campaigns.campaign_id='' and prospect_lists.deleted=0 and prospect_lists.list_type not like 'exempt%'";
