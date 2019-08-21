b58badbe92a759435f59229ecbe81da8542cbb31
www-data <jason.dang@salesagility.com>
2019-08-21 13:47:42 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/tests/unit/phpunit/modules/EAPM/EAPMTest.php.
    
    - EAPM

--- tests/unit/phpunit/modules/EAPM/EAPMTest.php
+++ tests/unit/phpunit/modules/EAPM/EAPMTest.php
@@ -15,7 +15,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
 
         //execute the contructor and check for the Object type and  attributes
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
         $this->assertInstanceOf('EAPM', $eapm);
         $this->assertInstanceOf('Basic', $eapm);
         $this->assertInstanceOf('SugarBean', $eapm);
@@ -49,7 +49,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
 
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
         $this->assertEquals(false, $eapm->bean_implements('')); //test with blank value
         $this->assertEquals(false, $eapm->bean_implements('test')); //test with invalid value
         $this->assertEquals(true, $eapm->bean_implements('ACL')); //test with valid value
@@ -73,7 +73,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         // test
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //test with default value/false
         $result = $eapm->getLoginInfo('');
@@ -102,7 +102,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         // test
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //test with empty string params
         $expected = " SELECT  eapm.*  , jt0.user_name modified_by_name , jt0.created_by modified_by_name_owner  , 'Users' modified_by_name_mod , jt1.user_name created_by_name , jt1.created_by created_by_name_owner  , 'Users' created_by_name_mod , jt2.user_name assigned_user_name , jt2.created_by assigned_user_name_owner  , 'Users' assigned_user_name_mod FROM eapm   LEFT JOIN  users jt0 ON eapm.modified_user_id=jt0.id AND jt0.deleted=0\n\n AND jt0.deleted=0  LEFT JOIN  users jt1 ON eapm.created_by=jt1.id AND jt1.deleted=0\n\n AND jt1.deleted=0  LEFT JOIN  users jt2 ON eapm.assigned_user_id=jt2.id AND jt2.deleted=0\n\n AND jt2.deleted=0 where ( eapm.assigned_user_id ='' ) AND eapm.deleted=0";
@@ -134,7 +134,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         // test
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         $eapm->name = 'test';
         $eapm->url = 'test_url';
@@ -179,7 +179,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //execute the method and test if it works and does not throws an exception.
         try {
@@ -211,7 +211,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //execute the method and test if it works and does not throws an exception.
         try {
@@ -238,7 +238,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         // test
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //execute the method and verify attributes are set accordingly
         $eapm->save_cleanup();
@@ -267,7 +267,7 @@ class EAPMTest extends SuiteCRM\StateCheckerPHPUnitTestCaseAbstract
         
         
         
-        $eapm = new EAPM();
+        $eapm = BeanFactory::newBean('EAPM');
 
         //execute the method and test if it works and does not throws an exception.
         try {
