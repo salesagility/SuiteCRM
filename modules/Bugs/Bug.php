13434614c6e2c5563eb7e7a52e1ad2aa3bcee314
j.dang <jason.dang@salesagility.com>
2019-08-21 17:04:39 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Bugs/Bug.php.
    
    - Release

--- modules/Bugs/Bug.php
+++ modules/Bugs/Bug.php
@@ -402,7 +402,7 @@ function getReleaseDropDown()
 {
     static $releases = null;
     if (!$releases) {
-        $seedRelease = new Release();
+        $seedRelease = BeanFactory::newBean('Releases');
         $releases = $seedRelease->get_releases(true, "Active");
     }
     return $releases;
