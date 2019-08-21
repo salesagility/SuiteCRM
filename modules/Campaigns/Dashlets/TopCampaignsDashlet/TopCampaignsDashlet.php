5e576280b8d8a1bbc0a135284d482614f4ceca32
j.dang <jason.dang@salesagility.com>
2019-08-21 17:05:12 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Campaigns/Dashlets/TopCampaignsDashlet/TopCampaignsDashlet.php.
    
    - Opportunity

--- modules/Campaigns/Dashlets/TopCampaignsDashlet/TopCampaignsDashlet.php
+++ modules/Campaigns/Dashlets/TopCampaignsDashlet/TopCampaignsDashlet.php
@@ -72,7 +72,7 @@ class TopCampaignsDashlet extends Dashlet
             $this->autoRefresh = $def['autoRefresh'];
         }
         
-        $this->seedBean = new Opportunity();
+        $this->seedBean = BeanFactory::newBean('Opportunities');
 
         $qry = "SELECT C.name AS campaign_name, SUM(O.amount) AS revenue, C.id as campaign_id " .
                "FROM campaigns C, opportunities O " .
