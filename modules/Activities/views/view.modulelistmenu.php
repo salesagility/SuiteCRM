e8da24bc6aea8687072172723bf0246469551db7
j.dang <jason.dang@salesagility.com>
2019-08-21 17:03:03 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/Activities/views/view.modulelistmenu.php.
    
    - Tracker

--- modules/Activities/views/view.modulelistmenu.php
+++ modules/Activities/views/view.modulelistmenu.php
@@ -43,7 +43,7 @@ class ActivitiesViewModulelistmenu extends ViewModulelistmenu
 {
     public function display()
     {
-        $tracker = new Tracker();
+        $tracker = BeanFactory::newBean('Trackers');
         $history = $tracker->get_recently_viewed($GLOBALS['current_user']->id, array('Calls','Meetings','Tasks','Notes','Emails'));
         foreach ($history as $key => $row) {
             $history[$key]['item_summary_short'] = getTrackerSubstring($row['item_summary']);
