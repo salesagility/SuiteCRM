4808f4c6c45a7829320977a8d4d9993478226068
j.dang <jason.dang@salesagility.com>
2019-08-21 17:00:52 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/modules/ACLActions/ACLAction.php.
    
    - ACLAction

--- modules/ACLActions/ACLAction.php
+++ modules/ACLActions/ACLAction.php
@@ -88,7 +88,7 @@ class ACLAction extends SugarBean
         $db = DBManagerFactory::getInstance();
         if (isset($ACLActions[$type])) {
             foreach ($ACLActions[$type]['actions'] as $action_name =>$action_def) {
-                $action = new ACLAction();
+                $action = BeanFactory::newBean('ACLActions');
                 $query = "SELECT * FROM " . $action->table_name . " WHERE name='$action_name' AND category = '$category' AND acltype='$type' AND deleted=0 ";
                 $result = $db->query($query);
                 //only add if an action with that name and category don't exist
@@ -121,7 +121,7 @@ class ACLAction extends SugarBean
         $db = DBManagerFactory::getInstance();
         if (isset($ACLActions[$type])) {
             foreach ($ACLActions[$type]['actions'] as $action_name =>$action_def) {
-                $action = new ACLAction();
+                $action = BeanFactory::newBean('ACLActions');
                 $query = "SELECT * FROM " . $action->table_name . " WHERE name='$action_name' AND category = '$category' AND acltype='$type' and deleted=0";
                 $result = $db->query($query);
                 //only add if an action with that name and category don't exist
@@ -227,7 +227,7 @@ class ACLAction extends SugarBean
         $result = $db->query($query);
         $default_actions = array();
         while ($row = $db->fetchByAssoc($result)) {
-            $acl = new ACLAction();
+            $acl = BeanFactory::newBean('ACLActions');
             $acl->populateFromRow($row);
             $default_actions[] = $acl;
         }
@@ -340,7 +340,7 @@ class ACLAction extends SugarBean
                 break; //no need for default actions when a role is assigned to the user or user's group already
             }
             /* END - SECURITY GROUPS */
-            $acl = new ACLAction();
+            $acl = BeanFactory::newBean('ACLActions');
             $isOverride  = false;
             $acl->populateFromRow($row);
             if (!empty($row['access_override'])) {
