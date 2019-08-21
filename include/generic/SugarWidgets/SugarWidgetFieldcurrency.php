fd83680093720609a3674dea66aec2b6b61bdbc6
www-data <jason.dang@salesagility.com>
2019-08-21 13:44:59 +0000

    PHP Commit - BeanFactory Extend Core Modules
    Replace explicit module class instantiations for below module list in file: /var/www/SuiteCRM/include/generic/SugarWidgets/SugarWidgetFieldcurrency.php.
    
    - Currency

--- include/generic/SugarWidgets/SugarWidgetFieldcurrency.php
+++ include/generic/SugarWidgets/SugarWidgetFieldcurrency.php
@@ -53,7 +53,7 @@ function get_currency()
 {
     global $current_user,$global_currency_obj;
     if (empty($global_currency_obj)) {
-        $global_currency_obj = new Currency();
+        $global_currency_obj = BeanFactory::newBean('Currencies');
         //  $global_currency_symbol = '$';
 
         if ($current_user->getPreference('currency')) {
