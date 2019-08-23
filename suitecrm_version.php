ca6ba3d3a4e6dd69a2d57ee4c5fcfe99bee91576
samus-aran <ashjayne.nicolson@gmail.com>
2019-08-23 12:48:48 +0100

    SuiteCRM 7.11.8

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.11.7';
-$suitecrm_timestamp = '2019-07-31 17:00:00';
+$suitecrm_version = '7.11.8';
+$suitecrm_timestamp = '2019-08-22 17:00:00';
