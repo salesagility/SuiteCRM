07fa0f02bf31e7e3ed2584fbd6207be335149274
dominic <dominicchinkh@gmail.com>
2019-09-03 12:11:12 +1000

    Allow database character set configuration

---
+++ install/suite_install/collations.php
@@ -0,0 +1,15 @@
+<?php
+
+$collations = array(
+    'mysql' =>
+        array(
+	    array(
+	        'name' => 'utf8_general_ci',
+		'charset' => 'utf8',
+	    ),
+            array(
+                'name' => 'utf8mb4_general_ci',
+                'charset' => 'utf8mb4',
+            ),
+        ),
+);
