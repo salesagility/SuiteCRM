1d3dbf4baf7e80319ec4245bef83aa75dc9e7c7a
Dillon-Brown <dillon.brown@salesagility.com>
2019-09-09 14:32:07 +0100

    Update GetFieldsMetaCest to remove v8 exception handling

--- lib/Exception/InvalidArgumentException.php
+++ lib/Exception/InvalidArgumentException.php
@@ -5,7 +5,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -40,8 +40,6 @@
 
 namespace SuiteCRM\Exception;
 
-use Psr\Log\LogLevel;
-use SuiteCRM\API\v8\Controller\ApiController;
 use SuiteCRM\Enumerator\ExceptionCode;
 
 /**
