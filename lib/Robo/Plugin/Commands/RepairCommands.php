6148e43d8ea43de3974c44264ca4c1377bc78a4a
Jose <jose@gcoop.coop>
2019-09-19 01:18:30 -0300

    Update lib/Robo/Plugin/Commands/RepairCommands.php
    Co-Authored-By: Connor Shea <connor.james.shea@gmail.com>

--- lib/Robo/Plugin/Commands/RepairCommands.php
+++ lib/Robo/Plugin/Commands/RepairCommands.php
@@ -110,7 +110,7 @@ class RepairCommands extends \Robo\Tasks
      * This Robo task rebuilds the CRM extension files found in custom/Extension.
      * 
      * @param array $opts optional command line arguments
-     * show-output - Set if you want to see the rebuildExtensions() output.
+     * @option bool $show-output - Set if you want to see the rebuildExtensions() output.
      * @throws \RuntimeException
      */
     public function repairRebuildExtensions(array $opts = ['show-output' => false])
