781715c84311e947dbdce1b27fab7f6cd4f7d607
Dillon-Brown <dillon.brown@salesagility.com>
2019-10-03 12:04:04 +0100

    Updated the mail parser to handle email->description
    This change populated the email->description field when the type of
    inbound email is plaintext.

--- modules/InboundEmail/InboundEmail.php
+++ modules/InboundEmail/InboundEmail.php
@@ -5643,6 +5643,16 @@ class InboundEmail extends SugarBean
                 $emailBody = $this->imap->fetchBody($uid, '', FT_UID);
                 $contentType = $this->mailParser->parse($emailBody)->getHeaderValue('Content-Type');
 
+                if (!empty($contentType) && strtolower($contentType) === 'text/plain') {
+                    $email->description = $this->getMessageTextWithUid(
+                        $uid,
+                        $contentType,
+                        $structure = null,
+                        $fullHeader = null,
+                        true
+                    );
+                }
+
                 $email->description_html = $this->getMessageTextWithUid(
                     $uid,
                     $contentType,
