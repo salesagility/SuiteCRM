5987fbc6aaf6eef264783088983214859d5bcdba
Jose C. Massón <jose@gcoop.coop>
2019-10-08 20:10:56 -0300

    Format the array to make it more readable

--- metadata/user_feedsMetaData.php
+++ metadata/user_feedsMetaData.php
@@ -41,17 +41,41 @@ if (!defined('sugarEntry') || !sugarEntry) {
  * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
 
-$dictionary['users_feeds'] = array( 'table' => 'users_feeds'
-                                  , 'fields' => array(
-    
-       array('name' =>'user_id', 'type' =>'varchar', 'len'=>'36', )
-      , array('name' =>'feed_id', 'type' =>'varchar', 'len'=>'36', )
-      , array('name' =>'suite_rank', 'type' =>'int', 'required' => false)
-      , array('name' => 'date_modified','type' => 'datetime')
-      , array('name' =>'deleted', 'type' =>'bool', 'len'=>'', 'default'=>'0', 'required' => false)
-                                                      )
-                                 , 'indices' => array(
-  
-       array('name' =>'idx_ud_user_id', 'type' =>'index', 'fields'=>array('user_id', 'feed_id'))
-                                                      )
-                                  );
+$dictionary['users_feeds'] = [
+    'table' => 'users_feeds',
+    'fields' => [
+        [
+            'name' =>'user_id',
+            'type' =>'varchar',
+            'len'=>'36',
+        ],
+        [
+            'name' =>'feed_id',
+            'type' =>'varchar',
+            'len'=>'36',
+        ],
+        [
+            'name' => 'suite_rank',
+            'type' =>'int',
+            'required' => false
+        ],
+        [
+            'name' => 'date_modified',
+            'type' => 'datetime',
+        ],
+        [
+            'name' =>'deleted',
+            'type' =>'bool',
+            'len'=>'',
+            'default'=>'0',
+            'required' => false
+        ]
+    ],
+    'indices' => [
+        [
+            'name' => 'idx_ud_user_id',
+            'type' => 'index',
+            'fields' => ['user_id', 'feed_id']
+        ]
+    ]
+];
