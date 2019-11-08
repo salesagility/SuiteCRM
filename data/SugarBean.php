41a3b97031a2bc443bae081501d55288e5747db5
j.buchanan <jack.buchanan@salesagility.com>
2019-11-08 13:29:41 +0000

    Fix change log issue #8211
    Remove htmlentities when setting $this->$key as its not needed here

--- data/SugarBean.php
+++ data/SugarBean.php
@@ -2457,7 +2457,7 @@ class SugarBean
                     (strpos($type, 'char') !== false || strpos($type, 'text') !== false || $type == 'enum') &&
                     !empty($this->$key)
                 ) {
-                    $this->$key = htmlentities(SugarCleaner::cleanHtml($this->$key, true));
+                    $this->$key = SugarCleaner::cleanHtml($this->$key, true);
                 }
             }
         }
