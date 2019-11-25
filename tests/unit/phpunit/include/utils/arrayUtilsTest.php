1a3432fb243446e2d0aafd1b354a7bdbfc146563
Connor Shea <connor.james.shea@gmail.com>
2019-11-25 12:42:04 -0700

    Fix parameter order for asserts in unit tests.
    The expected value is supposed to go first, followed by the actual
    value. This swaps them in a few places that used the wrong order.
    
    This is important because PHPUnit depends on this when showing
    diffs on test failure.

--- tests/unit/phpunit/include/utils/arrayUtilsTest.php
+++ tests/unit/phpunit/include/utils/arrayUtilsTest.php
@@ -12,7 +12,7 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
 
         $expected = "array (\n  'Key1' => 'value1',\n  'Key2' => 'value2',\n)";
         $actual = var_export_helper($tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testoverride_value_to_string()
@@ -20,7 +20,7 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         //execute the method and test if it returns expected values
         $expected = "\$array_name['value_name'] = 'value';";
         $actual = override_value_to_string('array_name', 'value_name', 'value');
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testadd_blank_option()
@@ -30,14 +30,14 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $expected = array('' => '', 'Key1' => 'value1', 'Key2' => 'value2');
 
         $actual = add_blank_option($tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //execute the method with array having a blank key value pair. function will return the same array back without any change.
         $tempArray = array('' => '', 'Key1' => 'value1', 'Key2' => 'value2');
         $expected = array('' => '', 'Key1' => 'value1', 'Key2' => 'value2');
 
         $actual = add_blank_option($tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testoverride_value_to_string_recursive()
@@ -48,7 +48,7 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $tempArray = array('Key1' => 'value1', 'Key2' => 'value2');
         $expected = "\$tempArray=array (\n  'Key1' => 'value1',\n  'Key2' => 'value2',\n);";
         $actual = override_value_to_string_recursive('', 'tempArray', $tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //with keys
         $tempArray = array();
@@ -56,7 +56,7 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $expected = "\$tempArray['key1']['key2']=array (\n  'Key1' => \n  array (\n    'Key2' => \n    array (\n      'Key3' => 'value',\n      'Key4' => 'value',\n    ),\n  ),\n);";
         $actual = override_value_to_string_recursive(array('key1', 'key2'), 'tempArray', $tempArray);
 
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testoverride_recursive_helper()
@@ -67,14 +67,14 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $tempArray = array('Key1' => 'value1', 'Key2' => 'value2');
         $expected = "=array (\n  'Key1' => 'value1',\n  'Key2' => 'value2',\n);";
         $actual = override_recursive_helper('', 'tempArray', $tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //with keys
         $tempArray = array();
         $tempArray['Key1']['Key2'] = array('Key3' => 'value', 'Key4' => 'value');
         $expected = "['key1']['key2']=array (\n  'Key1' => \n  array (\n    'Key2' => \n    array (\n      'Key3' => 'value',\n      'Key4' => 'value',\n    ),\n  ),\n);";
         $actual = override_recursive_helper(array('key1', 'key2'), 'tempArray', $tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testoverride_value_to_string_recursive2()
@@ -84,20 +84,20 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         //null array
         $expected = null;
         $actual = override_value_to_string_recursive2('tempArray', 'key1', '', false);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //simple array
         $tempArray = array('Key1' => 'value1', 'Key2' => 'value2');
         $expected = "\$['tempArray']['Key1'] = 'value1';\n$['tempArray']['Key2'] = 'value2';\n";
         $actual = override_value_to_string_recursive2('', 'tempArray', $tempArray);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //complex array
         $tempArray = array();
         $tempArray['Key1']['Key2'] = array('Key3' => 'value', 'Key4' => 'value');
         $expected = "\$tempArray['key1']['Key2']['Key3'] = 'value';\n\$tempArray['key1']['Key2']['Key4'] = 'value';\n";
         $actual = override_value_to_string_recursive2('tempArray', 'key1', $tempArray['Key1']);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testobject_to_array_recursive()
@@ -108,14 +108,14 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $obj = '';
         $expected = '';
         $actual = object_to_array_recursive($obj);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //test with a valid object
         $obj = new TimeDate();
         $expected = array('dbDayFormat' => 'Y-m-d', 'dbTimeFormat' => 'H:i:s', 'allow_cache' => true);
         $actual = object_to_array_recursive($obj);
 
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testdeepArrayDiff()
@@ -127,35 +127,35 @@ class array_utilsTest extends SuitePHPUnitFrameworkTestCase
         $tempArray2 = array('Key1' => 'value1', 'Key2' => 'value2');
         $expected = array();
         $actual = deepArrayDiff($tempArray1, $tempArray2);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //different simple arrays
         $tempArray1 = array('Key1' => 'value1', 'Key2' => 'value2');
         $tempArray2 = array('Key1' => 'value1', 'Key2' => 'value3');
         $expected = array('Key2' => 'value2');
         $actual = deepArrayDiff($tempArray1, $tempArray2);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //same complex arrays
         $tempArray1 = array('Key1' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $tempArray2 = array('Key1' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $expected = array();
         $actual = deepArrayDiff($tempArray1, $tempArray2);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //complex arrays with different root node
         $tempArray1 = array('Key1' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $tempArray2 = array('Key2' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $expected = array('Key1' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $actual = deepArrayDiff($tempArray1, $tempArray2);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
 
         //complex arrays with different child node
         $tempArray1 = array('Key1' => array('Key2' => 'value2', 'Key3' => 'value3'));
         $tempArray2 = array('Key1' => array('Key2' => 'value2', 'Key4' => 'value4'));
         $expected = array('Key1' => array('Key3' => 'value3'));
         $actual = deepArrayDiff($tempArray1, $tempArray2);
-        $this->assertSame($actual, $expected);
+        $this->assertSame($expected, $actual);
     }
 
     public function testsetDeepArrayValue()
