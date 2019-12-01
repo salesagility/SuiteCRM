7311c17a9cd41e838e9f8b983804642e3d29f679
Petr <56717039+pkonetskiy@users.noreply.github.com>
2019-12-01 21:19:50 +0300

    Add files via upload

--- include/SugarFields/Fields/Collection/CollectionDetailViewRow.tpl
+++ include/SugarFields/Fields/Collection/CollectionDetailViewRow.tpl
@@ -38,44 +38,27 @@
  ********************************************************************************/
 
 *}
-
-<div id='{{sugarvar key='name'}}_image_div' name='{{sugarvar key='name'}}_image_div'><img src="{sugar_getimagepath file='sqsWait.gif'}" alt="loading..." id="{{sugarvar key="name"}}_loading_img" style="display:none"></div>
-<div id='{{sugarvar key='name'}}_div' name='{{sugarvar key='name'}}_div'></div>
-<script type="text/javascript">
-    document.getElementById('{{sugarvar key="name"}}_image_div').parentElement.previousElementSibling.style.width="10%";
-    document.getElementById('{{sugarvar key="name"}}_image_div').parentElement.style.width="90%";
-{literal}
-    var callback = {
-        success:function(o){
-           {/literal}
-
-            document.getElementById('{{sugarvar key="name"}}_loading_img').style.display="none";
-            document.getElementById('{{sugarvar key="name"}}_div').innerHTML = o.responseText;
-            SUGAR.util.evalScript(o.responseText);
-            {literal}
-        },
-        failure:function(o){
-            alert(SUGAR.language.get('app_strings','LBL_AJAX_FAILURE'));
-        }
-    }
-    {/literal}
-    var action_type = 'editview';
-    if (document.getElementById('{{sugarvar key="name"}}_collection_action_type'))
-        if (document.getElementById('{{sugarvar key="name"}}_collection_action_type').value != '')
-            var action_type = document.getElementById('{{sugarvar key="name"}}_collection_action_type').value;
-    document.getElementById('{{sugarvar key="name"}}_loading_img').style.display="inline";
-{literal}
-    if (typeof(document.forms.EditView) == 'undefined')
-        for(var s=0; s < document.forms.length; s++){
-            if (document.forms[s].getAttribute("name").indexOf('QuickCreate') >= 0) {
-                var formnamefound = document.forms[s].getAttribute("name");
-            }
-        }
-     else
-         var formnamefound = 'EditView';
-{/literal}
-    postData = '&displayParams=' + '{{$displayParamsJSON}}' + '&vardef=' + '{{$vardefJSON}}' + '&module_dir=' + document.forms[formnamefound].module.value + '&bean_id=' + document.forms[formnamefound].record.value + '&action_type=' + action_type;
-    {literal}
-    YAHOO.util.Connect.asyncRequest('POST', 'index.php?action=viewsugarfieldcollection', callback, postData);
-{/literal}
-</script>
+<div id="{$displayParams.formName}_{$vardef.name}" name="{$displayParams.formName}_{$vardef.name}">
+<table id="table_collection_{$vardef.name}" style="border-collapse:separate;border-top:solid 1px #999999;border-left:solid 1px #999999;border-right:solid 1px #999999; margin-top: 5px !important;border-radius: 6px;">
+    <tr style="font-weight: bold;" id="lineLabel_{$displayParams.formName}_{$vardef.name}" name="lineLabel_{$displayParams.formName}_{$vardef.name}">
+        {foreach item=extra_field from=$displayParams.collection_field_list key=key_extra}
+            {if $extra_field.displayParams.hidden != 'hidden'}
+                <th style="padding: 5px 5px 10px 5px !important; text-align: center;{if $extra_field.displayParams.size != ''}width:{$extra_field.displayParams.size}{/if}">
+                    {$extra_field.label}
+                </th>
+            {/if}
+        {/foreach}
+   </tr>
+    {foreach item=extra_value from=$count_values key=key_extra_value}
+        <tr id="lineFields_{$displayParams.formName}_{$vardef.name}_{$extra_value}">
+            {foreach item=extra_field from=$displayParams.to_display.$extra_value key=key_extra}
+                {if $extra_field.hidden != 'hidden'}
+                    <td nowrap style="padding: 5px 5px 10px 5px !important; vertical-align: middle;text-align: right;">
+                        {$extra_field.field}
+                    </td>
+                {/if}
+            {/foreach}
+       </tr>
+   {/foreach}
+</table>
+</div>
--- include/SugarFields/Fields/Collection/SugarFieldCollection.php
+++ include/SugarFields/Fields/Collection/SugarFieldCollection.php
@@ -1,11 +1,10 @@
 <?php
-/**
- *
+/*********************************************************************************
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- *
- * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by Salesagility Ltd.
+ * Copyright (C) 2011 - 2014 Salesagility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -16,7 +15,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
  * details.
  *
  * You should have received a copy of the GNU Affero General Public License along with
@@ -34,205 +33,109 @@
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
  * these Appropriate Legal Notices must retain the display of the "Powered by
  * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- */
+ * reasonably feasible for  technical reasons, the Appropriate Legal Notices must
+ * display the words  "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ ********************************************************************************/
 
 require_once('include/SugarFields/Fields/Base/SugarFieldBase.php');
-class SugarFieldCollection extends SugarFieldBase
-{
-    public $tpl_path;
-    
-    public function getDetailViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex)
-    {
-        $nolink = array('Users');
-        if (in_array($vardef['module'], $nolink)) {
-            $displayParams['nolink']=true;
-        } else {
-            $displayParams['nolink']=false;
-        }
-        $json = getJSONobj();
+class SugarFieldCollection extends SugarFieldBase {
+	var $tpl_path;
+	
+	function getDetailViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex) {
+		$nolink = array('Users');
+		if(in_array($vardef['module'], $nolink)){
+			$displayParams['nolink']=true;
+		}else{
+			$displayParams['nolink']=false;
+		}
+		$json = getJSONobj();
         $displayParamsJSON = $json->encode($displayParams);
         $vardefJSON = $json->encode($vardef);
         $this->ss->assign('displayParamsJSON', '{literal}'.$displayParamsJSON.'{/literal}');
         $this->ss->assign('vardefJSON', '{literal}'.$vardefJSON.'{/literal}');
         $this->setup($parentFieldArray, $vardef, $displayParams, $tabindex);
-        if (empty($this->tpl_path)) {
-            $this->tpl_path = $this->findTemplate('DetailView');
+        if(empty($this->tpl_path)){
+        	$this->tpl_path = $this->findTemplate('DetailView');
         }
         return $this->fetch($this->tpl_path);
     }
 
-    public function getEditViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex, $searchView = false)
-    {
-        if ($searchView) {
-            $form_name = 'search_form';
-        } else {
-            $form_name = 'EditView';
+    function getEditViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex, $searchView = false) {
+        if($searchView){
+        	$form_name = 'search_form';
+        }else{
+    		$form_name = 'EditView';
         }
         $json = getJSONobj();
-        $displayParamsJSON = $json->encode($displayParams);
+        $displayParamsJSON = json_encode($displayParams, JSON_HEX_APOS);
         $vardefJSON = $json->encode($vardef);
         $this->ss->assign('required', !empty($vardef['required']));
         $this->ss->assign('displayParamsJSON', '{literal}'.$displayParamsJSON.'{/literal}');
         $this->ss->assign('vardefJSON', '{literal}'.$vardefJSON.'{/literal}');
 
-        $keys = $this->getAccessKey($vardef, 'COLLECTION', $vardef['module']);
-        $displayParams['accessKeySelect'] = $keys['accessKeySelect'];
-        $displayParams['accessKeySelectLabel'] = $keys['accessKeySelectLabel'];
-        $displayParams['accessKeySelectTitle'] = $keys['accessKeySelectTitle'];
-        $displayParams['accessKeyClear'] = $keys['accessKeyClear'];
-        $displayParams['accessKeyClearLabel'] = $keys['accessKeyClearLabel'];
-        $displayParams['accessKeyClearTitle'] = $keys['accessKeyClearTitle'];
-
         $this->setup($parentFieldArray, $vardef, $displayParams, $tabindex);
-        if (!$searchView) {
-            if (empty($this->tpl_path)) {
-                $this->tpl_path = $this->findTemplate('EditView');
-            }
-            return $this->fetch($this->tpl_path);
-        }
+	    if(!$searchView) {
+	    	if(empty($this->tpl_path)){
+        		$this->tpl_path = $this->findTemplate('EditView');
+        	}
+	    	return $this->fetch($this->tpl_path);
+	    }
     }
 
-    public function getSearchViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex)
-    {
-        $this->getEditViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex, true);
+	function getSearchViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex) {
+		$this->getEditViewSmarty($parentFieldArray, $vardef, $displayParams, $tabindex, true);
     }
-    /**
-    * This should be called when the bean is saved. The bean itself will be passed by reference
-    * @param SugarBean bean - the bean performing the save
-    * @param array params - an array of paramester relevant to the save, most likely will be $_REQUEST
-    */
-    public function save(&$bean, $params, $field, $properties, $prefix = '')
-    {
-        if (isset($_POST["primary_" . $field . "_collection"])) {
-            $save = false;
-            $value_name = $field . "_values";
-            $link_field = array();
-            // populate $link_field from POST
-            foreach ($_POST as $name=>$value) {
-                if (strpos($name, $field . "_collection_") !== false) {
-                    $num = substr($name, -1);
-                    if (is_numeric($num)) {
-                        $num = (int)$num;
-                        if (strpos($name, $field . "_collection_extra_") !== false) {
-                            $extra_field = substr($name, $field . "_collection_extra_" . $num);
-                            $link_field[$num]['extra_field'][$extra_field]=$value;
-                        } elseif ($name == $field . "_collection_" . $num) {
-                            $link_field[$num]['name']=$value;
-                        } elseif ($name == "id_" . $field . "_collection_" . $num) {
-                            $link_field[$num]['id']=$value;
+     /**
+     * This should be called when the bean is saved. The bean itself will be passed by reference
+     * @param SugarBean bean - the bean performing the save
+     * @param array params - an array of paramester relevant to the save, most likely will be $_REQUEST
+     */
+	public function save(&$bean, $params, $field, $properties, $prefix = ''){
+        $link_field = $params[$field];
+        if(!empty($link_field) && ($bean->field_defs[$link_field]['type'] == 'link')){
+            require_once('data/Link2.php');
+            $bean->load_relationship($link_field);
+            $actual_field_list = Array();
+            foreach($params as $name=>$value){
+                $explode_string = '_'.$link_field.'_collection_';
+                $new_array = explode($explode_string, $name);
+                if (count($new_array) == 2){
+                    $actual_field_list[$new_array[1]][$new_array[0]] = $value;
+                }
+            }
+            $change_list = explode(';', $params['collection_'.$link_field.'_change']);
+            $bean_name = $bean->field_defs[$link_field]['bean_name'];
+            foreach($actual_field_list as $key => $value_list){
+                if(in_array($key, $change_list)) {
+                    $bean_collection = new $bean_name();
+                    if (!empty($value_list['id'])) {
+                        $bean_collection->retrieve($value_list['id']);
+                    }
+                    $empty_field = 0;
+                    foreach($value_list as $name=>$value){
+                        if ($name != 'id') {
+                            $bean_collection->$name = $value;
+                            if (!empty($value)) $empty_field += 1;
                         }
+                    }
+                    if ($empty_field > 0) {
+                        $bean_collection->assigned_user_id = $bean->assigned_user_id;
+                        $bean_collection->save();
+                        if (empty($bean->id))
+                            $bean->save();
+                        $bean->{$link_field}->add($bean_collection->id);
                     }
                 }
             }
-            // Set Primary
-            if (isset($_POST["primary_" . $field . "_collection"])) {
-                $primary = $_POST["primary_" . $field . "_collection"];
-                $primary = (int)$primary;
-                $link_field[$primary]['primary']=true;
-            }
-            // Create or update record and take care of the extra_field
-            require('include/modules.php');
-            require_once('data/Link.php');
-            $class = load_link_class($bean->field_defs[$field]);
-            
-            $link_obj = new $class($bean->field_defs[$field]['relationship'], $bean, $bean->field_defs[$field]);
-            $module = $link_obj->getRelatedModuleName();
-            $beanName = $beanList[$module];
-            require_once($beanFiles[$beanName]);
-            foreach ($link_field as $k=>$v) {
-                $save = false;
-                $update_fields = array();
-                $obj = new $beanName();
-                if (!isset($link_field[$k]['name']) || empty($link_field[$k]['name'])) {
-                    // There is no name so it is an empty record -> ignore it!
-                    unset($link_field[$k]);
-                    break;
-                }
-                if (!isset($link_field[$k]['id']) || empty($link_field[$k]['id']) || (isset($_POST[$field . "_new_on_update"]) && $_POST[$field . "_new_on_update"] === 'true')) {
-                    // Create a new record
-                    if (isset($_POST[$field . "_allow_new"]) && ($_POST[$field . "_allow_new"] === 'false' || $_POST[$field . "_allow_new"] === false)) {
-                        // Not allow to create a new record so remove from $link_field
-                        unset($link_field[$k]);
-                        break;
-                    }
-                    if (!isset($link_field[$k]['id']) || empty($link_field[$k]['id'])) {
-                        // There is no ID so it is a new record
-                        $save = true;
-                        $obj->name=$link_field[$k]['name'];
-                    } else {
-                        // We duplicate an existing record because new_on_update is set
-                        $obj->retrieve($link_field[$k]['id']);
-                        $obj->id='';
-                        $obj->name = $obj->name . '_DUP';
-                    }
-                } else {
-                    // id exist so retrieve the data
-                    $obj->retrieve($link_field[$k]['id']);
-                }
-                // Update the extra field for the new or the existing record
-                if (isset($v['extra_field']) && is_array($v['extra_field'])) {
-                    // Retrieve the changed fields
-                    if (isset($_POST["update_fields_{$field}_collection"]) && !empty($_POST["update_fields_{$field}_collection"])) {
-                        $JSON = getJSONobj();
-                        $update_fields = $JSON->decode(html_entity_decode($_POST["update_fields_{$field}_collection"]));
-                    }
-                    // Update the changed fields
-                    foreach ($update_fields as $kk=>$vv) {
-                        if (!isset($_POST[$field . "_allow_update"]) || ($_POST[$field . "_allow_update"] !== 'false' && $_POST[$field . "_allow_update"] !== false)) {
-                            //allow to update the extra_field in the record
-                            if (isset($v['extra_field'][$kk]) && $vv == true) {
-                                $extra_field_name = str_replace("_".$field."_collection_extra_".$k, "", $kk);
-                                if ($obj->$extra_field_name != $v['extra_field'][$kk]) {
-                                    $save = true;
-                                    $obj->$extra_field_name=$v['extra_field'][$kk];
-                                }
-                            }
-                        }
-                    }
-                }
-                // Save the new or updated record
-                if ($save) {
-                    if (!$obj->ACLAccess('save')) {
-                        ACLController::displayNoAccess(true);
-                        sugar_cleanup(true);
-                    }
-                    $obj->save();
-                    $link_field[$k]['id']=$obj->id;
-                }
-            }
-            // Save new relationship or delete deleted relationship
-            if (!empty($link_field)) {
-                if ($bean->load_relationship($field)) {
-                    $oldvalues = $bean->$field->get(true);
-                    $role_field = $bean->$field->_get_link_table_role_field($bean->$field->_relationship_name);
-                    foreach ($link_field as $new_v) {
-                        if (!empty($new_v['id'])) {
-                            if (!empty($role_field)) {
-                                if (isset($new_v['primary']) && $new_v['primary']) {
-                                    $bean->$field->add($new_v['id'], array($role_field=>'primary'));
-                                } else {
-                                    $bean->$field->add($new_v['id'], array($role_field=>'NULL'));
-                                }
-                            } else {
-                                $bean->$field->add($new_v['id'], array());
-                            }
-                        }
-                    }
-                    foreach ($oldvalues as $old_v) {
-                        $match = false;
-                        foreach ($link_field as $new_v) {
-                            if ($new_v['id'] == $old_v['id']) {
-                                $match = true;
-                            }
-                        }
-                        if (!$match) {
-                            $bean->$field->delete($bean->id, $old_v['id']);
-                        }
-                    }
+            $delete_id_list = explode(';', $params['collection_'.$link_field.'_remove']);
+            if (!empty($delete_id_list)){
+                foreach ($delete_id_list as $delete_id) {
+                    $bean_collection = new $bean_name();
+                    $bean_collection->retrieve($delete_id);
+                    $bean_collection->mark_deleted($delete_id);
                 }
             }
         }
     }
 }
+?>
