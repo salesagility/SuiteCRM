82dbb069c96c561296d1d9d59b110b1a67b1f6b6
_camy_ <42933743+cameronblaikie@users.noreply.github.com>
2019-12-02 13:07:17 +0000

    Update Api/Core/Config/ApiConfig.php
    Co-Authored-By: Dillon Brown <26431166+Dillon-Brown@users.noreply.github.com>

--- Api/Core/Config/ApiConfig.php
+++ Api/Core/Config/ApiConfig.php
@@ -18,7 +18,7 @@ class ApiConfig
 
     const OAUTH2_PRIVATE_KEY = 'Api/V8/OAuth2/private.key';
     const OAUTH2_PUBLIC_KEY = 'Api/V8/OAuth2/public.key';
-    const OAUTH2_ENCRYPTION_KEY = 'WvHCQfzJCmKsGnN4EL+K2IUzM37BRd9yJZCKA6bEAOM=';
+    const OAUTH2_ENCRYPTION_KEY = '';
     
     /**
      *
