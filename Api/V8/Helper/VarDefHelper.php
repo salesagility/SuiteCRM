b0d3dc7d813af6e9d8ae8d74550f64fef741913f
cameronblaikie <camy.blaikie@gmail.com>
2019-12-02 11:52:36 +0000

    API Relationships fix - better way of fixing issue

--- Api/V8/Helper/VarDefHelper.php
+++ Api/V8/Helper/VarDefHelper.php
@@ -1,37 +1,21 @@
 <?php
 namespace Api\V8\Helper;
-
 class VarDefHelper
 {
     /**
      * @param \SugarBean $bean
      *
-     * @return void
+     * @return array
      */
     public function getAllRelationships(\SugarBean $bean)
     {
-        $tempBean = \BeanFactory::getBean($bean->module_name,$bean->id);
         $relations = [];
         $linkedFields = $bean->get_linked_fields();
-        array_push($linkedFields, 'Email_addresses');
-
         foreach ($linkedFields as $relation => $varDef) {
             if (isset($varDef['module']) && $bean->load_relationship($relation)) {
                 $relations[$relation] = $varDef['module'];
             }
         }
-
-        $email = [
-            'email_addresses' => 'EmailAddresses'
-        ];
-
-        $bean->load_relationship('email_addresses');
-        $emailIds = $bean->email_addresses->get();
-
-        if (!empty($emailIds)){
-            $relations = array_merge($relations, $email);
-        }
-
         return $relations;
     }
 }
