5f0ae27f17420b6e3c9d030770f171e26a420ad8
Noah Hummel <n.p.hummel@gmail.com>
2019-12-03 17:08:07 +0100

    FIX ControllerFactory crashes
    ControllerFactory crashes when the custom controller
    class name is wrong. In other cases, we already default to
    the SugarController. This is now the default for all
    cases where a custom controller could not be loaded
    for any reason. Whenever a custom controller file is
    found, but the controller can't be instantiated,
    appropriate log messages are emitted.

--- include/MVC/Controller/ControllerFactory.php
+++ include/MVC/Controller/ControllerFactory.php
@@ -52,38 +52,47 @@ class ControllerFactory
      */
     public static function getController($module)
     {
+        $logger = LoggerManager::getLogger();
+
+        $controller = null;
+        // below are the allowed class names for controller classes
         $class = ucfirst($module).'Controller';
         $customClass = 'Custom' . $class;
+        // try all known controller locations
         if (file_exists('custom/modules/'.$module.'/controller.php')) {
-            $customClass = 'Custom' . $class;
             require_once('custom/modules/'.$module.'/controller.php');
             if (class_exists($customClass)) {
                 $controller = new $customClass();
-            } else {
-                if (class_exists($class)) {
+            } elseif (class_exists($class)) {
                     $controller = new $class();
-                }
+            } else {
+                $logger->warn('A custom controller was loaded from custom/modules/'.$module.'/controller.php, but it neither contains the class '.$class.', nor the custom class'.$customClass.'. This is most likely a programming error.');
             }
         } elseif (file_exists('modules/'.$module.'/controller.php')) {
             require_once('modules/'.$module.'/controller.php');
             if (class_exists($customClass)) {
                 $controller = new $customClass();
+            } elseif (class_exists($class)) {
+                $controller = new $class();
             } else {
-                if (class_exists($class)) {
-                    $controller = new $class();
-                }
+                $logger->warn('A controller was loaded from modules/'.$module.'/controller.php, but it neither contains the class '.$class.', nor the custom class'.$customClass.'. This is most likely a programming error.');
             }
-        } else {
-            if (file_exists('custom/include/MVC/Controller/SugarController.php')) {
-                require_once('custom/include/MVC/Controller/SugarController.php');
-            }
+
+        } elseif (file_exists('custom/include/MVC/Controller/SugarController.php')) {
+            require_once('custom/include/MVC/Controller/SugarController.php');
             if (class_exists('CustomSugarController')) {
                 $controller = new CustomSugarController();
             } else {
-                $controller = new SugarController();
+                $logger->warn('A custom controller was loaded from custom/include/MVC/Controller/SugarController.php, but it contains no CustomSugarController class. This is most likely a programming error.');
             }
         }
-        //setup the controller
+        // if controller is still unset, no custom controller could be loaded
+        if ($controller == null) {
+            // default to default controller in ALL other cases
+            $logger->debug('Defaulting to the default SugarController.');
+            $controller = new SugarController();
+        }
+
         $controller->setup($module);
         return $controller;
     }
