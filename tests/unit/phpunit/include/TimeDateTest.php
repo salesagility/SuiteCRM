6c890f957cb8b2f520a6f5017971257c5f541df1
Dillon-Brown <dillon.brown@salesagility.com>
2019-12-20 15:43:20 +0000

    Fixed TimeDateTest.php
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- tests/unit/phpunit/include/TimeDateTest.php
+++ tests/unit/phpunit/include/TimeDateTest.php
@@ -4,6 +4,29 @@ use SuiteCRM\Test\SuitePHPUnitFrameworkTestCase;
 
 class TimeDateTest extends SuitePHPUnitFrameworkTestCase
 {
+    /**
+     * @var TimeDate
+     */
+    private $timeDate;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        global $current_user;
+        get_sugar_config_defaults();
+        $current_user = new User();
+
+        $this->timeDate = new TimeDate();
+    }
+
+    public function testGetTimeLapse()
+    {
+        $result = $this->timeDate->getTimeLapse('2016-01-15 11:16:02');
+        $this->assertTrue(isset($result));
+        $this->assertInternalType('string', $result);
+    }
+
     public function testget_date_format()
     {
         // Validate that it gets the date format from the user's preferences.
