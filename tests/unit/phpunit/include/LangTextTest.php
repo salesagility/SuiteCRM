e2a67cb91db521122e281d732384298260b9ad7b
Dillon-Brown <dillon.brown@salesagility.com>
2020-01-14 15:17:34 +0000

    Fix access level to LangTextTest::setUp() must be public
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- tests/unit/phpunit/include/LangTextTest.php
+++ tests/unit/phpunit/include/LangTextTest.php
@@ -54,7 +54,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
  */
 class LangTextTest extends SuitePHPUnitFrameworkTestCase
 {
-    protected function setUp()
+    public function setUp()
     {
         parent::setUp();
         if (!defined('sugarEntry')) {
@@ -78,7 +78,7 @@ class LangTextTest extends SuitePHPUnitFrameworkTestCase
         include_once __DIR__ . '/../../../../include/LangException.php';
     }
 
-    protected function tearDown()
+    public function tearDown()
     {
         parent::tearDown();
     }
