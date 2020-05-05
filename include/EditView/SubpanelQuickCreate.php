313c2412992efe3d182ac3d1bf265d276a158559
Connor Shea <connor.james.shea@gmail.com>
2020-05-05 13:42:06 -0600

    Fix a few more PHPDocs.

--- include/EditView/SubpanelQuickCreate.php
+++ include/EditView/SubpanelQuickCreate.php
@@ -200,8 +200,8 @@ class SubpanelQuickCreate
      *  3. custom/module/editviewdefs.php
      *  4. module/editviewdefs.php
      *
-     * @param $module
-     * @param $view
+     * @param string $module
+     * @param string $view
      * @return string The path to the viewdefs file to use
      */
     public function getModuleViewDefsSourceFile($module, $view)
