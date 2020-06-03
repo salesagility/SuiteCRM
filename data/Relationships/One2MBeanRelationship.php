fcb44f3c801b8840eed7f4fdf5c887db5fad8bdf
Frank S <fgunni@01019freenet.de>
2020-06-03 21:47:48 +0200

    Better check for not empty than isset
    For me i following problem:
    I have a custom module that has the column "parent_type" in the database.
    Now i added activities to that custom module and I got an MySQL-Error on ambigous column "parent_type"
    Hunted it down to where the table name wont be added to the query.

--- data/Relationships/One2MBeanRelationship.php
+++ data/Relationships/One2MBeanRelationship.php
@@ -408,7 +408,7 @@ class One2MBeanRelationship extends One2MRelationship
 
     public function getRelationshipTable()
     {
-        if (isset($this->def['table'])) {
+        if (!empty($this->def['table'])) {
             return $this->def['table'];
         }
         return $this->def['rhs_table'];
