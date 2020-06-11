e523df75ffaeaa593a1eebb4f7306b9e9c81759c
Matt Lorimer <matthew.lorimer@salesagility.com>
2020-06-11 10:25:49 +0100

    7.11.15

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.11.14';
-$suitecrm_timestamp = '2020-06-10 17:00:00';
+$suitecrm_version = '7.11.15';
+$suitecrm_timestamp = '2020-06-11 12:00:00';
