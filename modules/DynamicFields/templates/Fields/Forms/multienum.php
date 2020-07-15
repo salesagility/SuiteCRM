6535789db5bfd2eba6a73259a2fbb37f154049da
dominic <dominicchinkh@gmail.com>
2020-07-16 09:51:09 +1000

    Fix enum language

--- modules/DynamicFields/templates/Fields/Forms/multienum.php
+++ modules/DynamicFields/templates/Fields/Forms/multienum.php
@@ -45,7 +45,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 
 
-$edit_mod_strings = return_module_language($GLOBALS['current_language'], 'EditCustomFields');
+$edit_mod_strings = return_module_language($GLOBALS['current_language'], 'DynamicFields');
 $edit_mod_strings['LBL_DROP_DOWN_LIST'] = $edit_mod_strings['LBL_MULTI_SELECT_LIST'];
 $multi = 'true';
 require_once('modules/DynamicFields/templates/Fields/Forms/enum2.php');
--- modules/DynamicFields/templates/Fields/Forms/radioenum.php
+++ modules/DynamicFields/templates/Fields/Forms/radioenum.php
@@ -45,6 +45,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 
 
-$edit_mod_strings = return_module_language($GLOBALS['current_language'], 'EditCustomFields');
+$edit_mod_strings = return_module_language($GLOBALS['current_language'], 'DynamicFields');
 $edit_mod_strings['LBL_DROP_DOWN_LIST'] = $edit_mod_strings['LBL_RADIO_FIELDS'];
 require_once('modules/DynamicFields/templates/Fields/Forms/enum2.php');
