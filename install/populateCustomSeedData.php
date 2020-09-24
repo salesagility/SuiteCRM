8dd224d4b93ee79630f112bf290c7b76fb21e68a
Jose C. Massón <jose@gcoop.coop>
2020-09-24 17:51:05 -0300

    FIX #8873 - Silent Install - Load custom data

---
+++ install/populateCustomSeedData.php
@@ -0,0 +1,40 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$customDataFiles = glob("custom/install/customData*.php");
+
+if (empty($customDataFiles)) {
+    $customDataFiles = ['install/customData.php'];
+}
+
+foreach ($customDataFiles as $file) {
+    require_once $file;
+
+    if (isset($suitecrm_custom_data)) {
+        importCustomData($suitecrm_custom_data);
+        unset($suitecrm_custom_data);
+    }
+}
+
+
+function importCustomData(array $suitecrm_custom_data)
+{
+    foreach ($suitecrm_custom_data as $module => $data) {
+        foreach ($data as $record) {
+            $bean = BeanFactory::newBean($module);
+
+            if (!$bean) {
+                LoggerManager::getLogger()->error("Unable to import custom data. The module {$module} does not exists");
+                continue;
+            }
+            LoggerManager::getLogger()->info("Importing custom data for module: {$module}");
+
+            foreach ($record as $field => $value) {
+                $bean->$field = $value;
+            }
+            $bean->save();
+        }
+    }
+}
