8c8186f668056c7f89552d5f99220775ea44cd7b
Clemente Raposo <clemente.raposo@salesagility.com>
2020-12-09 18:03:31 +0000

    Build 8.0.0-alpha.3.1

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.0.0-alpha.3';
+$suitecrm_version = '8.0.0-alpha.3.1';
 $suitecrm_legacy = '7.11.18';
-$suitecrm_timestamp = '2020-10-15 17:00:00';
+$suitecrm_timestamp = '2020-12-10 09:00:00';
