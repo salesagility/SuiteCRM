c358eea768c48f309fddad1cdc0e9059ba74a41f
Clemente Raposo <clemente.raposo@salesagility.com>
2021-03-25 11:15:43 +0000

    [Legacy] Update Contacts and Accounts menu config
    - Update config to re-redirect to detail view after create.

--- modules/Contacts/Menu.php
+++ modules/Contacts/Menu.php
@@ -45,7 +45,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
 
 global $mod_strings, $app_strings, $sugar_config;
     if (ACLController::checkAccess('Contacts', 'edit', true)) {
-        $module_menu[] = array("index.php?module=Contacts&action=EditView&return_module=Contacts&return_action=index", $mod_strings['LNK_NEW_CONTACT'],"Create", 'Contacts');
+        $module_menu[] = array("index.php?module=Contacts&action=EditView&return_module=Contacts&return_action=DetailView", $mod_strings['LNK_NEW_CONTACT'],"Create", 'Contacts');
     }
 
     if (ACLController::checkAccess('Contacts', 'import', true)) {
