817a12dc0c30c189f56d5cb1f7dc37a9631bdbe3
Dillon-Brown <dillon.brown@salesagility.com>
2021-03-31 10:54:13 +0100

    [Legacy] Build 8.0.0-beta-1
    Increment suitecrm_version
    Update README.md

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.0.0-alpha.3.1';
+$suitecrm_version = '8.0.0-beta.1';
 $suitecrm_legacy = '7.11.18';
 $suitecrm_timestamp = '2020-12-10 09:00:00';
