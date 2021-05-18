cde96fd75811ea37e0e9cad7bf15494ac052b7b2
Clemente Raposo <clemente.raposo@salesagility.com>
2021-05-18 18:33:45 +0100

    [Legacy] Change Case update wiget items display
    - Change items display
    - Move case updates widget to the top

--- modules/Cases/metadata/detailviewdefs.php
+++ modules/Cases/metadata/detailviewdefs.php
@@ -92,6 +92,122 @@ array(
       ],
       'sidebarWidgets' => [
           [
+              'type' => 'record-thread',
+              'labelKey' => 'LBL_CASE_UPDATES',
+              'options' => [
+                  'recordThread' => [
+                      'module' => 'case-updates',
+                      'class' => 'case-updates',
+                      'filters' => [
+                          'parentFilters' => [
+                              'id' => 'case_id'
+                          ],
+                          'orderBy' => 'date_entered',
+                          'sortOrder' => 'DESC'
+                      ],
+                      'item' => [
+                          'itemClass' => 'case-updates-item pt-2 pb-2',
+                          'collapsible' => true,
+                          'dynamicClass' => ['source', 'internal'],
+                          'layout' => [
+                              'header' => ['rows' => []],
+                              'body' =>[
+                                  'rows' => [
+                                      [
+                                          'align' => 'end',
+                                          'justify' => 'between',
+                                          'cols' => [
+                                              [
+                                                  'field' => 'author',
+                                                  'labelDisplay' => 'none',
+                                                  'hideIfEmpty' => true,
+                                                  'class' => 'font-weight-bold item-title'
+                                              ],
+                                              [
+                                                  'field' => 'internal',
+                                                  'labelDisplay' => 'inline',
+                                                  'labelClass' => 'm-0',
+                                                  'display' => 'none',
+                                                  'hideIfEmpty' => true,
+                                                  'class' => 'small ml-auto font-weight-light'
+                                              ],
+                                          ]
+                                      ],
+                                      [
+                                          'align' => 'start',
+                                          'justify' => 'start',
+                                          'class' => 'flex-grow-1 item-content',
+                                          'cols' => [
+                                              [
+                                                  'field' => [
+                                                      'name' => 'description',
+                                                      'type' => 'html',
+                                                  ],
+                                                  'labelDisplay' => 'none',
+                                              ]
+                                          ]
+                                      ],
+                                      [
+                                          'justify' => 'end',
+                                          'class' => 'flex-grow-1',
+                                          'cols' => [
+                                              [
+                                                  'field' => 'date_entered',
+                                                  'labelDisplay' => 'none',
+                                                  'hideIfEmpty' => true,
+                                                  'class' => 'small ml-auto font-weight-light'
+                                              ],
+                                          ]
+                                      ]
+                                  ]
+                              ]
+                          ],
+                      ],
+                      'create' => [
+                          'presetFields' => [
+                              'parentValues' => [
+                                  'id' => 'case_id'
+                              ],
+                          ],
+                          'layout' => [
+                              'header' => ['rows' => []],
+                              'body' =>[
+                                  'rows' => [
+                                      [
+                                          'justify' => 'start',
+                                          'class' => 'flex-grow-1',
+                                          'cols' => [
+                                              [
+                                                  'field' => [
+                                                      'name' => 'description',
+                                                      'metadata' => [
+                                                          'rows' => 3
+                                                      ]
+                                                  ],
+                                                  'labelDisplay' => 'top',
+                                                  'class' => 'flex-grow-1',
+                                              ]
+                                          ]
+                                      ],
+                                      [
+                                          'align' => 'end',
+                                          'justify' => 'start',
+                                          'class' => 'flex-grow-1',
+                                          'cols' => [
+                                              [
+                                                  'field' => 'internal',
+                                                  'labelDisplay' => 'inline',
+                                              ],
+                                          ]
+                                      ]
+                                  ]
+                              ]
+                          ],
+                      ]
+                  ]
+              ]
+          ],
+          [
               'type' => 'statistics',
               'labelKey' => 'LBL_NUMBER_OF_CASES_PER_ACCOUNT',
               'options' => [
@@ -131,123 +247,6 @@ array(
                               ]
                           ],
                       ]
-                  ]
-              ]
-          ],
-          [
-              'type' => 'record-thread',
-              'labelKey' => 'LBL_CASE_UPDATES',
-              'options' => [
-                  'recordThread' => [
-                    'module' => 'case-updates',
-                    'class' => 'case-updates',
-                    'filters' => [
-                        'parentFilters' => [
-                            'id' => 'case_id'
-                        ],
-                        'orderBy' => 'date_entered',
-                        'sortOrder' => 'DESC'
-                    ],
-                    'item' => [
-                        'itemClass' => 'case-updates-item pt-2 pb-2',
-                        'collapsible' => true,
-                        'layout' => [
-                            'header' => [
-                                'rows' => [
-                                    [
-                                        'justify' => 'start',
-                                        'align' => 'end',
-                                        'cols' => [
-                                            [
-                                                'field' => 'author',
-                                                'labelDisplay' => 'none',
-                                                'hideIfEmpty' => true,
-                                                'class' => 'font-weight-light'
-                                            ],
-                                            [
-                                                'field' => 'date_entered',
-                                                'labelDisplay' => 'none',
-                                                'hideIfEmpty' => true,
-                                                'class' => 'small ml-auto font-weight-light'
-                                            ],
-                                        ]
-                                    ],
-                                ]
-                            ],
-                            'body' =>[
-                                'rows' => [
-                                    [
-                                        'align' => 'end',
-                                        'justify' => 'end',
-                                        'cols' => [
-                                            [
-                                                'field' => 'internal',
-                                                'labelDisplay' => 'inline',
-                                                'labelClass' => 'm-0',
-                                                'display' => 'none',
-                                                'hideIfEmpty' => true,
-                                                'class' => 'small ml-auto font-weight-light'
-                                            ],
-                                        ]
-                                    ],
-                                    [
-                                        'justify' => 'start',
-                                        'class' => 'flex-grow-1',
-                                        'cols' => [
-                                            [
-                                                'field' => [
-                                                    'name' => 'description',
-                                                    'type' => 'html',
-                                                ],
-                                                'labelDisplay' => 'none',
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ],
-                    ],
-                    'create' => [
-                        'presetFields' => [
-                            'parentValues' => [
-                                'id' => 'case_id'
-                            ],
-                        ],
-                        'layout' => [
-                            'header' => ['rows' => []],
-                            'body' =>[
-                                'rows' => [
-                                    [
-                                        'justify' => 'start',
-                                        'class' => 'flex-grow-1',
-                                        'cols' => [
-                                            [
-                                                'field' => [
-                                                    'name' => 'description',
-                                                    'metadata' => [
-                                                        'rows' => 3
-                                                    ]
-                                                ],
-                                                'labelDisplay' => 'top',
-                                                'class' => 'flex-grow-1',
-                                            ]
-                                        ]
-                                    ],
-                                    [
-                                        'align' => 'end',
-                                        'justify' => 'start',
-                                        'class' => 'flex-grow-1',
-                                        'cols' => [
-                                            [
-                                                'field' => 'internal',
-                                                'labelDisplay' => 'inline',
-                                            ],
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ],
-                    ]
                   ]
               ]
           ],
