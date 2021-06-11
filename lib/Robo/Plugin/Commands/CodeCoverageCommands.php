1adb8a69a85e036f43ca1e2605debb0cc76a7d7a
Dillon-Brown <dillon.brown@salesagility.com>
2021-06-11 20:29:42 +0100

    Upgrade Robo to v3.x
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- lib/Robo/Plugin/Commands/CodeCoverageCommands.php
+++ lib/Robo/Plugin/Commands/CodeCoverageCommands.php
@@ -40,13 +40,13 @@
 
 namespace SuiteCRM\Robo\Plugin\Commands;
 
+use Robo\Tasks;
+use RuntimeException;
 use SuiteCRM\Utility\OperatingSystem;
 use SuiteCRM\Robo\Traits\RoboTrait;
-use Robo\Task\Base\loadTasks;
 
-class CodeCoverageCommands extends \Robo\Tasks
+class CodeCoverageCommands extends Tasks
 {
-    use loadTasks;
     use RoboTrait;
 
     /**
@@ -63,7 +63,7 @@ class CodeCoverageCommands extends \Robo\Tasks
             if ($this->isEnvironmentTravisCI()) {
                 $range = $this->getCommitRangeForTravisCi();
             } else {
-                throw new \RuntimeException('Unable to detect continuous integration environment');
+                throw new RuntimeException('Unable to detect continuous integration environment');
             }
         }
         $this->generateCodeCoverageFile();
