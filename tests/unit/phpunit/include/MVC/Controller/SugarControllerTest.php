e95e8bca95b772960a8cbaed7081d42cc7c02c22
Dillon-Brown <dillon.brown@salesagility.com>
2021-06-13 17:53:30 +0100

    Fix test case class not matching filename deprecation warnings
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- tests/unit/phpunit/include/MVC/Controller/SugarControllerTest.php
+++ tests/unit/phpunit/include/MVC/Controller/SugarControllerTest.php
@@ -1,22 +1,59 @@
 <?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2021 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
 
+namespace SuiteCRM\Tests\Unit\MVC\Controller;
+
+use BeanFactory;
+use DBManagerFactory;
+use Exception;
+use SugarApplication;
+use SugarController;
 use SuiteCRM\Tests\SuiteCRM\Test\SuitePHPUnitFrameworkTestCase;
 use SuiteCRM\Tests\SuiteCRM\Test\TestLogger;
 
+/**
+ * Class SugarControllerTest
+ * @package SuiteCRM\Tests\Unit\MVC\Controller
+ */
 class SugarControllerTest extends SuitePHPUnitFrameworkTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        global $current_user;
-        $current_user = BeanFactory::newBean('Users');
-        get_sugar_config_defaults();
-        if (!isset($GLOBALS['app']) || !$GLOBALS['app']) {
-            $GLOBALS['app'] = new SugarApplication();
-        }
-    }
-
     public function testsetup(): void
     {
         $SugarController = new SugarController();
@@ -64,7 +101,8 @@ class SugarControllerTest extends SuitePHPUnitFrameworkTestCase
     public function testexecute(): void
     {
         // suppress output during the test
-        $this->setOutputCallback(function () {});
+        $this->setOutputCallback(function () {
+        });
 
         // test
         $SugarController = new SugarController();
@@ -216,5 +254,17 @@ class SugarControllerTest extends SuitePHPUnitFrameworkTestCase
         // check with a valid False value
         $result = $SugarController->checkEntryPointRequiresAuth('GeneratePassword');
         self::assertFalse($result);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        global $current_user;
+        $current_user = BeanFactory::newBean('Users');
+        get_sugar_config_defaults();
+        if (!isset($GLOBALS['app']) || !$GLOBALS['app']) {
+            $GLOBALS['app'] = new SugarApplication();
+        }
     }
 }
