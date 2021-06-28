2b741a982a1735ddc2eb2ac48b992141ba45c7da
balumahendran <balu.mahendran@salesagility.com>
2021-06-28 12:14:48 +0530

    [Legacy] Fix Role List User Styling

--- modules/ACLRoles/ListUsers.php
+++ modules/ACLRoles/ListUsers.php
@@ -54,8 +54,7 @@ if (isset($_REQUEST['record'])) {
 			<input type="hidden" name="module" value="Users">
 			<input type="hidden" name="user_id" value="">
 			<input type="hidden" name="record" value="<?php echo $record; ?>">
-			<input type="hidden" name="isDuplicate" value=''>
-			
+			<input type="hidden" name="isDuplicate" value=''>		
 			
 			<input type="hidden" name="action">
 </form>
@@ -63,6 +62,9 @@ if (isset($_REQUEST['record'])) {
 <?php
 
 $users = get_user_array(true, "Active", $record);
+
+echo '<div class="role-list">';
+
 echo getClassicModuleTitle($mod_strings['LBL_MODULE_NAME'], array($mod_strings['LBL_MODULE_NAME']), true);
 echo "<form action='index.php' name='Users'>
 <input type='hidden' name='action' value='ListRoles'>
@@ -70,10 +72,12 @@ echo "<form action='index.php' name='Users'>
 <select name='record' onchange='document.Users.submit();'>";
 echo get_select_options_with_id($users, $record);
 echo "</select></form>";
+
+
 if (!empty($record)) {
     $hideTeams = true; // to not show the teams subpanel in the following file
     require_once('modules/ACLRoles/DetailUserRole.php');
 }
-
+echo '</div>';
 
 ?>
