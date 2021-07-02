21c3adc06fa12365e8b30d03a83d04aceb4cf3a6
Dillon-Brown <dillon.brown@salesagility.com>
2021-07-02 07:26:29 +0100

    SuiteCRM 7.11.21 Release
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- modules/AOR_Reports/AOR_Report.php
+++ modules/AOR_Reports/AOR_Report.php
@@ -5,7 +5,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2021 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -37,6 +37,8 @@
  * reasonably feasible for technical reasons, the Appropriate Legal Notices must
  * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
  */
+
+use SuiteCRM\CleanCSV;
 
 if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
@@ -1010,9 +1012,15 @@ class AOR_Report extends Basic
         }
     }
 
+    /**
+     * @param string $field
+     * @return string
+     */
     private function encloseForCSV($field)
     {
-        return '"' .  cleanCSV($field) . '"';
+        $cleanCSV = new CleanCSV();
+
+        return '"' . $cleanCSV->escapeField($field) . '"';
     }
 
     public function build_report_csv()
