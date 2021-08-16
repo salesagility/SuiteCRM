5653a6904ad2e1fed0b0de4fcec3f38531a357be
Dillon-Brown <dillon.brown@salesagility.com>
2021-08-16 09:45:28 +0100

    [Legacy] - Increment suitecrm_version.php
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.0.0-beta.2';
+$suitecrm_version = '8.0.0-beta.3';
 $suitecrm_legacy = '7.11.19';
 $suitecrm_timestamp = '2021-05-20 09:00:00';
