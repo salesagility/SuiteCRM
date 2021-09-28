abafbc280bc8efd1d9861f809b5a0f8e48ee2cbe
Clemente Raposo <clemente.raposo@salesagility.com>
2021-09-28 17:43:49 +0100

    [Legacy] Remove license from install app strings

--- install/language/en_us.lang.php
+++ install/language/en_us.lang.php
@@ -584,28 +584,5 @@ $contact_user_link_guid',
     'LBL_SYS_CHECK_WARNING' => 'Ignore System Check Warnings',
     'LBL_PROCEED' => 'PROCEED',
     'LBL_CONFIG' => 'CONFIGURATION',
-    'LBL_VALIDATION_ERRORS' => 'There are validation errors, unable to perform action.',
-    'SUITE8_LICENSE_CONTENT' => 'SuiteCRM is a customer relationship management program developed by SalesAgility Ltd.
-Copyright (C) 2021 SalesAgility Ltd.
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License version 3 as published by the
-Free Software Foundation with the addition of the following permission added
-to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
-IN WHICH THE COPYRIGHT IS OWNED BY SALESAGILITY, SALESAGILITY DISCLAIMS THE
-WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
-details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
-
-In accordance with Section 7(b) of the GNU Affero General Public License
-version 3, these Appropriate Legal Notices must retain the display of the
-"Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
-feasible for technical reasons, the Appropriate Legal Notices must display
-the words "Supercharged by SuiteCRM".'
+    'LBL_VALIDATION_ERRORS' => 'There are validation errors, unable to perform action.'
 );
