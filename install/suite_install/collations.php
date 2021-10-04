0960af0ced6a86cb9620c70aae366ab34b7892eb
Matt Lorimer <matthew.lorimer@salesagility.com>
2021-10-04 13:41:27 +0100

    Default new MySQL based installs to use utf8mb4

--- install/suite_install/collations.php
+++ install/suite_install/collations.php
@@ -3,13 +3,13 @@
 $collations = array(
     'mysql' =>
         array(
-	    array(
-	        'name' => 'utf8_general_ci',
-		'charset' => 'utf8',
-	    ),
             array(
                 'name' => 'utf8mb4_general_ci',
                 'charset' => 'utf8mb4',
+            ),
+            array(
+                'name' => 'utf8_general_ci',
+                'charset' => 'utf8',
             ),
         ),
 );
