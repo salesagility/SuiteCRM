91cbf38af402854e987b7c6f80f60760b19edeb7
Dillon-Brown <dillon.brown@salesagility.com>
2021-10-05 10:09:25 +0100

    SuiteCRM 7.12.0-RC Release
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.11.22';
-$suitecrm_timestamp = '2021-09-24 17:00:00';
+$suitecrm_version = '7.12.0-RC';
+$suitecrm_timestamp = '2021-10-05 17:00:00';
