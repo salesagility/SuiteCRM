6eb475101420a6ddbb0708ef481688e1c8d82901
p.kumar <pankaj.kumar@salesagility.com>
2021-10-15 05:22:26 +0530

    remove photo widgets for contacts and leads

--- modules/Leads/metadata/detailviewdefs.php
+++ modules/Leads/metadata/detailviewdefs.php
@@ -39,147 +39,135 @@
  */
 
 $viewdefs ['Leads'] =
-array(
-  'DetailView' =>
-  array(
-    'templateMeta' =>
     array(
-      'form' =>
-      array(
-        'buttons' =>
-        array(
-            'SEND_CONFIRM_OPT_IN_EMAIL' => EmailAddress::getSendConfirmOptInEmailActionLinkDefs('Leads'),
-          0 => 'EDIT',
-          1 => 'DUPLICATE',
-          2 => 'DELETE',
-          3 =>
-          array(
-            'customCode' => '{if $bean->aclAccess("edit") && !$DISABLE_CONVERT_ACTION}<input title="{$MOD.LBL_CONVERTLEAD_TITLE}" accessKey="{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}" type="button" class="button" onClick="document.location=\'index.php?module=Leads&action=ConvertLead&record={$fields.id.value}\'" name="convert" value="{$MOD.LBL_CONVERTLEAD}">{/if}',
-            'sugar_html' =>
+        'DetailView' =>
             array(
-              'type' => 'button',
-              'value' => '{$MOD.LBL_CONVERTLEAD}',
-              'htmlOptions' =>
-              array(
-                'title' => '{$MOD.LBL_CONVERTLEAD_TITLE}',
-                'accessKey' => '{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}',
-                'class' => 'button',
-                'onClick' => 'document.location=\'index.php?module=Leads&action=ConvertLead&record={$fields.id.value}\'',
-                'name' => 'convert',
-                'id' => 'convert_lead_button',
-              ),
-              'template' => '{if $bean->aclAccess("edit") && !$DISABLE_CONVERT_ACTION}[CONTENT]{/if}',
-            ),
-          ),
-          4 => 'FIND_DUPLICATES',
-          5 =>
-          array(
-            'customCode' => '<input title="{$APP.LBL_MANAGE_SUBSCRIPTIONS}" class="button" onclick="this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'DetailView\';this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'Subscriptions\'; this.form.module.value=\'Campaigns\'; this.form.module_tab.value=\'Leads\';" type="submit" name="Manage Subscriptions" value="{$APP.LBL_MANAGE_SUBSCRIPTIONS}">',
-            'sugar_html' =>
-            array(
-              'type' => 'submit',
-              'value' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
-              'htmlOptions' =>
-              array(
-                'title' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
-                'class' => 'button',
-                'id' => 'manage_subscriptions_button',
-                'onclick' => 'this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'DetailView\';this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'Subscriptions\'; this.form.module.value=\'Campaigns\'; this.form.module_tab.value=\'Leads\';',
-                'name' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
-              ),
-            ),
-          ),
-          'AOS_GENLET' =>
-          array(
-            'customCode' => '<input type="button" class="button" onClick="showPopup();" value="{$APP.LBL_PRINT_AS_PDF}">',
-          ),
-        ),
-        'headerTpl' => 'modules/Leads/tpls/DetailViewHeader.tpl',
-      ),
-      'maxColumns' => '2',
-      'widths' =>
-      array(
-        0 =>
-        array(
-          'label' => '10',
-          'field' => '30',
-        ),
-        1 =>
-        array(
-          'label' => '10',
-          'field' => '30',
-        ),
-      ),
-      'includes' =>
-      array(
-        0 =>
-        array(
-          'file' => 'modules/Leads/Lead.js',
-        ),
-      ),
-      'useTabs' => true,
-      'tabDefs' =>
-      array(
-        'LBL_CONTACT_INFORMATION' =>
-        array(
-          'newTab' => true,
-          'panelDefault' => 'expanded',
-        ),
-        'LBL_PANEL_ADVANCED' =>
-        array(
-          'newTab' => true,
-          'panelDefault' => 'expanded',
-        ),
-        'LBL_PANEL_ASSIGNMENT' =>
-        array(
-          'newTab' => true,
-          'panelDefault' => 'expanded',
-        ),
-      ),
-    ),
-      'topWidget' => [
-          'type' => 'statistics',
-          'options' => [
-              'statistics' => [
-                  [
-                      'labelKey' => 'LBL_DAYS_OPEN',
-                      'type' => 'lead-days-open'
-                  ],
-              ],
-          ]
-      ],
-      'sidebarWidgets' => [
-          [
-              'type' => 'statistics',
-              'labelKey' => 'LBL_PHOTO',
-              'options' => [
-                  'sidebarStatistic' => [
-                      'rows' => [
-                          [
-                              'align' => 'start',
-                              'cols' => [
-                                  [
-                                      'icon' => 'person-icon',
-                                      'iconClass' => 'img-fluid',
-                                  ]
-                              ]
-                          ],
-                      ]
-                  ]
-              ]
-          ],
-          ['type' => 'history-timeline'],
-      ],
-      'recordActions' => [
-          'actions' => [
-                'convert-lead' => [
-                    'key' => 'convert-lead',
-                    'labelKey' => 'LBL_CONVERTLEAD',
-                    'asyncProcess' => true,
-                    'params' => [],
-                    'modes' => ['detail'],
-                    'acl' => ['edit'],
+                'templateMeta' =>
+                    array(
+                        'form' =>
+                            array(
+                                'buttons' =>
+                                    array(
+                                        'SEND_CONFIRM_OPT_IN_EMAIL' => EmailAddress::getSendConfirmOptInEmailActionLinkDefs('Leads'),
+                                        0 => 'EDIT',
+                                        1 => 'DUPLICATE',
+                                        2 => 'DELETE',
+                                        3 =>
+                                            array(
+                                                'customCode' => '{if $bean->aclAccess("edit") && !$DISABLE_CONVERT_ACTION}<input title="{$MOD.LBL_CONVERTLEAD_TITLE}" accessKey="{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}" type="button" class="button" onClick="document.location=\'index.php?module=Leads&action=ConvertLead&record={$fields.id.value}\'" name="convert" value="{$MOD.LBL_CONVERTLEAD}">{/if}',
+                                                'sugar_html' =>
+                                                    array(
+                                                        'type' => 'button',
+                                                        'value' => '{$MOD.LBL_CONVERTLEAD}',
+                                                        'htmlOptions' =>
+                                                            array(
+                                                                'title' => '{$MOD.LBL_CONVERTLEAD_TITLE}',
+                                                                'accessKey' => '{$MOD.LBL_CONVERTLEAD_BUTTON_KEY}',
+                                                                'class' => 'button',
+                                                                'onClick' => 'document.location=\'index.php?module=Leads&action=ConvertLead&record={$fields.id.value}\'',
+                                                                'name' => 'convert',
+                                                                'id' => 'convert_lead_button',
+                                                            ),
+                                                        'template' => '{if $bean->aclAccess("edit") && !$DISABLE_CONVERT_ACTION}[CONTENT]{/if}',
+                                                    ),
+                                            ),
+                                        4 => 'FIND_DUPLICATES',
+                                        5 =>
+                                            array(
+                                                'customCode' => '<input title="{$APP.LBL_MANAGE_SUBSCRIPTIONS}" class="button" onclick="this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'DetailView\';this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'Subscriptions\'; this.form.module.value=\'Campaigns\'; this.form.module_tab.value=\'Leads\';" type="submit" name="Manage Subscriptions" value="{$APP.LBL_MANAGE_SUBSCRIPTIONS}">',
+                                                'sugar_html' =>
+                                                    array(
+                                                        'type' => 'submit',
+                                                        'value' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
+                                                        'htmlOptions' =>
+                                                            array(
+                                                                'title' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
+                                                                'class' => 'button',
+                                                                'id' => 'manage_subscriptions_button',
+                                                                'onclick' => 'this.form.return_module.value=\'Leads\'; this.form.return_action.value=\'DetailView\';this.form.return_id.value=\'{$fields.id.value}\'; this.form.action.value=\'Subscriptions\'; this.form.module.value=\'Campaigns\'; this.form.module_tab.value=\'Leads\';',
+                                                                'name' => '{$APP.LBL_MANAGE_SUBSCRIPTIONS}',
+                                                            ),
+                                                    ),
+                                            ),
+                                        'AOS_GENLET' =>
+                                            array(
+                                                'customCode' => '<input type="button" class="button" onClick="showPopup();" value="{$APP.LBL_PRINT_AS_PDF}">',
+                                            ),
+                                    ),
+                                'headerTpl' => 'modules/Leads/tpls/DetailViewHeader.tpl',
+                            ),
+                        'maxColumns' => '2',
+                        'widths' =>
+                            array(
+                                0 =>
+                                    array(
+                                        'label' => '10',
+                                        'field' => '30',
+                                    ),
+                                1 =>
+                                    array(
+                                        'label' => '10',
+                                        'field' => '30',
+                                    ),
+                            ),
+                        'includes' =>
+                            array(
+                                0 =>
+                                    array(
+                                        'file' => 'modules/Leads/Lead.js',
+                                    ),
+                            ),
+                        'useTabs' => true,
+                        'tabDefs' =>
+                            array(
+                                'LBL_CONTACT_INFORMATION' =>
+                                    array(
+                                        'newTab' => true,
+                                        'panelDefault' => 'expanded',
+                                    ),
+                                'LBL_PANEL_ADVANCED' =>
+                                    array(
+                                        'newTab' => true,
+                                        'panelDefault' => 'expanded',
+                                    ),
+                                'LBL_PANEL_ASSIGNMENT' =>
+                                    array(
+                                        'newTab' => true,
+                                        'panelDefault' => 'expanded',
+                                    ),
+                            ),
+                    ),
+                'topWidget' => [
+                    'type' => 'statistics',
+                    'options' => [
+                        'statistics' => [
+                            [
+                                'labelKey' => 'LBL_DAYS_OPEN',
+                                'type' => 'lead-days-open'
+                            ],
+                        ],
+                    ],
+                    'acls' => [
+                        'Leads' => ['view', 'list']
+                    ]
                 ],
+                'sidebarWidgets' => [
+                    ['type' => 'history-timeline',
+                        'acls' => [
+                            'Leads' => ['view', 'list']
+                        ]
+                    ],
+                ],
+                'recordActions' => [
+                    'actions' => [
+                        'convert-lead' => [
+                            'key' => 'convert-lead',
+                            'labelKey' => 'LBL_CONVERTLEAD',
+                            'asyncProcess' => true,
+                            'params' => [],
+                            'modes' => ['detail'],
+                            'acl' => ['edit'],
+                        ],
 
                 'print-as-pdf' => [
                     'key' => 'print-as-pdf',
