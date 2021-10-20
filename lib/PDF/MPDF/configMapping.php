575561903b64f7a77c237615e4befb854320c648
Matt Lorimer <matthew.lorimer@salesagility.com>
2021-10-20 12:01:58 +0100

    Update PDF Engines for backward compatibility

--- lib/PDF/MPDF/configMapping.php
+++ lib/PDF/MPDF/configMapping.php
@@ -2,14 +2,14 @@
 return $configOptions = [
     'mode' => $options['mode'] ?? '',
     'page_size' => $options['page_size'] ?? 'A4',
-    'default_font_size' => $options['fontSize'] ?? 0,
-    'default_font' => $options['font'] ?? '',
-    'mgl' => $options['mgl'] ?? 15,
-    'mgr' => $options['mgr'] ?? 15,
-    'mgt' => $options['mgt'] ?? 16,
-    'mgb' => $options['mgb'] ?? 16,
-    'mgh' => $options['mgh'] ?? 9,
-    'mgf' => $options['mgf'] ?? 9,
+    'default_font_size' => $options['fontSize'] ?? 11,
+    'default_font' => $options['font'] ?? 'DejaVuSansCondensed',
+    'margin_left' => $options['margin_left'] ?? 15,
+    'margin_right' => $options['margin_right'] ?? 15,
+    'margin_top' => $options['margin_top'] ?? 16,
+    'margin_bottom' => $options['margin_bottom'] ?? 16,
+    'margin_header' => $options['margin_header'] ?? 9,
+    'margin_footer' => $options['margin_footer'] ?? 9,
     'orientation' => $options['orientation'] ?? 'P',
     'unit' => $options['unit'] ?? 'mm'
 ];
