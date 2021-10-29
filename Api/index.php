41e84b1d177d1ded8fca09058de2ea4e25145959
Clemente Raposo <clemente.raposo@salesagility.com>
2021-10-29 17:52:41 +0100

    [Legacy] Override server variables when doing kernel bypass
    - Override SCRIPT_NAME and REQUEST_URI, to mimic regular route call

--- Api/index.php
+++ Api/index.php
@@ -1,4 +1,12 @@
 <?php
+
+global $legacyRoute;
+
+if (isset($legacyRoute['script-name'], $legacyRoute['request-uri'])) {
+    $_SERVER['SCRIPT_NAME'] = $legacyRoute['script-name'];
+    $_SERVER['REQUEST_URI'] = $legacyRoute['request-uri'];
+}
+
 chdir('../');
 require_once __DIR__ . '/Core/app.php';
 $app->run();
