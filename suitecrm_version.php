a22626df0983cb9ea4afef739cd09a2f492da1b1
Clemente Raposo <clemente.raposo@salesagility.com>
2021-11-23 10:49:20 +0000

    [Legacy] Bump version to 8.0.0

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.0.0-rc.2';
+$suitecrm_version = '8.0.0';
 $suitecrm_legacy = '7.12.1';
-$suitecrm_timestamp = '2021-11-19 17:00:00';
+$suitecrm_timestamp = '2021-11-23 12:00:00';
