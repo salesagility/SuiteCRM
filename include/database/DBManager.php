241a9520f0c632f17a47cab4304fe6b6428115f3
SinergiaCRM <61022311+SinergiaCRM@users.noreply.github.com>
2022-01-20 13:04:34 +0100

    Fix #9440 - Forcing default null value for numeric core fields.

--- include/database/DBManager.php
+++ include/database/DBManager.php
@@ -2209,7 +2209,7 @@ abstract class DBManager
                         return 0;
                     }
 
-                    return (int)$val;
+                    return $val === '' ? 'NULL' : (int)$val; // Fix #9440 - Forcing default null value for numeric fields.
                 case 'bigint':
                     $val = (float)$val;
                     if (!empty($fieldDef['required']) && $val == false) {
@@ -2230,7 +2230,7 @@ abstract class DBManager
                         return 0;
                     }
 
-                    return (float)$val;
+                    return $val === '' ? 'NULL' : (float)$val; // Fix #9440 - Forcing default null value for numeric fields.
                 case 'time':
                 case 'date':
                     // empty date can't be '', so convert it to either NULL or empty date value
