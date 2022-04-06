cf116595e3a53aa95c3f348fbfc5ba8cb0e59c6f
pgorod <pgorod@users.noreply.github.com>
2022-04-06 15:38:49 +0100

    Fix #7759, #8273 - Double compose button in subpanels

--- include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeEmailButton.php
+++ include/generic/SugarWidgets/SugarWidgetSubPanelTopComposeEmailButton.php
@@ -101,13 +101,12 @@ class SugarWidgetSubPanelTopComposeEmailButton extends SugarWidgetSubPanelTopBut
 
             $emailUI = new EmailUI();
             $emailUI->appendTick = false;
-            $button = '<a class="email-link" onclick="$(document).openComposeViewModal(this);" data-module="'
-            . $bean->module_name . '" data-record-id="'
-            . $bean->id . '" data-module-name="'
-            . $bean->name .'" data-email-address="'
-            . $bean->email1 .'">'
-            . $app_strings['LBL_COMPOSE_EMAIL_BUTTON_LABEL']
-            . '</a>';
+            $button = '<div type="hidden" onclick="currentModule=\''
+              . $bean->module_name . '\';$(document).openComposeViewModal(this);" data-module="'
+              . $bean->module_name . '" data-record-id="'
+              . $bean->id . '" data-module-name="'
+              . $bean->name .'" data-email-address="'
+              . $bean->email1 .'">';
         }
 
         return $button;
