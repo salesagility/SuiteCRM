c12e61c324bb7a48915809be7cc0664f705b324c
Rahul Shinde <rahulshinde2007@gmail.com>
2022-08-25 22:55:45 +0800

    Closing count bracket before condition
    Count bracket is closed after relational operator it must be typo mistake done years ago
    before PHP 8.0 count() use to yield a warning but after PHP 8.0 count() will throw TypeError if countable types parameter is not passed, in this case Boolean value was feeding to count so it was throwing TypeError in PHP 8.0+

--- include/ListView/ListViewData.php
+++ include/ListView/ListViewData.php
@@ -512,7 +512,7 @@ class ListViewData
         $queryString = '';
 
         if (isset($_REQUEST["searchFormTab"]) && $_REQUEST["searchFormTab"] == "advanced_search" ||
-            isset($_REQUEST["type_basic"]) && (count($_REQUEST["type_basic"] > 1) || $_REQUEST["type_basic"][0] != "") ||
+            isset($_REQUEST["type_basic"]) && (count($_REQUEST["type_basic"]) > 1 || $_REQUEST["type_basic"][0] != "") ||
             isset($_REQUEST["module"]) && $_REQUEST["module"] == "MergeRecords") {
             $queryString = "-advanced_search";
         } else {
