5665502a32e4831eea7a6b24100021b9bcbb4045
Clemente Raposo <clemente.raposo@salesagility.com>
2022-11-04 12:56:22 +0000

    Fix #9839 - Tidy OutboundEmailAccounts view definitions

--- modules/OutboundEmailAccounts/metadata/listviewdefs.php
+++ modules/OutboundEmailAccounts/metadata/listviewdefs.php
@@ -1,26 +1,22 @@
 <?php
 $module_name = 'OutboundEmailAccounts';
-$listViewDefs [$module_name] =
-array(
-  'NAME' =>
-  array(
-    'width' => '32%',
-    'label' => 'LBL_NAME',
-    'default' => true,
-    'link' => true,
-  ),
-  'MAIL_SMTPUSER' =>
-  array(
-    'type' => 'varchar',
-    'label' => 'LBL_USERNAME',
-    'width' => '10%',
-    'default' => true,
-  ),
-  'MAIL_SMTPSERVER' =>
-  array(
-    'type' => 'varchar',
-    'label' => 'LBL_SMTP_SERVERNAME',
-    'width' => '10%',
-    'default' => true,
-  ),
-);
+$listViewDefs [$module_name] = [
+    'NAME' => [
+        'width' => '32%',
+        'label' => 'LBL_NAME',
+        'default' => true,
+        'link' => true,
+    ],
+    'MAIL_SMTPUSER' => [
+        'type' => 'varchar',
+        'label' => 'LBL_USERNAME',
+        'width' => '10%',
+        'default' => true,
+    ],
+    'MAIL_SMTPSERVER' => [
+        'type' => 'varchar',
+        'label' => 'LBL_SMTP_SERVERNAME',
+        'width' => '10%',
+        'default' => true,
+    ],
+];
