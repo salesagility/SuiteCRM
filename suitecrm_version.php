cea3e00eb4990d143c91935013058488c0964ee6
Clemente Raposo <clemente.raposo@salesagility.com>
2022-11-17 15:03:02 +0000

    SuiteCRM 7.12.8 Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.12.7';
-$suitecrm_timestamp = '2022-08-10 12:00:00';
+$suitecrm_version = '7.12.8';
+$suitecrm_timestamp = '2022-11-17 12:00:00';
