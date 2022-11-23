a828bfb594e9e79a39c82fa701dab86c049cdda7
Clemente Raposo <clemente.raposo@salesagility.com>
2022-11-23 17:09:00 +0000

    Fix #9839 - Add ExternalOAuthConnection entrypoints
    - Entry points used when authenticating with an external oauth provider

--- include/MVC/Controller/entry_point_registry.php
+++ include/MVC/Controller/entry_point_registry.php
@@ -98,4 +98,6 @@ $entry_point_registry = array(
     'sendConfirmOptInEmail' => array('file' => 'include/entryPointConfirmOptInConnector.php', 'auth' => true),
     'saveGoogleApiKey' => array('file' => 'modules/Users/entryPointSaveGoogleApiKey.php', 'auth' => true),
     'setImapTestSettings' => ['file' => 'include/Imap/ImapTestSettingsEntry.php', 'auth' => true],
+    'redirectToExternalOAuth' => ['file' => 'modules/ExternalOAuthConnection/entrypoint/redirectToExternalOAuth.php', 'auth' => true],
+    'setExternalOAuthToken' => ['file' => 'modules/ExternalOAuthConnection/entrypoint/setExternalOAuthToken.php', 'auth' => true],
 );
