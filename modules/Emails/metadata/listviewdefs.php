6b511a708f1d7ac3404fc3d7cbd80e5fb0812c37
Clemente Raposo <clemente.raposo@salesagility.com>
2022-12-05 12:32:38 +0000

    Fix #9839 - Always sort emails by date received
    - Make all Email fields non sortable
    - For the listview to sort by date_sent_received
    - For show sort direction for date_sent_received

--- modules/Emails/metadata/listviewdefs.php
+++ modules/Emails/metadata/listviewdefs.php
@@ -126,6 +126,7 @@ $listViewDefs['Emails'] = array(
         'width' => '32',
         'label' => 'LBL_LIST_FROM_ADDR',
         'default' => true,
+        'sortable' => false,
     ),
     'INDICATOR' => array(
         'width' => '32',
@@ -140,7 +141,8 @@ $listViewDefs['Emails'] = array(
         'label' => 'LBL_LIST_SUBJECT',
         'default' => true,
         'link' => false,
-        'customCode' => ''
+        'customCode' => '',
+        'sortable' => false,
     ),
     'HAS_ATTACHMENT' => array(
         'width' => '32',
@@ -154,27 +156,32 @@ $listViewDefs['Emails'] = array(
         'label' => 'LBL_ASSIGNED_TO_NAME',
         'module' => 'Employees',
         'id' => 'ASSIGNED_USER_ID',
-        'default' => false
+        'default' => false,
+        'sortable' => false,
     ),
     'DATE_ENTERED' => array(
         'width' => '32',
         'label' => 'LBL_DATE_ENTERED',
         'default' => true,
+        'sortable' => false,
     ),
     'DATE_SENT_RECEIVED' => array(
         'width' => '32',
         'label' => 'LBL_LIST_DATE_SENT_RECEIVED',
         'default' => true,
+        'sortable' => false,
+        'force_show_sort_direction' => true
     ),
     'TO_ADDRS_NAMES' => array(
         'width' => '32',
         'label' => 'LBL_LIST_TO_ADDR',
         'default' => false,
+        'sortable' => false,
     ),
-    'CATEGORY_ID' =>
-        array(
-            'width' => '10%',
-            'label' => 'LBL_LIST_CATEGORY',
-            'default' => true,
-        ),
+    'CATEGORY_ID' => array(
+        'width' => '10%',
+        'label' => 'LBL_LIST_CATEGORY',
+        'default' => true,
+        'sortable' => false,
+    ),
 );
