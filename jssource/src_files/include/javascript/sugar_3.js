3c1478e330d5e07ee8685a88a0aac10e9343ef34
Clemente Raposo <clemente.raposo@salesagility.com>
2022-12-05 12:51:09 +0000

    Fix #9839 - Hide sort fields in search modal if none is sortable

--- jssource/src_files/include/javascript/sugar_3.js
+++ jssource/src_files/include/javascript/sugar_3.js
@@ -2993,7 +2993,7 @@ SUGAR.util = function () {
               // Bug #49205 : Subpanels fail to load when selecting subpanel tab
               // Create a YUI instance using the io-base module.
               (function (srcResult) {
-                $.ajax({ 
+                $.ajax({
                   url: srcResult,
                   async: false,
                   method: 'GET'
@@ -3652,6 +3652,7 @@ SUGAR.savedViews = function () {
         }
         // Firefox needs this to be set after all the option nodes are created.
         document.getElementById('orderBySelect').selectedIndex = selectedIndex;
+
       };
       SUGAR.tabChooser.movementCallback(document.getElementById('display_tabs_td').getElementsByTagName('select')[0]);
 
@@ -3668,6 +3669,25 @@ SUGAR.savedViews = function () {
       // handle direction
       if (SUGAR.savedViews.selectedSortOrder == 'DESC') document.getElementById('sort_order_desc_radio').checked = true;
       else document.getElementById('sort_order_asc_radio').checked = true;
+      var sortableColumnsCount = 0;
+
+      var columnDefs = SUGAR.savedViews.columnsMeta;
+      if(columnDefs && typeof columnDefs === 'object' && Object.keys(columnDefs))  {
+        Object.keys(columnDefs).forEach(function (columnKey) {
+          var column = columnDefs[columnKey];
+          if (!column || !column.sortable) {
+            return;
+          }
+
+          if (column.sortable === true) {
+            sortableColumnsCount++;
+          }
+        })
+      }
+
+      if (sortableColumnsCount < 1 ) {
+        $('.saved-search-sort-column-config-row').hide();
+      }
     }
   };
 }();
