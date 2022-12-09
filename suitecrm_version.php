f98ef14b6f82564dbe82fb61278bd46401e83f5b
Clemente Raposo <clemente.raposo@salesagility.com>
2022-12-09 13:07:18 +0000

    SuiteCRM 7.13.0-beta Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.12.8';
-$suitecrm_timestamp = '2022-11-17 12:00:00';
+$suitecrm_version = '7.13.0-beta';
+$suitecrm_timestamp = '2022-12-12 12:00:00';
