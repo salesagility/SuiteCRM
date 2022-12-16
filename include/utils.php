571b3315f69ecfdb40a49b388fff3f670142e8b0
QuickCRM <quickcrm@ns-team.fr>
2022-12-16 08:46:31 +0100

    Fix #9849 - allowed_preview is defined twice in the array

--- include/utils.php
+++ include/utils.php
@@ -503,13 +503,6 @@ function get_sugar_config_defaults(): array
             'jpg'
         ],
         'upload_maxsize' => 30000000,
-        'allowed_preview' => [
-            'pdf',
-            'gif',
-            'png',
-            'jpeg',
-            'jpg'
-        ],
         'import_max_execution_time' => 3600,
 //	'use_php_code_json' => returnPhpJsonStatus(),
         'verify_client_ip' => true,
