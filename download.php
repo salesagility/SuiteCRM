523a6cf39cdc292e3ef8d343be0b9166cff92937
sweettbug3 <73710862+sweettbug3@users.noreply.github.com>
2023-03-08 11:31:54 -0700

    Fix #8980 - Check beanFiles for class path
    BeanFiles global provides the path to the overwritten class for customizations.

--- download.php
+++ download.php
@@ -75,7 +75,8 @@ if ((!isset($_REQUEST['isProfile']) && empty($_REQUEST['id'])) || empty($_REQUES
         if ($bean_name == 'aCase') {
             $bean_name = 'Case';
         }
-        if (!file_exists('modules/' . $module . '/' . $bean_name . '.php')) {
+        global $beanFiles;
+        if (!file_exists($beanFiles[$bean_name])){
             die($app_strings['ERROR_TYPE_NOT_VALID']);
         }
 
