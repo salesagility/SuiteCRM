7f019a3622c172ff9a7a4d586e7a929f05f22f27
Jack Anderson <jack.anderson@salesagility.com>
2023-04-11 13:19:03 +0100

    Fix #10120 - test settings to include auth type

--- modules/InboundEmail/InboundEmail.php
+++ modules/InboundEmail/InboundEmail.php
@@ -3352,6 +3352,8 @@ class InboundEmail extends SugarBean
                     $GLOBALS['log']->debug($l . ': I-E failed using [' . $serviceTest . '] - error: ' . $errors);
                     $retArray['err'][$k] = $errors;
                     $retArray['bad'][$k] = $serviceTest;
+
+                    return $retArray;
                 }
             } else {
                 $GLOBALS['log']->debug($l . ': I-E found good connect using [' . $serviceTest . ']');
@@ -6366,9 +6368,9 @@ class InboundEmail extends SugarBean
 
         $useSsl = ($requestSsl == 'true') ? true : false; // TODO: validate the ssl request variable value (for e.g its posibble to give a numeric 1 as true)
         if ($test) {
-            $this->getImap()->setTimeout(1, 15); // 60 secs is the default
-            $this->getImap()->setTimeout(2, 15);
-            $this->getImap()->setTimeout(3, 15);
+            $this->getImap()->setTimeout(1, 5); // 60 secs is the default
+            $this->getImap()->setTimeout(2, 5);
+            $this->getImap()->setTimeout(3, 5);
 
             $opts = $this->findOptimumSettings($useSsl);
             if (!empty($opts) && isset($opts['good']) && empty($opts['good'])) {
@@ -6553,6 +6555,10 @@ class InboundEmail extends SugarBean
             }
 
             $connection = $this->getImap()->open($mailbox, $username, $password, $options, 0, $params);
+
+            if (!$connection){
+                break;
+            }
 
         }
 
