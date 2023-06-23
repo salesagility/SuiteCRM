3cdb36cf654e0c9e01294d745d665c71f83af868
Clemente Raposo <clemente.raposo@salesagility.com>
2023-06-23 15:14:17 +0100

    PHP 8.2 - Fix ListView

--- include/ListView/ListViewSmarty.php
+++ include/ListView/ListViewSmarty.php
@@ -291,8 +291,8 @@ class ListViewSmarty extends ListViewDisplay
         global $app_strings, $sugar_version, $sugar_flavor, $currentModule, $app_list_strings;
         $this->ss->assign('moduleListSingular', $app_list_strings["moduleListSingular"]);
         $this->ss->assign('moduleList', $app_list_strings['moduleList']);
-        $this->ss->assign('data', $this->data['data']);
-        $this->ss->assign('query', $this->data['query']);
+        $this->ss->assign('data', $this->data['data'] ?? []);
+        $this->ss->assign('query', $this->data['query'] ?? '');
         $this->ss->assign('sugar_info', array("sugar_version" => $sugar_version,
             "sugar_flavor" => $sugar_flavor));
 
@@ -308,7 +308,7 @@ class ListViewSmarty extends ListViewDisplay
             $this->data['pageData']['offsets']['lastOffsetOnPage'] = $this->data['pageData']['offsets']['current'] + count((array)$this->data['data']);
         }
 
-        $this->ss->assign('pageData', $this->data['pageData']);
+        $this->ss->assign('pageData', $this->data['pageData'] ?? []);
 
         $navStrings = array('next' => $app_strings['LNK_LIST_NEXT'],
             'previous' => $app_strings['LNK_LIST_PREVIOUS'],
