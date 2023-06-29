e1139a5bf060530ae6d5fdfb5347847e908cae38
claudesimo1990 <claudesimo1990@gmail.com>
2023-06-29 15:31:27 +0200

    Fix #10096 - count ampersand before truncate

--- include/database/DBManager.php
+++ include/database/DBManager.php
@@ -2087,7 +2087,7 @@ abstract class DBManager
                     }
                 }
 
-                if (!empty($val) && !empty($fieldDef['len']) && strlen($val) > $fieldDef['len']) {
+                if (!empty($val) && !empty($fieldDef['len']) && strlen(from_html($val)) > $fieldDef['len']) {
                     $val = $this->truncate($val, $fieldDef['len']);
                 }
 
@@ -2257,7 +2257,7 @@ abstract class DBManager
                     break;
             }
         } else {
-            if (!empty($val) && !empty($fieldDef['len']) && strlen($val) > $fieldDef['len']) {
+            if (!empty($val) && !empty($fieldDef['len']) && strlen(from_html($val)) > $fieldDef['len']) {
                 $val = $this->truncate($val, $fieldDef['len']);
             }
         }
