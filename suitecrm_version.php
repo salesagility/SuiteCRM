512250750d3607d19d977e03d69465e6c8f7b31e
Jack Anderson <jack.anderson@salesagility.com>
2023-06-30 10:21:31 +0100

    SuiteCRM 7.13.4 Release

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.13.3';
-$suitecrm_timestamp = '2023-04-24 12:00:00';
+$suitecrm_version = '7.13.4';
+$suitecrm_timestamp = '2023-07-11 12:00:00';
