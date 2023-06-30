0825b779c6e0aecfa1e9002f445f134194f58e05
Matt Lorimer <matthew.lorimer@salesagility.com>
2023-06-30 18:16:17 +0100

    SuiteCRM 7.14 Alpha Build

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -3,5 +3,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '7.13.4';
-$suitecrm_timestamp = '2023-07-11 12:00:00';
+$suitecrm_version = '7.14.0.alpha';
+$suitecrm_timestamp = '2023-06-30 18:00:00';
