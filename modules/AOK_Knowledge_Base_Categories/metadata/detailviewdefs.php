f1be439137abc3b7bc5af127079c49c9a606f2a1
s.samko <serhii.samko@salesagility.com>
2023-10-05 11:12:48 +0100

    Fix #10224 - KB and KB categories metadata changes

--- modules/AOK_Knowledge_Base_Categories/metadata/detailviewdefs.php
+++ modules/AOK_Knowledge_Base_Categories/metadata/detailviewdefs.php
@@ -5,7 +5,7 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2023 SalesAgility Ltd.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -40,69 +40,75 @@
 
 $module_name = 'AOK_Knowledge_Base_Categories';
 $viewdefs [$module_name] =
-array(
-  'DetailView' =>
-  array(
-    'templateMeta' =>
     array(
-      'form' =>
-      array(
-        'buttons' =>
-        array(
-          0 => 'EDIT',
-          1 => 'DUPLICATE',
-          2 => 'DELETE',
-          3 => 'FIND_DUPLICATES',
-        ),
-      ),
-      'maxColumns' => '2',
-      'widths' =>
-      array(
-        0 =>
-        array(
-          'label' => '10',
-          'field' => '30',
-        ),
-        1 =>
-        array(
-          'label' => '10',
-          'field' => '30',
-        ),
-      ),
-      'useTabs' => false,
-      'tabDefs' =>
-      array(
-        'DEFAULT' =>
-        array(
-          'newTab' => false,
-          'panelDefault' => 'expanded',
-        ),
-      ),
-      'syncDetailEditViews' => true,
-    ),
-    'panels' =>
-    array(
-      'default' =>
-      array(
-        0 =>
-        array(
-          0 => 'name',
-          1 => '',
-        ),
-        1 =>
-        array(
-          0 => 'description',
-        ),
-        2 =>
-        array(
-          0 =>
-          array(
-            'name' => 'created_by_name',
-            'label' => 'LBL_CREATED',
-          ),
-          1 => 'date_entered',
-        ),
-      ),
-    ),
-  ),
-);
+        'DetailView' =>
+            array(
+                'templateMeta' =>
+                    array(
+                        'form' =>
+                            array(
+                                'buttons' =>
+                                    array(
+                                        0 => 'EDIT',
+                                        1 => 'DUPLICATE',
+                                        2 => 'DELETE',
+                                        3 => 'FIND_DUPLICATES',
+                                    ),
+                            ),
+                        'maxColumns' => '2',
+                        'widths' =>
+                            array(
+                                0 =>
+                                    array(
+                                        'label' => '10',
+                                        'field' => '30',
+                                    ),
+                                1 =>
+                                    array(
+                                        'label' => '10',
+                                        'field' => '30',
+                                    ),
+                            ),
+                        'useTabs' => false,
+                        'tabDefs' =>
+                            array(
+                                'DEFAULT' =>
+                                    array(
+                                        'newTab' => false,
+                                        'panelDefault' => 'expanded',
+                                    ),
+                            ),
+                        'syncDetailEditViews' => true,
+                    ),
+                'panels' =>
+                    array(
+                        'default' =>
+                            array(
+                                0 =>
+                                    array(
+                                        0 => 'name',
+                                        1 => '',
+                                    ),
+                                1 =>
+                                    array(
+                                        0 => 'description',
+                                    ),
+                                2 =>
+                                    array(
+                                        0 =>
+                                            array(
+                                                'name' => 'date_entered',
+                                                'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
+                                                'label' => 'LBL_DATE_ENTERED',
+                                            ),
+                                        1 =>
+                                            array(
+                                                'name' => 'date_modified',
+                                                'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
+                                                'label' => 'LBL_DATE_MODIFIED',
+                                            ),
+                                    ),
+                            ),
+                    ),
+            ),
+    );
