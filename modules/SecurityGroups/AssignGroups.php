f5f59b8767508f88f193fdad362b0430b4f31f52
s.samko <serhii.samko@salesagility.com>
2023-10-25 12:35:44 +0100

    Fix #10242 - Mass Security Group Assignment fails when multiple items from the same page are chosen

--- modules/SecurityGroups/AssignGroups.php
+++ modules/SecurityGroups/AssignGroups.php
@@ -180,87 +180,92 @@ EOQ;
                     $mass_assign = <<<EOQ
 
 <script type="text/javascript" language="javascript">
-function confirm_massassign(del,start_string, end_string) {
-	if (del == 1) {
-		return confirm( start_string + sugarListView.get_num_selected_string()  + end_string);
-	}
-	else {
-		return confirm( start_string + sugarListView.get_num_selected_string()  + end_string);
-	}
+function confirm_massassign(del, start_string, end_string) {
+    if (del == 1) {
+        return confirm(start_string + sugarListView.get_num_selected_string() + end_string);
+    } else {
+        return confirm(start_string + sugarListView.get_num_selected_string() + end_string);
+    }
 }
 
 function send_massassign(mode, no_record_txt, start_string, end_string, del) {
 
-	if(!sugarListView.confirm_action(del, start_string, end_string))
-		return false;
+    if (!sugarListView.confirm_action(del, start_string, end_string))
+        return false;
 
-	if(document.MassAssign_SecurityGroups.massassign_group.selectedIndex == 0) {
-		alert("{$current_module_strings['LBL_SELECT_GROUP_ERROR']}");
-		return false;
-	}
+    if (document.MassAssign_SecurityGroups.massassign_group.selectedIndex == 0) {
+        alert("{$current_module_strings['LBL_SELECT_GROUP_ERROR']}");
+        return false;
+    }
 
-	if (document.MassUpdate.select_entire_list &&
-		document.MassUpdate.select_entire_list.value == 1)
-		mode = 'entire';
-	else if (document.MassUpdate.massall.checked == true)
-		mode = 'page';
-	else
-		mode = 'selected';
+    if (document.MassUpdate.select_entire_list &&
+        document.MassUpdate.select_entire_list.value == 1)
+        mode = 'entire';
+    else if (document.MassUpdate.massall.checked == true)
+        mode = 'page';
+    else
+        mode = 'selected';
 
-	var ar = new Array();
-	if(del == 1) {
-		var deleteInput = document.createElement('input');
-		deleteInput.name = 'Delete';
-		deleteInput.type = 'hidden';
-		deleteInput.value = true;
-		document.MassAssign_SecurityGroups.appendChild(deleteInput);
-	}
+    var ar = new Array();
+    if (del == 1) {
+        var deleteInput = document.createElement('input');
+        deleteInput.name = 'Delete';
+        deleteInput.type = 'hidden';
+        deleteInput.value = true;
+        document.MassAssign_SecurityGroups.appendChild(deleteInput);
+    }
 
-	switch(mode) {
-		case 'page':
-			document.MassAssign_SecurityGroups.uid.value = '';
-			for(wp = 0; wp < document.MassUpdate.elements.length; wp++) {
-				if(typeof document.MassUpdate.elements[wp].name != 'undefined'
-					&& document.MassUpdate.elements[wp].name == 'mass[]' && document.MassUpdate.elements[wp].checked) {
-							ar.push(document.MassUpdate.elements[wp].value);
-				}
-			}
-			document.MassAssign_SecurityGroups.uid.value = ar.join(',');
-			if(document.MassAssign_SecurityGroups.uid.value == '') {
-				alert(no_record_txt);
-				return false;
-			}
-			break;
-		case 'selected':
-			for(wp = 0; wp < document.MassUpdate.elements.length; wp++) {
-				if(typeof document.MassUpdate.elements[wp].name != 'undefined'
-					&& document.MassUpdate.elements[wp].name == 'mass[]'
-						&& document.MassUpdate.elements[wp].checked) {
-							ar.push(document.MassUpdate.elements[wp].value);
-				}
-			}
-			if(document.MassUpdate.uid.value != '') {
+    switch (mode) {
+        case 'page':
+            document.MassAssign_SecurityGroups.uid.value = '';
+            for (wp = 0; wp < document.MassUpdate.elements.length; wp++) {
+                if (typeof document.MassUpdate.elements[wp].name != 'undefined'
+                    && document.MassUpdate.elements[wp].name == 'mass[]' && document.MassUpdate.elements[wp].checked) {
+                    ar.push(document.MassUpdate.elements[wp].value);
+                }
+            }
+            document.MassAssign_SecurityGroups.uid.value = ar.join(',');
+            if (document.MassAssign_SecurityGroups.uid.value == '') {
+                alert(no_record_txt);
+                return false;
+            }
+            break;
+        case 'selected':
+            for (wp = 0; wp < document.MassUpdate.elements.length; wp++) {
+                if (typeof document.MassUpdate.elements[wp].name != 'undefined'
+                    && document.MassUpdate.elements[wp].name == 'mass[]'
+                    && document.MassUpdate.elements[wp].checked) {
+                    ar.push(document.MassUpdate.elements[wp].value);
+                }
+            }
+            if (document.MassUpdate.uid.value != '') {
                 document.MassUpdate.uid.value += ',';
                 document.MassUpdate.uid.value += ar.join(',');
                 document.MassAssign_SecurityGroups.uid.value = document.MassUpdate.uid.value;
-                if(document.MassAssign_SecurityGroups.uid.value == '') {
-				alert(no_record_txt);
-				return false;
+                if (document.MassAssign_SecurityGroups.uid.value == '') {
+                    alert(no_record_txt);
+                    return false;
+                }
+            } else {
+                document.MassAssign_SecurityGroups.uid.value += ar.join(',');
+
+                if (document.MassAssign_SecurityGroups.uid.value == '') {
+                    alert(no_record_txt);
+                    return false;
                 }
             }
-			break;
-		case 'entire':
-			var entireInput = document.createElement('input');
-			entireInput.name = 'entire';
-			entireInput.type = 'hidden';
-			entireInput.value = 'index';
-			document.MassAssign_SecurityGroups.appendChild(entireInput);
-			//confirm(no_record_txt);
-			break;
-	}
+            break;
+        case 'entire':
+            var entireInput = document.createElement('input');
+            entireInput.name = 'entire';
+            entireInput.type = 'hidden';
+            entireInput.value = 'index';
+            document.MassAssign_SecurityGroups.appendChild(entireInput);
+            break;
+    }
 
-	document.MassAssign_SecurityGroups.submit();
-	return false;
+    document.MassAssign_SecurityGroups.submit();
+    return false;
 }
 
 </script>
