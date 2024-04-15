618f58345f0eaf4b2dc2a24ca736c50c79cc4669
root <root@ip-172-31-29-204.eu-north-1.compute.internal>
2024-04-15 09:26:48 +0000

    from dev

---
+++ modules/SPL_Suppliers/language/en_us.lang.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "Powered by
+ * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
+ */
+$mod_strings = array (
+  'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+  'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+  'LBL_SECURITYGROUPS' => 'Security Groups',
+  'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groups',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Date Created',
+  'LBL_DATE_MODIFIED' => 'Date Modified',
+  'LBL_MODIFIED' => 'Modified By',
+  'LBL_MODIFIED_NAME' => 'Modified By Name',
+  'LBL_CREATED' => 'Created By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_DELETED' => 'Deleted',
+  'LBL_NAME' => 'Name',
+  'LBL_CREATED_USER' => 'Created by User',
+  'LBL_MODIFIED_USER' => 'Modified by User',
+  'LBL_LIST_NAME' => 'Name',
+  'LBL_EDIT_BUTTON' => 'Edit',
+  'LBL_REMOVE' => 'Remove',
+  'LBL_ASCENDING' => 'Ascending',
+  'LBL_DESCENDING' => 'Descending',
+  'LBL_OPT_IN' => 'Opt In',
+  'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pending Confirm opt in, Confirm opt in not sent',
+  'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pending Confirm opt in, Confirm opt in sent',
+  'LBL_OPT_IN_CONFIRMED' => 'Opted in',
+  'ERR_DELETE_RECORD' => 'A record number must be specified to delete the account.',
+  'LBL_ACCOUNT_NAME' => 'Company Name:',
+  'LBL_ACCOUNT' => 'Company:',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+  'LBL_ADDRESS_INFORMATION' => 'Address Information',
+  'LBL_ANNUAL_REVENUE' => 'Annual Revenue:',
+  'LBL_ANY_ADDRESS' => 'Any Address:',
+  'LBL_ANY_EMAIL' => 'Any Email:',
+  'LBL_EMAIL_NON_PRIMARY' => 'Non Primary E-mails',
+  'LBL_ANY_PHONE' => 'Any Phone:',
+  'LBL_RATING' => 'Rating',
+  'LBL_ASSIGNED_TO' => 'Assigned to:',
+  'LBL_ASSIGNED_USER' => 'Assigned to:',
+  'LBL_BILLING_ADDRESS_CITY' => 'Billing City:',
+  'LBL_BILLING_ADDRESS_COUNTRY' => 'Billing Country:',
+  'LBL_BILLING_ADDRESS_POSTALCODE' => 'Billing Postal Code:',
+  'LBL_BILLING_ADDRESS_STATE' => 'Billing State:',
+  'LBL_BILLING_ADDRESS_STREET_2' => 'Billing Street 2',
+  'LBL_BILLING_ADDRESS_STREET_3' => 'Billing Street 3',
+  'LBL_BILLING_ADDRESS_STREET_4' => 'Billing Street 4',
+  'LBL_BILLING_ADDRESS_STREET' => 'Billing Street:',
+  'LBL_BILLING_ADDRESS' => 'Billing Address:',
+  'LBL_ACCOUNT_INFORMATION' => 'Company Information',
+  'LBL_CITY' => 'City:',
+  'LBL_CONTACTS_SUBPANEL_TITLE' => 'Contacts',
+  'LBL_COUNTRY' => 'Country:',
+  'LBL_DEFAULT_SUBPANEL_TITLE' => 'Accounts',
+  'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
+  'LBL_DUPLICATE' => 'Possible Duplicate Account',
+  'LBL_EMAIL' => 'Email Address:',
+  'LBL_EMPLOYEES' => 'Employees:',
+  'LBL_FAX' => 'Fax:',
+  'LBL_INDUSTRY' => 'Industry:',
+  'LBL_LIST_ACCOUNT_NAME' => 'Account Name',
+  'LBL_LIST_CITY' => 'City',
+  'LBL_LIST_EMAIL_ADDRESS' => 'Email Address',
+  'LBL_LIST_PHONE' => 'Phone',
+  'LBL_LIST_STATE' => 'State',
+  'LBL_MEMBER_OF' => 'Member of:',
+  'LBL_MEMBER_ORG_SUBPANEL_TITLE' => 'Member Organizations',
+  'LBL_OTHER_EMAIL_ADDRESS' => 'Other Email:',
+  'LBL_OTHER_PHONE' => 'Other Phone:',
+  'LBL_OWNERSHIP' => 'Ownership:',
+  'LBL_PARENT_ACCOUNT_ID' => 'Parent Account ID',
+  'LBL_PHONE_ALT' => 'Alternate Phone:',
+  'LBL_PHONE_FAX' => 'Phone Fax:',
+  'LBL_PHONE_OFFICE' => 'Office Phone:',
+  'LBL_PHONE' => 'Phone:',
+  'LBL_EMAIL_ADDRESS' => 'Email Address',
+  'LBL_EMAIL_ADDRESSES' => 'Email Address(es)',
+  'LBL_POSTAL_CODE' => 'Postal Code:',
+  'LBL_SAVE_ACCOUNT' => 'Save Account',
+  'LBL_SHIPPING_ADDRESS_CITY' => 'Shipping City:',
+  'LBL_SHIPPING_ADDRESS_COUNTRY' => 'Shipping Country:',
+  'LBL_SHIPPING_ADDRESS_POSTALCODE' => 'Shipping Postal Code:',
+  'LBL_SHIPPING_ADDRESS_STATE' => 'Shipping State:',
+  'LBL_SHIPPING_ADDRESS_STREET_2' => 'Shipping Street 2',
+  'LBL_SHIPPING_ADDRESS_STREET_3' => 'Shipping Street 3',
+  'LBL_SHIPPING_ADDRESS_STREET_4' => 'Shipping Street 4',
+  'LBL_SHIPPING_ADDRESS_STREET' => 'Shipping Street:',
+  'LBL_SHIPPING_ADDRESS' => 'Shipping Address:',
+  'LBL_STATE' => 'State:',
+  'LBL_TICKER_SYMBOL' => 'Ticker Symbol:',
+  'LBL_TYPE' => 'Type:',
+  'LBL_WEBSITE' => 'Website:',
+  'LNK_ACCOUNT_LIST' => 'Accounts',
+  'LNK_NEW_ACCOUNT' => 'Create Account',
+  'MSG_DUPLICATE' => 'The account record you are about to create might be a duplicate of an account record that already exists. Account records containing similar names are listed below.<br>Click Create Account to continue creating this new account, or select an existing account listed below.',
+  'MSG_SHOW_DUPLICATES' => 'The account record you are about to create might be a duplicate of an account record that already exists. Account records containing similar names are listed below.<br>Click Save to continue creating this new account, or click Cancel to return to the module without creating the account.',
+  'NTC_DELETE_CONFIRMATION' => 'Are you sure you want to delete this record?',
+  'LBL_LIST_FORM_TITLE' => 'Suppliers List',
+  'LBL_MODULE_NAME' => 'Suppliers',
+  'LBL_MODULE_TITLE' => 'Suppliers',
+  'LBL_HOMEPAGE_TITLE' => 'My Suppliers',
+  'LNK_NEW_RECORD' => 'Create Suppliers',
+  'LNK_LIST' => 'View Suppliers',
+  'LNK_IMPORT_SPL_SUPPLIERS' => 'Import Suppliers',
+  'LBL_SEARCH_FORM_TITLE' => 'Search Suppliers',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+  'LBL_SPL_SUPPLIERS_SUBPANEL_TITLE' => 'Suppliers',
+  'LBL_NEW_FORM_TITLE' => 'New Suppliers',
+);
