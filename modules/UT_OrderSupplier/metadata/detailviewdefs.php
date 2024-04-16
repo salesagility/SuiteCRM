88dc025fe0395680975cfd8284078a2d29957a5c
Dev <contact@urdhva-tech.com>
2024-04-16 14:55:41 +0530

    Order supplier detailview
    Order supplier detailview

--- modules/UT_OrderSupplier/metadata/detailviewdefs.php
+++ modules/UT_OrderSupplier/metadata/detailviewdefs.php
@@ -15,18 +15,6 @@ array(
           1 => 'DUPLICATE',
           2 => 'DELETE',
           3 => 'FIND_DUPLICATES',
-          4 =>
-          array(
-            'customCode' => '<input type="button" class="button" onClick="showPopup(\'pdf\');" value="{$MOD.LBL_PRINT_AS_PDF}">',
-          ),
-          5 =>
-          array(
-            'customCode' => '<input type="button" class="button" onClick="showPopup(\'emailpdf\');" value="{$MOD.LBL_EMAIL_PDF}">',
-          ),
-          6 =>
-          array(
-            'customCode' => '<input type="button" class="button" onClick="showPopup(\'email\');" value="{$MOD.LBL_EMAIL_INVOICE}">',
-          ),
         ),
       ),
       'maxColumns' => '2',
