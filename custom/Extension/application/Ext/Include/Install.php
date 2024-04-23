9bb5961b560607484db1496379085782b785bf41
Dev <contact@urdhva-tech.com>
2024-04-23 11:40:14 +0530

    Installation module partial commit
    Installation module partial commit

---
+++ custom/Extension/application/Ext/Include/Install.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['UT_Installation'] = 'UT_Installation';
+$beanFiles['UT_Installation'] = 'modules/UT_Installation/UT_Installation.php';
+$moduleList[] = 'UT_Installation';
+
+?>
---
+++ modules/UT_Installation/metadata/detailviewdefs.php
@@ -0,0 +1,103 @@
+<?php
+$module_name = 'UT_Installation';
+$viewdefs [$module_name] = 
+array (
+  'DetailView' => 
+  array (
+    'templateMeta' => 
+    array (
+      'form' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 'EDIT',
+          1 => 'DUPLICATE',
+          2 => 'DELETE',
+          3 => 'FIND_DUPLICATES',
+        ),
+      ),
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'useTabs' => false,
+      'tabDefs' => 
+      array (
+        'DEFAULT' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
+      'syncDetailEditViews' => true,
+    ),
+    'panels' => 
+    array (
+      'default' => 
+      array (
+        0 => 
+        array (
+          0 => 'name',
+          1 => 
+          array (
+            'name' => 'account',
+            'studio' => 'visible',
+            'label' => 'LBL_ACCOUNT',
+          ),
+        ),
+        1 => 
+        array (
+          0 => 
+          array (
+            'name' => 'system_id',
+            'studio' => 'visible',
+            'label' => 'LBL_SYSTEM_ID',
+          ),
+          1 => 
+          array (
+            'name' => 'installation_id',
+            'label' => 'LBL_INSTALLATION_ID',
+          ),
+        ),
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'software_version',
+            'label' => 'LBL_SOFTWARE_VERSION',
+          ),
+          1 => 
+          array (
+            'name' => 'latest_software_version',
+            'label' => 'LBL_LATEST_SOFTWARE_VERSION',
+          ),
+        ),
+        3 => 
+        array (
+          0 => 
+          array (
+            'name' => 'firmware_version',
+            'label' => 'LBL_FIRMWARE_VERSION',
+          ),
+          1 => 'assigned_user_name',
+        ),
+        4 => 
+        array (
+          0 => 'description',
+        ),
+      ),
+    ),
+  ),
+);
+;
+?>
