fe1c12f58a3a9948542c11051c80f274101c8e2f
Dev <contact@urdhva-tech.com>
2024-04-23 12:18:58 +0530

    Revert "Installation module partial commit"
    This reverts commit 9bb5961b560607484db1496379085782b785bf41.

--- custom/Extension/application/Ext/Include/Install.php
+++
@@ -1,7 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-$beanList['UT_Installation'] = 'UT_Installation';
-$beanFiles['UT_Installation'] = 'modules/UT_Installation/UT_Installation.php';
-$moduleList[] = 'UT_Installation';
-
-?>
