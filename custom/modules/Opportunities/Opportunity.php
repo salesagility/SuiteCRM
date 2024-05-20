9601c7fae6c4b2aa0cf89a39f16be1b2976100c1
Vibha <vibha.gopal@gmail.com>
2024-05-20 12:15:54 +0530

    Changes in Oppurtionity

--- custom/modules/Opportunities/Opportunity.php
+++ custom/modules/Opportunities/Opportunity.php
@@ -87,7 +87,7 @@ class Opportunity extends SugarBean
 
     public $importable = true;
     public $object_name = "Opportunity";
-//	public $lineItems = true;
+  	public $lineItems = true;
 
     // This is used to retrieve related fields from form posts.
     public $additional_column_fields = array('assigned_user_name', 'assigned_user_id', 'account_name', 'account_id', 'contact_id', 'task_id', 'note_id', 'meeting_id', 'call_id', 'email_id'
@@ -369,12 +369,15 @@ class Opportunity extends SugarBean
         }
 
         require_once('modules/Opportunities/SaveOverload.php');
-
+        require_once('modules/AOS_Products_Quotes/AOS_Utils.php');
         perform_save($this);
+
+        $return_id = parent::save($check_notify);
         require_once('modules/AOS_Line_Item_Groups/AOS_Line_Item_Groups.php');
         $productQuoteGroup = BeanFactory::newBean('AOS_Line_Item_Groups');
         $productQuoteGroup->save_groups($_POST, $this, 'group_');
-        return parent::save($check_notify);
+
+        return $return_id;
     }
 
     public function save_relationship_changes($is_update, $exclude = array())
