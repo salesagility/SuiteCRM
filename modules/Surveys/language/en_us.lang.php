5df9f916a4af12522e42663bdfeba430501318c8
SinergiaCRM <desarrollo@sinergiacrm.org>
2024-10-29 15:56:25 +0000

    Fix salesagility#10564 - Thank you message in Surveys only in English

--- modules/Surveys/language/en_us.lang.php
+++ modules/Surveys/language/en_us.lang.php
@@ -105,4 +105,5 @@ $mod_strings = array(
     'LBL_SUBMIT' => 'Submit',
     'LBL_STARS' => 'Stars',
     'LBL_SURVEY_CLOSE_RESPONSE' => 'Thanks for your interest but this survey is now closed.',
+    'LBL_SURVEY_THANKS' => 'Thanks for completing this survey.',
 );
